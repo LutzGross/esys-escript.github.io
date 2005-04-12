349aec2ca9a287853745eec02f90e3df79f2a01c
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-04-12 04:44:55 +0000

    Merge of development branch back to main trunk on 2005-04-12

--- esys2/escript/escript.mk
+++ esys2/escript/escript.mk
@@ -7,8 +7,6 @@
 
 # Matt Davies 04/08/03
 
-DODEBUG = 
-
 DEFAULT_TARGET := libescriptcpp.so pyc
 
 INSTALL_PYTH := ./py_src/__init__.py ./py_src/linearPDEs.py ./py_src/projector.py ./py_src/util.py ./py_src/escript.py ./py_src/esysXML.py ./py_src/timeseries.py
@@ -21,9 +19,4 @@ L_DEFS := DOASSERT
 
 L_SRC_DIR:= ./src ./py_src 
 
-#L_INSTLIB_DIR := /raid2/tools/esys/lib
-
-#L_PYTH_DIR := /raid2/tools/esys/esys
-
 include $(ESYS_ROOT)/make/Makefile.default
-
