c0a608896705b98e70f97bf00d3aa0b6eaad4b2b
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-07-22 07:04:52 +0000

    makefile for modellib module

---
+++ trunk/esys2/modellib/modellib.mk
@@ -0,0 +1,18 @@
+# $Id$
+
+# This makefile automatically finds, calculates dependencies, compiles
+# and makes a static library from all C++ files found in the directories
+# under ${L_SRC_DIR} (see below). Files and even whole directory trees
+# under ${L_SRC_DIR} can be skipped by postfixing the name with a "-".
+
+# Matt Davies 04/08/03
+
+DEFAULT_TARGET := pyc
+
+L_PYTH_DIR := $(ESYS_ROOT)/esys/modellib
+
+INSTALL_PYTH := ./lib/__init__.pyc ./lib/darcy.pyc ./lib/geometry.pyc ./lib/input.pyc ./lib/materials.pyc ./lib/temperature.pyc ./lib/visualization.pyc
+
+L_SRC_DIR:= ./py_src
+
+include $(ESYS_ROOT)/make/Makefile.default
