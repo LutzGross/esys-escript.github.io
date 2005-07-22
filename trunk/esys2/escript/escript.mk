766f7708e6d2bdd5b1a45e27a51be0594736f77e
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-07-22 05:17:51 +0000

    removed references to modellib

--- trunk/esys2/escript/escript.mk
+++ trunk/esys2/escript/escript.mk
@@ -9,12 +9,12 @@
 
 DEFAULT_TARGET := libescriptcpp.so pyc
 
-INSTALL_PYTH := ./lib/py_src/__init__.pyc ./lib/py_src/linearPDEs.pyc ./lib/py_src/pdetools.pyc ./lib/py_src/util.pyc ./lib/py_src/escript.pyc ./lib/py_src/modelframe.pyc ./lib/py_src/timeseries.pyc # ./lib/py_src/modellib/*.pyc
+INSTALL_PYTH := ./lib/py_src/__init__.pyc ./lib/py_src/linearPDEs.pyc ./lib/py_src/pdetools.pyc ./lib/py_src/util.pyc ./lib/py_src/escript.pyc ./lib/py_src/modelframe.pyc ./lib/py_src/timeseries.pyc
 
 INSTALL_LIB := ./lib/libescriptcpp.so
 
 PACKAGES := esysUtils python23 boostStatic
 
-L_SRC_DIR:= ./src ./py_src ./py_src/modellib
+L_SRC_DIR:= ./src ./py_src
 
 include $(ESYS_ROOT)/make/Makefile.default
