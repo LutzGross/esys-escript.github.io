e8845b3ba8e12dbbd9a6d3aae414fd55dfc327ba
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-11-23 04:10:21 +0000

    copy finleyC and CPPAdapter to finley and finley/CPPAdapter to facilitate scons builds

---
+++ trunk/finley/src/finley/CPPAdapter/FinleyError.h
@@ -0,0 +1,44 @@
+/* 
+ ******************************************************************************
+ *                                                                            *
+ *       COPYRIGHT  ACcESS 2004 -  All Rights Reserved                        *
+ *                                                                            *
+ * This software is the property of ACcESS. No part of this code              *
+ * may be copied in any form or by any means without the expressed written    *
+ * consent of ACcESS.  Copying, use or modification of this software          *
+ * by any unauthorised person is illegal unless that person has a software    *
+ * license agreement with ACcESS.                                             *
+ *                                                                            *
+ ******************************************************************************
+*/
+                                                                           
+#if !defined  finley_FinleyError_20040528_H
+#define finley_FinleyError_20040528_H
+
+extern "C" {
+#include "finley/finleyC/Finley.h"
+}
+
+#include <string>
+
+namespace finley {
+  /**
+     \brief
+     Provide a C++ interface to the finley C funcion of the same name.
+     Needed because of constness problems.
+  */
+  void setFinleyError(Finley_ErrorCodeType errorCode, 
+		      const std::string& errMess);
+ 
+  /**
+     \brief
+     Convert a C finley error into a C++ exception.
+  */
+  void checkFinleyError();
+  /**
+     \brief
+     Convert a C paso  error into a C++ exception.
+  */
+  void checkPasoError();
+} // end of namespace
+#endif
