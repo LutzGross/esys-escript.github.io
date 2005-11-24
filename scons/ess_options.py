771370cdb780df0c08bd7c54b2a4296d84fbcabb
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-11-24 23:12:13 +0000

    pass in cc/cxx settings from config file

--- scons/ess_options.py
+++ scons/ess_options.py
@@ -1,6 +1,9 @@
 python_path = '/raid2/tools/python-2.3.4/include/python2.3'
 boost_path = '/raid2/tools/boost/include/boost-1_31'
 
+cc = 'icc'
+cxx = 'icc'
+
 cxx_flags = '-O0 -openmp -openmp_report0 -tpp2 -ansi -ansi_alias -no-gcc -fpic -w1'
 cc_flags  = '-O0 -openmp -openmp_report0 -tpp2 -c99 -ansi_alias -no-gcc -c -fpic -w1'
 
