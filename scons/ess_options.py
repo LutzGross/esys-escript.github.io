42850d5f8c14e6ed993da0a17f56d6b66d14e066
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-11-24 06:35:14 +0000

    first pass at importing compile options from an external file'

--- scons/ess_options.py
+++ scons/ess_options.py
@@ -2,3 +2,6 @@ python_path = '/raid2/tools/python-2.3.4/include/python2.3'
 boost_path = '/raid2/tools/boost/include/boost-1_31'
 
 cxx_flags = '-O0 -openmp -openmp_report0 -tpp2 -ansi -ansi_alias -no-gcc -fpic -w1'
+cc_flags  = '-O0 -openmp -openmp_report0 -tpp2 -c99 -ansi_alias -no-gcc -c -fpic -w1'
+
+ar_flags = 'crus'
