f2842ea5847a38ec2ba9061d995e1ae71fbd3c27
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-11-28 05:25:35 +0000

    add names of python and boost libraries to config file

--- trunk/scons/ess_options.py
+++ trunk/scons/ess_options.py
@@ -4,6 +4,9 @@ boost_path = '/raid2/tools/boost/include/boost-1_31'
 python_lib_path = '/raid2/tools/python-2.3.4/lib'
 boost_lib_path = '/raid2/tools/boost/lib'
 
+python_lib = python2.3
+boost_lib = boost_python-intel-d-1_31
+
 cc = 'icc'
 cxx = 'icc'
 
