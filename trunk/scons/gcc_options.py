faa87a50709b88cafd285c6e7fd1354615b08876
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-11-29 02:11:05 +0000

    added some comments and reasonable default values

--- trunk/scons/gcc_options.py
+++ trunk/scons/gcc_options.py
@@ -1,19 +1,26 @@
-python_path = ?
+# locations of include files for python and boost
+python_path = '/usr/include'
 boost_path = '/usr/include'
 
+# locations of libraries for python and boost
 python_lib_path = '/usr/lib'
 boost_lib_path = '/usr/lib'
 
-python_lib = ?
-boost_lib = boost_python
+# names of libraries for python and boost
+python_lib = 'python2.3'
+boost_lib = 'boost_python'
 
+# names of c and c++ compilers to use
 cc = 'gcc'
 cxx = 'g++'
 
+# c flags to use
 cc_flags  = '-O3 -std=c99 -fpic -W -Wall -Wno-unknown-pragmas'
 cc_flags_debug  = '-g -O0 -std=c99 -fpic -W -Wall -Wno-unknown-pragmas'
 
-cc_flags  = '-O3 -ansi -fpic -W -Wall -Wno-unknown-pragmas'
-cc_flags_debug  = '-g -O0 -ansi -fpic -W -Wall -Wno-unknown-pragmas -DDOASSERT -DDOPROF'
+# c++ flags to use
+cxx_flags  = '-O3 -ansi -fpic -W -Wall -Wno-unknown-pragmas'
+cxx_flags_debug  = '-g -O0 -ansi -fpic -W -Wall -Wno-unknown-pragmas -DDOASSERT -DDOPROF'
 
+# static library archiver flags to use
 ar_flags = 'crus'
