06b85c9231a08b8e7d427c9e0ed02a9a9a8e6fab
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-12-02 03:08:26 +0000

    new test for util.py on bruce

---
+++ trunk/bruce/test/python/test_utilOnBruce.py
@@ -0,0 +1,22 @@
+# $Id$
+
+import unittest
+from esys.escript.test_util import Test_util_no_tagged_data as Test_util
+from esys.escript import ContinuousFunction
+from esys.finley import Rectangle
+import sys
+
+class Test_UtilOnBruce(Test_util):
+   def setUp(self):
+       self.__dom =Rectangle(10,10,2)
+       self.functionspace = ContinuousFunction(self.__dom)
+
+if __name__ == '__main__':
+   suite = unittest.TestSuite()
+   suite.addTest(unittest.makeSuite(Test_UtilOnBruce))
+   s=unittest.TextTestRunner(verbosity=2).run(suite)
+   if s.wasSuccessful():
+     sys.exit(0)
+   else:
+     sys.exit(1)
+   
