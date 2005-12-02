002527760198e4bd6d22ca22318f726d93222795
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-12-02 03:47:30 +0000

    test of symbols.py with python

---
+++ trunk/finley/test/python/test_symbolsOnFinley.py
@@ -0,0 +1,39 @@
+# $Id:$
+
+#
+#      COPYRIGHT ACcESS 2004 -  All Rights Reserved
+#
+#   This software is the property of ACcESS.  No part of this code
+#   may be copied in any form or by any means without the expressed written
+#   consent of ACcESS.  Copying, use or modification of this software
+#   by any unauthorised person is illegal unless that
+#   person has a software license agreement with ACcESS.
+#
+
+"""
+runs the test_symbol test for the symbols.py module
+
+@remark:
+@var __author__: name of author
+@var __licence__: licence agreement
+@var __url__: url entry point on documentation
+@var __version__: version
+@var __date__: date of the version
+"""
+
+__author__="Lutz Gross, l.gross@uq.edu.au"
+__licence__="contact: esys@access.uq.edu.au"
+__url__="http://www.iservo.edu.au/esys/escript"
+__version__="$Revision$"
+__date__="$Date$"
+
+from test_symbols import Test_symbols
+import unittest
+suite = unittest.TestSuite()
+suite.addTest(unittest.makeSuite(Test_symbols))
+s=unittest.TextTestRunner(verbosity=2).run(suite)
+if s.wasSuccessful():
+   sys.exit(0)
+else:
+  sys.exit(1)
+
