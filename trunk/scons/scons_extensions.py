c5d0ddca440b209158de8aba17c18d7de9a0d86a
jgs <jgs@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-12-02 04:57:48 +0000

    bruce tests cases are now automatically run whenever they are rebuilt

--- trunk/scons/scons_extensions.py
+++ trunk/scons/scons_extensions.py
@@ -2,8 +2,16 @@
 
 import py_compile
 import sys
+import os
 
 # Code to build .pyc from .py
 def build_py(target, source, env):
   py_compile.compile(str(source[0]), str(target[0]))
+  return None
+
+# Code to run unit_test executables
+def runUnitTest(target, source, env):
+  app = str(source[0].abspath)
+  if not os.system(app):
+    open(str(target[0]),'w').write("PASSED\n")
   return None
