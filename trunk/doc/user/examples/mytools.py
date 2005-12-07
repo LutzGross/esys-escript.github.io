8a24a2a6d3a893e9c5c1ba1bdf58795e33383e01
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-12-07 04:32:28 +0000

    make them run again

--- trunk/doc/user/examples/mytools.py
+++ trunk/doc/user/examples/mytools.py
@@ -1,11 +1,10 @@
 # $Id$
-from esys.linearPDEs import LinearPDE
+from esys.escript.linearPDEs import LinearPDE
+from esys.escript import kronecker
 import numarray
 class Helmholtz(LinearPDE):
    def setValue(self,kappa=0,omega=1,f=0,eta=0,g=0):
         # get spatial dimension 
         ndim=self.getDim()
-        # get kronecker symbol from numarray: kronecker[i,j]=1 for i=j and =0 else
-        kronecker=numarray.identity(ndim)
         # map kappa, omega, f, eta, g to the coefficients of the general PDE
-        self._LinearPDE__setValue(A=kappa*kronecker,D=omega,Y=f,d=eta,y=g)
+        super(Helmholtz, self).setValue(A=kappa*kronecker(ndim),D=omega,Y=f,d=eta,y=g)
