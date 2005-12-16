cd522ab120b983dfdeeda299aeb945d468ba8271
cochrane <cochrane@77569008-7704-0410-b7a0-a92fef0b09fd>
2005-12-16 00:52:34 +0000

    Updated to allow for building of API documentation.

--- scons/scons_extensions.py
+++ scons/scons_extensions.py
@@ -17,3 +17,53 @@ def runUnitTest(target, source, env):
   else:
     return 1
   return None
+
+# code to build epydoc docs
+def build_epydoc(target, source, env):
+    # get where I am currently, just as a reference
+    pwd = os.getcwd()
+
+    # get the full path of the runepydoc script
+    runepydoc = str(source[0].abspath)
+
+    # use this path to work out where the doc directory is
+    dirs = runepydoc.split('/')
+    dirs = dirs[:-3] # trim the last two entries: this is now the doc dir path
+    docdir = '/'.join(dirs) # this is the backwards python way to do it
+    # (I'm feeling in a perl mood today...)
+
+    # change into the relevant dir
+    os.chdir(docdir)
+
+    # run the epydoc script
+    if not os.system(runepydoc):
+	os.chdir(pwd)
+	open(str(target[0]), 'w').write("Documentation built\n")
+    else:
+	return 1
+    return None
+
+# build doxygen docs
+def build_doxygen(target, source, env):
+    # get where I am currently, just as a reference
+    pwd = os.getcwd()
+
+    # get the full path of the rundoxygen script
+    rundoxygen = str(source[0].abspath)
+
+    # use this path to work out where the doc directory is
+    dirs = rundoxygen.split('/')
+    dirs = dirs[:-2] # trim the last two entries: this is now the doc dir path
+    docdir = '/'.join(dirs) # this is the backwards python way to do it
+    # (I'm feeling in a perl mood today...)
+
+    # change into the relevant dir
+    os.chdir(docdir)
+
+    # run the doxygen script
+    if not os.system(rundoxygen):
+	os.chdir(pwd)
+	open(str(target[0]), 'w').write("Documentation built\n")
+    else:
+	return 1
+    return None
