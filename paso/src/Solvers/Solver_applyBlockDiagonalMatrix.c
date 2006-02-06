cacf9dca82223dc15344302cac322f2611c702dc
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-02-06 06:32:06 +0000

    performance monitoring added. complies without PAPI.

--- paso/src/Solvers/Solver_applyBlockDiagonalMatrix.c
+++ paso/src/Solvers/Solver_applyBlockDiagonalMatrix.c
@@ -21,23 +21,48 @@
 
 void Paso_Solver_applyBlockDiagonalMatrix(dim_t n_block,dim_t n,double* D,index_t* pivot,double* x,double* b) {
      dim_t i;
+     register dim_t i3,i9;
+     register double b0,b1,b2,D00,D10,D20,D01,D11,D21,D02,D12,D22;
+
      if (n_block==1) {
          #pragma omp for private(i) schedule(static)
-         for (i=0;i<n;i++) {
+         for (i=0;i<n;++i) {
             x[i]=D[i]*b[i];
          }
      } else if (n_block==2) {
-         #pragma omp for private(i) schedule(static)
-         for (i=0;i<n;i++) {
-            x[2*i  ]=D[4*i  ]*b[2*i]+D[4*i+2]*b[2*i+1];
-            x[2*i+1]=D[4*i+1]*b[2*i]+D[4*i+3]*b[2*i+1];
+         #pragma omp for private(i,b0,b1,D00,D10,D01,D11,i3,i9) schedule(static)
+         for (i=0;i<n;++i) {
+            i3=2*i;
+            i9=4*i;
+            b0=b[i3];
+            b1=b[i3+1];
+            D00=D[i9  ];
+            D10=D[i9+1];
+            D01=D[i9+2];
+            D11=D[i9+3];
+            x[i3  ]=D00*b0+D01*b1;
+            x[i3+1]=D10*b0+D11*b1;
          }
      } else if (n_block==3) {
-         #pragma omp for private(i) schedule(static)
-         for (i=0;i<n;i++) {
-            x[3*i  ]=D[9*i  ]*b[3*i]+D[9*i+3]*b[3*i+1]+D[9*i+6]*b[3*i+2];
-            x[3*i+1]=D[9*i+1]*b[3*i]+D[9*i+4]*b[3*i+1]+D[9*i+7]*b[3*i+2];
-            x[3*i+2]=D[9*i+2]*b[3*i]+D[9*i+5]*b[3*i+1]+D[9*i+8]*b[3*i+2];
+         #pragma omp for private(i,b0,b1,b2,D00,D10,D20,D01,D11,D21,D02,D12,D22,i3,i9) schedule(static)
+         for (i=0;i<n;++i) {
+            i3=3*i;
+            i9=9*i;
+            b0=b[i3];
+            b1=b[i3+1];
+            b2=b[i3+2];
+            D00=D[i9  ];
+            D10=D[i9+1];
+            D20=D[i9+2];
+            D01=D[i9+3];
+            D11=D[i9+4];
+            D21=D[i9+5];
+            D02=D[i9+6];
+            D12=D[i9+7];
+            D22=D[i9+8];
+            x[i3  ]=D00*b0+D01*b1+D02*b2;
+            x[i3+1]=D10*b0+D11*b1+D12*b2;
+            x[i3+2]=D20*b0+D21*b1+D22*b2;
          }
      }
      return;
