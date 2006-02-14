931147cada7364a725184545945146d148d69e1e
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-02-14 05:53:52 +0000

    now done by test_utilOnBruce.py

--- trunk/bruce/test/python/test_utilOnBruce.py
+++ trunk/bruce/test/python/test_utilOnBruce.py
@@ -2,11 +2,12 @@
 
 import unittest
 from esys.escript.test_util import Test_util_no_tagged_data as Test_util
+from esys.escript.test_symbols import Test_symbols
 from esys.escript import ContinuousFunction
 from esys.finley import Rectangle
 import sys
 
-class Test_UtilOnBruce(Test_util):
+class Test_UtilOnBruce(Test_util,Test_symbols):
    def setUp(self):
        self.domain =Rectangle(10,10,2)
        self.functionspace = ContinuousFunction(self.domain)
