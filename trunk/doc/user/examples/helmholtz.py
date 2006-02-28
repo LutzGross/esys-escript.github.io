d260cdfc21bdfb2fa7057a8de80e0f98e4a3624a
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-02-28 03:58:05 +0000

    updates examples

--- trunk/doc/user/examples/helmholtz.py
+++ trunk/doc/user/examples/helmholtz.py
@@ -1,6 +1,6 @@
 # $Id$
-from mytools import Helmholtz
-from esys.escript import Lsup
+from esys.escript import *
+from linearPDEs import LinearPDE
 from esys.finley import Rectangle
 #... set some parameters ...
 kappa=1.
@@ -9,11 +9,12 @@ eta=10.
 #... generate domain ...
 mydomain = Rectangle(l0=5.,l1=1.,n0=50, n1=10)
 #... open PDE and set coefficients ...
-mypde=Helmholtz(mydomain)
+mypde=LinearPDE(mydomain)
+mypde.setSymmetryOn()
 n=mydomain.getNormal()
 x=mydomain.getX()
-mypde.setValue(kappa,omega,omega*x[0],eta,kappa*n[0]+eta*x[0])
+mypde.setValue(A=kappa*kronecker(mydomain),D=omega,Y=omega*x[0],d=eta,y=kappa*n[0]+eta*x[0])
 #... calculate error of the PDE solution ...
 u=mypde.getSolution()
-print "error is ",Lsup(u-x[0])
 # output should be similar to "error is 1.e-7" 
+print "error is ",Lsup(u-x[0])
