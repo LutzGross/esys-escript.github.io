cb818d762b8f5fa3af110a9b79c7b63f0b755306
lkettle <lkettle@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-03-08 05:54:06 +0000

    Changed example script for wave.py to a point source wave propagation problem in 3D.

--- doc/user/examples/wave.py
+++ doc/user/examples/wave.py
@@ -1,39 +1,66 @@
-# $Id$
 from esys.escript import *
+from esys.escript.pdetools import Locator
 from esys.escript.linearPDEs import LinearPDE
 from esys.finley import Brick
-ne=5           # number of cells in x_0-direction
-depth=10000.   # length in x_0-direction
-width=100000.  # length in x_1 and x_2 direction
+from numarray import identity,zeros,ones
+
+ne=32          # number of cells in x_0 and x_1 directions
+width=10000.  # length in x_0 and x_1 directions
 lam=3.462e9
 mu=3.462e9
 rho=1154.
-tau=10.
-umax=2.
 tend=60
-h=1./5.*sqrt(rho/(lam+2*mu))*(depth/ne)
+h=(1./5.)*sqrt(rho/(lam+2*mu))*(width/ne)
 print "time step size = ",h
 
-def s_tt(t): return umax/tau**2*(6*t/tau-9*(t/tau)**4)*exp(-(t/tau)**3)
+U0=0.01 # amplitude of point source
 
-def wavePropagation(domain,h,tend,lam,mu,rho,s_tt):
+def wavePropagation(domain,h,tend,lam,mu,rho,U0):
    x=domain.getX()
    # ... open new PDE ...
    mypde=LinearPDE(domain)
-   mypde.setSolverMethod(mypde.LUMPING)
-   mypde.setValue(D=kronecker(mypde.getDim())*rho, \
-                  q=whereZero(x[0])*kronecker(mypde.getDim())[1,:])
+   mypde.setSolverMethod(LinearPDE.LUMPING)
+   kronecker=identity(mypde.getDim())
+
+   #  spherical source at middle of bottom face
+
+   xc=[width/2.,width/2.,0.]
+   # define small radius around point xc
+   # Lsup(x) returns the maximum value of the argument x
+   src_radius = 0.1*Lsup(domain.getSize())
+   print "src_radius = ",src_radius
+
+   dunit=numarray.array([1.,0.,0.]) # defines direction of point source
+
+   mypde.setValue(D=kronecker*rho)
    # ... set initial values ....
    n=0
-   u=Vector(0,ContinuousFunction(domain))
-   u_last=Vector(0,ContinuousFunction(domain))
+   # initial value of displacement at point source is constant (U0=0.01)
+   # for first two time steps
+   u=U0*whereNegative(length(x-xc)-src_radius)*dunit
+   u_last=U0*whereNegative(length(x-xc)-src_radius)*dunit
    t=0
+
+   # define the location of the point source 
+   L=Locator(domain,xc)
+   # find potential at point source
+   u_pc=L.getValue(u)
+   print "u at point charge=",u_pc
+  
+   u_pc_x = u_pc[0]
+   u_pc_y = u_pc[1]
+   u_pc_z = u_pc[2]
+
+   # open file to save displacement at point source
+   u_pc_data=open('./data/U_pc.out','w')
+   u_pc_data.write("%f %f %f %f\n"%(t,u_pc_x,u_pc_y,u_pc_z))
+ 
    while t<tend:
      # ... get current stress ....
      g=grad(u)
-     stress=lam*trace(g)*kronecker(mypde.getDim())+mu*(g+transpose(g))
+     stress=lam*trace(g)*kronecker+mu*(g+transpose(g))
      # ... get new acceleration ....
-     mypde.setValue(X=-stress,r=s_tt(t+h)*kronecker(mypde.getDim())[1,:])
+     mypde.setValue(X=-stress)          
      a=mypde.getSolution()
      # ... get new displacement ...
      u_new=2*u-u_last+h**2*a
@@ -43,11 +70,23 @@ def wavePropagation(domain,h,tend,lam,mu,rho,s_tt):
      t+=h
      n+=1
      print n,"-th time step t ",t
-     print "a=",inf(a),sup(a)
-     print "u=",inf(u),sup(u)
-     # ... save current acceleration in units of gravity
-     if n%10==0: saveVTK("u.%i.xml"%(n/10),a=length(a)/9.81)
+     L=Locator(domain,xc)
+     u_pc=L.getValue(u)
+     print "u at point charge=",u_pc
+     
+     u_pc_x=u_pc[0]
+     u_pc_y=u_pc[1]
+     u_pc_z=u_pc[2]
+      
+     # save displacements at point source to file for t > 0
+     u_pc_data.write("%f %f %f %f\n"%(t,u_pc_x,u_pc_y,u_pc_z))
+ 
+     # ... save current acceleration in units of gravity and displacements 
+     if n==1 or n%10==0: saveVTK("./data/usoln.%i.vtu"%(n/10),acceleration=length(a)/9.81,
+     displacement = length(u), Ux = u[0] )
 
-mydomain=Brick(ne,int(width/depth)*ne,int(width/depth)*ne,l0=depth,l1=width,l2=width)
-wavePropagation(mydomain,h,tend,lam,mu,rho,s_tt)
+   u_pc_data.close()
+  
+mydomain=Brick(ne,ne,10,l0=width,l1=width,l2=10.*width/32.)
+wavePropagation(mydomain,h,tend,lam,mu,rho,U0)
 
