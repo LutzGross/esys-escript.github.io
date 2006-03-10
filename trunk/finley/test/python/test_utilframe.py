a54172b9bde60a68f91bf8be115521560c501d5d
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-03-10 06:28:35 +0000

    my little test generator

--- trunk/finley/test/python/test_utilframe.py
+++ trunk/finley/test/python/test_utilframe.py
@@ -13,19 +13,19 @@ class Test_util2(unittest.TestCase):
    def setUp(self):
        self.__dom =Rectangle(10,10,2)
        self.functionspace = FunctionOnBoundary(self.__dom) # due to a bug in escript python needs to hold a reference to the domain
-
-   def test_wherePositive_array_rank0(self):
-      arg=numarray.array(-15.0739210922)
-      res=wherePositive(arg)
-      ref=numarray.array(0.0)
-      print res
-      if isinstance(res,numarray.NumArray):
-         self.failUnlessEqual(res.shape,(),"wrong shape of result.")
-      else:
-         self.failUnless(isinstance(res,float),"wrong type of result.")
-      self.failUnless(Lsup(res-ref)<=self.RES_TOL*Lsup(ref),"wrong result")
-
-
+   def test_add_overloaded_constData_rank1_taggedData_rank0(self):
+       arg0=Data(numarray.array([4.5897569702707663, 3.4489828945022865]),self.functionspace)
+       arg1=Data(0.812494849561,self.functionspace)
+       arg1.setTaggedValue(1,-0.798066999908)
+       print arg0
+       print arg1
+       res=arg0+arg1
+       print res
+       ref=Data(numarray.array([5.4022518198315126, 4.2614777440630327]),self.functionspace)
+       ref.setTaggedValue(1,numarray.array([3.7916899703627909, 2.650915894594311]))
+       self.failUnless(isinstance(res,Data),"wrong type of result.")
+       self.failUnlessEqual(res.getShape(),(2,),"wrong shape of result.")
+       self.failUnless(Lsup(res-ref)<=self.RES_TOL*Lsup(ref),"wrong result") 
 if __name__ == '__main__':
    suite = unittest.TestSuite()
    suite.addTest(unittest.makeSuite(Test_util2))
