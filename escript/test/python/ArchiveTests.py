ac564decd3b91f4999fe727ad096eb18f54ae2c7
elspeth <elspeth@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-03-22 01:12:32 +0000

    More copyright information inserted.

--- escript/test/python/ArchiveTests.py
+++ escript/test/python/ArchiveTests.py
@@ -1,3 +1,4 @@
+
 import sys
 import unittest
 import os
@@ -17,6 +18,11 @@ Version $Id: MiscTests.py 153 2005-10-25 01:51:20Z jgs $
 
 """
 
+__copyright__="""  Copyright (c) 2006 by ACcESS MNRF
+                    http://www.access.edu.au
+                Primary Business: Queensland, Australia"""
+__licence__="""Licensed under the Open Software License version 3.0
+             http://www.opensource.org/licences/osl-3.0.php"""
 #
 # ==============================================================
 
--- escript/test/python/TimingTests.py
+++ escript/test/python/TimingTests.py
@@ -4,6 +4,11 @@ Miscellaneous escript/Data timing tests.
 Version $Id$
 """
 
+__copyright__="""  Copyright (c) 2006 by ACcESS MNRF
+                    http://www.access.edu.au
+                Primary Business: Queensland, Australia"""
+__licence__="""Licensed under the Open Software License version 3.0
+             http://www.opensource.org/licences/osl-3.0.php"""
 import sys
 import os
 import time
