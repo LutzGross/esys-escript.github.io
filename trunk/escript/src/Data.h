e2573a09a81ef88522748b4d4412028b94765dee
elspeth <elspeth@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-03-22 02:12:00 +0000

    More copyright information.

--- trunk/escript/src/Data.h
+++ trunk/escript/src/Data.h
@@ -1,16 +1,14 @@
 // $Id$
 /*
- ******************************************************************************
- *                                                                            *
- *       COPYRIGHT ACcESS 2004 -  All Rights Reserved                         *
- *                                                                            *
- * This software is the property of ACcESS.  No part of this code             *
- * may be copied in any form or by any means without the expressed written    *
- * consent of ACcESS.  Copying, use or modification of this software          *
- * by any unauthorised person is illegal unless that                          *
- * person has a software license agreement with ACcESS.                       *
- *                                                                            *
- ******************************************************************************
+ ************************************************************
+ *          Copyright 2006 by ACcESS MNRF                   *
+ *                                                          *
+ *              http://www.access.edu.au                    *
+ *       Primary Business: Queensland, Australia            *
+ *  Licensed under the Open Software License version 3.0    *
+ *     http://www.opensource.org/licenses/osl-3.0.php       *
+ *                                                          *
+ ************************************************************
 */
 
 /** \file Data.h */
