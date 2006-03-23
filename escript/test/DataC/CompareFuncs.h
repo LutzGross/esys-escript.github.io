c3733a07b96283a7e5d078c20606bf7018f987b7
elspeth <elspeth@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-03-23 02:41:25 +0000

    Copyright information added.

--- escript/test/DataC/CompareFuncs.h
+++ escript/test/DataC/CompareFuncs.h
@@ -1,5 +1,15 @@
 #include "DataC.h"
-
+/*
+ ************************************************************
+ *          Copyright 2006 by ACcESS MNRF                   *
+ *                                                          *
+ *              http://www.access.edu.au                    *
+ *       Primary Business: Queensland, Australia            *
+ *  Licensed under the Open Software License version 3.0    *
+ *     http://www.opensource.org/licenses/osl-3.0.php       *
+ *                                                          *
+ ************************************************************
+*/
 /**
    \brief
    Compare the result obtained with the C interface to the given value.
--- escript/test/DataEmpty/DataEmptyTestCase.h
+++ escript/test/DataEmpty/DataEmptyTestCase.h
@@ -1,15 +1,54 @@
 /* 
- *****************************************************************************
- *                                                                           *
- *       COPYRIGHT  ACcESS  -  All Rights Reserved                           *
- *                                                                           *
- * This software is the property of ACcESS. No part of this code             *
- * may be copied in any form or by any means without the expressed written   *
- * consent of ACcESS.  Copying, use or modification of this software         *
- * by any unauthorised person is illegal unless that person has a software   *
- * license agreement with ACcESS.                                            *
- *                                                                           *
- *****************************************************************************
+ ************************************************************
+ *          Copyright 2006 by ACcESS MNRF                   *
+ *                                                          *
+ *              http://www.access.edu.au                    *
+ *       Primary Business: Queensland, Australia            *
+ *  Licensed under the Open Software License version 3.0    *
+ *     http://www.opensource.org/licenses/osl-3.0.php       *
+ *                                                          *
+ ************************************************************
+*/
+#if !defined  DataTaggedTestCase_20040616_H
+#define  DataTaggedTestCase_20040616_H
+
+#include "CppUnitTest/TestCase.h"
+#include "CppUnitTest/TestSuite.h"
+#include "CppUnitTest/TestCaller.h"
+
+class DataTaggedTestCase : public CppUnitTest::TestCase
+{
+ public:
+
+  //
+  // setUp is called before each test method to set up test state
+  void setUp();
+  //
+  // tearDown is called after each test method is called.
+  void tearDown(); 
+
+  //
+  // A test method must return void and have no arguments
+  // DataTagged class
+  void testAll();
+  void testAddTaggedValues();
+  void testSetTaggedValue();
+  void testCopyConstructors();
+  void testReshape();
+  void testOperations();
+  void testGetSlice();
+  void testSetSlice();
+
+  DataTaggedTestCase (std::string name) : TestCase (name) {}
+  ~DataTaggedTestCase() {}
+  //
+  //
+  // return the suite of tests to perform
+  //
+  static CppUnitTest::TestSuite* suite ();
+};
+
+#endif
 */
 #if !defined  DataEmptyTestCase_20040824_H
 #define  DataEmptyTestCase_20040824_H
