acd776a4f1804ccd1a449d2e9403b5b065a4b2e2
robwdcock <robwdcock@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-03-24 04:22:28 +0000

    + Now compiles and passes tests on AcCESS machine. Some minor mods to the configuraiton file required as we've moved to the intel 9.0.026 compiler

--- scons/ess_options.py
+++ scons/ess_options.py
@@ -9,14 +9,14 @@ scsl_lib_path = '/usr/lib'
 scsl_libs = ['scs_mp']
 
 # locations of include files for python
-python_path = '/raid2/tools/python-2.3.4/include/python2.3'
-python_lib_path = '/raid2/tools/python-2.3.4/lib'
-python_lib = 'python2.3'
+python_path = '/data/raid2/toolspp4/python/2.4.1/gcc-3.3.6/include/python2.4'
+python_lib_path = '/data/raid2/toolspp4/python/2.4.1/gcc-3.3.6/lib'
+python_lib = 'python2.4'
 
 # locations of libraries for boost
-boost_path = '/raid2/tools/boost/include/boost-1_31'
-boost_lib_path = '/raid2/tools/boost/lib'
-boost_lib = 'boost_python-intel-d-1_31'
+boost_path = '/data/raid2/toolspp4/boost/1.33.0/python-2.4.1/gcc-3.3.6/include'
+boost_lib_path = '/data/raid2/toolspp4/boost/1.33.0/python-2.4.1/gcc-3.3.6/lib'
+boost_lib = 'boost_python-mt-d'
 
 # locations of doc building executables
 doxygen_path = '/raid2/tools/doxygen/1.4.2/gcc-3.3.5/bin'
@@ -28,17 +28,13 @@ epydoc_pythonpath = '/raid2/tools/epydoc/2.1/python-2.3.4/lib/python2.3/site-pac
 # papi_lib_path = '/data/raid2/toolspp4/papi/3.0.8.1/gcc-3.3.6/lib'
 # papi_libs = [ 'papi' ]
 
-# names of c and c++ compilers to use
-cc = 'icc'
-cxx = 'icc'
-
 # c flags to use
-cc_flags  = "-O3 -ftz -IPF_ftlacc- -IPF_fma -fno-alias -openmp -openmp_report0 -fno-alias -c99 -no-gcc -w1 -fpic"
-cc_flags_debug  = '-g -O0 -openmp -openmp_report0 -c99 -ansi_alias -no-gcc -w1 -fpic'
+cc_flags  = "-O3 -ftz -IPF_ftlacc- -IPF_fma -fno-alias -openmp -openmp_report0 -fno-alias -c99 -w1 -fpic"
+cc_flags_debug  = '-g -O0 -openmp -openmp_report0 -c99 -ansi_alias -w1 -fpic'
 
 # c++ flags to use
-cxx_flags = '-O3 -ftz -IPF_ftlacc- -IPF_fma -fno-alias -openmp -openmp_report0 -ansi -ansi_alias -no-gcc -w1 -fpic'
-cxx_flags_debug = '-g -O0 -openmp -openmp_report0 -ansi -ansi_alias -no-gcc -w1  -fpic -DDOASSERT -DDOPROF'
+cxx_flags = '-O3 -ftz -IPF_ftlacc- -IPF_fma -fno-alias -openmp -openmp_report0 -ansi -ansi_alias -w1 -fpic'
+cxx_flags_debug = '-g -O0 -openmp -openmp_report0 -ansi -ansi_alias -w1  -fpic -DDOASSERT -DDOPROF'
 
 # system specific libraries to link with
 sys_libs = ['guide', 'irc']
