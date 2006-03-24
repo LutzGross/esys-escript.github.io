93a87a735839e39835c688d5889dd1d33f38034a
elspeth <elspeth@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-03-24 01:10:42 +0000

    More copyright.

--- trunk/scons/esys_options.py
+++ trunk/scons/esys_options.py
@@ -1,3 +1,14 @@
+
+#          Copyright 2006 by ACcESS MNRF                   
+#                                                          
+#              http://www.access.edu.au                    
+#       Primary Business: Queensland, Australia            
+#  Licensed under the Open Software License version 3.0    
+#     http://www.opensource.org/licenses/osl-3.0.php       
+#                                                          
+
+
+
 # this code should be called by all esys13 scons builder and testing scripts
 # it sets default values for relevant variables and overloads them with the 
 # standard gcc settings by importing gcc_options. If there is 
