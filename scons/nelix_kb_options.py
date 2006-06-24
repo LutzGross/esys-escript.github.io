7af0e9eca0b52622c6f1844d261d742292014bba
woo409 <woo409@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-06-24 11:44:55 +0000

    + Some additional files for the win32 port   + The additional test meshes are because windows produces floating point numbers like this 12e+001 where are unix is 12e+01   + nelix_kb_options.py is my notebooks config file. I've included it as an example of win32 compilation options   + system_dep.h is there for win32 shared library function export/import macros and also to use the intel mathimf.h library since the standard MS VC++ math.h doesn't have all the necessary functions. NOTE this file must be included before all other headers to prevent inclusion of math.h

---
+++ scons/nelix_kb_options.py
@@ -0,0 +1,28 @@
+# locations of include files for python
+python_path = 'C:/python23/include'
+python_lib_path = 'C:/python23/libs'
+python_lib = 'python23'
+
+# locations of libraries for boost
+boost_path = 'E:/woo409/development/boost_1_33'
+boost_lib_path = 'E:/woo409/development/boost_1_33/windows_binary/lib'
+boost_lib = 'boost_python-vc71-mt-gd'
+
+cc_defines = ['_USE_MATH_DEFINES', ]
+# c flags to use
+# 1563 - taking adress of a temporary
+# 811 - exception specification for implicitly declared virtual function (destructor usually) incompatible with that of override
+# 161 - openmp pargmas are unknown when not compiling with openmp
+cc_flags  = '/GR /EHsc /MD /Qc99 /Qwd161'
+cc_flags_debug  = '/Od /MDd /RTC1 /GR /EHsc /Qc99 /Qwd161'
+#cc_flags  = '/GR /EHsc /MD /Qc99 /Qopenmp'
+#cc_flags_debug  = '/Od /MDd /RTC1 /GR /EHsc /Qc99 /Qopenmp'
+
+# c++ flags to use
+cxx_flags = ''
+cxx_flags_debug = ''
+# static library archiver flags to use
+#ar_flags = 'crus'
+
+# system specific libraries to link with
+sys_libs = []
