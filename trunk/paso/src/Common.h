51bf6e67ca9f92e2211d383bb0a5d782e67da542
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-06-30 06:33:37 +0000

    The statement '#ifdef MACRO1 && MACRO2' is not supported by the gnu processor. This has been modified.

--- trunk/paso/src/Common.h
+++ trunk/paso/src/Common.h
@@ -28,7 +28,7 @@
 /**************************************************************/
 
 /* some system values */
-#ifdef _WIN32 && __INTEL_COMPILER
+#if ( defined _WIN32 ) && ( defined __INTEL_COMPILER )
 #include <mathimf.h>
 #else
 #include <math.h>
