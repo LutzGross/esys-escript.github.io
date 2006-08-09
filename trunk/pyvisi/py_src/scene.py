f827eeaafdcc760b5f5282789e787a2123c0f665
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-08-09 09:21:00 +0000

    some stuff for john

--- trunk/pyvisi/py_src/scene.py
+++ trunk/pyvisi/py_src/scene.py
@@ -24,27 +24,27 @@ from colors import White
 
 
 
-def Scene(PyvisiObject):
+def Scene(Component):
    """
    a scene for visualization
    """
-   def __init__(self,x_size,y_size,camera,background):
-      self.setSize(x_size,y_size)
-      self.setBackgroundColor(backgound)
-      self.setCamera(backgound)
+   def __init__(self,x_size,y_size)
+      super(Scene, self).__init__()
+      self.x_size=x_size
+      self.y_size=y_size
+      self.setBackgroundColor()
+      self.setCamera()
 
    def render(self,filename=None,wait=False):
       """
       renders the scene. If filename is present the file is rendered in the file filename
       """
       pass
-   def setSize(self,x_size,y_size):
-      """
-      sets the size of the output window
-      """
-      pass
 
-   def setBackgroundColor(self,color):
+   def _addComponent(self,comp):
+       pass
+
+   def setBackgroundColor(self,color=White()):
       """
       Sets the background color of the Scene
 
@@ -53,13 +53,7 @@ def Scene(PyvisiObject):
       """
       pass
 
-   def setCamera(self,camera):
-      """
-      sets the camera
-`     """
-      pass
-
-def openScene(renderer="pyvisi",x_size=640,y_size=480,camera=IsometricCamera(),background=White()):
+def openScene(renderer="pyvisi",x_size=640,y_size=480):
      """
      opens a scene 
 
@@ -72,28 +66,51 @@ def openScene(renderer="pyvisi",x_size=640,y_size=480,camera=IsometricCamera(),b
      """
      if renderer == "vtk_online":
          import renderers.vtk.VTKScene as VTKScene
-         return VTKScene(x_size,y_size,camera,background,"online", offscreen=False)
+         return VTKScene(x_size,y_size,"online", offscreen=False)
      elif renderer == "vtk_ps":
          import renderers.vtk.VTKScene as VTKScene
-         return VTKScene(x_size,y_size,camera,background,"ps", offscreen=True)
+         return VTKScene(x_size,y_size,"ps", offscreen=True)
      elif renderer == "vtk_png":
          import renderers.vtk.VTKScene as VTKScene
-         return VTKScene(x_size,y_size,camera,background,"png", offscreen=True)
+         return VTKScene(x_size,y_size,"png", offscreen=True)
      elif renderer == "vtk_jpeg":
          import renderers.vtk.VTKScene as VTKScene
-         return VTKScene(x_size,y_size,camera,background,"jpeg", offscreen=True)
+         return VTKScene(x_size,y_size,"jpeg", offscreen=True)
      elif renderer == "vtk_tiff":
          import renderers.vtk.VTKScene as VTKScene
-         return VTKScene(x_size,y_size,camera,background,"tiff", offscreen=True)
+         return VTKScene(x_size,y_size,"tiff", offscreen=True)
      elif renderer == "vtk_bmp":
          import renderers.vtk.VTKScene as VTKScene
-         return VTKScene(x_size,y_size,camera,background,"bmp", offscreen=True)
+         return VTKScene(x_size,y_size,"bmp", offscreen=True)
      elif renderer == "vtk_pnm":
          import renderers.vtk.VTKScene as VTKScene
-         return VTKScene(x_size,y_size,camera,background,"pnm", offscreen=True)
+         return VTKScene(x_size,y_size,"pnm", offscreen=True)
      elif renderer == "pyvisi" or renderer==None:
          import Scene
-         return Scene(x_size,y_size,camera,background)
+         return Scene(x_size,y_size)
      else:
          raise ValueError, "Unknown renderer %s."%renderer
-
+##########################################################################################
+## vtk specific stuff (should be in renderers.vtk)
+##########################################################################################
+def renderArrow(comp):
+      pass # 
+class VTKScene(Scene):
+    def __init__(x_size,y_size,format,offscreen):
+         super(VTKScene, self).__init__()
+         self.__comps=[]
+         pass
+    def _addComponent(self,comp):
+        if isinstance(Arrow,comp):
+           # ... some vtk set up here"
+           comp._render=renderArrow
+        self.__comps.append(comp)
+ 
+   def render(self,filename=None,wait=False):
+      """
+      renders the scene. If filename is present the file is rendered in the file filename
+      """
+      for i in self.__comps: i.render()
+      pass # more rendering work
+      for i in self.__comps: i.markFeaturesAsUsed()
+  
