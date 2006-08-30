09c289c3af9467c5ebe11db9a13e8d6e41373fb9
jongui <jongui@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-08-30 07:52:35 +0000

    Tidy up the codes to include inheritance of common features for reusability. Also added options to change opacity, scale factor, color mode and contour values.

--- pyvisi/py_src/contour.py
+++ pyvisi/py_src/contour.py
@@ -19,39 +19,50 @@ __version__="$Revision:$"
 __date__="$Date:$"
 
 import vtk
+from common import *
 
-class Contour:
+class Contour(Common):
 	
 	def __init__(self, open_scene, data_collector):
-		self.open_scene = open_scene
-		self.data_collector = data_collector
-		self.vtk_contour = None
-		self.vtk_contour_mapper = None
-		self.vtk_contour_actor = None
+		Common.__init__(self, open_scene, data_collector)
+		#self.open_scene = open_scene
+		#self.data_collector = data_collector
+		#self.vtk_contour = None
+		#self.vtk_contour_mapper = None
+		#self.vtk_contour_actor = None
 
 		self.setContour()
-		self.setMapper()
-		self.setActor()
+		#self.setMapper()
+		#self.setActor()
+
+		Common.setMapper(self, "self.vtk_contour.GetOutput()")
+		Common.setActor(self)
+		Common.addActor(self)
 
 	# set up the contour and specify the number and range
 	def setContour(self):
 		self.vtk_contour = vtk.vtkContourFilter()
 		self.vtk_contour.SetInput(self.data_collector.getReader().GetOutput())
-		self.vtk_contour.GenerateValues(5, 0.0, 1.2)
-		
+
+	def setValue(self, contour_number, value):
+		self.vtk_contour.SetValue(contour_number, value)
+
+	def generateValues(self, contour_number, min_range, max_range):
+		self.vtk_contour.GenerateValues(contour_number, min_range, max_range)
+
 	# set up the mapper and data	
-	def setMapper(self):
-		self.vtk_contour_mapper = vtk.vtkPolyDataMapper()
-		self.vtk_contour_mapper.SetInput(
-			self.vtk_contour.GetOutput())
+	#def setMapper(self):
+	#	self.vtk_contour_mapper = vtk.vtkPolyDataMapper()
+	#	self.vtk_contour_mapper.SetInput(
+	#		self.vtk_contour.GetOutput())
 	
 	# set up the actor and add the actor to the scene
-	def setActor(self):
-		self.vtk_contour_actor = vtk.vtkActor()
-		self.vtk_contour_actor.SetMapper(self.vtk_contour_mapper)
-		self.vtk_contour_actor.GetProperty().SetOpacity(0.6)
+	#def setActor(self):
+	#	self.vtk_contour_actor = vtk.vtkActor()
+	#	self.vtk_contour_actor.SetMapper(self.vtk_contour_mapper)
+	#	self.vtk_contour_actor.GetProperty().SetOpacity(0.6)
 
-		self.open_scene.getRenderer().AddActor(self.vtk_contour_actor)
+	#	self.open_scene.getRenderer().AddActor(self.vtk_contour_actor)
 
 
 #class ContourOnPlane(Component):
