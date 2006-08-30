c70dbe48177edd8f53bc20461a75d9f347c4df51
jongui <jongui@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-08-30 00:08:02 +0000

    Added: contour.py and arrows.py

--- pyvisi/py_src/contour.py
+++ pyvisi/py_src/contour.py
@@ -18,17 +18,44 @@ __url__="http://www.iservo.edu.au/esys"
 __version__="$Revision:$"
 __date__="$Date:$"
 
-from common import Component
+import vtk
 
-class Contour(Component):
-    """
-    shows scalar data by contour surfaces
-    """
-    pass
+class Contour:
+	
+	def __init__(self, open_scene, data_collector):
+		self.open_scene = open_scene
+		self.data_collector = data_collector
+		self.vtk_contour = None
+		self.vtk_contour_mapper = None
+		self.vtk_contour_actor = None
+
+		self.setContour()
+		self.setMapper()
+		self.setActor()
+
+	# set up the contour and specify the number and range
+	def setContour(self):
+		self.vtk_contour = vtk.vtkContourFilter()
+		self.vtk_contour.SetInput(self.data_collector.getReader().GetOutput())
+		self.vtk_contour.GenerateValues(5, 0.0, 1.2)
+		
+	# set up the mapper and data	
+	def setMapper(self):
+		self.vtk_contour_mapper = vtk.vtkPolyDataMapper()
+		self.vtk_contour_mapper.SetInput(
+			self.vtk_contour.GetOutput())
+	
+	# set up the actor and add the actor to the scene
+	def setActor(self):
+		self.vtk_contour_actor = vtk.vtkActor()
+		self.vtk_contour_actor.SetMapper(self.vtk_contour_mapper)
+		self.vtk_contour_actor.GetProperty().SetOpacity(0.6)
+
+		self.open_scene.getRenderer().AddActor(self.vtk_contour_actor)
 
 
-class ContourOnPlane(Component):
-    """
-    shows scalar data by contour surfaces on a given plane
-    """
-    pass
+#class ContourOnPlane(Component):
+"""
+shows scalar data by contour surfaces on a given plane
+"""
+pass
