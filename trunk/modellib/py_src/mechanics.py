5f2285599a1261db020605810099857b6ef634fc
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-08-31 01:31:15 +0000

    some changes on the drucker-prager (it does not work yet)

--- trunk/modellib/py_src/mechanics.py
+++ trunk/modellib/py_src/mechanics.py
@@ -18,7 +18,7 @@ class Mechanics(Model):
       @ivar internal_force: =Data()
       @ivar external_force: =Data()
       @ivar prescribed_velocity: =Data()
-      @ivar location_prescribed_velocity: =Data()
+      @ivar location_fixed_velocity: =Data()
       @ivar temperature:  = None
       @ivar expansion_coefficient:  = 0.
       @ivar bulk_modulus: =1.
@@ -44,8 +44,7 @@ class Mechanics(Model):
                                velocity=None, \
                                internal_force=Data(), \
                                external_force=Data(), \
-                               prescribed_velocity=Data(), \
-                               location_prescribed_velocity=Data(), \
+                               location_fixed_velocity=Data(), \
                                temperature = None, \
                                expansion_coefficient = 0., \
                                bulk_modulus=2., \
@@ -60,13 +59,17 @@ class Mechanics(Model):
            if not self.displacement: self.displacement=Vector(0.,ContinuousFunction(self.domain))
            if not self.velocity: self.velocity=Vector(0.,ContinuousFunction(self.domain))
            if not self.stress: self.stress=Tensor(0.,ContinuousFunction(self.domain))
-           self.__pde=LinearPDE(self.domain)
-           self.__pde.setSymmetryOn()
-           self.__displacement_old=self.displacement
+           # open PDE:
            self.stress_old=self.stress
            self.__velocity_old=self.velocity
            self.__temperature_old=self.temperature
-           
+           self.__displacement_old=self.displacement
+           # get node cooridnates:
+           self.__x=self.domain.getX()
+           self.domain.setX(self.__x+self.displacement)
+           # open PDE:
+           self.__pde=LinearPDE(self.domain)
+           self.__pde.setSymmetryOn()
       def doStepPreprocessing(self,dt):
             """
             step up pressure iteration
@@ -76,12 +79,20 @@ class Mechanics(Model):
             """
             self.__iter=0
             self.__diff=self.UNDEF_DT
-            # set new values:
-            self.displacement=self.__displacement_old
-            self.stress=self.stress_old
+            # set initial values for the iteration:
             self.velocity=self.__velocity_old
             self.temperature=self.__temperature_old
-            self.__velocity_last=self.velocity
+            self.displacement=self.__displacement_old+self.velocity*dt
+
+            # set temperature strain:
+            if self.temperature:
+               self.deps_therm=self.self.expansion_coefficient*(self.temperature-self.__temperature_old)
+            else:
+               self.deps_therm=0.
+
+            # update geometry
+            self.domain.setX(self.__x+self.displacement)
+            self.stress=self.stress_old
 
       def doStep(self,dt):
           """
@@ -102,50 +113,51 @@ class Mechanics(Model):
           # set PDE coefficients:
           self.__pde.setValue(A=self.S)
           self.__pde.checkSymmetry()
-          self.__pde.setValue(X=self.stress_old-self.bulk_modulus*self.deps_therm*k3)
+          self.__pde.setValue(X=-self.stress+self.stress_old-self.bulk_modulus*self.deps_therm*k3)
           if self.internal_force: self.__pde.setValue(Y=self.internal_force)
           if self.external_force: self.__pde.setValue(y=self.external_force)
-          self.__pde.setValue(r=self.prescribed_velocity, \
-                              q=self.location_prescribed_velocity)
+          self.__pde.setValue(q=self.location_fixed_velocity)
           # solve the PDE:
-          self.__pde.setTolerance(self.rel_tol/100.)
-          self.velocity=self.__pde.getSolution(verbose=True)
-          # calculate convergence indicators:
-          self.__diff,diff_old=Lsup(self.velocity-self.__velocity_last),self.__diff
-          self.__velocity_last=self.velocity
+          self.__pde.setTolerance(self.rel_tol**2)
+          du=self.__pde.getSolution(verbose=True)
+          self.velocity+=du/dt
+          # update geometry
           self.displacement=self.__displacement_old+dt*self.velocity
+          self.domain.setX(self.__x+self.displacement)
+
           self.__iter+=1
           if self.debug:
              for i in range(self.domain.getDim()):
                 self.trace("velocity %d range %e:%e"%(i,inf(self.velocity[i]),sup(self.velocity[i])))
-             self.trace("velocity increment %s"%self.__diff)
-          if self.__iter>2 and diff_old<self.__diff:
-              raise IterationDivergenceError,"no improvement in stress iteration"
-          if self.__iter>self.max_iter:
-              raise IterationDivergenceError,"Maximum number of iterations steps reached"
+             for i in range(self.domain.getDim()):
+                self.trace("displacement %d range %e:%e"%(i,inf(self.displacement[i]),sup(self.displacement[i])))
 
       def terminateIteration(self):
           """iteration is terminateIterationd if relative pressure change is less then rel_tol"""
-          return self.__diff<=self.rel_tol*Lsup(self.velocity)+self.abs_tol
+          if self.__iter>2 and self.diff_last<self.diff:
+              raise IterationDivergenceError,"no improvement in stress iteration"
+          if self.__iter>self.max_iter:
+              raise IterationDivergenceError,"Maximum number of iterations steps reached"
+          return self.diff<=self.rel_tol*1.e-2*Lsup(self.stress)+self.abs_tol
 
       def doStepPostprocessing(self,dt):
            """
            accept all the values:
            """
            self.__displacement_old=self.displacement
-           self.stress_old=self.stress
-           self.__velocity_old=self.velocity
+           self.__velocity_old,self.__velocity_old=self.velocity,self.__velocity_old
            self.__temperature_old=self.temperature
+           self.stress_old=self.stress
 
       def getSafeTimeStepSize(self,dt):
            """
            returns new step size
            """
-           d=Lsup(self.velocity-self.__velocity_old)/dt
+           d=Lsup(self.velocity-self.__velocity_old)*Lsup(self.displacement)
            if d>0:
-               return Lsup(self.displacement)/d
+              return dt/d*self.rel_tol
            else:
-               return self.UNDEF_DT
+              return self.UNDEF_DT
 
 
 
@@ -162,93 +174,85 @@ class DruckerPrager(Mechanics):
            self.declareParameter(plastic_stress=0.,
                                  friction_parameter=0.,
                                  dilatancy_parameter=0.,
-                                 shear_length=1.e15,
-                                 hardening=0.)
-
+                                 shear_length=1.e15)
       def doInitialization(self):
-           """
-           initialize model
-           """
-           super(DruckerPrager, self).doInitialization()
-           self.__plastic_stress_old=self.plastic_stress
-           self.__tau_y_old=self.shear_length
-           self.__hardening_old=self.hardening
+          """
+          """
+          super(DruckerPrager, self).doInitialization()
 
-      def doStepPreprocessing(self,dt):
-            """
-            step up pressure iteration
+      def doStepPreprocessing(self):
+          """
+          """
+          super(DruckerPrager, self).doStepPreprocessing(dt)
+          # set initial guess for iteration:
+          self.shear_length=self.__shear_length_old
+          self.plastic_stress=self.__plastic_stress_old
+          # set initial stress:
+          self.updateStress()
 
-            if run within a time dependend problem extrapolation of pressure from previous time steps is used to
-            get an initial guess (that needs some work!!!!!!!)
-            """
-            super(DruckerPrager, self).doStepPreprocessing(dt)
-            self.plastic_stress=self.__plastic_stress_old
-            self.shear_length=self.__tau_y_old
-            self.hardening=self.__hardening_old
+ 
 
-      def doStep(self,dt):
+#================================
+           
+          # set new tangential operator:
+          self.S=self.getTangentialTensor()
+          # do the update step:
+
+          super(DruckerPrager, self).doStep(dt)
+          # update stresses:
+          self.updateStress()
+
+      def doStepPostprocessing(self,dt):
+          super(DruckerPrager, self).doStepPostprocessing(dt)
+          self.__plastic_stress_old=self.plastic_stress
+          self.__shear_length_old=self.shear_length
+
+      def setStress(self):
+           G=self.shear_modulus
+           K=self.bulk_modulus
+           alpha=self.friction_parameter
+           beta=self.dilatancy_parameter
+           h=self.hardening
+           k3=kronecker(self.domain)
+           # elastic trial stress:
+           g=grad(self.velocity)
+           D=symmetric(g)
+           W=nonsymmetric(g)
+           s_e=self.stress_old+K*self.deps_therm*k3+ \
+                      dt*(2*G*D+(K-2./3*G)*trace(D)*k3 \
+                          +2*symmetric(matrix_mult(W,self.stress_old)))
+           p_e=-1./3*trace(s_e)
+           s_e_dev=s_e+p_e*k3
+           tau_e=sqrt(1./2*inner(s_e_dev,s_e_dev))
+           # yield conditon for elastic trial stress:
+           F=tau_e-alpha*p_e-self.shear_length
+           self.__chi=whereNonNegative(F)
+           # plastic stress increment:
+           l=self.__chi*F/(h+G+beta*K)
+           self.__tau=tau_e-G*l
+           self.stress=self.__tau/(tau_e+self.abs_tol*whereZero(tau_e,self.abs_tol))*s_e_dev+(p_e+l*beta*K)*k3
+           self.plastic_stress=self.plastic_stress_old+l
+           # update hardening
+           self.hardening=(self.shear_length-self.__shear_length_old)/(l+self.abs_tol*whereZero(l))
+
+      def setTangentialTensor(self):
            G=self.shear_modulus
            K=self.bulk_modulus
            alpha=self.friction_parameter
            beta=self.dilatancy_parameter
            tau_Y=self.shear_length
+           chi=self.__chi
            h=self.hardening
-           # set new tangential operator:
-           self.S=self.getTangentialTensor(self.stress,
-                                           tau_Y,G,K,alpha,beta,h)
-           # do the update step:
-           super(DruckerPrager, self).doStep(dt)
+           tmp=G*s_dev/(tau+self.abs_tol*whereZero(tau,self.abs_tol))
 
-           # update stresses:
-           self.stress,self.plastic_stress=self.getNewStress(self.stress_old,self.__plastic_stress_old,
-                                                        self.velocity*dt,
-                                                        self.deps_therm,tau_Y,G,K,alpha,beta,h)
-           if self.__plastic_stress_old:
-              dps=self.plastic_stress-self.__plastic_stress_old
-              self.hardening=(tau_Y-self.__tau_y_old)/(dps+self.abs_tol*whereZero(dps))
-           else:
-              self.hardening=0
-
-      def doStepPostprocessing(self,dt):
-          super(DruckerPrager, self).doStepPostprocessing(dt)
-          self.__plastic_stress_old=self.plastic_stress
-          self.__tau_y_old=self.shear_length
-          self.__hardening_old=self.hardening
-
-      def getNewStress(self,s,gamma_p,du,deps_therm,tau_Y,G,K,alpha,beta,h):
-            k3=kronecker(self.domain)
-            dt=1.
-            g=grad(du)
-            D=symmetric(g)
-            W=nonsymmetric(g)
-            s_e=s+K*deps_therm*k3 +dt*(2*G*D+(K-2./3*G)*trace(D)*k3 \
-                                     +2*nonsymmetric(matrix_mult(W,s)))
-            p_e=-1./3*trace(s_e)
-            s_e_dev=s_e+p_e*k3
-            tau_e=sqrt(1./2*inner(s_e_dev,s_e_dev))
-            F=tau_e-alpha*p_e-tau_Y
-            chi=whereNonNegative(F)
-            l=chi*F/(h+G+beta*K)
-            s=(1.-l*G/tau_e)*s_e_dev+(p_e+l*beta*K)*k3
-            gamma_p=gamma_p+l
-            return s, gamma_p
-            
-
-      def getTangentialTensor(self,s,tau_Y,G,K,alpha,beta,h):
-           d=self.domain.getDim()
            k3=kronecker(Function(self.domain))
-           p=-1./d*trace(s)
-           s_dev=s+p*k3 
-           tau=sqrt(1./2*inner(s_dev,s_dev))
-           chi=whereNonNegative(tau-alpha*p-tau_Y)
-           sXk3=outer(s,k3)
+           s_dev=self.stress+trace(self.stress)*(k3/d)
+           sXk3=outer(self.stress_old,k3)
            k3Xk3=outer(k3,k3)
-           tmp=G*s_dev/(tau+tau_Y*1.e-15*whereZero(tau,1.e-15))
            S=G*(swap_axes(k3Xk3,1,2)+swap_axes(k3Xk3,1,3)) \
                + (K-2./3*G)*k3Xk3 \
-               + sXk3-swap_axes(sXk3,1,3) \
+               + (sXk3-swap_axes(sXk3,1,3)) \
                + 1./2*(swap_axes(sXk3,0,3)-swap_axes(sXk3,1,2) \
                       -swap_axes(sXk3,1,3)+swap_axes(sXk3,0,2)) \
-               - chi/(h+G+alpha*beta*K)*outer(tmp+beta*K*k3,tmp+alpha*K*k3)\
-           # print S
+               - outer(chi/(h+G+alpha*beta*K)*(tmp+beta*K*k3),tmp+alpha*K*k3)
            return S
