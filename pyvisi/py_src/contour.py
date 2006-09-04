942bfc766537f0ab85e2082700c04036c9459649
jongui <jongui@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-09-04 22:20:28 +0000

    Added more docstrings and updated the way positions, focal point and view angle are specified.

--- pyvisi/py_src/contour.py
+++ pyvisi/py_src/contour.py
@@ -1,68 +1,65 @@
 """
-class that shows scalar data by contour surfaces
-
-@var __author__: name of author
-@var __license__: licence agreement
-@var __copyright__: copyrights
-@var __url__: url entry point on documentation
-@var __version__: version
-@var __date__: date of the version
+Class that shows a scalar field by contour surfaces.
 """
-__copyright__="""  Copyright (c) 2006 by ACcESS MNRF
-                    http://www.access.edu.au
-                Primary Business: Queensland, Australia"""
-__license__="""Licensed under the Open Software License version 3.0
-             http://www.opensource.org/licenses/osl-3.0.php"""
-__author__="Paul Cochrane, L. Gross"
-__url__="http://www.iservo.edu.au/esys"
-__version__="$Revision:$"
-__date__="$Date:$"
 
 import vtk
 from common import *
 
 class Contour(Common):
+	"""
+	@author: John Ngui
+	@author: Lutz Gross	
+	"""
 	
 	def __init__(self, open_scene, data_collector):
-		Common.__init__(self, open_scene, data_collector)
-		#self.open_scene = open_scene
-		#self.data_collector = data_collector
-		#self.vtk_contour = None
-		#self.vtk_contour_mapper = None
-		#self.vtk_contour_actor = None
+		"""
+		@type open_scene: L{OpenScene <openscene.OpenScene>} object
+		@param open_scene: Scene in which components are to be added to
+		@type data_collector: L{DataCollector <datacollector.DataCollector>}
+			object
+		@param data_collector: Source of data for visualization
+		"""
 
+		Common.__init__(self, open_scene, data_collector)
 		self.setContour()
-		#self.setMapper()
-		#self.setActor()
 
 		Common.setMapper(self, "self.vtk_contour.GetOutput()")
 		Common.setActor(self)
 		Common.addActor(self)
 
-	# set up the contour and specify the number and range
 	def setContour(self):
+		"""
+		Set up the contour and its input.
+		"""
+
 		self.vtk_contour = vtk.vtkContourFilter()
 		self.vtk_contour.SetInput(self.data_collector.getReader().GetOutput())
 
 	def setValue(self, contour_number, value):
+		"""
+		Set the contour number and its value.
+
+		@type contour_number: Number
+		@param contour_number: Contour number
+		@type value: Number
+		@param value: Contour value
+		"""
+
 		self.vtk_contour.SetValue(contour_number, value)
 
 	def generateValues(self, number_contours, min_range, max_range):
+		"""
+		Generate the specified number of contours within the specified range.
+
+		@type number_contours: Number
+		@param number_contours: Number of contours to generate	
+		@type min_range: Number
+		@param min_range: Minimum contour value
+		@type max_range: Number 
+		@param max_range: Maximum contour value
+		"""
+
 		self.vtk_contour.GenerateValues(number_contours, min_range, max_range)
-
-	# set up the mapper and data	
-	#def setMapper(self):
-	#	self.vtk_contour_mapper = vtk.vtkPolyDataMapper()
-	#	self.vtk_contour_mapper.SetInput(
-	#		self.vtk_contour.GetOutput())
-	
-	# set up the actor and add the actor to the scene
-	#def setActor(self):
-	#	self.vtk_contour_actor = vtk.vtkActor()
-	#	self.vtk_contour_actor.SetMapper(self.vtk_contour_mapper)
-	#	self.vtk_contour_actor.GetProperty().SetOpacity(0.6)
-
-	#	self.open_scene.getRenderer().AddActor(self.vtk_contour_actor)
 
 
 #class ContourOnPlane(Component):
--- pyvisi/py_src/map.py
+++ pyvisi/py_src/map.py
@@ -1,60 +1,29 @@
 """
-class that shows scalar data by color on the domain surface
-
-@var __author__: name of author
-@var __license__: licence agreement
-@var __copyright__: copyrights
-@var __url__: url entry point on documentation
-@var __version__: version
-@var __date__: date of the version
+Class that shows a scalar field by a surface map. 
 """
-__copyright__="""  Copyright (c) 2006 by ACcESS MNRF
-                    http://www.access.edu.au
-                Primary Business: Queensland, Australia"""
-__license__="""Licensed under the Open Software License version 3.0
-             http://www.opensource.org/licenses/osl-3.0.php"""
-__author__="Paul Cochrane, L. Gross"
-__url__="http://www.iservo.edu.au/esys"
-__version__="$Revision:$"
-__date__="$Date:$"
 
 import vtk
 from common import * 
 
 class Map(Common):
+	"""
+	@author: John Ngui
+	@author: Lutz Gross
+	"""
 
 	def __init__(self, open_scene, data_collector):
-		Common.__init__(self, open_scene, data_collector)
-		#self.open_scene = open_scene
-		#self.data_collector = data_collector
-		#self.vtk_xml_mapper = None
-		#self.vtk_xml_actor = None
+		"""
+		@type open_scene: L{OpenScene <openscene.OpenScene>} object
+		@param open_scene: Scene in which components are to be added to
+		@type data_collector: L{DataCollector <datacollector.DataCollector>}
+			object
+		@param data_collector: Source of data for visualization	
+		"""
 
-		#self.setMapper()
-		#self.setActor()
+		Common.__init__(self, open_scene, data_collector)
 		Common.setMapper(self, "self.data_collector.getReader().GetOutput()")
 		Common.setActor(self)
 		Common.addActor(self)
-
-
-		
-
-	# set up the mapper and data
-	#def setMapper(self):
-		
-		#self.vtk_xml_mapper = vtk.vtkDataSetMapper()
-		#self.vtk_xml_mapper.SetInput(
-		#	self.data_collector.getReader().GetOutput())
-
-	# set up the actor and add the actor to the scene
-	#def setActor(self):
-		#self.vtk_xml_actor = vtk.vtkActor()
-		#self.vtk_xml_actor.SetMapper(self.vtk_xml_mapper)
-		#self.vtk_xml_actor.GetProperty().SetOpacity(0.2)
-
-		#self.open_scene.getRenderer().AddActor(self.vtk_xml_actor)
-
-	
 
 """
 class MapOnPlane():
