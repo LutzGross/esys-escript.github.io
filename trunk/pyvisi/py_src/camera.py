ce60bf5c372e35b21519cc08b19485be2f137398
jongui <jongui@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-09-05 07:23:50 +0000

    added more epydocs and improved the way colors are specified in the driver.

--- trunk/pyvisi/py_src/camera.py
+++ trunk/pyvisi/py_src/camera.py
@@ -1,32 +1,76 @@
 """
-Class and functions associated with cameras and views
+Class that controls the camera and its settings.
 """
 
 import vtk
 
 class Camera:
+	"""
+	@author: John Ngui
+	@author: Lutz Gross
+	"""
+
 	def __init__(self, open_scene):
+		"""
+		@type open_scene: L{OpenScene <openscene.OpenScene>} object
+		@param open_scene: Scene in which components are to be added to
+		"""
+
 		self.open_scene = open_scene
 		self.vtk_camera = None 
 
 		self.setCamera()
 
 	def setCamera(self):
+		"""
+		Set up the camera and associate it with the renderer.
+		"""
+
 		self.vtk_camera = vtk.vtkCamera()		
 		self.open_scene.getRenderer().SetActiveCamera(self.vtk_camera)
 
 	def setClippingRange(self, near_clipping, far_clipping):
+		"""
+		Set the near and far clipping plane of the camera.
+
+		@type near_clipping: Number
+		@param near_clipping: Distance to the near clipping range
+		@type far_clipping: Number
+		@param far_clipping: Distance to the far clipping plane
+		"""
+
 		self.vtk_camera.SetClippingRange(near_clipping, far_clipping)
 
 	def setFocalPoint(self, position):
+		"""
+		Set the focal point of the camera.
+	
+		@type position: L{Position <geo.Position>} object
+		@param position: Camera focal point position 
+		"""
+
 		self.vtk_camera.SetFocalPoint(position.getXCoor(), position.getYCoor(),
 			position.getZCoor())	
 
 	def setPosition(self, position):
+		"""
+		Set the position of the camera.
+
+		@type position: L{Position <geo.Position>} object
+		@param position: Camera position 
+		"""
+
 		self.vtk_camera.SetPosition(position.getXCoor(), position.getYCoor(),
 			position.getZCoor())
 
 	def setViewUp(self, position):
+		"""
+		Set the up direction of the camera.
+
+		@type position: L{Position <geo.Position>} object
+		@param position: Camera view up position
+		"""
+
 		self.vtk_camera.SetViewUp(position.getXCoor(), position.getYCoor(), 
 			position.getZCoor())
 
--- trunk/pyvisi/test/python/test_pyvisi.py
+++ trunk/pyvisi/test/python/test_pyvisi.py
@@ -1,25 +1,7 @@
-# $Id$
 """
 Test suite for the pyvisi
-
-@var __author__: name of author
-@var __copyright__: copyrights
-@var __license__: licence agreement
-@var __url__: url entry point on documentation
-@var __version__: version
-@var __date__: date of the version
 """
-
-__author__="Lutz Gross, l.gross@uq.edu.au"
-__copyright__="""  Copyright (c) 2006 by ACcESS MNRF
-                    http://www.access.edu.au
-                Primary Business: Queensland, Australia"""
-__license__="""Licensed under the Open Software License version 3.0
-             http://www.opensource.org/licenses/osl-3.0.php"""
-__url__="http://www.iservo.edu.au/esys/escript"
-__version__="$Revision$"
-__date__="$Date$"
-
+"""
 import unittest
 from pyvisi import *
 
@@ -27,4 +9,4 @@ class Test_pyvisi(unittest.TestCase):
     def test_camera(self):
       c=Camera(self.scene)
       self.failUnless(abs(c.getPosition()[0])==0.,"wrong default camera position")
-
+"""
