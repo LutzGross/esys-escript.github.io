a9eb69ea5dba59a57a506acf4bc6637e795a3572
jongui <jongui@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-09-14 07:54:04 +0000

    Added vtkTextProperty to control the style of text and partially completed the carpet module.

--- trunk/pyvisi/py_src/text.py
+++ trunk/pyvisi/py_src/text.py
@@ -4,6 +4,8 @@
 """
 
 import vtk
+from style import Style
+#from colormap import *
 
 class Text:
 	"""
@@ -31,77 +33,14 @@ class Text:
 		self.vtk_text_mapper = vtk.vtkTextMapper()
 		self.vtk_text_mapper.SetInput(text)
 		
-		#self.setFontSize(10)
-		self.setFontFamily("Times")
-		#self.setJustification("Right")
-		self.bold()
-		self.italic()
-		self.shadow()
-		# Default text color is black
-		self.getProperty().SetColor(0, 0, 0)
-	
 		self.setActor()
 		self.addActor()
 
-	def getProperty(self):
-		"""
-		Return the property of the text mapper.
+	def setPosition(self, x_coor, y_coor):
+		self.vtk_text_actor.SetDisplayPosition(x_coor, y_coor)
 
-		@rtype: vtkTextProperty
-		@return: VTK text property
-		"""
-
-		return self.vtk_text_mapper.GetTextProperty()
-
-	#def setFontSize(self, size):
-	#	eval("self.getProperty().SetFontSize(%s)" % size)
-
-	def setFontFamily(self, font):
-		"""
-		Set the font of the text.
-
-		@type font: String
-		@param font: Font of the text (i.e. Times, Arial and Courier)
-		"""
-
-		eval("self.getProperty().SetFontFamilyTo%s()" % font)
-	
-	#def setJustification(self, justification):
-	#	eval("self.getProperty().SetJustificationTo%s()" % justification) 
-
-	def bold(self):
-		"""
-		Bold the text.
-		"""
-
-		self.getProperty().BoldOn()
-
-	def italic(self):
-		"""
-		Italize the text.
-		"""
-
-		self.getProperty().ItalicOn()
-
-	def shadow(self):
-		"""
-		Shadow the text.
-		"""
-
-		self.getProperty().ShadowOn()
-
-	def setTextColor(self, colorMap):
-		"""
-		Set the color of the text.
-
-		@type colorMap: L{ColorMap <colormap.ColorMap>} object
-		@param colorMap: Color of the text
-		"""
-
-		#self.vtk_text_actor.GetProperty().SetColor(colorMap.getR(),
-		#	colorMap.getG(), colorMap.getB())
-		self.getProperty().SetColor(colorMap.getR(), colorMap.getG(),
-			colorMap.getB())
+	def setStyle(self, style):
+		self.vtk_text_mapper.SetTextProperty(style.getTextProperty())		
 
 
 	def setActor(self):
@@ -111,7 +50,7 @@ class Text:
 
 		self.vtk_text_actor = vtk.vtkScaledTextActor()
 		self.vtk_text_actor.SetMapper(self.vtk_text_mapper)
-		self.vtk_text_actor.SetDisplayPosition(10, 10)
+		self.vtk_text_actor.SetDisplayPosition(50, 20)
 
 	def addActor(self):
 		"""
@@ -119,3 +58,6 @@ class Text:
 		"""
 
 		self.scene.getRenderer().AddActor2D(self.vtk_text_actor)
+
+
+	
