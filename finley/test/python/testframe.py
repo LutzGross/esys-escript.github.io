790550d4647b3534c9a8fbb1afe4a0000974b078
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-09-26 01:00:45 +0000

    tests for order1 tetrahedron elements added

--- finley/test/python/testframe.py
+++ finley/test/python/testframe.py
@@ -16,23 +16,42 @@ import numarray
 FINLEY_TEST_MESH_PATH="data_meshes/"
 
 class Test_util2(unittest.TestCase):
-   RES_TOL=1.e-2
-
+   RES_TOL=1.e-8
    def setUp(self):
         self.order=1
-        self.domain = ReadMesh(FINLEY_TEST_MESH_PATH+"tet_2D_order2.fly")
+        self.domain = ReadMesh(FINLEY_TEST_MESH_PATH+"tet_3D_order1.fly")
    def tearDown(self):
         del self.order
         del self.domain
-   def testProjector_rank0_fast(self):
-      x=ContinuousFunction(self.domain).getX()
-      h=Lsup(self.domain.getSize())
-      p=Projector(self.domain,reduce=False,fast=True)
-      td_ref=x[0]
-      td=p(td_ref.interpolate(Function(self.domain)))
-      print td
-      print td_ref-td
-      self.failUnless(Lsup(td-td_ref)<Lsup(td_ref)*h,"value wrong")
+
+   def test_normal_FunctionOnBoundary(self):
+     """
+     test getNormal() on boundary
+
+     assumptions: FunctionOnBoundary(self.domain) exists
+     """
+     dim=self.domain.getDim()
+     f=FunctionOnBoundary(self.domain)
+     x=f.getX()
+     ref=Vector(0.,what=f)
+     if dim==3:
+         ref+=whereZero(x[0]-1.,tol=self.RES_TOL)*[1,0,0]
+         ref+=whereZero(x[0],tol=self.RES_TOL)*[-1,0,0]
+         ref+=whereZero(x[1]-1.,tol=self.RES_TOL)*[0,1,0]
+         ref+=whereZero(x[1],tol=self.RES_TOL)*[0,-1,0]
+         ref+=whereZero(x[2]-1.,tol=self.RES_TOL)*[0,0,1]
+         ref+=whereZero(x[2],tol=self.RES_TOL)*[0,0,-1]
+     else:
+         ref+=whereZero(x[0]-1.,tol=self.RES_TOL)*[1,0]
+         ref+=whereZero(x[0],tol=self.RES_TOL)*[-1,0]
+         ref+=whereZero(x[1]-1.,tol=self.RES_TOL)*[0,1]
+         ref+=whereZero(x[1],tol=self.RES_TOL)*[0,-1]
+
+     res=f.getNormal()
+     print length(ref-res)
+     self.failUnlessEqual(res.getShape(),(dim,),"wrong shape of result.")
+     self.failUnlessEqual(res.getFunctionSpace(),f,"wrong functionspace of result.")
+     self.failUnless(Lsup(ref-res)<=self.RES_TOL,"wrong result")
 
 if __name__ == '__main__':
    suite = unittest.TestSuite()
