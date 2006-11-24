d89b6c38203e19c3738e00b5ff00a1f336ee9bca
jongui <jongui@77569008-7704-0410-b7a0-a92fef0b09fd>
2006-11-24 02:15:51 +0000

    some cleanup

--- trunk/pyvisi/drivers/driverimage.py
+++
@@ -1,8 +0,0 @@
-from scene import Scene
-from image import Image
-
-s = Scene(renderer = "vtk_online", x_size = 800, y_size = 600)
-# Display an image.
-i = Image(scene = s, format = "jpeg")
-i.setFileName("../test/python/data_data/Flinders_eval.jpg" )
-s.render()
