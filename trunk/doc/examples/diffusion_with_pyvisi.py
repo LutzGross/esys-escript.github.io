3d5038d335e84aab335af008469ea9720989b12c
jongui <jongui@77569008-7704-0410-b7a0-a92fef0b09fd>
2007-02-28 01:15:27 +0000

    Added diffusion_with_pyvisi.py, but the reading of multiple temporary files (created in the background) appears not to work properly. The first file is always displayed.

---
+++ trunk/doc/examples/diffusion_with_pyvisi.py
@@ -0,0 +1,62 @@
+# $Id: diffusion.py 575 2006-03-03 03:33:07Z lkettle $
+from esys.escript import *
+from esys.escript.linearPDEs import LinearPDE
+from esys.finley import Rectangle
+from esys.pyvisi import Scene, DataCollector, Map
+from esys.pyvisi.constant import *
+
+#... set some parameters ...
+xc=[0.02,0.002]
+r=0.001
+qc=50.e6
+Tref=0.
+rhocp=2.6e6
+eta=75.
+kappa=240.
+tend=5.
+# ... time, time step size and counter ...
+t=0
+h=0.1
+i=0
+#... generate domain ...
+mydomain = Rectangle(l0=0.05,l1=0.01,n0=250, n1=50)
+#... open PDE ...
+mypde=LinearPDE(mydomain)
+mypde.setSymmetryOn()
+mypde.setValue(A=kappa*kronecker(mydomain),D=rhocp/h,d=eta,y=eta*Tref)
+# ... set heat source: ....
+x=mydomain.getX()
+qH=qc*whereNegative(length(x-xc)-r)
+# ... set initial temperature ....
+T=Tref
+
+s = Scene(renderer = Renderer.ONLINE, x_size = 800, y_size = 600)
+dc = DataCollector(source = Source.ESCRIPT)
+
+
+
+# ... start iteration:
+while t<tend:
+      i+=1
+      t+=h
+      print "time step :",t
+      mypde.setValue(Y=qH+rhocp/h*T)
+      T=mypde.getSolution()
+      #saveVTK("T.%d.xml"%i,temp=T)
+
+      #dc.setData(temp=T)
+      #Map(scene = s, data_collector = dc)
+      #s.animate()
+
+      """
+      print i
+      if(i == 10):
+        dc.setData(temp=T)
+        Map(scene = s, data_collector = dc)
+        s.animate()
+      elif(i == 20):
+        dc.setData(temp=T)
+        Map(scene = s, data_collector = dc)
+        s.animate()
+      """
+	 
