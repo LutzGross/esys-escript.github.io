e5f72a4d5319cb177bdda6aca7ee98f85a543f87
phornby <phornby@77569008-7704-0410-b7a0-a92fef0b09fd>
2007-03-12 10:04:54 +0000

    More windows options changes.

--- scons/windows_intelc_options.py
+++ scons/windows_intelc_options.py
@@ -4,17 +4,21 @@ python_lib_path = 'C:/python23/libs'
 python_lib = 'python23'
 
 # locations of libraries for boost
-boost_path = 'c:/woo409/development/boost_1_33'
-boost_lib_path = 'c:/woo409/development/boost_1_33/windows_binary/lib'
-boost_lib = 'boost_python-vc71-mt-gd'
+boost_path = 'Q:/src/boost'
+boost_lib_path = 'Q:/src/boost/windows_binary/lib'
+boost_lib = 'boost_python-vc71-mt-s-1_31'
 
-cc_defines = ['_USE_MATH_DEFINES', ]
+# locations of netcdf
+netCDF_path = "Q:/src/netcdf-3.6.1/src/include"
+netCDF_lib_path = "Q:/src/netcdf-3.6.1/src/win32/NET/debug"
+netCDF_libs_cxx = [ 'netcdf' ]
+
+cc_defines = ['_USE_MATH_DEFINES', 'BOOST_NO_INTRINSIC_WCHAR_T', 'DLL_NETCDF' ]
 # c flags to use
 # 1563 - taking adress of a temporary
 # 811 - exception specification for implicitly declared virtual function (destructor usually) incompatible with that of override
 # 161 - openmp pargmas are unknown when not compiling with openmp
 cc_flags  = '/GR /EHsc /MD /Qc99 /Qopenmp /Qopenmp-report1 /O3 /G7 /Qprec /Qparallel /Qpar-report1 /QxP /QaxP'
-#cc_flags  = '/GR /EHsc /MD /Qc99 /O3 /G7 /Qprec /QxP /QaxP'
 
 cc_flags_debug  = '/Od /MDd /RTC1 /GR /EHsc /Qc99 /Qopenmp /Qopenmp-report1 /Qprec'
 
--- scons/windows_mscv71_options.py
+++ scons/windows_mscv71_options.py
@@ -9,18 +9,18 @@ boost_lib_path = 'Q:/src/boost/windows_binary/lib'
 boost_lib = 'boost_python-vc71-mt-s-1_31'
 
 # locations of netcdf
-netCDF_path = "Q:/src/netcdf-3.6.1/src/libsrc"
+netCDF_path = "Q:/src/netcdf-3.6.1/src/include"
 netCDF_lib_path = "Q:/src/netcdf-3.6.1/src/win32/NET/debug"
 netCDF_libs_cxx = [ 'netcdf' ]
 
-# cc_defines = ['_USE_MATH_DEFINES', ]
+cc_defines = ['_USE_MATH_DEFINES', ]
 # c flags to use
 # 1563 - taking adress of a temporary
 # 811 - exception specification for implicitly declared virtual function (destructor usually) incompatible with that of override
 # 161 - openmp pargmas are unknown when not compiling with openmp
-cc_flags  = '/FD /EHsc /GR /c /O2 /Op /MT /W3'
+cc_flags  = '/FD /EHsc /GR /O2 /Op /MT /W3'
 
-cc_flags_debug  = '/FD /EHsc /GR /c /Od /RTC1 /MTd /ZI'
+cc_flags_debug  = '/FD /EHsc /GR /Od /RTC1 /MTd /ZI'
 
 # c++ flags to use
 cxx_flags = ''
