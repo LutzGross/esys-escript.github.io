ea8a784ce63167cde242b8a20ea484a267d5ca39
phornby <phornby@77569008-7704-0410-b7a0-a92fef0b09fd>
2007-03-12 07:35:46 +0000

    Windows options files.

---
+++ trunk/scons/windows_mscv71_example.py
@@ -0,0 +1,32 @@
+# locations of include files for python
+python_path = 'C:/python23/include'
+python_lib_path = 'C:/python23/libs'
+python_lib = 'python23'
+
+# locations of libraries for boost
+boost_path = 'Q:/src/boost'
+boost_lib_path = 'Q:/src/boost/windows_binary/lib'
+boost_lib = 'boost_python-vc71-mt-s-1_31'
+
+# locations of netcdf
+netCDF_path = "Q:/src/netcdf-3.6.1/src/libsrc"
+netCDF_lib_path = "Q:/src/netcdf-3.6.1/src/win32/NET/debug"
+netCDF_libs_cxx = [ 'netcdf' ]
+
+# cc_defines = ['_USE_MATH_DEFINES', ]
+# c flags to use
+# 1563 - taking adress of a temporary
+# 811 - exception specification for implicitly declared virtual function (destructor usually) incompatible with that of override
+# 161 - openmp pargmas are unknown when not compiling with openmp
+cc_flags  = '/FD /EHsc /GR /c /O2 /Op /MT /W3'
+
+cc_flags_debug  = '/FD /EHsc /GR /c /Od /RTC1 /MTd /ZI'
+
+# c++ flags to use
+cxx_flags = ''
+cxx_flags_debug = ''
+# static library archiver flags to use
+#ar_flags = 'crus'
+
+# system specific libraries to link with
+sys_libs = []
