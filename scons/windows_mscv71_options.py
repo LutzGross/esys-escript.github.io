86c5bafe9da8f3d75b7acef584885bbf566e9f4e
phornby <phornby@77569008-7704-0410-b7a0-a92fef0b09fd>
2007-03-16 08:43:08 +0000

    Now links against netcdf and netcdf_cpp on windows.

--- scons/windows_mscv71_options.py
+++ scons/windows_mscv71_options.py
@@ -10,8 +10,8 @@ boost_lib = 'boost_python-vc71-mt-s-1_31'
 
 # locations of netcdf
 netCDF_path = "Q:/src/netcdf-3.6.1/src/include"
-netCDF_lib_path = "Q:/src/netcdf-3.6.1/src/win32/NET/debug"
-netCDF_libs_cxx = [ 'netcdf' ]
+netCDF_lib_path = "Q:/src/netcdf-3.6.1/src/win32/NET/release"
+netCDF_libs_cxx = [ 'netcdf', 'netcdf_cpp' ]
 
 cc_defines = ['_USE_MATH_DEFINES', 'BOOST_NO_INTRINSIC_WCHAR_T', 'DLL_NETCDF' ]
 # c flags to use
