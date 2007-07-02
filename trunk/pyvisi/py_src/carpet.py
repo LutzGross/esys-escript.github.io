9ae76feaad6b0afdc2e51bfc3c0b0d532f9f87c9
jongui <jongui@77569008-7704-0410-b7a0-a92fef0b09fd>
2007-07-02 05:46:49 +0000

    Fixed a minor problem with regards to the Carpet module distinguishing between 2D and 3D data.

--- trunk/pyvisi/py_src/carpet.py
+++ trunk/pyvisi/py_src/carpet.py
@@ -54,6 +54,20 @@ class Carpet(DataSetMapper, Actor3D, Warp, Transform, Plane, Cutter):
 		'True', otherwise 'False' (which is the default). On occasions, an
 		inaccurate object may be rendered from cell data even after conversion.
 
+		@attention:  When 3D data is used, a cut will be performed on the 
+		scalar field using a 
+		plane before deformation occurs on the plane. However, if 2D data is 
+		used a cut will NOT be performed and deformation will instead occur 
+		immediately on the scalar field.  Pyvisi distinguishes 2D from 3D data 
+		by retrieving the 
+		length of the z-axis. A 2D data is assumed to have a z-axis length of 
+		zero 
+		while a 3D data is assumed to have a z-axis length of non-zero. 
+		There are 
+		exceptions to these rules where some 2D data may have a non-zero 
+		z-axis length. However, such exceptions are not taken into account 
+		at this stage.
+
 		@type scene: L{Scene <scene.Scene>} object
 		@param scene: Scene in which objects are to be rendered on
 		@type data_collector: L{DataCollector <datacollector.DataCollector>}
@@ -112,52 +126,14 @@ class Carpet(DataSetMapper, Actor3D, Warp, Transform, Plane, Cutter):
 		# before DataSetMapper. If it is done after DataSetMapper, no effect
 		# will take place.
 		if(self.__lut == Lut.COLOR): # Colored lookup table.
-			lookup_table = LookupTable()
-			lookup_table._setTableValue()
+			self.__lookup_table = LookupTable()
+			self.__lookup_table._setTableValue()
 		elif(self.__lut == Lut.GREY_SCALE): # Grey scaled lookup table.
-			lookup_table = LookupTable()
-			lookup_table._setLookupTableToGreyScale()
+			self.__lookup_table = LookupTable()
+			self.__lookup_table._setLookupTableToGreyScale()
 
 		self._setupPlane(self._getTransform())
-"""
-		# Get the bounds of the object in the form of
-		# (xmin, xmax, ymin, ymax, zmin, zmax).
-		bounds = self.__data_collector._getDataCollectorOutput().GetBounds()
-		print "bounds: ", bounds
-		# Length of the z-axis (max - min). Assumption is made that if the
-		# length of the z-axis is equal to zero, the the data set is 2D.
-		# Otherwise, the data set is 3D. However, there are exceptions to this
-		# rule as some 2D data sets may have a z-axis length of non-zero, but
-		# such exceptions are not taken into account here.
-		z_axis_length = bounds[5] - bounds[4]
 
-		if(self.__cell_to_point == True): # Converts cell data to point data.
-			c2p = CellDataToPointData(\
-					self.__data_collector._getDataCollectorOutput())
-			if(z_axis_length != 0): # A cutter is used for 3D data.
-				self._setupCutter(c2p._getCellToPointOutput(), \
-						self._getPlane()) 	
-				self._setupWarp(self._getCutterOutput())
-			elif(z_axis_length == 0): # A cutter is not used for 2D data.
-				self._setupWarp(c2p._getCellToPointOutput())
-		elif(self.__cell_to_point == False): # No conversion happens.	
-			if(z_axis_length != 0): # A cutter is used for 3D data.
-				print "if"
-				self._setupCutter(\
-						self.__data_collector._getDataCollectorOutput(), \
-						self._getPlane()) 	
-				self._setupWarp(self._getCutterOutput())
-			elif(z_axis_length == 0): # A cutter is not used for 2D data.
-				print "else"
-				self._setupWarp(
-						self.__data_collector._getDataCollectorOutput())
-
-		self._setupDataSetMapper(self._getWarpOutput(),
-				lookup_table._getLookupTable())
-
-		self._setupActor3D(self._getDataSetMapper())
-		scene._addActor3D(self.__viewport, self._getActor3D())
-"""
 	def _isModified(self):	
 		"""
 		Return whether the Carpet or DataCollector has been modified.
@@ -176,19 +152,22 @@ class Carpet(DataSetMapper, Actor3D, Warp, Transform, Plane, Cutter):
 		@param scene: Scene in which objects are to be rendered on
 		"""
 
+		# This entire 'if' section had to be moved from the __init__ method
+		# due to the use of 'GetBounds()'. A source (i.e. xml file) must be
+		# supplied before 'GetBounds()' is able to return the correct value
+		# when executed. This is to accommodate for lazy evaluation.
 		if(self.__modified):
 			# Get the bounds of the object in the form of
 			# (xmin, xmax, ymin, ymax, zmin, zmax).
 			bounds = self.__data_collector._getDataCollectorOutput().GetBounds()
-			print "bounds: ", bounds
 			# Length of the z-axis (max - min). Assumption is made that if the
 			# length of the z-axis is equal to zero, the the data set is 2D.
-			# Otherwise, the data set is 3D. However, there are exceptions to this
-			# rule as some 2D data sets may have a z-axis length of non-zero, but
-			# such exceptions are not taken into account here.
+			# Otherwise, the data set is 3D. However, there are exceptions 
+			# to this rule as some 2D data sets may have a z-axis length 
+			# of non-zero, but such exceptions are not taken into account here.
 			z_axis_length = bounds[5] - bounds[4]
 
-			if(self.__cell_to_point == True): # Converts cell data to point data.
+			if(self.__cell_to_point == True): #Converts cell data to point data.
 				c2p = CellDataToPointData(\
 						self.__data_collector._getDataCollectorOutput())
 				if(z_axis_length != 0): # A cutter is used for 3D data.
@@ -199,27 +178,19 @@ class Carpet(DataSetMapper, Actor3D, Warp, Transform, Plane, Cutter):
 					self._setupWarp(c2p._getCellToPointOutput())
 			elif(self.__cell_to_point == False): # No conversion happens.	
 				if(z_axis_length != 0): # A cutter is used for 3D data.
-					print "if"
 					self._setupCutter(\
 							self.__data_collector._getDataCollectorOutput(), \
 							self._getPlane()) 	
 					self._setupWarp(self._getCutterOutput())
 				elif(z_axis_length == 0): # A cutter is not used for 2D data.
-					print "else"
 					self._setupWarp(
 							self.__data_collector._getDataCollectorOutput())
 
 			self._setupDataSetMapper(self._getWarpOutput(),
-					lookup_table._getLookupTable())
+					self.__lookup_table._getLookupTable())
 
 			self._setupActor3D(self._getDataSetMapper())
 			scene._addActor3D(self.__viewport, self._getActor3D())
-
-
-
-
-
-
 
 
 		if (self._isModified() == True):
@@ -233,7 +204,5 @@ class Carpet(DataSetMapper, Actor3D, Warp, Transform, Plane, Cutter):
 				self._setScalarRange(self.__data_collector._getScalarRange())
 			self.__modified = False
 
-		bounds = self.__data_collector._getDataCollectorOutput().GetBounds()
-		print "bounds2: ", bounds
 
 
