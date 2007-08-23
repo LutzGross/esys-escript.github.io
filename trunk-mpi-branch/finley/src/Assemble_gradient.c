64a287ade74625e027396422267dcd9a082a1883
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2007-08-23 08:05:04 +0000

    bug with gradient on contacts fixed.

--- trunk-mpi-branch/finley/src/Assemble_gradient.c
+++ trunk-mpi-branch/finley/src/Assemble_gradient.c
@@ -12,7 +12,7 @@
 
 /**************************************************************/
 
-/*    assemblage rjacines: calculate the gradient of nodal data at quadrature points */
+/*    assemblage jacobiens: calculate the gradient of nodal data at quadrature points */
 
 /**************************************************************/
 
@@ -33,39 +33,40 @@
 void Finley_Assemble_gradient(Finley_NodeFile* nodes, Finley_ElementFile* elements,
                               escriptDataC* grad_data,escriptDataC* data) {
 
+  register dim_t e,q,l,s,n;
+  register double* data_array,  *grad_data_e;
   dim_t numNodes, numShapes, numLocalNodes, numComps, NN;
   type_t data_type=getFunctionSpaceType(data);
   bool_t reducedShapefunction=FALSE, reducedIntegrationOrder=FALSE;
-  Finley_NodeMapping *mapping=NULL;
   index_t dof_offset, s_offset;
   Finley_ElementFile_Jacobeans* jac=NULL;
   type_t grad_data_type=getFunctionSpaceType(grad_data);
   Finley_resetError();
   if (nodes==NULL || elements==NULL) return;
   numComps=getDataPointSize(data);
-  NN=elements->ReferenceElement->Type->numNodes;
+  NN=elements->numNodes;
   reducedIntegrationOrder=Finley_Assemble_reducedIntegrationOrder(grad_data);
 
   if (data_type==FINLEY_NODES) {
        reducedShapefunction=FALSE;
-       mapping=nodes->nodesMapping;
+       numNodes=nodes->nodesMapping->numTargets;
   } else if (data_type==FINLEY_REDUCED_NODES) { 
        reducedShapefunction=TRUE;
-       mapping=nodes->reducedNodesMapping;
+       numNodes=nodes->reducedNodesMapping->numTargets;
   } else if (data_type==FINLEY_DEGREES_OF_FREEDOM) {
        if (elements->MPIInfo->size>1) {
           Finley_setError(TYPE_ERROR,"Finley_Assemble_gradient: for more than one processor DEGREES_OF_FREEDOM data are not accepted as input.");
           return;
        }
        reducedShapefunction=FALSE;
-       mapping=nodes->degreesOfFreedomMapping;
+       numNodes=nodes->degreesOfFreedomMapping->numTargets;
   } else if (data_type==FINLEY_REDUCED_DEGREES_OF_FREEDOM) {
        if (elements->MPIInfo->size>1) {
           Finley_setError(TYPE_ERROR,"Finley_Assemble_gradient: for more than one processor REDUCED_DEGREES_OF_FREEDOM data are not accepted as input.");
           return;
        }
        reducedShapefunction=TRUE;
-       mapping=nodes->reducedDegreesOfFreedomMapping;
+       numNodes=nodes->reducedDegreesOfFreedomMapping->numTargets;
   } else {
        Finley_setError(TYPE_ERROR,"Finley_Assemble_gradient: Cannot calculate gradient of data because of unsuitable input data representation.");
   }
@@ -87,7 +88,7 @@ void Finley_Assemble_gradient(Finley_NodeFile* nodes, Finley_ElementFile* elemen
 
       if (! numSamplesEqual(grad_data,jac->ReferenceElement->numQuadNodes,elements->numElements)) {
            Finley_setError(TYPE_ERROR,"Finley_Assemble_gradient: illegal number of samples in gradient Data object");
-      } else if (! numSamplesEqual(data,1,mapping->numTargets)) {
+      } else if (! numSamplesEqual(data,1,numNodes)) {
            Finley_setError(TYPE_ERROR,"Finley_Assemble_gradient: illegal number of samples of input Data object");
       } else if (jac->numDim*numComps!=getDataPointSize(grad_data)) {
            Finley_setError(TYPE_ERROR,"Finley_Assemble_gradient: illegal number of components in gradient data object.");
@@ -100,20 +101,18 @@ void Finley_Assemble_gradient(Finley_NodeFile* nodes, Finley_ElementFile* elemen
       }
   }
   /* now we can start */
-
   if (Finley_noError()) {
-      register dim_t e,q,l,s,n;
-      register double* data_array,  *grad_data_e;
       #pragma omp parallel private(e,q,l,s,n,data_array,grad_data_e)
       {
-         if (jac->numDim==1) {
+         if (data_type==FINLEY_NODES) {
+            if (jac->numDim==1) {
                 #define DIM 1
                 #pragma omp for schedule(static)
    	        for (e=0;e<elements->numElements;e++) {
                     grad_data_e=getSampleData(grad_data,e);
                     for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
                     for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
-                       n=mapping->target[elements->Nodes[INDEX2(dof_offset+s,e,NN)]];
+                       n=elements->Nodes[INDEX2(dof_offset+s,e,NN)];
                        data_array=getSampleData(data,n);
                        for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
                            for (l=0;l<numComps;l++) {
@@ -125,14 +124,14 @@ void Finley_Assemble_gradient(Finley_NodeFile* nodes, Finley_ElementFile* elemen
                 }
 
                 #undef DIM
-         } else if (jac->numDim==2) {
+            } else if (jac->numDim==2) {
                 #define DIM 2
                 #pragma omp for schedule(static)
    	        for (e=0;e<elements->numElements;e++) {
                     grad_data_e=getSampleData(grad_data,e);
                     for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
                     for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
-                       n=mapping->target[elements->Nodes[INDEX2(dof_offset+s,e,NN)]];
+                       n=elements->Nodes[INDEX2(dof_offset+s,e,NN)];
                        data_array=getSampleData(data,n);
                        for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
                            for (l=0;l<numComps;l++) {
@@ -145,14 +144,14 @@ void Finley_Assemble_gradient(Finley_NodeFile* nodes, Finley_ElementFile* elemen
                     }
                 }
                 #undef DIM
-         } else if (jac->numDim==3) {
+            } else if (jac->numDim==3) {
                 #define DIM 3
                 #pragma omp for private(e,grad_data_e,s,n,data_array,q,l) schedule(static)
    	        for (e=0;e<elements->numElements;e++) {
                     grad_data_e=getSampleData(grad_data,e); 
                     for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
                     for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
-                       n=mapping->target[elements->Nodes[INDEX2(dof_offset+s,e,NN)]];
+                       n=elements->Nodes[INDEX2(dof_offset+s,e,NN)];
                        data_array=getSampleData(data,n);
                        for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
                            for (l=0;l<numComps;l++) {
@@ -167,6 +166,202 @@ void Finley_Assemble_gradient(Finley_NodeFile* nodes, Finley_ElementFile* elemen
                     }
                 }
                 #undef DIM
+            }
+
+         } else if (data_type==FINLEY_REDUCED_NODES) {
+            if (jac->numDim==1) {
+                #define DIM 1
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(elements->ReferenceElement->Type->linearNodes[dof_offset+s],e,NN)];
+                       data_array=getSampleData(data,nodes->reducedNodesMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                               grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+
+                #undef DIM
+            } else if (jac->numDim==2) {
+                #define DIM 2
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(elements->ReferenceElement->Type->linearNodes[dof_offset+s],e,NN)];
+                       data_array=getSampleData(data,nodes->reducedNodesMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                               grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                     jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                               grad_data_e[INDEX3(l,1,q,numComps,DIM)]+=data_array[l]* 
+                                     jac->DSDX[INDEX4(s_offset+s,1,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+
+                #undef DIM
+
+            } else if (jac->numDim==3) {
+                #define DIM 3
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(elements->ReferenceElement->Type->linearNodes[dof_offset+s],e,NN)];
+                       data_array=getSampleData(data,nodes->reducedNodesMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                               grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                               grad_data_e[INDEX3(l,1,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,1,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                               grad_data_e[INDEX3(l,2,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,2,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+                #undef DIM
+            }
+         } else if (data_type==FINLEY_DEGREES_OF_FREEDOM) {
+
+            if (jac->numDim==1) {
+                #define DIM 1
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(dof_offset+s,e,NN)];
+                       data_array=getSampleData(data,nodes->degreesOfFreedomMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                               grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+
+                #undef DIM
+            } else if (jac->numDim==2) {
+                #define DIM 2
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(dof_offset+s,e,NN)];
+                       data_array=getSampleData(data,nodes->degreesOfFreedomMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                                   grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                                   grad_data_e[INDEX3(l,1,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,1,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+                #undef DIM
+            } else if (jac->numDim==3) {
+                #define DIM 3
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(dof_offset+s,e,NN)];
+                       data_array=getSampleData(data,nodes->degreesOfFreedomMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                               grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                    jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                               grad_data_e[INDEX3(l,1,q,numComps,DIM)]+=data_array[l]* 
+                                    jac->DSDX[INDEX4(s_offset+s,1,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                               grad_data_e[INDEX3(l,2,q,numComps,DIM)]+=data_array[l]* 
+                                    jac->DSDX[INDEX4(s_offset+s,2,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+                #undef DIM
+            }
+         } else if (data_type==FINLEY_REDUCED_DEGREES_OF_FREEDOM) {
+            if (jac->numDim==1) {
+                #define DIM 1
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(elements->ReferenceElement->Type->linearNodes[dof_offset+s],e,NN)];
+                       data_array=getSampleData(data,nodes->reducedDegreesOfFreedomMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                               grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+
+                #undef DIM
+            } else if (jac->numDim==2) {
+                #define DIM 2
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(elements->ReferenceElement->Type->linearNodes[dof_offset+s],e,NN)];
+                       data_array=getSampleData(data,nodes->reducedDegreesOfFreedomMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                               grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                     jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                               grad_data_e[INDEX3(l,1,q,numComps,DIM)]+=data_array[l]* 
+                                     jac->DSDX[INDEX4(s_offset+s,1,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+
+                #undef DIM
+
+            } else if (jac->numDim==3) {
+                #define DIM 3
+                #pragma omp for schedule(static)
+   	        for (e=0;e<elements->numElements;e++) {
+                    grad_data_e=getSampleData(grad_data,e);
+                    for (q=0;q<DIM*(jac->ReferenceElement->numQuadNodes)*numComps; q++) grad_data_e[q]=0;
+                    for (s=0;s<jac->ReferenceElement->Type->numShapes;s++) {
+                       n=elements->Nodes[INDEX2(elements->ReferenceElement->Type->linearNodes[dof_offset+s],e,NN)];
+                       data_array=getSampleData(data,nodes->reducedDegreesOfFreedomMapping->target[n]);
+                       for (q=0;q<jac->ReferenceElement->numQuadNodes;q++) {
+                           for (l=0;l<numComps;l++) {
+                               grad_data_e[INDEX3(l,0,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,0,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                               grad_data_e[INDEX3(l,1,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,1,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                               grad_data_e[INDEX3(l,2,q,numComps,DIM)]+=data_array[l]* 
+                                        jac->DSDX[INDEX4(s_offset+s,2,q,e,jac->ReferenceElement->Type->numNodes,DIM,jac->ReferenceElement->numQuadNodes)];
+                           }
+                       }
+                    }
+                }
+                #undef DIM
+            }
          }
       } /* end parallel region */
   }
