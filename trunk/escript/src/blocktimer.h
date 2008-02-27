3a50d97e423da648f2ea08a0f627054f084f5549
trankine <trankine@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-02-27 09:52:25 +0000

    This trunk now compiles and links under windows. It passes the FunctionSpace.__str__() test 50000 times on windows. It fails scons run_tests and scons_py_tests as it always did, but I have an idea why.
    Please remember to add the appropriate dll linkage info to declarations that are to be visible outside a .so or .dll. Terry (BTW, PGH here) spent a lot of hacks clearing out blocktimer from the rest of the code when these 5 or 6 lines would have done the trick.

--- trunk/escript/src/blocktimer.h
+++ trunk/escript/src/blocktimer.h
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <search.h>
+#include "system_dep.h"
 
 /* Enable the block timer (or remove this and use -DBLOCKTIMER) */
 /* # define BLOCKTIMER */
 
 # define NUM_TIMERS 1024
 
+ESCRIPT_DLL_API
 void blocktimer_initialize();
+ESCRIPT_DLL_API
 void blocktimer_increment(char *name, double start_time);
+ESCRIPT_DLL_API
 int blocktimer_getOrCreateTimerId(char *name);
+ESCRIPT_DLL_API
 void blocktimer_reportSortByName();
+ESCRIPT_DLL_API
 void blocktimer_reportSortByTime();
+ESCRIPT_DLL_API
 double blocktimer_time();
 
