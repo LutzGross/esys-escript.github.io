4e78a1c0df4d0f7cbf57d711ed4841e09875c00e
trankine <trankine@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-02-27 07:54:48 +0000

    brought from the windows port branch. Should make no difference to *nix's

--- scons/windows_intelc_options.py
+++ scons/windows_intelc_options.py
@@ -24,29 +24,30 @@ libinstall = os.path.join(source_root,'lib')
 exinstall = os.path.join(source_root,'examples')
 
 # locations of include files for python
-python_root = 'C:/python23'
+python_root = 'C:/python25'
 python_cmd = os.path.join(python_root,'python')
 python_path =  os.path.join(python_root,'include')
 python_lib_path = os.path.join(python_root,'libs')
-python_lib = 'python23'
+python_lib = 'python25'
 
 # locations of libraries for boost
-boost_path = os.path.realpath('../boost')
-boost_lib_path = os.path.realpath('../boost/windows_binary/lib')
-boost_lib = 'boost_python-vc71-mt-s-1_31'
+dotdot = os.path.realpath('..')
+boost_path = os.path.join(dotdot,'boost-1.33')
+boost_lib_path = os.path.join(boost_path,'windows_binary','lib')
+boost_lib = 'boost_python-vc71-mt-s-1_33_1.lib'
 
 # locations of netcdf
-useNetCDF = "yes"
-netCDF_path = os.path.realpath("../netcdf/src/include")
-netCDF_lib_path = os.path.realpath("../netcdf/src/win32/NET/release")
-netCDF_libs = [ 'netcdf', 'netcdf_cpp' ]
+useNetCDF = "no"
+netCDF_path = os.path.realpath(".")
+netCDF_lib_path = os.path.realpath(".")
+netCDF_libs = [ ]
 
-cc_defines = ['_USE_MATH_DEFINES', 'DLL_NETCDF' ]
+cc_defines = ['_USE_MATH_DEFINES', 'DLL_NETCDF', 'BOOST_NO_INTRINSIC_WCHAR_T' ]
 # c flags to use
 # 1563 - taking adress of a temporary
 # 811 - exception specification for implicitly declared virtual function (destructor usually) incompatible with that of override
 # 161 - openmp pargmas are unknown when not compiling with openmp
-cc_common_flags = '/FD /EHsc /GR /wd4068 '
+cc_common_flags = '/FD /EHsc /GR /Qvc7.1 '
 cc_flags  = cc_common_flags + '/O2 /Op /MT /W3'
 
 cc_flags_debug  = cc_common_flags + '/Od /RTC1 /MTd /ZI'
