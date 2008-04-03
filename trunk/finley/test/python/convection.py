aef33ade4ddfb0a7158854a44d343f75933870b7
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-04-03 09:07:07 +0000

    some first steps towards restart added

--- trunk/finley/test/python/convection.py
+++ trunk/finley/test/python/convection.py
@@ -1,9 +1,11 @@
 from esys.escript import *
 from esys.escript.models import TemperatureCartesian, StokesProblemCartesian
 from esys.finley import Rectangle, Brick
+from optparse import OptionParser
 from math import pi, ceil
-NE=40
-DIM=3
+
+NE=20
+DIM=2
 H=1.
 L=4*H
 THETA=0.5
@@ -17,13 +19,61 @@ A=22.  # Arenious number
 DI = 0.  # dissipation number
 SUPG=False
 print "total number of elements = ",NE**DIM*int(L/H)**(DIM-1)
+
+# read options:
+parser = OptionParser(usage="%prog [-r [DIR]]")
+parser.add_option("-r", "--restart", dest="restart", help="restart from latest directory", default=False, action="store_true")
+parser.add_option("-d", "--dir", dest="restart_dir", help="restart from directory DIR",metavar="DIR", default=None)
+(options, args) = parser.parse_args()
+restart=options.restart or (options.restart_dir !=None)
+print restart
 #
 #   set up domain:
 #
-if DIM==2:
-  dom=Rectangle(int(ceil(L*NE/H)),NE,l0=L,l1=H,order=2, useFullElementOrder=True)
+if restart:
+   if options.restart_dir ==None:
+      restart_files=[]
+      for f in os.listdir("."):
+          if f.startswith("restart"): restart_files.append(f)
+      if len(restart_files)==0:
+          raise RunTimeError,"no restart files"
+      restart_files.sort()
+      f=restart_files[-1]
+   else:
+       f=options.restart_dir
+   print "restart from ",f
+   dom=finley.load("mesh.nc")
+   velocity=load(os.path.join(f,"v.nc"),dom)
+   pressure=load(os.path.join(f,"p.nc"),dom)
+   ff=open(os.path.join(f,"s.%d"%dom.getMPIRank()),"r").read().split(";")
+   t=float(ff[0])
+   t_out=float(ff[1])
+   n=int(ff[2])
+   n_out=int(ff[3])
+   dt=float(ff[4])
+   if dom.getMPIRank()==0: nusselt_file=open("nusselt.csv","a")
 else:
-  dom=Brick(int(ceil(L*NE/H)),int(ceil(L*NE/H)),NE,l0=L,l1=L,l2=H,order=2, useFullElementOrder=True)
+  if DIM==2:
+    dom=Rectangle(int(ceil(L*NE/H)),NE,l0=L,l1=H,order=2, useFullElementOrder=True,optimize=True)
+  else:
+    dom=Brick(int(ceil(L*NE/H)),int(ceil(L*NE/H)),NE,l0=L,l1=L,l2=H,order=2, useFullElementOrder=True,optimize=True)
+  dom.dump("mesh.nc")
+  x=dom.getX()
+  T=Scalar(1,ReducedSolution(dom))
+  for d in range(DIM):
+      if d == DIM-1: 
+         T*=sin(x[d]/H*pi)
+      else:
+         T*=cos(x[d]*((d+1)/L*pi))
+
+  T=1.-x[DIM-1]+PERTURBATION*T
+  v=Vector(0,Solution(dom))
+  if dom.getMPIRank() ==0: nusselt_file=open("nusselt.csv","w")
+  t=0
+  t_out=0
+  n=0
+  n_out=0
+  dt=None
 
 vol=integrate(1.,Function(dom))
 x=dom.getX()
@@ -34,13 +84,6 @@ heat=TemperatureCartesian(dom,theta=THETA,useSUPG=SUPG)
 heat.setTolerance(TOL)
 
 fixed_T_at=whereZero(x[DIM-1])+whereZero(H-x[DIM-1])
-T=Scalar(1,ReducedSolution(dom))
-for d in range(DIM):
-    if d == DIM-1: 
-       T*=sin(x[d]/H*pi)
-    else:
-       T*=cos(x[d]/H*pi)
-T=1.-x[DIM-1]+PERTURBATION*T
 heat.setInitialTemperature(T)
 print "initial Temperature range ",inf(T),sup(T)
 heat.setValue(rhocp=Scalar(1.,Function(dom)),k=Scalar(1.,Function(dom)),given_T_mask=fixed_T_at)
@@ -52,7 +95,6 @@ sp.setTolerance(TOL)
 sp.setToleranceReductionFactor(TOL)
 x2=ReducedSolution(dom).getX()
 p=-RA*(x2[DIM-1]-0.5*x2[DIM-1]**2)
-v=Vector(0,Solution(dom))
 
 fixed_v_mask=Vector(0,Solution(dom))
 for d in range(DIM):
@@ -65,43 +107,42 @@ for d in range(DIM):
 #
 #  let the show begin:
 #
-t=0
-t_out=0
-n=0
-n_out=0
-dt=None
 dt_new=1.
-a=0
+a=None
+a_alt=None
 if dom.getMPIRank() ==0: 
      nusselt_file=open("nusselt.csv","w")
 while t<T_END:
-    v_last=v*1.
+    v_last=v*1
     print "============== solve for v ========================"
-    viscosity=exp(A*(1./(1+T.interpolate(Function(dom)))-0.5))
+    viscosity=exp(A*(1./(1+T.interpolate(Function(dom)))-2./3.))
     print "viscosity range :", inf(viscosity), sup(viscosity)
     sp.initialize(f=T*(RA*unitVector(DIM-1,DIM)),eta=viscosity,fixed_u_mask=fixed_v_mask)
-    v,p=sp.solve(v,p,show_details=VERBOSE, verbose=True,max_iter=500,solver='GMRES')
+    v,p=sp.solve(v,p,show_details=VERBOSE, verbose=True,max_iter=500,solver='PCG')
+    # v,p=sp.solve(v,p,show_details=VERBOSE, verbose=True,max_iter=500,solver='GMRES')
 
     for d in range(DIM):
          print "range %d-velocity"%d,inf(v[d]),sup(v[d])
 
     if t>=t_out:
-      saveVTK("state.%d.vtu"%n_out,T=T,v=v)
+      saveVTK("state.%d.vtu"%n_out,T=T,v=v,p=p)
       print "visualization file %d for time step %e generated."%(n_out,t)
       n_out+=1
       t_out+=DT_OUT
     Nu=1.+integrate(viscosity*length(grad(v))**2)/(RA*vol)
     if dom.getMPIRank() ==0: nusselt_file.write("%e %e\n"%(t,Nu))
     print "nusselt number = ",Nu
-    if n>0:
+    if a==None:
+       a=0
+    else:
        a,a_alt = (v_last-v)/dt, a
        dt_alt=dt
-    if n>1: 
-       z=(a-a_alt)/((dt+dt_alt)/2)
-       f=Lsup(z)/Lsup(v)
-       print "estimated error ",f*dt**2
-       dt_new, dt_alt =min(2*dt,max(dt/2,sqrt(0.05/f))), dt
-       # dt_new, dt_alt =sqrt(0.05/f), dt
+       if a_alt!=None:
+          z=(a-a_alt)/((dt+dt_alt)/2)
+          f=Lsup(z)/Lsup(v)
+          print "estimated error ",f*dt**2
+          dt_new, dt_alt =min(2*dt,max(dt/2,sqrt(0.05/f))), dt
+          # dt_new, dt_alt =sqrt(0.05/f), dt
     heat.setValue(v=v,Q=DI/RA*viscosity*length(symmetric(grad(v)))**2)
     dt=min(dt_new,heat.getSafeTimeStepSize())
     print n,". time step t=",t," step size ",dt
