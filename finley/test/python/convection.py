a83609bdecc6672721577e884d6d036b5fa148ca
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-04-07 01:27:01 +0000

    restart is supported now.

--- finley/test/python/convection.py
+++ finley/test/python/convection.py
@@ -1,8 +1,15 @@
 from esys.escript import *
 from esys.escript.models import TemperatureCartesian, StokesProblemCartesian
-from esys.finley import Rectangle, Brick
+from esys.finley import Rectangle, Brick, LoadMesh
 from optparse import OptionParser
 from math import pi, ceil
+def removeRestartDirectory(dir_name):
+   if os.path.isdir(dir_name):
+       for root, dirs, files in os.walk(dir_name, topdown=False):
+           for name in files: os.remove(os.path.join(root,name))
+           for name in dirs: os.remove(os.path.join(root,name))
+       os.rmdir(dir_name)
+       print "Restart files %s have been removed."%dir_name
 
 NE=20
 DIM=2
@@ -18,15 +25,16 @@ RA=1.e5 # Rayleigh number
 A=22.  # Arenious number 
 DI = 0.  # dissipation number
 SUPG=False
+create_restartfiles_every_step=10
+
 print "total number of elements = ",NE**DIM*int(L/H)**(DIM-1)
 
 # read options:
 parser = OptionParser(usage="%prog [-r [DIR]]")
-parser.add_option("-r", "--restart", dest="restart", help="restart from latest directory", default=False, action="store_true")
-parser.add_option("-d", "--dir", dest="restart_dir", help="restart from directory DIR",metavar="DIR", default=None)
+parser.add_option("-r", "--restart", dest="restart", help="restart from latest directory. It will be deleted after a new directory has been created.", default=False, action="store_true")
+parser.add_option("-d", "--dir", dest="restart_dir", help="restart from directory DIR. The directory will not be deleted but new restart directories are created.",metavar="DIR", default=None)
 (options, args) = parser.parse_args()
 restart=options.restart or (options.restart_dir !=None)
-print restart
 #
 #   set up domain:
 #
@@ -36,21 +44,28 @@ if restart:
       for f in os.listdir("."):
           if f.startswith("restart"): restart_files.append(f)
       if len(restart_files)==0:
-          raise RunTimeError,"no restart files"
+          raise IOError,"no restart files"
       restart_files.sort()
       f=restart_files[-1]
    else:
        f=options.restart_dir
-   print "restart from ",f
-   dom=finley.load("mesh.nc")
-   velocity=load(os.path.join(f,"v.nc"),dom)
-   pressure=load(os.path.join(f,"p.nc"),dom)
-   ff=open(os.path.join(f,"s.%d"%dom.getMPIRank()),"r").read().split(";")
+   print "restart from directory ",f
+   dom=LoadMesh("mesh.nc")
+   ff=open(os.path.join(f,"stamp.%d"%dom.getMPIRank()),"r").read().split(";")
    t=float(ff[0])
    t_out=float(ff[1])
    n=int(ff[2])
    n_out=int(ff[3])
    dt=float(ff[4])
+   v=load(os.path.join(f,"v.nc"),dom)
+   p=load(os.path.join(f,"p.nc"),dom)
+   T=load(os.path.join(f,"T.nc"),dom)
+   if n>1:
+      dt_a=float(ff[5])
+      a=load(os.path.join(f,"a.nc"),dom)
+   else:
+      dt_a=None
+      a=None
    if dom.getMPIRank()==0: nusselt_file=open("nusselt.csv","a")
 else:
   if DIM==2:
@@ -74,6 +89,8 @@ else:
   n=0
   n_out=0
   dt=None
+  a=None
+  dt_a=None
 
 vol=integrate(1.,Function(dom))
 x=dom.getX()
@@ -107,11 +124,6 @@ for d in range(DIM):
 #
 #  let the show begin:
 #
-dt_new=1.
-a=None
-a_alt=None
-if dom.getMPIRank() ==0: 
-     nusselt_file=open("nusselt.csv","w")
 while t<T_END:
     v_last=v*1
     print "============== solve for v ========================"
@@ -131,23 +143,44 @@ while t<T_END:
       t_out+=DT_OUT
     Nu=1.+integrate(viscosity*length(grad(v))**2)/(RA*vol)
     if dom.getMPIRank() ==0: nusselt_file.write("%e %e\n"%(t,Nu))
-    print "nusselt number = ",Nu
-    if a==None:
-       a=0
-    else:
-       a,a_alt = (v_last-v)/dt, a
-       dt_alt=dt
-       if a_alt!=None:
-          z=(a-a_alt)/((dt+dt_alt)/2)
-          f=Lsup(z)/Lsup(v)
-          print "estimated error ",f*dt**2
-          dt_new, dt_alt =min(2*dt,max(dt/2,sqrt(0.05/f))), dt
-          # dt_new, dt_alt =sqrt(0.05/f), dt
     heat.setValue(v=v,Q=DI/RA*viscosity*length(symmetric(grad(v)))**2)
-    dt=min(dt_new,heat.getSafeTimeStepSize())
+    print "nusselt number = ",Nu,n
+    if n>0:
+        a,a_alt = (v_last-v)/dt, a
+        dt_a,dt_a_alt = dt, dt_a
+    if n>1:
+       z=(a-a_alt)/((dt_a+dt_a_alt)/2)
+       f=Lsup(z)/Lsup(v)
+       print "estimated error ",f*dt**2
+       dt_new=min(2*dt,max(dt/2,sqrt(0.05/f)))
+       dt=min(dt_new,heat.getSafeTimeStepSize())
+    else:
+       dt=heat.getSafeTimeStepSize()
     print n,". time step t=",t," step size ",dt
     print "============== solve for T ========================"
     T=heat.solve(dt, verbose=VERBOSE)	
     print "Temperature range ",inf(T),sup(T)
     n+=1
     t+=dt
+    # =========================
+    #
+    #    create restart files:
+    #
+    if create_restartfiles_every_step>0:
+       if (n-1)%create_restartfiles_every_step == 0:
+         c=(n-1)/create_restartfiles_every_step
+         old_restart_dir="restart_%s_"%(c-1)
+         new_restart_dir="restart_%s_"%c
+
+         print "Write restart files to ",new_restart_dir
+         if not os.path.isdir(new_restart_dir): os.mkdir(new_restart_dir)
+         v.dump(os.path.join(new_restart_dir,"v.nc"))
+         p.dump(os.path.join(new_restart_dir,"p.nc"))
+         T.dump(os.path.join(new_restart_dir,"T.nc"))
+         if n>1:
+             file(os.path.join(new_restart_dir,"stamp.%d"%dom.getMPIRank()),"w").write("%e; %e; %s; %s; %e; %e;\n"%(t, t_out, n, n_out, dt, dt_a))
+             a.dump(os.path.join(new_restart_dir,"a.nc"))
+         else:
+             file(os.path.join(new_restart_dir,"stamp.%d"%dom.getMPIRank()),"w").write("%e; %e; %s; %s; %e;\n"%(t, t_out, n, n_out, dt))
+         removeRestartDirectory(old_restart_dir)
+
