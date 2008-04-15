ac98389ca35fdedc7966a57af923d05888aacf64
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-04-15 00:54:25 +0000

    Some typos

--- trunk/finley/test/python/run_linearPDEsOnFinley.py
+++ trunk/finley/test/python/run_linearPDEsOnFinley.py
@@ -259,7 +259,7 @@ if __name__ == '__main__':
       suite.addTest(unittest.makeSuite(Test_AssemblePDEwithFinley_2Do2_Contact_withElementsOnFace))
       suite.addTest(unittest.makeSuite(Test_AssemblePDEwithFinley_3Do1_Contact_withElementsOnFace))
       suite.addTest(unittest.makeSuite(Test_AssemblePDEwithFinley_3Do2_Contact_withElementsOnFace))
- else:
+   else:
       pass
 
    s=unittest.TextTestRunner(verbosity=2).run(suite)
