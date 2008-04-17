fe26972aace83d89fb9b9529b602b355d8515aa4
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-04-17 23:36:31 +0000

    Change to 3-D and change viscosity

--- trunk/finley/test/python/convection.py
+++ trunk/finley/test/python/convection.py
@@ -31,7 +31,7 @@ if solver!='PCG':
 else:
  extratol=1
 
-DIM=2
+DIM=3
 H=1.
 L=4*H
 THETA=0.5
@@ -152,7 +152,7 @@ for d in range(DIM):
 while t<T_END:
     v_last=v*1
     print "============== solve for v ========================"
-    viscosity=exp(A*(1./(1+T.interpolate(Function(dom)))-2./3.))
+    viscosity=exp(A*(1./(1+T.interpolate(Function(dom)))-1./2.))
     print "viscosity range :", inf(viscosity), sup(viscosity)
     sp.initialize(f=T*(RA*unitVector(DIM-1,DIM)),eta=viscosity,fixed_u_mask=fixed_v_mask)
     #v,p=sp.solve(v,p,show_details=VERBOSE, verbose=True,max_iter=500,solver='PCG')
@@ -164,7 +164,7 @@ while t<T_END:
          print "range %d-velocity"%d,inf(v[d]),sup(v[d])
 
     if t>=t_out:
-      saveVTK("state.%d.vtu"%n_out,T=T,v=v,p=p)
+      saveVTK("state.%d.vtu"%n_out,T=T,v=v)
       print "visualization file %d for time step %e generated."%(n_out,t)
       n_out+=1
       t_out+=DT_OUT
