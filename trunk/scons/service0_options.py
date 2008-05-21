c51507f1ac2568f9f7d882b620a2989aad355d30
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-05-21 05:55:30 +0000

    Improvements to Savanna configuration

--- trunk/scons/service0_options.py
+++ trunk/scons/service0_options.py
@@ -9,87 +9,60 @@
 
 # Configuration for Savanna
 
-# flag the MPI settings
-# useMPI = 'yes'
+# MKL
+# mkl_path		= '/sw/sdev/cmkl/10.0.2.18/include'
+# mkl_lib_path		= '/sw/sdev/cmkl/10.0.2.18/lib/em64t'
+# mkl_libs		= ['mkl_solver', 'mkl_lapack']
 
-# TODO: Variables named *_path should be *_include
+# ParMETIS
+# parmetis_path		= '/sw/libs/parmetis/x86_64/gcc-4.1.2/parmetis-3.1/include'
+# parmetis_lib_path	= '/sw/libs/parmetis/x86_64/gcc-4.1.2/parmetis-3.1/lib'
+# parmetis_lib		= ['parmetis', 'metis']
 
-# locations of libs etc used by mkl
-### mkl_path = '/sw/sdev/cmkl/10.0.2.18/include'
-### mkl_lib_path = '/sw/sdev/cmkl/10.0.2.18/lib/em64t'
-### mkl_libs = ['mkl_solver', 'mkl_lapack']
-### mkl_libs = ['mkl_solver', 'mkl_lapack', 'mkl_ipf']		# Library mkl_ipf does not exist anymore
+# Python
+python_path		= '/sw/apps/python/x86_64/gcc-4.1.2/python-2.4.4/include/python2.4'
+python_lib_path		= '/sw/apps/python/x86_64/gcc-4.1.2/python-2.4.4/lib'
+python_lib		= 'python2.4'
 
-# locations of libs etc used by SCSL
-### scsl_path = '/usr/include'
-### scsl_lib_path = '/usr/lib'
-### scsl_libs = ['scs_mp']
-### scsl_libs_MPI = [ 'scs', 'mpi' ]
+# Boost
+boost_path		= '/usr/include/boost'
+boost_lib_path		= '/usr/lib64'
+boost_lib		= 'boost_python'
 
-# Location of ParMETIS library
-### parmetis_path = '/data/raid2/toolspp4/parmetis/include'
-### parmetis_lib_path = '/data/raid2/toolspp4/parmetis/lib'
-### parmetis_lib = ['parmetis', 'metis']
+# Documentation
+# doxygen_path		= '/sw/apps/.../bin'
+# epydoc_path		= '/sw/apps/.../bin'
 
-# locations of include files for python
-python_path = '/sw/apps/python/x86_64/gcc-4.1.2/python-2.4.4/include/python2.4'
-python_lib_path = '/sw/apps/python/x86_64/gcc-4.1.2/python-2.4.4/lib'
-python_lib = 'python2.4'
+# NetCDF
+useNetCDF		= 'yes'
+netCDF_path		= '/sw/libs/netcdf/x86_64/gcc-4.1.2/netcdf-3.6.2/include'
+netCDF_lib_path		= '/sw/libs/netcdf/x86_64/gcc-4.1.2/netcdf-3.6.2/lib'
+netCDF_libs		= ['netcdf_c++', 'netcdf']
 
-# locations of libraries for boost
-boost_path = '/usr/include/boost'
-boost_lib_path = '/usr/lib64'
-boost_lib = 'boost_python'
-
-# locations of doc building executables
-### doxygen_path = '/data/raid2/toolspp4/doxygen/1.4.6/gcc-3.3.6/bin'
-### epydoc_path = '/raid2/tools/epydoc/2.1/python-2.3.4/bin'
-
-# locations of netcdf
-useNetCDF = 'yes'
-netCDF_path = '/sw/libs/netcdf/x86_64/gcc-4.1.2/netcdf-3.6.2/include'
-netCDF_lib_path = '/sw/libs/netcdf/x86_64/gcc-4.1.2/netcdf-3.6.2/lib'
-netCDF_libs = [ 'netcdf_c++', 'netcdf']
-
-# locations of PAPI
+# PAPI
 papi_instrument_solver = 0
-# papi_path = '/data/raid2/toolspp4/papi/3.0.8.1/gcc-3.3.6/include'
-# papi_lib_path = '/data/raid2/toolspp4/papi/3.0.8.1/gcc-3.3.6/lib'
-# papi_libs = [ 'papi' ]
+# papi_path		= '/sw/.../include'
+# papi_lib_path		= '/sw/.../lib'
+# papi_libs		= ['papi']
 
-mpi_path = '/usr/include'
-mpi_lib_path = '/usr/lib64'
-mpi_libs = [ 'mpi' ]
-mpi_run = 'mpirun -np 1'
+# MPI
+mpi_path		= '/usr/include'
+mpi_lib_path		= '/usr/lib64'
+mpi_libs		= ['mpi']
+mpi_run			= 'mpirun -np 1'
 
-# Change -fno-alias to -fargument-noalias???
-# -ivdep-parallel is i64 only, not Savanna
-# Use -ax for vectorization?
-# -axN and -axW are identical?
-# -axK not for SSE2
+# OpenMP (comment out to disable OpenMP)
+omp_flags 		= '-openmp -openmp_report0'
+omp_flags_debug 	= '-openmp -openmp_report0'
 
-# Which manual has information about the compiler options?
+# C flags (also used by C++)
+cc_flags		= '-O3 -ansi -fPIC -vec-report0 -ftz -IPF-fltacc- -IPF-fma -fno-alias -DBLOCKTIMER'
+cc_flags_debug		= '-g  -ansi -fPIC'
 
-omp_flags = '-openmp -openmp_report2 '
-omp_flags_debug = '-openmp -openmp_report0'
+# C++ flags
+cxx_flags		= ''
+cxx_flags_debug		= '-DDOASSERT -DDOPROF'
 
-# c flags to use
-cc_flags  = '-O3 -ftz -IPF-ftlacc- -IPF-fma -fno-alias -wd161 -fPIC -DBLOCKTIMER'
-cc_flags_debug  = '-g -O0 -wd161 -fPIC -DBLOCKTIMER'
-
-# c++ flags to use
-cxx_flags = '-ansi -wd161 -DMPI_NO_CPPBIND -DBLOCKTIMER'
-cxx_flags_debug = '-g -ansi -wd161 -DDOASSERT -DDOPROF -DMPI_NO_CPPBIND -fPIC -DBLOCKTIMER'
-
-# c and c++ flags for MPI compilation
-# c flags to use
-cc_flags_MPI  = '-O3 -ftz -IPF-ftlacc- -IPF-fma -fno-alias -wd161 -fPIC -DPASO_MPI -DBLOCKTIMER'
-cc_flags_debug_MPI  = '-g -O0 -wd161 -fPIC -DPASO_MPI -DBLOCKTIMER'
-
-# c++ flags to use
-cxx_flags_MPI = '-ansi -wd1563 -wd161 -DMPI_NO_CPPBIND -DBLOCKTIMER'
-cxx_flags_debug_MPI = '-ansi -DDOASSERT -DDOPROF -wd1563 -wd161 -DMPI_NO_CPPBIND -DBLOCKTIMER'
-
-# system specific libraries to link with
-sys_libs = ['stdc++']
+# System-specific libraries to link with
+sys_libs 		= ['stdc++']
 
