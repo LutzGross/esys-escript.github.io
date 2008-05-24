ebfab53661d0733e9b17472c804564c4622b5b71
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-05-24 04:51:38 +0000

    Config for new installation at APAC with offscreen rendering

--- scons/ac_options.py
+++ scons/ac_options.py
@@ -12,9 +12,9 @@ mpi_libs = ['mpi']
 mpi_run = 'mpirun -np 1'
 
 # locations of libs etc used by mkl
-### mkl_path = '/opt/intel-mkl/8.0/include'
-### mkl_lib_path ='/opt/intel-mkl/8.0/lib/64'
-### mkl_libs = ['mkl_solver', 'mkl_lapack', 'mkl_ipf']
+mkl_path = '/opt/intel-mkl/8.0/include'
+mkl_lib_path ='/opt/intel-mkl/8.0/lib/64'
+mkl_libs = ['mkl_solver', 'mkl_lapack', 'mkl_ipf']
 
 # locations of libs etc used by SCSL
 ### scsl_path = '/opt/scsl-1.6.1.0/include/'
@@ -36,10 +36,10 @@ boost_lib = ['boost_python-gcc-mt']
 #epydoc_path = '/raid2/tools/epydoc/2.1/python-2.3.4/bin'
 
 # locations of netcdf
-### useNetCDF = 'yes'
-### netCDF_path = "/opt/netcdf/3.6.2/gcc-4.1.2/include"
-### netCDF_lib_path = "/opt/netcdf/3.6.2/gcc-4.1.2/lib"
-### netCDF_libs = [ 'netcdf_c++', 'netcdf']
+useNetCDF = 'yes'
+netCDF_path = "/opt/netcdf/3.6.2/gcc-4.1.2/include"
+netCDF_lib_path = "/opt/netcdf/3.6.2/gcc-4.1.2/lib"
+netCDF_libs = [ 'netcdf_c++', 'netcdf']
 
 # locations of PAPI
 # papi_path = '/data/raid2/toolspp4/papi/3.0.8.1/gcc-3.3.6/include'
@@ -55,5 +55,4 @@ cxx_flags = '-ansi -ansi_alias'
 cxx_flags_debug = '-ansi -UDOASSERT -DDOPROF'
 
 # system specific libraries to link with
-# sys_libs = ['guide', 'irc', 'tcl8.4', 'tk8.4']
 sys_libs = ['guide', 'irc']
