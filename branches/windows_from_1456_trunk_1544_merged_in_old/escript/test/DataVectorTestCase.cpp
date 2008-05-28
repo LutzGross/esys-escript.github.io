3dc7860086d04ec243573e8c90c42f0ceb2bf8ba
phornby <phornby@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-05-28 11:07:33 +0000

    Prepare for a shuffle of branch names to bring in the new trunk merge.

---
+++ branches/windows_from_1456_trunk_1544_merged_in_old/escript/test/DataVectorTestCase.cpp
@@ -0,0 +1,199 @@
+
+/* $Id$ */
+
+/*******************************************************
+ *
+ *           Copyright 2003-2007 by ACceSS MNRF
+ *       Copyright 2007 by University of Queensland
+ *
+ *                http://esscc.uq.edu.au
+ *        Primary Business: Queensland, Australia
+ *  Licensed under the Open Software License version 3.0
+ *     http://www.opensource.org/licenses/osl-3.0.php
+ *
+ *******************************************************/
+
+#include "escript/DataVector.h"
+#include "esysUtils/EsysException.h"
+
+#include "DataVectorTestCase.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace CppUnitTest;
+using namespace escript;
+using namespace esysUtils;
+
+void DataVectorTestCase::setUp() {
+  //
+  // This is called before each test is run
+ 
+}
+
+void DataVectorTestCase::tearDown() {
+  //
+  // This is called after each test has been run
+ 
+}
+
+void DataVectorTestCase::testAll() {
+
+  cout << endl;
+
+  {
+    cout << "\tCreate and check an empty DataVector object." << endl;
+
+    DataVector vec;
+    assert(vec.size() == 0);
+  }
+ 
+  {
+    cout << "\tCheck DataVector resize operation." << endl;
+
+    DataVector vec;
+    assert(vec.size() == 0);
+
+    vec.resize(1,0,1);
+    assert(vec.size() == 1);
+
+    vec.resize(1000,0,1);
+    assert(vec.size() == 1000);
+
+    vec.resize(0,0,1);
+    assert(vec.size() == 0);
+  }
+
+  {
+    cout << "\tCreate and check DataVector objects of various sizes." << endl;
+
+    DataVector vec1(0,0,1);
+    assert(vec1.size() == 0);
+
+    DataVector vec2(1,0,1);
+    assert(vec2.size() == 1);
+
+    DataVector vec3(1000,0,1);
+    assert(vec3.size() == 1000);
+  }
+
+  {
+    cout << "\tAssign and check various elements to a DataVector." << endl;
+
+    DataVector vec(1000,0,1);
+
+    for (int i=0; i < 1000; i++) {
+      vec[i] = i;
+    }
+
+    for (int i=0; i < 1000; i++) {
+      assert(vec[i] == i);
+    }
+
+    for (int i=0; i < 1000; i++) {
+      vec[i] = i/1000;
+    }
+
+    for (int i=0; i < 1000; i++) {
+      assert(vec[i] == i/1000);
+    }
+  }
+
+  {
+    cout << "\tCheck DataVector copy constructor." << endl;
+
+    DataVector vec1(1000,0,1);
+
+    for (int i=0; i < 1000; i++) {
+      vec1[i] = i;
+    }
+
+    DataVector vec2(vec1);
+
+    assert(vec1.size() == vec2.size());
+
+    for (int i=0; i < 1000; i++) {
+      assert(vec2[i] == i);
+    }
+  }
+ 
+  {
+    cout << "\tCheck DataVector = operator." << endl;
+
+    DataVector vec1(1000,0,1);
+
+    for (int i=0; i < 1000; i++) {
+      vec1[i] = i;
+    }
+
+    DataVector vec2;
+
+    vec2 = vec1;
+
+    assert(vec1.size() == vec2.size());
+
+    for (int i=0; i < 1000; i++) {
+      assert(vec2[i] == i);
+    }
+  }
+ 
+  {
+    cout << "\tCheck DataVector == operator." << endl;
+
+    DataVector vec1(1000,0,1);
+
+    for (int i=0; i < 1000; i++) {
+      vec1[i] = i;
+    }
+
+    DataVector vec2;
+
+    vec2 = vec1;
+
+    assert(vec1 == vec2);
+  }
+ 
+  {
+    cout << "\tCheck DataVector != operator." << endl;
+
+    DataVector vec1(1000,0,1);
+
+    for (int i=0; i < 1000; i++) {
+      vec1[i] = i;
+    }
+
+    DataVector vec2;
+
+    assert(vec1 != vec2);
+  }
+  #if defined DOASSERT
+  {
+    cout << "\tCheck DataVector index exception." << endl;
+
+    DataVector vec(1000,0,1);
+
+    try {
+      double x = vec[1001];
+      assert(false);
+    }
+
+    catch (EsysException& e) {
+      //cout << e.toString() << endl;
+      assert(true);
+    }
+
+  }
+  #endif
+
+}
+
+TestSuite* DataVectorTestCase::suite ()
+{
+  //
+  // create the suite of tests to perform.
+  TestSuite *testSuite = new TestSuite ("DataVectorTestCase");
+
+  testSuite->addTest (new TestCaller< DataVectorTestCase>("testAll",&DataVectorTestCase::testAll));
+  return testSuite;
+}
+
---
+++ branches/windows_from_1456_trunk_1544_merged_in_old/finley/src/ElementFile_relableNodes.c
@@ -0,0 +1,41 @@
+
+/* $Id$ */
+
+/*******************************************************
+ *
+ *           Copyright 2003-2007 by ACceSS MNRF
+ *       Copyright 2007 by University of Queensland
+ *
+ *                http://esscc.uq.edu.au
+ *        Primary Business: Queensland, Australia
+ *  Licensed under the Open Software License version 3.0
+ *     http://www.opensource.org/licenses/osl-3.0.php
+ *
+ *******************************************************/
+
+/**************************************************************/
+
+/*   Finley: ElementFile */
+
+/*                                                                      */
+/*   assigns new node reference numbers to elements in element file in. */
+/*   if k is the old node, the new node is newNode[k-offset].           */
+
+/**************************************************************/
+
+#include "ElementFile.h"
+
+/**************************************************************/
+
+void Finley_ElementFile_relableNodes(index_t* newNode,index_t offset,Finley_ElementFile* in) {
+   dim_t i,j,NN;
+   if (in!=NULL) {
+     NN=in->ReferenceElement->Type->numNodes;
+     #pragma omp parallel for private(j,i) schedule(static)
+     for (j=0;j<in->numElements;j++) {
+       for (i=0;i<NN;i++) {
+         in->Nodes[INDEX2(i,j,NN)]=newNode[in->Nodes[INDEX2(i,j,NN)]-offset];
+       }
+     }
+   }
+}
