738fc93da68d9d34525de58ac6d5d8cb66062915
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-06-24 01:16:40 +0000

    Trying to get documentation builds going again after reinstalling my desktop OS

--- scons/shake71_options.py
+++ scons/shake71_options.py
@@ -9,51 +9,40 @@
 
 # Configuration for shake71 (Linux)
 
-# ParMETIS
-parmetis_path		= '/home/Work/Escript.x86_64/parmetis-3.1/include'
-parmetis_lib_path	= '/home/Work/Escript.x86_64/parmetis-3.1/lib'
-parmetis_lib		= ['parmetis', 'metis']
-
 # Python
-python_path		= '/home/Work/Escript.x86_64/python-2.4.4/include/python2.4'
-python_lib_path		= '/home/Work/Escript.x86_64/python-2.4.4/lib'
-python_lib		= 'python2.4'
+python_path		= '/usr/include/python2.5'
+python_lib_path		= '/usr/lib/python2.5'
+python_lib		= 'python2.5'
 
 # Boost
-boost_path		= '/home/Work/Escript.x86_64/boost_1_33/include/boost-1_33'
-boost_lib_path		= '/home/Work/Escript.x86_64/boost_1_33/lib'
-boost_lib		= 'boost_python-gcc-mt'
+boost_path		= '/usr/include'
+boost_lib_path		= '/usr/lib'
+boost_lib		= 'boost_python'
 
 # Documentation
 doxygen_path		= '/usr/bin'
 epydoc_path		= '/usr/bin'
 
 # NetCDF
-useNetCDF		= "yes"
-netCDF_path		= "/home/Work/Escript.x86_64/netcdf-3.6.2/include"
-netCDF_lib_path		= "/home/Work/Escript.x86_64/netcdf-3.6.2/lib"
+useNetCDF		= 'yes'
+netCDF_path		= '/usr/include'
+netCDF_lib_path		= '/usr/lib'
 netCDF_libs		= [ 'netcdf_c++', 'netcdf']
 
-# MPI
-mpi_path		= '/home/Work/mpich2-1.0.5p4/include'
-mpi_lib_path		= '/home/Work/mpich2-1.0.5p4/lib'
+# MPI (version: MPICH2)
+mpi_path		= '/home/Work/InstallArea/mpich2-1.0.7/include'
+mpi_lib_path		= '/home/Work/InstallArea/mpich2-1.0.7/lib'
 mpi_libs		= ['mpich', 'rt']
 mpi_run			= 'mpirun -np 1'
+
+# ParMETIS
+# parmetis_path		= '/usr/include'
+# parmetis_lib_path	= '/usr/lib'
+# parmetis_lib		= ['parmetis', 'metis']
 
 # PAPI
 # papi_instrument_solver	= 0
 # papi_path		= '/sw/.../include'
 # papi_lib_path		= '/sw/.../lib'
 # papi_libs		= ['papi']
-
-# C flags (also used by C++)
-# cc_flags		= '-O -fPIC'
-# cc_flags_debug	= '-g -fPIC -DTRILINOS -I/home/Work/trilinos-6/include'
-
-# C++ flags
-# cxx_flags		= '-fPIC'
-# cxx_flags_debug	= '-DDOASSERT -UDOPROF -fPIC -DTRILINOS -I/home/Work/trilinos-6/include'
-
-# System-specific libraries to link with
-# sys_libs		= []
 
