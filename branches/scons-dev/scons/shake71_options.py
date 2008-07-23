de1c4d21ccce9ffd24ec01ada919e236a62c6c9d
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-07-23 08:51:04 +0000

    Fixed bugs in configure...no longer caching results so needs some work

--- branches/scons-dev/scons/shake71_options.py
+++ branches/scons-dev/scons/shake71_options.py
@@ -10,24 +10,24 @@
 # Configuration for shake71 (Linux)
 
 # Python
-python_path		= '/usr/include/python2.5'
-python_lib_path		= '/usr/lib/python2.5'
-python_lib		= 'python2.5'
+# python_path		= '/usr/include/python2.5'
+# python_lib_path	= '/usr/lib/python2.5'
+# python_libs		= ['python2.5']
 
 # Boost
-boost_path		= '/usr/include'
-boost_lib_path		= '/usr/lib'
-boost_lib		= 'boost_python'
+# boost_path		= '/usr/include'
+# boost_lib_path	= '/usr/lib'
+# boost_libs		= ['boost_python']
 
 # Documentation
-doxygen_path		= '/usr/bin'
-epydoc_path		= '/usr/bin'
+# doxygen_path		= '/usr/bin'
+# epydoc_path		= '/usr/bin'
 
 # NetCDF
-useNetCDF		= 'yes'
-netCDF_path		= '/usr/include'
-netCDF_lib_path		= '/usr/lib'
-netCDF_libs		= [ 'netcdf_c++', 'netcdf']
+# usenetcdf		= 'yes'
+# netCDF_path		= '/usr/include'
+# netCDF_lib_path	= '/usr/lib'
+# netCDF_libs		= [ 'netcdf_c++', 'netcdf']
 
 # MPI (version: MPICH2)
 mpi_path		= '/home/Work/InstallArea/mpich2-1.0.7/include'
