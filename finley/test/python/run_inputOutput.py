0bada77adc40f1bea5581ea407dd256a7cce6fea
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-09-09 00:18:49 +0000

    Added tests for reading a finley mesh and comparing it to a newly-created mesh of the same type
    Moved my mesh comparison function to be a class method

--- finley/test/python/run_inputOutput.py
+++ finley/test/python/run_inputOutput.py
@@ -51,76 +51,74 @@ FINLEY_TEST_MESH_PATH=FINLEY_TEST_DATA+"/data_meshes/"
 SOLVER_TOL=1.e-8
 REL_TOL=1.e-6
 
-def domainsEqual(m1, m2, nft=10):
-   if m1.getDim() != m2.getDim():
-     print "domainsEqual: different dimensions %d, %d" % (m1.getDim(), m2.getDim())
-     return False
-   if m1.getNumDataPointsGlobal() != m2.getNumDataPointsGlobal():
-     print "domainsEqual: different num data points: %d, %d" % (m1.getNumDataPointsGlobal(), m2.getNumDataPointsGlobal())
-     return False
-   for tagName in m1.showTagNames().split(", "):
-     if not m2.isValidTagName(tagName):
-       print "domainsEqual: m1 has a tag '%s' not present in m2" % tagName
-       return False
-   for tagName in m2.showTagNames().split(", "):
-     if not m1.isValidTagName(tagName):
-       print "domainsEqual: m2 has a tag '%s' not present in m1" % tagName
-       return False
-     if m1.getTag(tagName) != m2.getTag(tagName):
-       print "domainsEqual: values of tag '%s' differ" % tagName
-       return False
-   for fs in ["Solution", "ReducedSolution", "Function", "ReducedFunction", "FunctionOnBoundary", "ReducedFunctionOnBoundary", "ContinuousFunction", "ReducedContinuousFunction"]:
-     fs1 = eval("%s(m1)" % fs)
-     fs2 = eval("%s(m2)" % fs)
-     x1 = fs1.getX()
-     x2 = fs2.getX()
-     for n in range(1, nft+1):
-       integ1 = integrate(sin(n*x1))
-       integ2 = integrate(sin(n*x2))
-       if Lsup(abs(integ1-integ2)) > REL_TOL:
-	 print "domainsEqual: integrals for n=%d differ" % n
-         return False
-   return True
-
 class InputOutput(unittest.TestCase):
+
+     def domainsEqual(self, m1, m2, nft=20):
+        self.failUnless(m1.getDim() == m2.getDim(), "Dimensions differ")
+        self.failUnless(m1.getNumDataPointsGlobal() == m2.getNumDataPointsGlobal(), "Num data points differ")
+        for tagName in m1.showTagNames().split(", "):
+          self.failUnless(m2.isValidTagName(tagName), "m1 has a tag '%s' not present in m2" % tagName)
+        for tagName in m2.showTagNames().split(", "):
+          self.failUnless(m1.isValidTagName(tagName), "m2 has a tag '%s' not present in m1" % tagName)
+          self.failUnless(m1.getTag(tagName) == m2.getTag(tagName), "values of tag '%s' differ" % tagName)
+        for fs in ["Solution", "ReducedSolution", "Function", "ReducedFunction", "FunctionOnBoundary", "ReducedFunctionOnBoundary", "ContinuousFunction", "ReducedContinuousFunction"]:
+          fs1 = eval("%s(m1)" % fs)
+          fs2 = eval("%s(m2)" % fs)
+          x1 = fs1.getX()
+          x2 = fs2.getX()
+          for n in range(1, nft+1):
+            integ1 = integrate(sin(n*x1))
+            integ2 = integrate(sin(n*x2))
+            self.failUnless(Lsup(abs(integ1-integ2)) <= REL_TOL, "integrals for n=%d differ" % n)
+        return True
 
      # Does optimize=True change Rectangle for order=1?
      def test_Rectangle_optimize_order1(self):
 	mydomain1 = Rectangle(n0=7, n1=11, order=1, l0=1., l1=1., optimize=False)
 	mydomain2 = Rectangle(n0=7, n1=11, order=1, l0=1., l1=1., optimize=True)
-        self.failUnless(domainsEqual(mydomain1, mydomain2), "Domains differ")
+        self.domainsEqual(mydomain1, mydomain2)
 
      # Does optimize=True change Rectangle for order=2?
      def test_Rectangle_optimize_order2(self):
 	mydomain1 = Rectangle(n0=7, n1=11, order=2, l0=1., l1=1., optimize=False)
 	mydomain2 = Rectangle(n0=7, n1=11, order=2, l0=1., l1=1., optimize=True)
-        self.failUnless(domainsEqual(mydomain1, mydomain2), "Domains differ")
+        self.domainsEqual(mydomain1, mydomain2)
 
      # Does optimize=True change Brick for order=1?
      def test_Brick_optimize_order1(self):
 	mydomain1 = Brick(n0=7, n1=11, n2=5, order=1, l0=1., l1=1., l2=1., optimize=False)
 	mydomain2 = Brick(n0=7, n1=11, n2=5, order=1, l0=1., l1=1., l2=1., optimize=True)
-        self.failUnless(domainsEqual(mydomain1, mydomain2), "Domains differ")
+        self.domainsEqual(mydomain1, mydomain2)
 
      # Does optimize=True change Brick for order=2?
      def test_Brick_optimize_order2(self):
 	mydomain1 = Brick(n0=7, n1=11, n2=5, order=2, l0=1., l1=1., l2=1., optimize=False)
 	mydomain2 = Brick(n0=7, n1=11, n2=5, order=2, l0=1., l1=1., l2=1., optimize=True)
-        self.failUnless(domainsEqual(mydomain1, mydomain2), "Domains differ")
+        self.domainsEqual(mydomain1, mydomain2)
 
      def test_mesh_dump_to_NetCDF_rectangle(self):
 	if loadIsConfigured():
 	  mydomain1 = Rectangle(n0=17, n1=111, order=1, l0=1., l1=1., optimize=False)
 	  mydomain1.dump("tt.mesh.nc")
 	  mydomain2=LoadMesh("tt.mesh.nc")
-          self.failUnless(domainsEqual(mydomain1, mydomain2), "Domains differ")
+          self.domainsEqual(mydomain1, mydomain2)
 
      def DISABLED_test_mesh_dump_to_NetCDF_brick(self):
 	if loadIsConfigured():
 	  mydomain1 = Brick(n0=7, n1=11, n2=5, order=2, l0=1., l1=1., l2=1., optimize=False)
 	  mydomain1.dump("tt.mesh.nc")
 	  mydomain2=LoadMesh("tt.mesh.nc")
-          self.failUnless(domainsEqual(mydomain1, mydomain2), "Domains differ")
+          self.domainsEqual(mydomain1, mydomain2)
+
+     def test_mesh_read_rectangle_from_finley_file(self):
+	mydomain1 = Rectangle(n0=8, n1=10, order=1, l0=1., l1=1., optimize=False)
+        mydomain2 = ReadMeshMPI(FINLEY_TEST_MESH_PATH+"rectangle_8x10.fly")
+        self.domainsEqual(mydomain1, mydomain2)
+
+     def test_mesh_read_brick_from_finley_file(self):
+        mydomain1 = Brick(n0=8, n1=10, n2=12, order=1, l0=1., l1=1., l2=1., optimize=False)
+        mydomain2 = ReadMeshMPI(FINLEY_TEST_MESH_PATH+"brick_8x10x12.fly")
+        self.domainsEqual(mydomain1, mydomain2)
 
 if __name__ == '__main__':
    suite = unittest.TestSuite()
