a204f54fdb3d84e745ab3c9b6e2d8808abbbd598
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-09-25 06:53:26 +0000

    Updated copyright for a few remaining python files

--- scons/vm_system_options.py
+++ scons/vm_system_options.py
@@ -1,11 +1,16 @@
 
-#          Copyright 2006 by ACcESS MNRF
+########################################################
 #
-#              http://www.access.edu.au
-#       Primary Business: Queensland, Australia
-#  Licensed under the Open Software License version 3.0
-#     http://www.opensource.org/licenses/osl-3.0.php
+# Copyright (c) 2003-2008 by University of Queensland
+# Earth Systems Science Computational Center (ESSCC)
+# http://www.uq.edu.au/esscc
 #
+# Primary Business: Queensland, Australia
+# Licensed under the Open Software License version 3.0
+# http://www.opensource.org/licenses/osl-3.0.php
+#
+########################################################
+
 
 # locations of include files for python
 ### python_path = '/data/raid2/toolspp4/python/2.4.3/gcc-3.3.6/include/python2.4'
--- tools/vizTools/make_movie.py
+++ tools/vizTools/make_movie.py
@@ -1,20 +1,24 @@
-#!/usr/bin/env python
 
+########################################################
 #
-# $Id$
+# Copyright (c) 2003-2008 by University of Queensland
+# Earth Systems Science Computational Center (ESSCC)
+# http://www.uq.edu.au/esscc
 #
-#######################################################
+# Primary Business: Queensland, Australia
+# Licensed under the Open Software License version 3.0
+# http://www.opensource.org/licenses/osl-3.0.php
 #
-#           Copyright 2003-2007 by ACceSS MNRF
-#       Copyright 2007 by University of Queensland
-#
-#                http://esscc.uq.edu.au
-#        Primary Business: Queensland, Australia
-#  Licensed under the Open Software License version 3.0
-#     http://www.opensource.org/licenses/osl-3.0.php
-#
-#######################################################
-#
+########################################################
+
+__copyright__="""Copyright (c) 2003-2008 by University of Queensland
+Earth Systems Science Computational Center (ESSCC)
+http://www.uq.edu.au/esscc
+Primary Business: Queensland, Australia"""
+__license__="""Licensed under the Open Software License version 3.0
+http://www.opensource.org/licenses/osl-3.0.php"""
+__url__="http://www.uq.edu.au/esscc/escript-finley"
+
 
 """
 Make an mpeg movie from the pnm files in the specified directory
