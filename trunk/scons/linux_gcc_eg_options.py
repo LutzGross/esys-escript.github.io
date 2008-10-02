f2aff8eb3def8cb6abc11e060042a6f2346d468e
phornby <phornby@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-10-02 03:13:27 +0000

    Updated for new build system. Still generating std::bad_alloc deep in VTK.

--- trunk/scons/linux_gcc_eg_options.py
+++ trunk/scons/linux_gcc_eg_options.py
@@ -1,23 +1,27 @@
-
-########################################################
 #
-# Copyright (c) 2003-2008 by University of Queensland
-# Earth Systems Science Computational Center (ESSCC)
-# http://www.uq.edu.au/esscc
+# $Id$
 #
-# Primary Business: Queensland, Australia
-# Licensed under the Open Software License version 3.0
-# http://www.opensource.org/licenses/osl-3.0.php
+#######################################################
 #
-########################################################
-
+#           Copyright 2003-2007 by ACceSS MNRF
+#       Copyright 2007 by University of Queensland
+#
+#                http://esscc.uq.edu.au
+#        Primary Business: Queensland, Australia
+#  Licensed under the Open Software License version 3.0
+#     http://www.opensource.org/licenses/osl-3.0.php
+#
+#######################################################
+#
 
 import sys
 
+py_vers_string = "%s.%s"%(sys.version_info[0],sys.version_info[1])
+
 # locations of include files for python
-python_path = "/usr/include/python%s.%s"%(sys.version_info[0],sys.version_info[1])
+python_path = "/usr/include/python" + py_vers_string
 python_lib_path = '/usr/lib'
-python_lib = "python%s.%s"%(sys.version_info[0],sys.version_info[1])
+python_lib = "python" + py_vers_string
 
 # locations of libraries for boost
 boost_path = '/usr/include'
@@ -25,6 +29,8 @@ boost_lib_path = '/usr/lib'
 boost_lib = 'boost_python'
 
 useNetCDF = 'yes'
+netCDF_path = '/usr/include'
+netCDF_lib_path = '/usr/lib'
 netCDF_libs = ['netcdf', 'netcdf_c++']
 
 # locations of doc building executables
@@ -32,12 +38,9 @@ doxygen_path = '/usr/bin'
 epydoc_path = '/usr/bin'
 
 # c flags to use
-cc_flags  = '-O3 -fpic  -Wno-unknown-pragmas'
-cc_flags_debug  = '-g -O0 -fpic -Wno-unknown-pragmas'
-
-# c++ flags to use
-cxx_flags  = '-ansi'
-cxx_flags_debug  = '-ansi -DDOASSERT -DDOPROF'
+cc_flags  = '-fpic  -Wno-unknown-pragmas'
+cc_optim  = '-O3'
+cc_debug  = '-g'
 
 # static library archiver flags to use
 #ar_flags = 'crus'
