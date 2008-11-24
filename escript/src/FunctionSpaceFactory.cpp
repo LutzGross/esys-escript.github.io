723815952499cb7e831afb778fc61e3d180bc400
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-11-24 06:07:29 +0000

    Resolve mantis issue 216.
    Removed the asAbstractContinuousDomain(X) member which did not take cast 
    failure into account.
    Rewrote the few places where it was used.

--- escript/src/FunctionSpaceFactory.cpp
+++ escript/src/FunctionSpaceFactory.cpp
@@ -14,85 +14,93 @@
 
 #include "FunctionSpaceFactory.h"
 #include "AbstractContinuousDomain.h"
+#include "FunctionSpaceException.h"
 
 namespace escript {
 
+#define CTS_CHECK const AbstractContinuousDomain* temp=dynamic_cast<const AbstractContinuousDomain*>(&domain);\
+if (temp==0)\
+{\
+   throw FunctionSpaceException("This method will only make FunctionSpaces for ContinuousDomains.");\
+}
+
+
 FunctionSpace continuousFunction(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getContinuousFunctionCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getContinuousFunctionCode());
 }
 
 FunctionSpace reducedContinuousFunction(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getReducedContinuousFunctionCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getReducedContinuousFunctionCode());
 }
  
 FunctionSpace function(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getFunctionCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getFunctionCode());
 }
 
 FunctionSpace reducedFunction(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getReducedFunctionCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getReducedFunctionCode());
 }
 
 FunctionSpace functionOnBoundary(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getFunctionOnBoundaryCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getFunctionOnBoundaryCode());
 }
 
 FunctionSpace reducedFunctionOnBoundary(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getReducedFunctionOnBoundaryCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getReducedFunctionOnBoundaryCode());
 }
 
 FunctionSpace functionOnContactZero(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getFunctionOnContactZeroCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getFunctionOnContactZeroCode());
 }
 
 FunctionSpace reducedFunctionOnContactZero(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getReducedFunctionOnContactZeroCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getReducedFunctionOnContactZeroCode());
 }
  
 FunctionSpace functionOnContactOne(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getFunctionOnContactOneCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getFunctionOnContactOneCode());
 }
 
 FunctionSpace reducedFunctionOnContactOne(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getReducedFunctionOnContactOneCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getReducedFunctionOnContactOneCode());
 }
 
 FunctionSpace solution(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getSolutionCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getSolutionCode());
 }
 
 FunctionSpace reducedSolution(const AbstractDomain& domain) 
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getReducedSolutionCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getReducedSolutionCode());
 }
 
 FunctionSpace diracDeltaFunction(const AbstractDomain& domain)
 {
-  const AbstractContinuousDomain& temp=AbstractContinuousDomain::asAbstractContinuousDomain(domain);
-  return FunctionSpace(domain.getPtr(),temp.getDiracDeltaFunctionCode());
+  CTS_CHECK
+  return FunctionSpace(domain.getPtr(),temp->getDiracDeltaFunctionCode());
 }
 
 }  // end of namespace
