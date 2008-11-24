75ea823909849f21b91735b8753ff8482b7dd88b
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-11-24 00:45:48 +0000

    Added c++ unit tests for new operations. Added resolve to some operations in Data

--- trunk/escript/test/DataLazyTestCase.cpp
+++ trunk/escript/test/DataLazyTestCase.cpp
@@ -96,6 +96,21 @@ getLazyU(DataTypes::ShapeType& shape, ES_optype typ)
   return DataAbstract_ptr(l);
 }
 
+DataAbstract_ptr
+getLazyUP(DataTypes::ShapeType& shape, ES_optype typ, int par)
+{
+  int pts=DataTypes::noValues(shape);
+  DataTypes::ValueType data(pts,0);
+  for (int i=0;i<pts;++i)
+  {
+	data[i]=(i+1);
+  }
+  DataConstant* p=new DataConstant(FunctionSpace(),shape,data);
+  DataAbstract_ptr pp(p);
+  DataLazy* l=new DataLazy(pp,typ,par);
+  return DataAbstract_ptr(l);
+}
+
 
 DataAbstract_ptr
 getLazyB(DataTypes::ShapeType& shape, ES_optype typ)
@@ -115,6 +130,27 @@ getLazyB(DataTypes::ShapeType& shape, ES_optype typ)
   DataLazy* l=new DataLazy(pp,pp2,typ);
   return DataAbstract_ptr(l);
 }
+
+DataAbstract_ptr
+getLazyGTP(DataTypes::ShapeType& shape, ES_optype typ, int ax, int tr)
+{
+  int pts=DataTypes::noValues(shape);
+  DataTypes::ValueType data(pts,0);
+  DataTypes::ValueType data2(pts,0);
+  for (int i=0;i<pts;++i)
+  {
+	data[i]=(i+1);
+	data2[i]=-(i+1);
+  }
+  DataConstant* p=new DataConstant(FunctionSpace(),shape,data);
+  DataConstant* p2=new DataConstant(FunctionSpace(),shape,data2);
+  DataAbstract_ptr pp(p);
+  DataAbstract_ptr pp2(p2);
+  DataLazy* l=new DataLazy(pp,pp2,typ,ax,tr);
+  return DataAbstract_ptr(l);
+}
+
+
 
 #define TESTOP(X,V) { DataAbstract_ptr d1=getLazy(shape); assert(d1->X()==V); assert(d1->isLazy());}
 
@@ -173,8 +209,49 @@ void DataLazyTestCase::testLazy2()
   }
 }
 
-#define TESTOPB(X,V,O) { DataAbstract_ptr d1=getLazyB(shape,O); assert(dynamic_pointer_cast<DataLazy>(d1)->X()==V); assert(dynamic_pointer_cast<DataLazy>(d1)->isLazy());}
+#define TESTOPUP(X,V,O) { DataAbstract_ptr d1=getLazyUP(shape,O,0); assert(dynamic_pointer_cast<DataLazy>(d1)->X()==V); assert(d1->isLazy());}
 // This method tests the unary op  constructor
+// We aren't checking the correctness of the results here, just that they have the right properties
+void DataLazyTestCase::testLazy2p()
+{
+  cout << endl;
+  cout << "\tTesting UNARY (with arg) constructor (basic checks only)\n";
+
+  DataTypes::ShapeType shape;
+  DataAbstract_ptr d1=getLazyUP(shape,TRANS,0);
+  assert(d1->isLazy());
+
+  for (int j=TRANS;j<=TRACE;++j)
+  {
+    shape=DataTypes::scalarShape;
+    ES_optype op=(ES_optype)(j);			// not even reinterpret_cast works here
+					// if other compilers object I'll write a switch 
+    cout << "\t" << opToString(op) << endl;
+    for (int i=0;i<5;++i)
+    {
+	if (op==TRACE)
+	{
+	   TESTOPUP(getRank,0,op);
+    	   TESTOPUP(getNoValues,1,op);
+    	   TESTOPUP(getShape,DataTypes::scalarShape,op);
+	}
+	else
+	{
+	   TESTOPUP(getRank,i,op);
+    	   TESTOPUP(getNoValues,DataTypes::noValues(shape),op);
+    	   TESTOPUP(getShape,shape,op);
+
+	}
+    	TESTOPUP(getNumDPPSample,1,op);
+    	TESTOPUP(getNumSamples,1,op);
+	TESTOPUP(getBuffsRequired,2,op);
+    	shape.push_back(3);
+    }
+  }
+}
+
+#define TESTOPB(X,V,O) { DataAbstract_ptr d1=getLazyB(shape,O); assert(dynamic_pointer_cast<DataLazy>(d1)->X()==V); assert(dynamic_pointer_cast<DataLazy>(d1)->isLazy());}
+// This method tests the binary op  constructor
 // We aren't checking the correctness of the results here, just that they have the right properties
 void DataLazyTestCase::testLazy3()
 {
@@ -204,6 +281,51 @@ void DataLazyTestCase::testLazy3()
   }
 }
 
+
+
+
+#define TESTOPGTP(X,V,O) { DataAbstract_ptr d1=getLazyGTP(shape,O,0,0); assert(dynamic_pointer_cast<DataLazy>(d1)->X()==V); assert(dynamic_pointer_cast<DataLazy>(d1)->isLazy());}
+
+// This method tests the GeneralTensorproduct  constructor
+// We aren't checking the correctness of the results here, just that they have the right properties
+void DataLazyTestCase::testLazy4()
+{
+  cout << endl;
+  cout << "\tTesting GTP constructor (basic checks only)\n";
+
+  DataTypes::ShapeType shape;
+  DataTypes::ShapeType prodshape;
+
+  DataAbstract_ptr d1=getLazyGTP(shape,PROD,0,0);
+  assert(d1->isLazy());
+
+  for (int j=PROD;j<=PROD;++j)
+  {
+    shape=DataTypes::scalarShape;
+    ES_optype op=(ES_optype)(j);			// not even reinterpret_cast works here
+					// if other compilers object I'll write a switch 
+    cout << "\t" << opToString(op) << endl;
+    for (int i=0;i<3;++i)
+    {
+	ShapeType ns;
+	for (int k=0;k<i;++k)
+	{
+	  ns.push_back(3);
+	  ns.push_back(3);
+	}
+	TESTOPGTP(getRank,i*2,op);
+    	TESTOPGTP(getNoValues,DataTypes::noValues(ns),op);
+    	TESTOPGTP(getShape,ns,op);
+    	TESTOPGTP(getNumDPPSample,1,op);
+    	TESTOPGTP(getNumSamples,1,op);
+	TESTOPGTP(getBuffsRequired,3,op);
+    	shape.push_back(3);
+    }
+  }
+}
+
+
+
 void DataLazyTestCase::testBuffers()
 {
   cout << endl;
@@ -216,6 +338,12 @@ void DataLazyTestCase::testBuffers()
   assert(dynamic_pointer_cast<DataLazy>(p4)->getBuffsRequired()==2);
   DataAbstract_ptr p5=(new DataLazy(p2,p4,ADD))->getPtr();
   assert(dynamic_pointer_cast<DataLazy>(p5)->getBuffsRequired()==3);
+  DataAbstract_ptr p6=(new DataLazy(p5,TRANS,0))->getPtr();
+  assert(dynamic_pointer_cast<DataLazy>(p6)->getBuffsRequired()==4);
+  DataAbstract_ptr p7=(new DataLazy(p6,p6,PROD,0,0))->getPtr();
+  assert(dynamic_pointer_cast<DataLazy>(p7)->getBuffsRequired()==6);
+  DataAbstract_ptr p8=(new DataLazy(p7,TRACE,0))->getPtr();
+  assert(dynamic_pointer_cast<DataLazy>(p8)->getBuffsRequired()==7);
 }
 
 
@@ -227,7 +355,9 @@ TestSuite* DataLazyTestCase::suite ()
 
   testSuite->addTest (new TestCaller< DataLazyTestCase>("Identity",&DataLazyTestCase::testLazy1));
   testSuite->addTest (new TestCaller< DataLazyTestCase>("Unary",&DataLazyTestCase::testLazy2));
+  testSuite->addTest (new TestCaller< DataLazyTestCase>("Unary (params)",&DataLazyTestCase::testLazy2p));
   testSuite->addTest (new TestCaller< DataLazyTestCase>("Binary",&DataLazyTestCase::testLazy3));
+  testSuite->addTest (new TestCaller< DataLazyTestCase>("GTP",&DataLazyTestCase::testLazy4));
   testSuite->addTest (new TestCaller< DataLazyTestCase>("Buffers",&DataLazyTestCase::testBuffers));
   return testSuite;
 }
