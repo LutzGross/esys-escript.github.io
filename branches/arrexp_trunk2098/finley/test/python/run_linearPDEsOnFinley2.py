e4eade03797f2873c79f385f6a74107d925fd3bc
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-12-01 01:16:18 +0000

    Doin' some experiments

---
+++ branches/arrexp_trunk2098/finley/test/python/run_linearPDEsOnFinley2.py
@@ -0,0 +1,101 @@
+
+########################################################
+#
+# Copyright (c) 2003-2008 by University of Queensland
+# Earth Systems Science Computational Center (ESSCC)
+# http://www.uq.edu.au/esscc
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Open Software License version 3.0
+# http://www.opensource.org/licenses/osl-3.0.php
+#
+########################################################
+
+__copyright__="""Copyright (c) 2003-2008 by University of Queensland
+Earth Systems Science Computational Center (ESSCC)
+http://www.uq.edu.au/esscc
+Primary Business: Queensland, Australia"""
+__license__="""Licensed under the Open Software License version 3.0
+http://www.opensource.org/licenses/osl-3.0.php"""
+__url__="http://www.uq.edu.au/esscc/escript-finley"
+
+"""
+Test suite for the linearPDE  and pdetools test on finley
+
+@remark:
+
+@var __author__: name of author
+@var __licence__: licence agreement
+@var __url__: url entry point on documentation
+@var __version__: version
+@var __date__: date of the version
+"""
+
+__author__="Lutz Gross, l.gross@uq.edu.au"
+
+import os
+
+import unittest
+from test_linearPDEs import Test_Poisson,Test_LinearPDE, Test_LinearPDE_noLumping, Test_TransportPDE
+from test_assemblage import Test_assemblage_2Do1, Test_assemblage_2Do2, Test_assemblage_3Do1, Test_assemblage_3Do2, \
+                            Test_assemblage_2Do1_Contact,Test_assemblage_2Do2_Contact, Test_assemblage_3Do1_Contact, Test_assemblage_3Do2_Contact
+from test_pdetools import Test_pdetools, Test_pdetools_noLumping
+from esys.escript import *
+from esys.finley import Rectangle,Brick,JoinFaces, ReadMesh
+import sys
+
+
+try:
+     FINLEY_TEST_DATA=os.environ['FINLEY_TEST_DATA']
+except KeyError:
+     FINLEY_TEST_DATA='.'
+
+FINLEY_TEST_MESH_PATH=FINLEY_TEST_DATA+"/data_meshes/"
+
+NE=6 # number of element in each spatial direction (must be even)
+
+class Test_LinearPDEOnFinleyTet2DOrder1(Test_LinearPDE,Test_pdetools,Test_assemblage_2Do1, Test_TransportPDE):
+   RES_TOL=1.e-7
+   ABS_TOL=1.e-8
+   def setUp(self):
+        self.domain = ReadMesh(FINLEY_TEST_MESH_PATH+"tet_2D_order1.fly",optimize=False)
+   def tearDown(self):
+        del self.domain
+
+class Test_LinearPDEOnFinleyTet2DOrder2(Test_LinearPDE_noLumping,Test_pdetools_noLumping,Test_assemblage_2Do2, Test_TransportPDE):
+   RES_TOL=1.e-7
+   ABS_TOL=1.e-8
+   def setUp(self):
+        self.domain = ReadMesh(FINLEY_TEST_MESH_PATH+"tet_2D_order2.fly",optimize=False)
+   def tearDown(self):
+        del self.domain
+
+class Test_LinearPDEOnFinleyTet3DOrder1(Test_LinearPDE,Test_pdetools,Test_assemblage_3Do1, Test_TransportPDE):
+   RES_TOL=1.e-7
+   ABS_TOL=1.e-8
+   def setUp(self):
+        self.domain = ReadMesh(FINLEY_TEST_MESH_PATH+"tet_3D_order1.fly",optimize=False)
+   def tearDown(self):
+        del self.domain
+
+class Test_LinearPDEOnFinleyTet3DOrder2(Test_LinearPDE,Test_pdetools,Test_assemblage_3Do2, Test_TransportPDE):
+   RES_TOL=1.e-7
+   ABS_TOL=1.e-8
+   def setUp(self):
+        self.domain = ReadMesh(FINLEY_TEST_MESH_PATH+"tet_3D_order2.fly",optimize=False)
+   def tearDown(self):
+        del self.domain
+
+if __name__ == '__main__':
+   suite = unittest.TestSuite()
+   if True :
+      suite.addTest(unittest.makeSuite(Test_LinearPDEOnFinleyTet2DOrder1))
+      suite.addTest(unittest.makeSuite(Test_LinearPDEOnFinleyTet2DOrder2))
+      suite.addTest(unittest.makeSuite(Test_LinearPDEOnFinleyTet3DOrder1))
+      suite.addTest(unittest.makeSuite(Test_LinearPDEOnFinleyTet3DOrder2))
+   else:
+      pass
+
+   s=unittest.TextTestRunner(verbosity=2).run(suite)
+   if not s.wasSuccessful(): sys.exit(1)
+
