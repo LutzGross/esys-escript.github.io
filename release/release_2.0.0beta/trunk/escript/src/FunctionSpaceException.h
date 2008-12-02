10566e2f8b091adb272f0aea33c7ebf5ac667837
ksteube <ksteube@77569008-7704-0410-b7a0-a92fef0b09fd>
2008-12-02 03:35:42 +0000

    Tagging the 2.0.0beta release

---
+++ release/release_2.0.0beta/trunk/escript/src/FunctionSpaceException.h
@@ -0,0 +1,99 @@
+
+/*******************************************************
+*
+* Copyright (c) 2003-2008 by University of Queensland
+* Earth Systems Science Computational Center (ESSCC)
+* http://www.uq.edu.au/esscc
+*
+* Primary Business: Queensland, Australia
+* Licensed under the Open Software License version 3.0
+* http://www.opensource.org/licenses/osl-3.0.php
+*
+*******************************************************/
+
+
+#if !defined  escript_FunctionSpaceException_20040602_H
+#define escript_FunctionSpaceException_20040602_H
+#include "system_dep.h"
+
+#include "esysUtils/EsysException.h"
+
+namespace escript
+{
+
+  /**
+  \brief
+  FunctionSpaceException exception class.
+
+  Description:
+  FunctionSpaceException exception class.
+  The class provides a public function returning the exception name
+  */
+  class FunctionSpaceException : public esysUtils::EsysException
+  {
+
+  protected:
+
+     typedef EsysException Parent;
+
+  public:
+    /**
+    \brief
+    Default constructor for the exception.
+    */
+    ESCRIPT_DLL_API
+    FunctionSpaceException() : Parent() { updateMessage();}
+    /**
+    \brief
+    Constructor for the exception.
+    */
+    ESCRIPT_DLL_API
+    FunctionSpaceException(const char *cstr) : Parent(cstr) { updateMessage();}
+    /**
+    \brief
+    Constructor for the exception.
+    */
+    ESCRIPT_DLL_API
+    FunctionSpaceException(const std::string &str) :
+    Parent(str) { updateMessage();}
+    /**
+    \brief
+    Copy Constructor for the exception.
+    */
+    ESCRIPT_DLL_API
+    FunctionSpaceException(const FunctionSpaceException &other) : Parent(other)
+      {
+        updateMessage();
+      }
+
+    ESCRIPT_DLL_API
+    inline FunctionSpaceException &
+    operator=(const FunctionSpaceException &other ) THROW(NO_ARG)
+       {
+         Parent::operator=(other);
+         updateMessage();
+         return *this;
+       }
+
+
+    /// Destructor
+    ESCRIPT_DLL_API
+    virtual ~FunctionSpaceException() THROW(NO_ARG) {}
+    /**
+    \brief
+    Returns the name of the exception.
+    */
+    ESCRIPT_DLL_API
+    virtual const std::string & exceptionName() const;
+
+  private:
+
+    //
+    // the exception name is immutable and class-wide.
+    // Inheritor note; you need one of these too.
+    // and an overloaded exceptionName() in your .cpp implementation file. 
+    static const std::string exceptionNameValue;
+  };
+
+} // end of namespace
+#endif
---
+++ release/release_2.0.0beta/trunk/escript/src/UnaryOp.h
@@ -0,0 +1,89 @@
+
+/*******************************************************
+*
+* Copyright (c) 2003-2008 by University of Queensland
+* Earth Systems Science Computational Center (ESSCC)
+* http://www.uq.edu.au/esscc
+*
+* Primary Business: Queensland, Australia
+* Licensed under the Open Software License version 3.0
+* http://www.opensource.org/licenses/osl-3.0.php
+*
+*******************************************************/
+
+
+#if !defined escript_UnaryOp_20040315_H
+#define escript_UnaryOp_20040315_H
+#include "system_dep.h"
+
+#include "DataConstant.h"
+#include "DataTagged.h"
+#include "DataExpanded.h"
+#include "DataTypes.h"
+
+namespace escript {
+
+/**
+   \brief
+   Perform the given unary operation on each data point of the given Data object.
+   Called by Data::unaryOp.
+   Calls DataArrayView::unaryOp.
+   For DataExpanded objects, operation is done in parallel.
+   \param data Input/Output - The data.
+   \param operation Input - The operation to perform.
+*/
+
+template <class UnaryFunction>
+inline
+void
+unaryOp(DataExpanded& data,
+        UnaryFunction operation)
+{
+  int i,j;
+  DataTypes::ValueType::size_type numDPPSample=data.getNumDPPSample();
+  DataTypes::ValueType::size_type numSamples=data.getNumSamples();
+  DataTypes::ValueType& left=data.getVector();
+  const DataTypes::ShapeType& shape=data.getShape();
+  #pragma omp parallel for private(i,j) schedule(static)
+  for (i=0;i<numSamples;i++) {
+    for (j=0;j<numDPPSample;j++) {
+      //data.getPointDataView().unaryOp(data.getPointOffset(i,j),operation);
+      DataMaths::unaryOp(left,shape,data.getPointOffset(i,j),operation);
+    }
+  }
+}
+
+template <class UnaryFunction>
+inline
+void
+unaryOp(DataTagged& data,
+        UnaryFunction operation)
+{
+  // perform the operation on each tagged value
+  const DataTagged::DataMapType& lookup=data.getTagLookup();
+  DataTagged::DataMapType::const_iterator i;
+  DataTagged::DataMapType::const_iterator lookupEnd=lookup.end();
+  //DataArrayView& dataView=data.getPointDataView();
+  DataTypes::ValueType& left=data.getVector();
+  const DataTypes::ShapeType& shape=data.getShape();
+  for (i=lookup.begin();i!=lookupEnd;i++) {
+//    dataView.unaryOp(i->second,operation);
+    DataMaths::unaryOp(left,shape,i->second,operation);
+  }
+  // perform the operation on the default value
+  //data.getDefaultValue().unaryOp(operation);
+  DataMaths::unaryOp(left,shape,data.getDefaultOffset(),operation);
+}
+
+template <class UnaryFunction>
+inline
+void
+unaryOp(DataConstant& data,
+        UnaryFunction operation)
+{
+  //data.getPointDataView().unaryOp(operation);
+  DataMaths::unaryOp(data.getVector(),data.getShape(),0,operation);
+}
+
+} // end of namespace
+#endif
