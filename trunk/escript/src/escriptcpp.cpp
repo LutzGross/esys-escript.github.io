b0c297579fc2816c2c6c33a683127d7467096c53
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2009-01-09 01:33:39 +0000

    Fixed a little doxygen typo in the hyperlink.

--- trunk/escript/src/escriptcpp.cpp
+++ trunk/escript/src/escriptcpp.cpp
@@ -54,7 +54,7 @@ using namespace boost::python;
  *
  * - \ref finley
  *
- * - <a href=http://shake200.esscc.uq.edu.au/esys/esys13/release/epydoc/index.html>Python module documentation (epydoc generated)</a>
+ * - <a href="http://shake200.esscc.uq.edu.au/esys/esys13/release/epydoc/index.html">Python module documentation (epydoc generated)</a>
  *
  */
 
