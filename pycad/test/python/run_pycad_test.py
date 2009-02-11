cd2ad1871fe8c6dae8a0d1c214eb101147f0c9bf
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2009-02-11 03:56:47 +0000

    test for gmsh command line generation fixed (didn't know that there is such a test)

--- pycad/test/python/run_pycad_test.py
+++ pycad/test/python/run_pycad_test.py
@@ -3669,13 +3669,13 @@ class Test_PyCAD_Design(unittest.TestCase):
        d.setMeshFileName(mesh_name)
        self.failUnless(mesh_name == d.getMeshFileName())
        
-       d.setOptions(algorithm=d.TETGEN,optimize_quality=False,smoothing=4,curvature_based_element_size=True)
+       d.setOptions(algorithm=d.TETGEN,optimize_quality=False,smoothing=4)
        cmd=d.getCommandString()
-       self.failUnless("gmsh -2 -algo tetgen -clcurv -smooth 4 -v 0 -order 1 -o .%smesh.msh .%sscript.geo"%(os.sep,os.sep) == cmd)
+       self.failUnless("gmsh -format msh -2 -algo tetgen -smooth 4 -v 0 -order 1 -o .%smesh.msh .%sscript.geo"%(os.sep,os.sep) == cmd)
 
-       d.setOptions(optimize_quality=True,curvature_based_element_size=True)
+       d.setOptions(optimize_quality=True)
        cmd=d.getCommandString()
-       self.failUnless("gmsh -2 -algo iso -clcurv -smooth 1 -optimize -v 0 -order 1 -o .%smesh.msh .%sscript.geo"%(os.sep,os.sep) == cmd)
+       self.failUnless("gmsh -format msh -2 -algo iso -smooth 1 -optimize -v 0 -order 1 -o .%smesh.msh .%sscript.geo"%(os.sep,os.sep) == cmd)
 
        p0=Point(0.,0.,0.)
        p1=Point(1.,0.,0.)
