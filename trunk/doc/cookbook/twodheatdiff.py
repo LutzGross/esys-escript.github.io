c9408469aabac639d9c66b2ef1e405af05a6e3e7
ahallam <ahallam@77569008-7704-0410-b7a0-a92fef0b09fd>
2009-04-24 04:24:28 +0000

    End Week 4, Finalised 1D examples, need work on text but script ok. Begining of 2D example builds on 1D. Script semi-finalised, text to complete.

--- trunk/doc/cookbook/twodheatdiff.py
+++ trunk/doc/cookbook/twodheatdiff.py
@@ -33,23 +33,33 @@ import os #This package is necessary to handle saving our data.
 
 ##ESTABLISHING VARIABLES
 #PDE related
-mx = 500 # model lenght
-my = 100 # model width
-ndx = 500 # steps in x direction 
-ndy = 1 # steps in y direction
+mx = 600 # model lenght
+my = 600 # model width
+ndx = 100 # steps in x direction 
+ndy = 100 # steps in y direction
+r = 200 # radius of intrusion
+ic = [300, 0] #centre of intrusion
 
 q=0 #our heat source temperature is now zero
-Tref=2273 # Kelvin #the starting temperature of our intrusion
-rho = 2750 #kg/m^{3} density
-cp = 790 #j/kg specific heat
-rhocp = rho*cp	#DENSITY * SPECIFIC HEAT
+Ti=2273 # Kelvin #the starting temperature of our iron bar
+rhoi = 2750 #kg/m^{3} density
+cpi = 790 #j/kg specific heat
+rhocpi = rhoi*cpi	#DENSITY * SPECIFIC HEAT
 eta=0.  # RADIATION CONDITION
-kappa=2.2 # Watts/(meter*Kelvin) DIFFUSION CONSTANT, HEAT PERMEABILITY
+kappai=2.2 # Watts/(meter*Kelvin) DIFFUSION CONSTANT, HEAT PERMEABILITY
+
+Tc = 200
+rhoc = 2200
+cpc = 400
+rhocpc = rhoc*cpc
+kappac = 0.1
+
+
 #Script/Iteration Related
 t=0. #our start time, usually zero
-tday=10*365. #the time we want to end the simulation in days
+tday=100*365. #the time we want to end the simulation in days
 tend=tday*24*60*60
-outputs = 400 # number of time steps required.
+outputs = 200 # number of time steps required.
 h=(tend-t)/outputs #size of time step
 
 print "Expected Number of Output Files is: ", outputs
@@ -57,40 +67,35 @@ print "Step size is: ", h/(24.*60*60), "days"
 
 
 i=0 #loop counter 
-save_path = "data/onedheatdiff_var001" #the folder to put our outputs in, leave blank "" for script path - note this folder path must exist to work
+save_path = "data/twodheatdiff" #the folder to put our outputs in, leave blank "" for script path - note this folder path must exist to work
 
 #... generate domain ...
 model = Rectangle(l0=mx,l1=my,n0=ndx, n1=ndy)
 # extract finite points
 x=model.getX()
+
 #... open PDE ...
 mypde=LinearPDE(model)
 mypde.setSymmetryOn()
-mypde.setValue(A=kappa*kronecker(model),D=rhocp/h,d=eta,y=eta*Tref)
+
+bound = length(x-ic)-r #where the boundary will be located
+
+A = (kappai)*whereNegative(bound)+(kappac)*wherePositive(bound)
+D = (rhocpi/h)*whereNegative(bound)+(rhocpc/h)*wherePositive(bound)
+
+mypde.setValue(A=A*kronecker(model),D=D,d=eta,y=eta*Tc)
 
 # ... set initial temperature ....
-bound = x[0]-mx/(ndx/250.)
-T= 0*Tref*whereNegative(bound)+Tref*wherePositive(bound)
 
+T= Ti*whereNegative(bound)+Tc*wherePositive(bound) #defining the initial temperatures.
+saveVTK(os.path.join(save_path,"dataedge.xml"), sol=bound)
 saveVTK(os.path.join(save_path,"data%03d.xml") %i,sol=T)
 
-# ... start iteration:
+#... start iteration:
 while t<=tend:
       i+=1
       t+=h
-      mypde.setValue(Y=rhocp/h*T)
+      Y = T*D
+      mypde.setValue(Y=Y)
       T=mypde.getSolution()
       saveVTK(os.path.join(save_path,"data%03d.xml") %i,sol=T)
-      
-# iteration var 2
-#Tl = 0
-#Tr = Tref
-#while Tl < Tr*0.8:
-      #mypde.setValue(Y=rhocp/h*T)
-      #T=mypde.getSolution()
-      #i+=1
-      #x=rod.getX()
-      #Tl= x[0]
-      #Tr= x[
-
-#print "Finish temp balance in:", i*h/(24.*60*60), " days"
