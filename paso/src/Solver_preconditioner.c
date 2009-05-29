6da5c292fac924f9bd9a3d38125fadefda4027f4
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2009-05-29 04:24:01 +0000

    same adjustment of nameing

--- paso/src/Solver_preconditioner.c
+++ paso/src/Solver_preconditioner.c
@@ -81,7 +81,7 @@ void Paso_Solver_setPreconditioner(Paso_SystemMatrix* A,Paso_Options* options) {
               break;
             case PASO_AMG:
               if (options->verbose) printf("AMG preconditioner is used.\n");
-              prec->amg=Paso_Solver_getAMG(A->mainBlock,options->verbose,options->AMGlevels,options->couplingParam);
+              prec->amg=Paso_Solver_getAMG(A->mainBlock,options->verbose,options->level_max,options->coarsening_threshold);
               prec->type=PASO_AMG;
               break;
  
