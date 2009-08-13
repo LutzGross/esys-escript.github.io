70ea824cecc8d65d3f78e0cfd6d9c568a47d040e
ahallam <ahallam@77569008-7704-0410-b7a0-a92fef0b09fd>
2009-08-13 04:32:23 +0000

    Updates to first chapter in cookbook -> approaching completion. New figures added for model descriptions. Scripts updated to use pylab/matplotlib and movie making scripts added for *.avi creation. Still to finalise in chapter 1 - twodheatdiff model of intrusion 	-contouring
    - heat refraction scripts and begin cookbook sections
    	- pycad discussion
    	- steady state pde discussion
    	- quiver plot discussion

--- doc/cookbook/twodheatdiff001.py
+++ doc/cookbook/twodheatdiff001.py
@@ -23,39 +23,50 @@ __url__="https://launchpad.net/escript-finley"
 Author: Antony Hallam antony.hallam@uqconnect.edu.au
 """
 
-# To solve the problem it is necessary to import the modules we require.
-from esys.escript import * # This imports everything from the escript library
-from esys.escript.linearPDEs import LinearPDE # This defines LinearPDE as LinearPDE
-from esys.finley import Rectangle # This imports the rectangle domain function from finley
+############################################################FILE HEADER
+# twodheatdiff002.py
+# Model temperature diffusion between a granite intrusion and sandstone 
+# country rock. This is a two dimensional problem with the granite as a
+# heat source.
+
+#######################################################EXTERNAL MODULES
+#To solve the problem it is necessary to import the modules we require.
+#This imports everything from the escript library
+from esys.escript import *
+# This defines the LinearPDE module as LinearPDE
+from esys.escript.linearPDEs import LinearPDE 
+# This imports the rectangle domain function from finley.
+from esys.finley import Rectangle 
+# A useful unit handling package which will make sure all our units
+# match up in the equations under SI.
+from esys.escript.unitsSI import * 
+import pylab as pl #Plotting package.
+import numpy as np #Array package.
 import os #This package is necessary to handle saving our data.
 
-
-
-##ESTABLISHING VARIABLES
+#################################################ESTABLISHING VARIABLES
 #PDE related
-mx = 600 # model lenght
-my = 600 # model width
-ndx = 100 # steps in x direction 
-ndy = 100 # steps in y direction
-r = 200 # radius of intrusion
-ic = [300, 0] #centre of intrusion
-
-q=0 #our heat source temperature is now zero
+mx = 600*m #meters - model length
+my = 600*m #meters - model width
+ndx = 100 #mesh steps in x direction 
+ndy = 100 #mesh steps in y direction
+r = 200*m #meters - radius of intrusion
+ic = [300, 0] #centre of intrusion (meters)
+q=0.*Celsius #our heat source temperature is now zero
 
 ## Intrusion Variables - Granite
-Ti=2273 # Kelvin #the starting temperature of our intrusion
-rhoi = 2750 #kg/m^{3} density
-cpi = 790 #j/kg.k specific heat
-rhocpi = rhoi*cpi #DENSITY * SPECIFIC HEAT
+Ti=2273.*Celsius # Kelvin -the starting temperature of our RHS Block
+rhoi = 2750*kg/m**3 #kg/m^{3} density of granite
+cpi = 790.*J/(kg*K) #j/Kg.K thermal capacity
+rhocpi = rhoi*cpi	#DENSITY * SPECIFIC HEAT
 eta=0.  # RADIATION CONDITION
-kappai=2.2 # Watts/(meter*Kelvin) DIFFUSION CONSTANT, HEAT PERMEABILITY
-
-Tc = 473 # Kelvin #the starting temperature of our country rock
-rhoc = 2000 #kg/m^{3} density
-cpc = 920 #j/kg.k specific heat
+kappai=2.2*W/m/K #watts/m.K thermal conductivity
+## Country Rock Variables - Sandstone
+Tc = 473*Celsius # Kelvin #the starting temperature of our country rock
+rhoc = 2000*kg/m**3 #kg/m^{3} density
+cpc = 920.*J/(kg*K) #j/kg.k specific heat
 rhocpc = rhoc*cpc #DENSITY * SPECIFIC HEAT
-kappac = 1.9 # Watts/(meter*Kelvin) DIFFUSION CONSTANT, HEAT PERMEABILITY
-
+kappac = 1.9*W/m/K #watts/m.K thermal conductivity
 
 #Script/Iteration Related
 t=0. #our start time, usually zero
@@ -63,35 +74,40 @@ tday=100*365. #the time we want to end the simulation in days
 tend=tday*24*60*60
 outputs = 200 # number of time steps required.
 h=(tend-t)/outputs #size of time step
-
+#user warning
 print "Expected Number of Output Files is: ", outputs
 print "Step size is: ", h/(24.*60*60), "days"
-
-
 i=0 #loop counter 
-save_path = "data/twodheatdiff" #the folder to put our outputs in, leave blank "" for script path - note this folder path must exist to work
+#the folder to put our outputs in, leave blank "" for script path 
+save_path="data/twodheatdiff"
+########## note this folder path must exist to work ###################
 
-#... generate domain ...
+################################################ESTABLISHING PARAMETERS
+#generate domain using rectangle
 model = Rectangle(l0=mx,l1=my,n0=ndx, n1=ndy)
-# extract finite points
+#extract finite points - the solution points
 x=model.getX()
-
-#... open PDE ...
-mypde=LinearPDE(model)
-mypde.setSymmetryOn()
-
+#create the PDE
+mypde=LinearPDE(model) #assigns a domain to our PDE
+mypde.setSymmetryOn() #set the fast solver on for symmetry
+#establish location of boundary between two materials
 bound = length(x-ic)-r #where the boundary will be located
-
 A = (kappai)*whereNegative(bound)+(kappac)*wherePositive(bound)
 D = (rhocpi/h)*whereNegative(bound)+(rhocpc/h)*wherePositive(bound)
-
+#define our PDE coeffs
 mypde.setValue(A=A*kronecker(model),D=D,d=eta,y=eta*Tc)
+#set initial temperature
+T= Ti*whereNegative(bound)+Tc*wherePositive(bound)
 
-# ... set initial temperature ....
-
-T= Ti*whereNegative(bound)+Tc*wherePositive(bound) #defining the initial temperatures.
-saveVTK(os.path.join(save_path,"dataedge.vtu"), sol=bound)
-saveVTK(os.path.join(save_path,"data%03d.vtu") %i,sol=T)
+# rearrage mymesh to suit solution function space for contouring      
+oldspacecoords=model.getX()
+coords=Data(oldspacecoords, T.getFunctionSpace())
+coords = np.array(coords.toListOfTuples())
+coordX = coords[:,0]
+coordY = coords[:,1]
+# create regular grid
+xi = np.linspace(0.0,600.0,100)
+yi = np.linspace(0.0,600.0,100)
 
 #... start iteration:
 while t<=tend:
@@ -100,5 +116,24 @@ while t<=tend:
       Y = T*D
       mypde.setValue(Y=Y)
       T=mypde.getSolution()
-      saveVTK(os.path.join(save_path,"data%03d.vtu") %i,sol=T)
+      tempT = T.toListOfTuples(scalarastuple=False)
+      # grid the data.
+      zi = pl.matplotlib.mlab.griddata(coordX,coordY,tempT,xi,yi)
+      # contour the gridded data, plotting dots at the randomly spaced data points.
+      pl.matplotlib.pyplot.autumn()
+      pl.contourf(xi,yi,zi,10)
+      CS = pl.contour(xi,yi,zi,5,linewidths=0.5,colors='k')
+      pl.clabel(CS, inline=1, fontsize=8)
+      pl.axis([0,600,0,600])
+      pl.title("Heat diffusion from an intrusion.")
+      pl.xlabel("Horizontal Displacement (m)")
+      pl.ylabel("Depth (m)")
+      pl.savefig(os.path.join(save_path,"heatrefraction%03d.png") %i)
+      pl.clf()            
+#     saveVTK(os.path.join(save_path,"data%03d.vtu") %i,sol=T)
 
+# compile the *.png files to create an *.avi video that shows T change
+# with time. This opperation uses linux mencoder.
+os.system("mencoder mf://"+save_path+"/*.png -mf type=png:\
+w=800:h=600:fps=25 -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o \
+twodheatdiff001tempT.avi")
