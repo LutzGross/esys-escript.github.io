0ccf641dc19372d67231c2b102d7db8cd9cf1dfd
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2009-08-20 05:56:15 +0000

    Added Docstrings to FunctionSpace and Data

--- branches/restext/escript/src/escriptcpp.cpp
+++ branches/restext/escript/src/escriptcpp.cpp
@@ -88,7 +88,8 @@ BOOST_PYTHON_MODULE(escriptcpp)
   #endif
 
   def("setNumberOfThreads",escript::setNumberOfThreads,"Use of this method is strongly discouraged.");
-  def("getNumberOfThreads",escript::getNumberOfThreads);
+  def("getNumberOfThreads",escript::getNumberOfThreads,"Return the maximum number of threads"
+" available to OpenMP.");
   def("releaseUnusedMemory",escript::releaseUnusedMemory);
   def("blocktimer_initialize",blocktimer_initialize);
   def("blocktimer_reportSortByName",blocktimer_reportSortByName);
@@ -97,11 +98,13 @@ BOOST_PYTHON_MODULE(escriptcpp)
   def("blocktimer_time",blocktimer_time);
   def("getVersion",escript::getSvnVersion,"This method will only report accurate version numbers for clean checkouts.");
   def("printParallelThreadCounts",escript::printParallelThreadCnt);
-  def("getMPISizeWorld",escript::getMPISizeWorld);
-  def("getMPIRankWorld",escript::getMPIRankWorld);
-  def("MPIBarrierWorld",escript::MPIBarrierWorld);
-  def("getMPIWorldMax",escript::getMPIWorldMax);
-  def("getMPIWorldSum",escript::getMPIWorldSum);
+  def("getMPISizeWorld",escript::getMPISizeWorld,"Return number of MPI processes in the job.");
+  def("getMPIRankWorld",escript::getMPIRankWorld,"Return the rank of this process in the MPI World.");
+  def("MPIBarrierWorld",escript::MPIBarrierWorld,"Wait until all MPI processes have reached this point.");
+  def("getMPIWorldMax",escript::getMPIWorldMax,"\nEach MPI process calls this function with a"
+" value for arg1. The maximum value is computed and returned.\n\n:rtype: int");
+  def("getMPIWorldSum",escript::getMPIWorldSum,"\nEach MPI process calls this function with a"
+" value for arg1. The values are added up and the total value is returned.\n\n:rtype: int");
   def("getMachinePrecision",escript::getMachinePrecision);
   def("getMaxFloat",escript::getMaxFloat);
   //
@@ -147,19 +150,20 @@ BOOST_PYTHON_MODULE(escriptcpp)
   //
   // Interface for FunctionSpace
   //
-  class_<escript::FunctionSpace> fs_definer("FunctionSpace","",init<>());	// Doco goes in the empty string param
-  fs_definer.def("getDim",&escript::FunctionSpace::getDim);
+  class_<escript::FunctionSpace> fs_definer("FunctionSpace","A FunctionSpace describes which points from the `Domain` to use to represent functions.",init<>());	// Doco goes in the empty string param
+  fs_definer.def("getDim",&escript::FunctionSpace::getDim,":return: the spatial dimension of the underlying domain.\n:rtype: int");
 //   fs_definer.def("getDomain",&escript::FunctionSpace::getDomain,
 //                  return_internal_reference<>());
-  fs_definer.def("getDomain",&escript::FunctionSpace::getDomainPython);
-  fs_definer.def("getX",&escript::FunctionSpace::getX);
-  fs_definer.def("getNormal",&escript::FunctionSpace::getNormal);
-  fs_definer.def("getSize",&escript::FunctionSpace::getSize);
-  fs_definer.def("setTags",&escript::FunctionSpace::setTags);
+  fs_definer.def("getDomain",&escript::FunctionSpace::getDomainPython,":return: the underlying `Domain` for this FunctionSpace.\n:rtype: `Domain`");
+  fs_definer.def("getX",&escript::FunctionSpace::getX,"\n:return: a function whose values are its input coordinates. ie an identity function.\n:rtype: `Data`");
+  fs_definer.def("getNormal",&escript::FunctionSpace::getNormal,":return: the surface normal field.\n\n:rtype: `Data`");
+  fs_definer.def("getSize",&escript::FunctionSpace::getSize,":return: sample size\n:rtype: `Data`");
+  fs_definer.def("setTags",&escript::FunctionSpace::setTags,args("newtag","mask"),
+"Set tags according to a mask\n\n:param newtag: tag number to set\n:type newtag: non-zero ``int``\n:param mask: Samples which correspond to positive values in the mask will be set to ``newtag``.\n:type mask: scalar `Data`");
   fs_definer.def("getTagFromDataPointNo",
-                 &escript::FunctionSpace::getTagFromDataPointNo);
-  fs_definer.def("getReferenceIDFromDataPointNo", &escript::FunctionSpace::getReferenceIDFromDataPointNo);
-  fs_definer.def("getListOfTags",&escript::FunctionSpace::getListOfTags);
+                 &escript::FunctionSpace::getTagFromDataPointNo,":return: the tag associated with the given sample number.\n:rtype: int");
+  fs_definer.def("getReferenceIDFromDataPointNo", &escript::FunctionSpace::getReferenceIDFromDataPointNo,args("dataPointNo"),":return: the reference number associated with ``dataPointNo``\n:rtype: int ");
+  fs_definer.def("getListOfTags",&escript::FunctionSpace::getListOfTags,":return: a list of the tags used in this function space\n:rtype: ``list``");
   fs_definer.def("__str__", &escript::FunctionSpace::toString);
   fs_definer.def(self == self);
   fs_definer.def(self != self);
@@ -175,42 +179,59 @@ BOOST_PYTHON_MODULE(escriptcpp)
     // Note for Lutz, Need to specify the call policy in order to return a
     // reference. In this case return_internal_reference.
     .def("__str__",&escript::Data::toString)
-    .def("getDomain",&escript::Data::getDomainPython)
-    .def("getFunctionSpace",&escript::Data::getFunctionSpace,return_value_policy<copy_const_reference>())
-    .def("isEmpty",&escript::Data::isEmpty)
-    .def("isProtected",&escript::Data::isProtected)
-    .def("setProtection",&escript::Data::setProtection)
-    .def("getShape",&escript::Data::getShapeTuple)
-    .def("getRank",&escript::Data::getDataPointRank)
-    .def("dump",&escript::Data::dump)
-    .def("toListOfTuples",&escript::Data::toListOfTuples, (arg("scalarastuple")=false))
-    .def("copyWithMask",&escript::Data::copyWithMask)
-    .def("setTaggedValue",&escript::Data::setTaggedValue)
-    .def("setTaggedValue",&escript::Data::setTaggedValueByName)
-    .def("getNumberOfDataPoints",&escript::Data::getNumDataPoints)
-    .def("isExpanded",&escript::Data::isExpanded)
-    .def("isTagged",&escript::Data::isTagged)
-    .def("isConstant",&escript::Data::isConstant)
-    .def("isLazy",&escript::Data::isLazy)
-    .def("isReady",&escript::Data::isReady)
-    .def("expand",&escript::Data::expand)
-    .def("tag",&escript::Data::tag)
-    .def("resolve",&escript::Data::resolve)
-    .def("copy",&escript::Data::copy)
-    .def("copy",&escript::Data::copySelf)
-    .def("delay",&escript::Data::delay)
-    .def("setValueOfDataPoint",&escript::Data::setValueOfDataPointToPyObject)
+    .def("getDomain",&escript::Data::getDomainPython,":rtype: `Domain`")
+    .def("getFunctionSpace",&escript::Data::getFunctionSpace,return_value_policy<copy_const_reference>(),":rtype: `FunctionSpace`")
+    .def("isEmpty",&escript::Data::isEmpty,"Is this object an instance of ``DataEmpty``\n\n:rtype: ``bool``\n:note: This is not the same thing as asking if the object contains datapoints.")
+    .def("isProtected",&escript::Data::isProtected,"Can this instance be modified.\n:rtype: ``bool``")
+    .def("setProtection",&escript::Data::setProtection,"Disallow modifications to this data object\n\n:note: This method does not allow you to undo protection.")
+    .def("getShape",&escript::Data::getShapeTuple,"\nReturns the shape of the datapoints in this object as a python tuple. Scalar data has the shape ``()``\n\n:rtype: ``tuple``")
+    .def("getRank",&escript::Data::getDataPointRank,":return: the number of indicies required to address a component of a datapoints\n:rtype: positive ``int``")
+    .def("dump",&escript::Data::dump,args("fileName"),"Save the data as a netCDF file\n\n:param fileName: \n:type fileName: ``string``")
+    .def("toListOfTuples",&escript::Data::toListOfTuples, (arg("scalarastuple")=false),
+"Return the datapoints of this object in a list. Each datapoint is stored as a tuple.\n\n"
+":param scalarastuple: if True, scalar data will be wrapped as a tuple."
+" True => [(0), (1), (2)]; False => [0, 1, 2]")
+    .def("copyWithMask",&escript::Data::copyWithMask,args("other","mask"),
+"Selectively copy values from ``other`` `Data`."
+"Datapoints which correspond to postive values in ``mask`` will be copied from ``other``\n"
+"\n:param other: source of values\n"
+":type other: `Data`\n:param mask:\n:type mask: Scalar `Data`")
+    .def("setTaggedValue",&escript::Data::setTaggedValue,args("tagKey","value"),
+"Set the value of tagged Data.\n\n:param tagKey: tag to update\n:type tagKey: ``int``\n")
+    .def("setTaggedValue",&escript::Data::setTaggedValueByName,args("name","value"),":param name: tag to update\n:type name: ``string``\n"
+":param value: value to set tagged data to\n:type value: ``object`` which acts like an array, ``tuple`` or ``list``\n")
+    .def("getNumberOfDataPoints",&escript::Data::getNumDataPoints,":rtype: ``int``\n:return: Number of datapoints in the object")
+    .def("isExpanded",&escript::Data::isExpanded,":rtype: ``bool``\n:return: True if this ``Data`` is expanded.")
+    .def("isTagged",&escript::Data::isTagged,":rtype: ``bool``\n:return: True if this ``Data`` is expanded.")
+    .def("isConstant",&escript::Data::isConstant,":rtype: ``bool``\n:return: True if this ``Data`` is an instance of ``DataConstant``\n:note: This does not mean the data is immutable.")
+    .def("isLazy",&escript::Data::isLazy,":rtype: ``bool``\n:return: True if this ``Data`` is lazy.")
+    .def("isReady",&escript::Data::isReady,":rtype: ``bool``\n:return: True if this ``Data`` is not lazy.")
+    .def("expand",&escript::Data::expand,"Convert the data to expanded representation if it is not expanded already.")
+    .def("tag",&escript::Data::tag,"Convert data to tagged representation if it is not already tagged or expanded")
+    .def("resolve",&escript::Data::resolve,"Convert the data to non-lazy representation.")
+    .def("copy",&escript::Data::copy,args("other"),"Make this object a copy of ``other``\n"
+"\n:note: The two objects will act independently from now on. That is, changing ``other`` "
+"after this call will not change this object and vice versa.")
+    .def("copy",&escript::Data::copySelf,":note: In the no argument form, a new object will be returned which is an independent copy of this object.")
+    .def("delay",&escript::Data::delay,"Convert this object into lazy representation")
+    .def("setValueOfDataPoint",&escript::Data::setValueOfDataPointToPyObject,args("dataPointNo","value"))
     .def("setValueOfDataPoint",&escript::Data::setValueOfDataPointToArray)
-    .def("setValueOfDataPoint",&escript::Data::setValueOfDataPoint)
-    .def("getTupleForDataPoint",&escript::Data::getValueOfDataPointAsTuple)
-    .def("getTupleForGlobalDataPoint",&escript::Data::getValueOfGlobalDataPointAsTuple)
-    .def("setToZero",&escript::Data::setToZero)
-    .def("interpolate",&escript::Data::interpolate)
+    .def("setValueOfDataPoint",&escript::Data::setValueOfDataPoint,"\nModify the value of a single datapoint.\n\n:param dataPointNo:\n"
+":type dataPointNo: int\n:param value: \n:type value: ``float`` or an object which acts like an array, ``tuple`` or ``list``\n:warning: Use of this operation is discouraged. It prevents some optimisations from operating.")
+    .def("getTupleForDataPoint",&escript::Data::getValueOfDataPointAsTuple,args("dataPointNo"),
+":return: Value of the specified datapoint\n:rtype: ``tuple``\n:param dataPointNo: datapoint to access\n:type dataPointNo: ``int``")
+    .def("getTupleForGlobalDataPoint",&escript::Data::getValueOfGlobalDataPointAsTuple,args("procNo","dataPointNo"),"Get a specific datapoint from a specific process\n\n"
+":rtype: ``tuple``\n:param procNo: MPI rank of the process\n:type procNo: positive ``int``"
+"\n:param dataPointNo: datapoint to access\n:type dataPointNo: int")
+    .def("setToZero",&escript::Data::setToZero,"After this call the object will store values of the same shape as before but all components will be zero.")
+    .def("interpolate",&escript::Data::interpolate,args("functionspace"),"Interpolate this object's values into a new functionspace.")
     .def("minGlobalDataPoint",&escript::Data::minGlobalDataPoint)
     .def("maxGlobalDataPoint",&escript::Data::maxGlobalDataPoint)
-    .def("saveDX",&escript::Data::saveDX)
-    .def("saveVTK",&escript::Data::saveVTK)
-    .def("getTagNumber",&escript::Data::getTagNumber)
+    .def("saveDX",&escript::Data::saveDX,args("fileName"),"Save the object in DX format.\n\n"
+":param fileName:\n:type fileName: ``string``")
+    .def("saveVTK",&escript::Data::saveVTK, args("fileName"),"Save the object in VTK format.\n\n"
+":param fileName:\n:type fileName: ``string``")
+    .def("getTagNumber",&escript::Data::getTagNumber,args("dpno"),"Return tag number for the specified datapoint\n\n:rtype: int\n:param dpno: datapoint number\n:type dpno: int")
     // Unary functions for Data
     .def("_interpolate",&escript::Data::interpolate)
     .def("_grad",&escript::Data::gradOn)
@@ -250,25 +271,25 @@ BOOST_PYTHON_MODULE(escriptcpp)
     .def("_eigenvalues",&escript::Data::eigenvalues)
     .def("_eigenvalues_and_eigenvectors",&escript::Data::eigenvalues_and_eigenvectors,(arg("tol")=1.e-13))
     // functions returning a single real number:
-    .def("_Lsup",&escript::Data::Lsup)
-    .def("_sup",&escript::Data::sup)
-    .def("_inf",&escript::Data::inf)
-    .def("_integrateToTuple",&escript::Data::integrateToTuple)
+    .def("_Lsup",&escript::Data::Lsup,":return: the Lsup-norm of the object\n:rtype: float\n:note: If the ``Data`` contains no values, zero will be returned instead.")
+    .def("_sup",&escript::Data::sup,":return: the maximum value over all data points.\n:rtype: float\n:note: If the ``Data`` contains no values a large negative value will be returned instead.")
+    .def("_inf",&escript::Data::inf,":return: minimum value over all components and all data points\n:rtype: float\n:note: If the ``Data`` contains no values a large positive value will be returned instead.")
+    .def("_integrateToTuple",&escript::Data::integrateToTuple,":return: Calculate the integral over the function space domain as a python tuple\n:rtype: tuple")
 
     // following implements the python abs operator
-    .def("__abs__",&escript::Data::abs)
+    .def("__abs__",&escript::Data::abs,":return: absolute value\n\n:rtype: `Data`")
     // following implements the python "-" negation operator
-    .def("__neg__",&escript::Data::neg)
+    .def("__neg__",&escript::Data::neg, ":return: negation of the values in this object\n:rtype: `Data`")
     // following implements the python "+" identity operator
-    .def("__pos__",&escript::Data::pos)
+    .def("__pos__",&escript::Data::pos, "\nThe unary + operator\n\n:rtype: Data`")
     // following two functions implement the python [] operator
-    .def("__getitem__",&escript::Data::getItem)
-    .def("__setitem__",&escript::Data::setItemO)
-    .def("__setitem__",&escript::Data::setItemD)
+    .def("__getitem__",&escript::Data::getItem,"Used by the python [] operator\n\n:rtype: `Data`")
+    .def("__setitem__",&escript::Data::setItemO,"Used by the python [] operator")
+    .def("__setitem__",&escript::Data::setItemD,"Used by the python [] operator")
     // following two functions implement the python ** operator
-    .def("__pow__",&escript::Data::powO)
+    .def("__pow__",&escript::Data::powO,"Used by the python ** operator\n\n:rtype: `Data`")
     .def("__pow__",&escript::Data::powD)
-    .def("__rpow__",&escript::Data::rpowO)
+    .def("__rpow__",&escript::Data::rpowO,"\nUsed by the python ** operator\n\n:rtype: `Data`")
     // NOTE:: The order of these declarations is important. Anything
     // declared before the generic declaration isn't found so the generic
     // version will be called. 
