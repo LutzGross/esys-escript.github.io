1dd190faad095e8c4f7e43d85202950b4b12d088
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2010-01-28 02:03:15 +0000

    Don't panic. Updating copyright stamps

--- trunk/doc/examples/cookbook/wavesolver2d002.py
+++ trunk/doc/examples/cookbook/wavesolver2d002.py
@@ -1,7 +1,7 @@
 
 ########################################################
 #
-# Copyright (c) 2009 by University of Queensland
+# Copyright (c) 2009-2010 by University of Queensland
 # Earth Systems Science Computational Center (ESSCC)
 # http://www.uq.edu.au/esscc
 #
@@ -11,7 +11,7 @@
 #
 ########################################################
 
-__copyright__="""Copyright (c) 2009 by University of Queensland
+__copyright__="""Copyright (c) 2009-2010 by University of Queensland
 Earth Systems Science Computational Center (ESSCC)
 http://www.uq.edu.au/esscc
 Primary Business: Queensland, Australia"""
