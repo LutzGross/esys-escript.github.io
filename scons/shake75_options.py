2a9872a88ce57e57cf65f31524561858e1f512ee
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2010-03-03 04:02:30 +0000

    Updated service0 & shake75 options to use cxx_extra

--- scons/shake75_options.py
+++ scons/shake75_options.py
@@ -13,8 +13,8 @@
 
 
 # Configuration for shake75 (32-bit Intel Core 2 running Debian Linux)
-cc="gcc-4.4"
-cxx="g++-4.4"
+#cc="gcc-4.4"
+#cxx="g++-4.4"
 # If you cannot use the default compiler flags set in SConstruct, then change them here
 # C/C++ Compiler flags (always use cc_flags and either cc_optim or cc_debug)
 # cc_flags      = '-ansi'
@@ -27,6 +27,7 @@ omp_libs        = []
 
 # Use the default C/C++ flags but add something only for this host:
 cc_extra        = '-Wall -mmmx -msse'
+cxx_extra       = '-Wall -mmmx -msse'
 ld_extra        = '-fopenmp'
 
 # Be picky about errors
