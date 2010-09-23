852266b1d39dc9d967554f38e31d3b9e58ff0b64
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2010-09-23 06:53:36 +0000

    more damage

--- finley/test/python/damage.py
+++ finley/test/python/damage.py
@@ -29,30 +29,60 @@ import sys
 import time
 
 # ======================= Default Values ==================================================
-DIM=2                           # spatial dimension
-H=3.*U.m                     # height
-L=2*H                           # length
-NE_H=30                           # number of elements in H-direction. 
+H=1.*U.m                     # height
+L=1.*H                           # length
+NE_H=5                           # number of elements in H-direction. 
 NE_L=int(ceil(L*NE_H/H))
 
+#Boundary conditions: 
+#	axial loading: they applied a stress inversely proportional to the acoustic emission rate. We could have the axial forcing a stress or velocity inversely proportional to dalpha/dt (only when it is positive, and with the applied forcing rate going to zero when damage accumulation rate goes to a value we can determine in a test run with constant forcing). If this is to challenging or time consuming we could have a constant axial strain rate with very short time steps (at least when alpha increases above 0.3).
+
+#Variables calculated and written to an output file:
+#	time
+#	differential stress (S_33-S_11)
+#	deviatoric stress (S_33 - p)
+#	Axial and transverse strain
+#	damage and damage rate
 
 
-C_V = 3e-11/U.Pa 
-C_D = 5/U.sec
-C_1 = 1e-12/U.sec
-C_2 = 0.03
-XI_0 = -0.8
-LAME_0 = 46e9*U.Pa
-MU_0 = 30e9*U.Pa
-ALPHA_0 = 0
-RHO = 2800*U.kg/U.m**3
-G = 10*U.m/U.sec**2     *0
-DT=1.*U.sec
-VMAX=-0.01*U.m/U.sec
-DT_MAX=0.1*U.sec
 
-T_END=100000*U.yr                       # end time
+if True:
+   C_V = 1.e-5/(U.Mega*U.Pa)
+   C_D = 3/U.sec
+   C_1 = 1e-12/U.sec
+   C_2 = 0.03
+   XI_0 = -0.56
+   LAME_0 = 29e9*U.Pa
+   MU_0 = 19e9*U.Pa
+   ALPHA_0 = 0.0
+   RHO = 2800*U.kg/U.m**3
+   G = 10*U.m/U.sec**2     *0
+   DT=1.*U.sec
+   SIGMA_N=50.*U.Mega*U.Pa
+   DIM=3                          
+   CASE=1
+   VMAX=-0.1*U.m/U.sec
+   DT_MAX=10.*U.sec
 
+else:
+   C_V = 3e-11/U.Pa 
+   C_D = 5/U.sec
+   C_1 = 1e-12/U.sec
+   C_2 = 0.03
+   XI_0 = -0.8
+   LAME_0 = 46e9*U.Pa
+   MU_0 = 30e9*U.Pa
+   ALPHA_0 = 0.1
+   RHO = 2800*U.kg/U.m**3
+   G = 10*U.m/U.sec**2     *0
+   DT=1.*U.sec/100.
+   VMAX=-0.01*U.m/U.sec
+   DT_MAX=10.*U.sec
+   SIGMA_N=0
+   DIM=2                      
+   CASE=0
+
+T_END=15.*U.sec                       # end time
 VERBOSE=True
 DT_VIS=T_END/100                # time distane between two visulaization files
 DN_VIS=5                        # maximum counter increment between two visulaization files
@@ -61,6 +91,8 @@ VIS_DIR="results"               # name of the director for vis files
 ODE_TOL=0.01
 ODE_ITER_TOL=1.e-8
 ODE_ITER_MAX=15
+
+diagnose=FileWriter("diagnose.csv",append=False)
 
 #===================================
 S=0.5*XI_0*((2.*MU_0+3.*LAME_0)/(3.-XI_0**2) + LAME_0)
@@ -113,7 +145,7 @@ x=dom.getX()
 #
 sigma=Tensor(0.,Function(dom))
 eps_e=Tensor(0.,Function(dom))
-alpha=Scalar(0.,Function(dom))
+alpha=Scalar(ALPHA_0,Function(dom))
 u=Vector(0.,ContinuousFunction(dom))
 
 pde=LinearPDESystem(dom)
@@ -124,13 +156,20 @@ pde.getSolverOptions().setSolverMethod(pde.getSolverOptions().DIRECT)
 
 fixed_v_mask=Vector(0,Solution(dom))
 u0=Vector(0.,ContinuousFunction(dom))
-for d in range(DIM):
-    fixed_v_mask+=whereZero(x[d]-BBOX[d][0])*unitVector(d,DIM)
-    if d == 0:
-       fixed_v_mask+=whereZero(x[d]-BBOX[d][1])*unitVector(d,DIM)
-       u0[d]=(x[d]-BBOX[d][0])/(BBOX[d][1]-BBOX[d][0])*VMAX
+if CASE == 1:
+    for d in range(DIM):
+       fixed_v_mask+=whereZero(x[d]-BBOX[d][0])*unitVector(d,DIM)
+       if d == DIM-1:
+          fixed_v_mask+=whereZero(x[d]-BBOX[d][1])*unitVector(d,DIM)
+          u0[d]=(x[d]-BBOX[d][0])/(BBOX[d][1]-BBOX[d][0])*VMAX
+else:
+    for d in range(DIM):
+        fixed_v_mask+=whereZero(x[d]-BBOX[d][0])*unitVector(d,DIM)
+        if d == 0:
+           fixed_v_mask+=whereZero(x[d]-BBOX[d][1])*unitVector(d,DIM)
+           u0[d]=(x[d]-BBOX[d][0])/(BBOX[d][1]-BBOX[d][0])*VMAX
 
-pde.setValue(Y=-G*RHO*kronecker(DIM)[DIM-1], q=fixed_v_mask, r=u0)
+pde.setValue(Y=-G*RHO*kronecker(DIM)[DIM-1], q=fixed_v_mask, r=u0, y=SIGMA_N*dom.getNormal())
 #
 #  let the show begin:
 #
@@ -138,31 +177,31 @@ k3=kronecker(DIM)
 k3Xk3=outer(k3,k3)
 alpha_old=alpha
 dt_old=None
+if CASE == 1:
+   diagnose.write("t,s22-s00, s22-p, e00, e11, alpha, alpha_dot\n")
+else:
+   diagnose.write("t, eps00, eps11, error0, sigma11/tau\n")
 while t<T_END:
 
     print "start time step %d"%(n+1,)
 
     I1=trace(eps_e)
     sqI2=length(eps_e)
+    print "II2 =",sqI2
     xi=safeDiv(I1,sqI2)
     i_xi=safeDiv(sqI2,I1)
-    print "\txi = [ %e, %e]"%(inf(xi),sup(xi))
-
-    if n>1:
-       print" error:",Lsup((2*mu*sqI2-gamma*I1)*eps_e[1,1]-(gamma*sqI2-lame*I1)*sqI2)/Lsup(length(sigma)*sqI2)
-       print" error:",Lsup(sigma[1,1])/Lsup(length(sigma))
-    
     # update damage parameter:
     m=wherePositive(xi-XI_0)
     a=sqI2**2*(xi-XI_0)*(m*C_D + (1-m)* C_1)
     b=(1-m)*(1./C_2)
 
     alpha, alpha_old, alpha_oold =solveODE(alpha, a,b, dt), alpha, alpha_old
+    alpha_dot=(alpha-alpha_old)/dt
 
 
     # step size for the next time step:
 
-    print "\talpha = [ %e, %e]"%(inf(alpha),sup(alpha))
+    print "\tXXX alpha = [ %e, %e]"%(inf(alpha),sup(alpha))
     if sup(alpha)  >1:
         raise ValueError,"damage parameter > 1 detected."
 
@@ -174,10 +213,28 @@ while t<T_END:
     mu_eff=mu-gamma*xi
     print "\tmu_eff = [ %e, %e]"%(inf(mu_eff),sup(mu_eff))
     print "\tlame_eff = [ %e, %e]"%(inf(lame_eff),sup(lame_eff))
+
+    if CASE == 1:
+      diagnose.write(("%e,"*7+"\n")%(t,meanValue(sigma[2,2]-sigma[0,0]), meanValue(deviatoric(sigma)[2,2]), meanValue(eps_e[0,0]), meanValue(eps_e[1,1]),
+                    meanValue(alpha), meanValue(alpha_dot)))
+    else:
+       if n>1:
+          print "\t xi = [ %e, %e]"%(inf(xi),sup(xi))
+          print "\t eps00 = ", eps_e[0,0]
+          print "\t eps10 = ", eps_e[1,0]
+          print "\t eps01 = ", eps_e[0,1]
+          print "\t eps11 = ", eps_e[1,1]
+          print "\t eps11/eps00 = ", eps_e[1,1]/eps_e[0,0]
+          print" error:",Lsup(((2*mu-gamma*safeDiv(I1,sqI2))*eps_e[1,1]-(gamma*sqI2-lame*I1)) )/Lsup(length(sigma))
+          print" error:",Lsup(sigma[1,1])/Lsup(length(sigma))
+          diagnose.write("%e, %e, %e, %e, %e\n"%(t,inf(eps_e[0,0]),inf(eps_e[1,1]), Lsup(((2*mu-gamma*safeDiv(I1,sqI2))*eps_e[1,1]-(gamma*sqI2-lame*I1)) )/Lsup(length(sigma)), Lsup(sigma[1,1])/Lsup(length(sigma))))
+
     if inf(mu_eff) <= 0: 
         raise ValueError,"Material failed due to non-positive mu_eff."
+    if inf(lame_eff) <= 0: 
+        raise ValueError,"Material failed due to non-positive lame_eff."
 
-    i_eta = clip(2*C_V*(alpha-alpha_old)/dt, minval=0.)
+    i_eta = clip(2*C_V*alpha_dot,minval=0.)
 
     # update strain:
 
