e46eace03b29abd15ee7ca2dabddaf21996bbbcf
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2011-03-15 23:52:18 +0000

    Added sympy-based evaluator that uses lambdify to resolve expressions.

---
+++ branches/symbolic_from_3470/escript/py_src/symbolic/__init__.py
@@ -0,0 +1,35 @@
+
+########################################################
+#
+# Copyright (c) 2003-2010 by University of Queensland
+# Earth Systems Science Computational Center (ESSCC)
+# http://www.uq.edu.au/esscc
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Open Software License version 3.0
+# http://www.opensource.org/licenses/osl-3.0.php
+#
+########################################################
+
+__copyright__="""Copyright (c) 2003-2010 by University of Queensland
+Earth Systems Science Computational Center (ESSCC)
+http://www.uq.edu.au/esscc
+Primary Business: Queensland, Australia"""
+__license__="""Licensed under the Open Software License version 3.0
+http://www.opensource.org/licenses/osl-3.0.php"""
+__url__="https://launchpad.net/escript-finley"
+
+from sympy import *
+from evaluator import *
+from functions import *
+
+# prefer escript's implementation of functions such as 'sign' etc.
+from sympy.utilities.lambdify import MODULES
+ESCRIPT_NAMESPACE = {}
+ESCRIPT_TRANSLATIONS = {
+    #"ln":"log",
+}
+MODULES['escript']=(ESCRIPT_NAMESPACE, ESCRIPT_TRANSLATIONS,('from esys.escript import *',))
+
+#
+# vim: expandtab shiftwidth=4:
