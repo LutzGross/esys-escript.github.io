623825e102d6696e98f2477190937d7c5a409511
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2011-03-30 06:13:34 +0000

    Added basic tests for added symbolic functions.

--- branches/symbolic_from_3470/escript/test/python/run_symbolic.py
+++ branches/symbolic_from_3470/escript/test/python/run_symbolic.py
@@ -65,7 +65,7 @@ class SymbolicTestCase(unittest.TestCase):
         self.assertEqual(e(x=3), (9,27,81,243), "wrong result after __call__")
 
         xx=RandomData((4,), FunctionSpace())
-        ref=[x.toListOfTuples() for x in (xx**2, xx**3, xx**4, xx**5)]
+        ref=[d.toListOfTuples() for d in (xx**2, xx**3, xx**4, xx**5)]
         res=e(x=xx)
         for d in res:
             self.assertTrue(isinstance(d, Data), "substituted expression not a Data object")
@@ -73,6 +73,93 @@ class SymbolicTestCase(unittest.TestCase):
         self.assertEqual(res, ref, "wrong result after substitution with Data object")
 
     #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    def test_Functions(self):
+        import esys.escript as es
+        x=Symbol('x')
+        xx=RandomData((4,4), FunctionSpace())
+        TOL_DIGITS=8
+
+        y=wherePositive(x)
+        ref=es.wherePositive(xx).toListOfTuples()
+        res=Evaluator(y)(x=xx)[0].toListOfTuples()
+        self.assertEqual(res, ref, "wrong result for wherePositive()")
+
+        y=whereNonPositive(x)
+        ref=es.whereNonPositive(xx).toListOfTuples()
+        res=Evaluator(y)(x=xx)[0].toListOfTuples()
+        self.assertEqual(res, ref, "wrong result for whereNonPositive()")
+
+        y=whereNegative(x)
+        ref=es.whereNegative(xx).toListOfTuples()
+        res=Evaluator(y)(x=xx)[0].toListOfTuples()
+        self.assertEqual(res, ref, "wrong result for whereNegative()")
+
+        y=whereNonNegative(x)
+        ref=es.whereNonNegative(xx).toListOfTuples()
+        res=Evaluator(y)(x=xx)[0].toListOfTuples()
+        self.assertEqual(res, ref, "wrong result for whereNonNegative()")
+
+        y=whereZero(x)
+        ref=es.whereZero(xx).toListOfTuples()
+        res=Evaluator(y)(x=xx)[0].toListOfTuples()
+        self.assertEqual(res, ref, "wrong result for whereZero()")
+
+        y=whereNonZero(x)
+        ref=es.whereNonZero(xx).toListOfTuples()
+        res=Evaluator(y)(x=xx)[0].toListOfTuples()
+        self.assertEqual(res, ref, "wrong result for whereNonZero()")
+
+        y=log10(x)
+        ref=es.log10(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for log10()")
+
+        y=inverse(x)
+        ref=es.inverse(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for inverse()")
+
+        y=minval(x)
+        ref=es.minval(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for minval()")
+
+        y=maxval(x)
+        ref=es.maxval(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for maxval()")
+
+        y=trace(x)
+        ref=es.trace(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for trace()")
+
+        y=transpose(x)
+        ref=es.transpose(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for transpose()")
+
+        y=symmetric(x)
+        ref=es.symmetric(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for symmetric()")
+
+        y=nonsymmetric(x)
+        ref=es.nonsymmetric(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for nonsymmetric()")
+
+        y=swap_axes(x)
+        ref=es.swap_axes(xx)
+        res=Evaluator(y)(x=xx)[0]
+        self.assertAlmostEqual(es.Lsup(res-ref), 0.0, TOL_DIGITS, "wrong result for swap_axes()")
+
+        #y=grad(x)
+        #ref=es.grad(xx).toListOfTuples()
+        #res=Evaluator(y)(x=xx)[0].toListOfTuples()
+        #self.assertEqual(res, ref, "wrong result for grad()")
+
 
 if __name__ == "__main__":
     suite = unittest.TestSuite()
