8661db978d5f95ec232af7cde7d46c246e260ba0
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2011-10-21 04:00:40 +0000

    Partially works but Is splitting the wrong cells in the committed configuration. (Look along Z axis)

--- refine/src/OctTree.h
+++ refine/src/OctTree.h
@@ -14,7 +14,7 @@ public:
     OctTree(double x, double y, double z);		// specifies the dimensions of bounding box
     ~OctTree();
     void allSplit(unsigned int d);
-    void splitPoint(double x, double y, double z);
+    void splitPoint(double x, double y, double z, unsigned desdepth);
     void walkLeaves(cellfunct c, void* v);
 private:
     double side[3];
