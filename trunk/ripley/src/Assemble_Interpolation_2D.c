57ba0872df4d74240d55c996a187928402af4895
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2011-10-27 02:16:12 +0000

    more work on the assemblage

--- trunk/ripley/src/Assemble_Interpolation_2D.c
+++ trunk/ripley/src/Assemble_Interpolation_2D.c
@@ -12,10 +12,10 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
       for (k1 =0; k1 < N1; ++k1) {
          for (k0 =0; k0 < N0; ++k0) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_10 = in[INDEX2(i,INDEX2(k0+1,k1, M0),NCOMP)];
-               register const double f_11 = in[INDEX2(i,INDEX2(k0+1,k1+1, M0),NCOMP)];
-               register const double f_01 = in[INDEX2(i,INDEX2(k0,k1+1, M0),NCOMP)];
-               register const double f_00 = in[INDEX2(i,INDEX2(k0,k1, M0),NCOMP)];
+               const register double f_10 = in[INDEX2(i,INDEX2(k0+1,k1, M0),NCOMP)];
+               const register double f_11 = in[INDEX2(i,INDEX2(k0+1,k1+1, M0),NCOMP)];
+               const register double f_01 = in[INDEX2(i,INDEX2(k0,k1+1, M0),NCOMP)];
+               const register double f_00 = in[INDEX2(i,INDEX2(k0,k1, M0),NCOMP)];
                out[INDEX3(i,0,INDEX2(k0,k1,N0),NCOMP,9)] = f_00*tmp0_2 + f_11*tmp0_1 + tmp0_0*(f_01 + f_10);
                out[INDEX3(i,1,INDEX2(k0,k1,N0),NCOMP,9)] = tmp0_3*(f_00 + f_10) + tmp0_4*(f_01 + f_11);
                out[INDEX3(i,2,INDEX2(k0,k1,N0),NCOMP,9)] = f_01*tmp0_1 + f_10*tmp0_2 + tmp0_0*(f_00 + f_11);
@@ -34,10 +34,10 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
       for (k1 =0; k1 < N1; ++k1) {
          for (k0 =0; k0 < N0; ++k0) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_01 = in[INDEX2(i,INDEX2(k0,k1+1, M0),NCOMP)];
-               register const double f_10 = in[INDEX2(i,INDEX2(k0+1,k1, M0),NCOMP)];
-               register const double f_00 = in[INDEX2(i,INDEX2(k0,k1, M0),NCOMP)];
-               register const double f_11 = in[INDEX2(i,INDEX2(k0+1,k1+1, M0),NCOMP)];
+               const register double f_01 = in[INDEX2(i,INDEX2(k0,k1+1, M0),NCOMP)];
+               const register double f_10 = in[INDEX2(i,INDEX2(k0+1,k1, M0),NCOMP)];
+               const register double f_00 = in[INDEX2(i,INDEX2(k0,k1, M0),NCOMP)];
+               const register double f_11 = in[INDEX2(i,INDEX2(k0+1,k1+1, M0),NCOMP)];
                out[INDEX3(i,0,INDEX2(k0,k1,N0),NCOMP,1)] = tmp0_0*(f_00 + f_01 + f_10 + f_11);
             } /* close component loop i */
          } /* close k0 loop */
@@ -50,8 +50,8 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
          #pragma omp parallel for private(i,k1)
          for (k1 =0; k1 < N1; ++k1) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_01 = in[INDEX2(i,INDEX2(0,k1+1, M0),NCOMP)];
-               register const double f_00 = in[INDEX2(i,INDEX2(0,k1, M0),NCOMP)];
+               const register double f_01 = in[INDEX2(i,INDEX2(0,k1+1, M0),NCOMP)];
+               const register double f_00 = in[INDEX2(i,INDEX2(0,k1, M0),NCOMP)];
                out[INDEX3(i,0,face_offset(0)+INDEX2(k0,k1,N0),NCOMP,3)] = f_00*tmp0_1 + f_01*tmp0_0;
                out[INDEX3(i,1,face_offset(0)+INDEX2(k0,k1,N0),NCOMP,3)] = tmp0_2*(f_00 + f_01);
                out[INDEX3(i,2,face_offset(0)+INDEX2(k0,k1,N0),NCOMP,3)] = f_00*tmp0_0 + f_01*tmp0_1;
@@ -65,8 +65,8 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
          #pragma omp parallel for private(i,k1)
          for (k1 =0; k1 < N1; ++k1) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_10 = in[INDEX2(i,INDEX2(M0-1,k1, M0),NCOMP)];
-               register const double f_11 = in[INDEX2(i,INDEX2(M0-1,k1+1, M0),NCOMP)];
+               const register double f_10 = in[INDEX2(i,INDEX2(M0-1,k1, M0),NCOMP)];
+               const register double f_11 = in[INDEX2(i,INDEX2(M0-1,k1+1, M0),NCOMP)];
                out[INDEX3(i,0,face_offset(1)+INDEX2(k0,k1,N0),NCOMP,3)] = f_10*tmp0_0 + f_11*tmp0_1;
                out[INDEX3(i,1,face_offset(1)+INDEX2(k0,k1,N0),NCOMP,3)] = tmp0_2*(f_10 + f_11);
                out[INDEX3(i,2,face_offset(1)+INDEX2(k0,k1,N0),NCOMP,3)] = f_10*tmp0_1 + f_11*tmp0_0;
@@ -80,8 +80,8 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
          #pragma omp parallel for private(i,k0)
          for (k0 =0; k0 < N0; ++k0) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_10 = in[INDEX2(i,INDEX2(k0+1,0, M0),NCOMP)];
-               register const double f_00 = in[INDEX2(i,INDEX2(k0,0, M0),NCOMP)];
+               const register double f_10 = in[INDEX2(i,INDEX2(k0+1,0, M0),NCOMP)];
+               const register double f_00 = in[INDEX2(i,INDEX2(k0,0, M0),NCOMP)];
                out[INDEX3(i,0,face_offset(2)+INDEX2(k0,k1,N0),NCOMP,3)] = f_00*tmp0_1 + f_10*tmp0_0;
                out[INDEX3(i,1,face_offset(2)+INDEX2(k0,k1,N0),NCOMP,3)] = tmp0_2*(f_00 + f_10);
                out[INDEX3(i,2,face_offset(2)+INDEX2(k0,k1,N0),NCOMP,3)] = f_00*tmp0_0 + f_10*tmp0_1;
@@ -95,8 +95,8 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
          #pragma omp parallel for private(i,k0)
          for (k0 =0; k0 < N0; ++k0) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_11 = in[INDEX2(i,INDEX2(k0+1,M1-1, M0),NCOMP)];
-               register const double f_01 = in[INDEX2(i,INDEX2(k0,M1-1, M0),NCOMP)];
+               const register double f_11 = in[INDEX2(i,INDEX2(k0+1,M1-1, M0),NCOMP)];
+               const register double f_01 = in[INDEX2(i,INDEX2(k0,M1-1, M0),NCOMP)];
                out[INDEX3(i,0,face_offset(3)+INDEX2(k0,k1,N0),NCOMP,3)] = f_01*tmp0_1 + f_11*tmp0_0;
                out[INDEX3(i,1,face_offset(3)+INDEX2(k0,k1,N0),NCOMP,3)] = tmp0_2*(f_01 + f_11);
                out[INDEX3(i,2,face_offset(3)+INDEX2(k0,k1,N0),NCOMP,3)] = f_01*tmp0_0 + f_11*tmp0_1;
@@ -109,8 +109,8 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
          #pragma omp parallel for private(i,k1)
          for (k1 =0; k1 < N1; ++k1) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_00 = in[INDEX2(i,INDEX2(0,k1, M0),NCOMP)];
-               register const double f_01 = in[INDEX2(i,INDEX2(0,k1+1, M0),NCOMP)];
+               const register double f_00 = in[INDEX2(i,INDEX2(0,k1, M0),NCOMP)];
+               const register double f_01 = in[INDEX2(i,INDEX2(0,k1+1, M0),NCOMP)];
                out[INDEX3(i,0,face_offset(0)+INDEX2(k0,k1,N0),NCOMP,1)] = tmp0_0*(f_00 + f_01);
             } /* close component loop i */
          } /* close k1 loop */
@@ -120,8 +120,8 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
          #pragma omp parallel for private(i,k1)
          for (k1 =0; k1 < N1; ++k1) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_10 = in[INDEX2(i,INDEX2(M0-1,k1, M0),NCOMP)];
-               register const double f_11 = in[INDEX2(i,INDEX2(M0-1,k1+1, M0),NCOMP)];
+               const register double f_10 = in[INDEX2(i,INDEX2(M0-1,k1, M0),NCOMP)];
+               const register double f_11 = in[INDEX2(i,INDEX2(M0-1,k1+1, M0),NCOMP)];
                out[INDEX3(i,0,face_offset(1)+INDEX2(k0,k1,N0),NCOMP,1)] = tmp0_0*(f_10 + f_11);
             } /* close component loop i */
          } /* close k1 loop */
@@ -131,8 +131,8 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
          #pragma omp parallel for private(i,k0)
          for (k0 =0; k0 < N0; ++k0) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_10 = in[INDEX2(i,INDEX2(k0+1,0, M0),NCOMP)];
-               register const double f_00 = in[INDEX2(i,INDEX2(k0,0, M0),NCOMP)];
+               const register double f_10 = in[INDEX2(i,INDEX2(k0+1,0, M0),NCOMP)];
+               const register double f_00 = in[INDEX2(i,INDEX2(k0,0, M0),NCOMP)];
                out[INDEX3(i,0,face_offset(2)+INDEX2(k0,k1,N0),NCOMP,1)] = tmp0_0*(f_00 + f_10);
             } /* close component loop i */
          } /* close k0 loop */
@@ -142,8 +142,8 @@ Ripley_Assemble_Interpolation_2D(Ripley_Grid *grid, Escript in, Escript out)
          #pragma omp parallel for private(i,k0)
          for (k0 =0; k0 < N0; ++k0) {
             for (i =0; i < NCOMP; ++i) {
-               register const double f_01 = in[INDEX2(i,INDEX2(k0,M1-1, M0),NCOMP)];
-               register const double f_11 = in[INDEX2(i,INDEX2(k0+1,M1-1, M0),NCOMP)];
+               const register double f_01 = in[INDEX2(i,INDEX2(k0,M1-1, M0),NCOMP)];
+               const register double f_11 = in[INDEX2(i,INDEX2(k0+1,M1-1, M0),NCOMP)];
                out[INDEX3(i,0,face_offset(3)+INDEX2(k0,k1,N0),NCOMP,1)] = tmp0_0*(f_01 + f_11);
             } /* close component loop i */
          } /* close k0 loop */
