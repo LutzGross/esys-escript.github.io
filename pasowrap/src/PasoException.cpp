6727359e1b343511069e8f33e38d7478f72b68fc
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2011-11-15 00:13:56 +0000

    Initial add for moving files

---
+++ pasowrap/src/PasoException.cpp
@@ -0,0 +1,43 @@
+
+/*******************************************************
+*
+* Copyright (c) 2003-2010 by University of Queensland
+* Earth Systems Science Computational Center (ESSCC)
+* http://www.uq.edu.au/esscc
+*
+* Primary Business: Queensland, Australia
+* Licensed under the Open Software License version 3.0
+* http://www.opensource.org/licenses/osl-3.0.php
+*
+*******************************************************/
+
+
+#include "DudleyAdapterException.h"
+
+
+using namespace dudley;
+
+
+const std::string 
+DudleyAdapterException::exceptionNameValue("DudleyAdapterException");
+
+
+const std::string &
+DudleyAdapterException::exceptionName() const
+{
+  return exceptionNameValue;
+}
+
+
+void checkPasoError() 
+{
+  if (Esys_noError()) {
+    return;
+  } else {
+    //
+    // reset the error code to no error otherwise the next call to
+    // this function may resurrect a previous error
+    Esys_resetError();
+    throw PasoException(Esys_getErrorMessage());
+  }
+}
