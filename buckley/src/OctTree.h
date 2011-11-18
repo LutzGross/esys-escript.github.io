b68c947538f740d40caefbf48e8b295d2c9a4fe5
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2011-11-18 04:48:53 +0000

    It has memory errors during a getX.

--- buckley/src/OctTree.h
+++ buckley/src/OctTree.h
@@ -17,15 +17,23 @@ public:
     void allCollapse(unsigned int d);
     void collapsePoint(double x, double y, double z, unsigned int d)    ;
     void splitPoint(double x, double y, double z, unsigned desdepth);
-    void walkLeaves(cellfunct c, void* v);
-    void walkWithKids(cellfunct2 c, void* v);
-    void assignIDs();
-    unsigned leafCount();	// inefficient, don't use too much
+    void walkLeaves(const_cellfn c, void* v) const;
+    void walkLeaves(cellfn c, void* v);
+    void walkWithKids(const_cellfn2 c, void* v);
+    void assignIDs() const;
+    unsigned leafCountByWalk() const;	// inefficient, don't use too much
+    unsigned leafCount() const;
+    
+    const OctCell** process() const;	// tree mutations are complete for now
     
     void debug();
 private:
+
     double side[3];
     OctCell p;
+    bool modified;
+    mutable size_t leafcount;
+    friend class OctCell;
 };
 
 
