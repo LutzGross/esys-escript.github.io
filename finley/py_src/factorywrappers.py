84bf40d42af814f52007b1c8329569902afbbdeb
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2012-06-08 01:20:09 +0000

    Only switch on useElementsOnFace  by default where we actually support it

--- finley/py_src/factorywrappers.py
+++ finley/py_src/factorywrappers.py
@@ -24,15 +24,21 @@ from .finleycpp import __Brick_driver, __Rectangle_driver
 
 
 def Rectangle(n0=1, n1=1, order=1, l0=1.0, l1=1.0, periodic0=False, periodic1=False, integrationOrder=-1, 
-      reducedIntegrationOrder=-1, useElementsOnFace=1, useFullElementOrder=0, optimize=0, **kwargs):
+      reducedIntegrationOrder=-1, useElementsOnFace=None, useFullElementOrder=0, optimize=0, **kwargs):
     points=[]
     tags=[]
     if 'diracPoints' in kwargs:
         points=kwargs['diracPoints']
     if 'diracTags' in kwargs:
         tags=kwargs['diracTags']
+    faceon=useElementsOnFace
+    if useElementsOnFace is None:	#We want to use 1 as the default, but only where it makes sense
+        if useFullElementOrder or order==-1:
+	    faceon=0	#Don't use it
+	else:
+	    faceon=1
     return __Rectangle_driver([n0, n1, order, l0, l1, periodic0, periodic1, integrationOrder, 
-      reducedIntegrationOrder, useElementsOnFace, useFullElementOrder, optimize, points, tags])
+      reducedIntegrationOrder, faceon, useFullElementOrder, optimize, points, tags])
 
 def Brick(n0=1, n1=1, n2=1, order=1, l0=1.0, l1=1.0, l2=1.0, periodic0=0, periodic1=0, periodic2=0,
     integrationOrder=-1, reducedIntegrationOrder=-1, useElementsOnFace=1, useFullElementOrder=0,
@@ -43,6 +49,12 @@ def Brick(n0=1, n1=1, n2=1, order=1, l0=1.0, l1=1.0, l2=1.0, periodic0=0, period
         points=kwargs['diracPoints']
     if 'diracTags' in kwargs:
         tags=kwargs['diracTags']
+    faceon=useElementsOnFace
+    if useElementsOnFace is None:	#We want to use 1 as the default, but only where it makes sense
+        if useFullElementOrder or order==-1:
+	    faceon=0	#Don't use it
+	else:
+	    faceon=1        
     return __Brick_driver([n0, n1, n2, order, l0, l1, l2, periodic0,  periodic1, periodic2,
-    integrationOrder, reducedIntegrationOrder, useElementsOnFace, useFullElementOrder,
+    integrationOrder, reducedIntegrationOrder, faceon, useFullElementOrder,
     optimize, points, tags])
