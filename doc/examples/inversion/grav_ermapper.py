7379598240140fc1aac263ee4449444ac7e6c2d8
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2013-03-12 04:26:24 +0000

    gracefully end if pyproj is not installed

--- doc/examples/inversion/grav_ermapper.py
+++ doc/examples/inversion/grav_ermapper.py
@@ -38,32 +38,43 @@ l_air = 6. * U.km
 n_cells_v = 25
 MU = 0.1
 
-# Setup and run the inversion
-source=ErMapperData(DataSource.GRAVITY, DATASET, scale_factor=DATA_UNITS)
-db=DomainBuilder(dim=3)
-db.addSource(source)
-db.setVerticalExtents(depth=thickness, air_layer=l_air, num_cells=n_cells_v)
-db.setFractionalPadding(pad_x=PAD_X, pad_y=PAD_Y)
-db.fixDensityBelow(depth=thickness)
+def work():
+  # Setup and run the inversion
+  source=ErMapperData(DataSource.GRAVITY, DATASET, scale_factor=DATA_UNITS)
+  db=DomainBuilder(dim=3)
+  db.addSource(source)
+  db.setVerticalExtents(depth=thickness, air_layer=l_air, num_cells=n_cells_v)
+  db.setFractionalPadding(pad_x=PAD_X, pad_y=PAD_Y)
+  db.fixDensityBelow(depth=thickness)
 
-inv=GravityInversion()
-inv.setSolverTolerance(1e-4)
-inv.setSolverMaxIterations(50)
-inv.setup(db)
-inv.getCostFunction().setTradeOffFactorsModels(MU)
+  inv=GravityInversion()
+  inv.setSolverTolerance(1e-4)
+  inv.setSolverMaxIterations(50)
+  inv.setup(db)
+  inv.getCostFunction().setTradeOffFactorsModels(MU)
 
-density = inv.run()
-print("density = %s"%density)
+  density = inv.run()
+  print("density = %s"%density)
 
-g, w =  db.getGravitySurveys()[0]
-saveSilo("result0.silo", density=density, gravity_anomaly=g, gravity_weight=w)
-print("Results saved in result0.silo")
+  g, w =  db.getGravitySurveys()[0]
+  saveSilo("result0.silo", density=density, gravity_anomaly=g, gravity_weight=w)
+  print("Results saved in result0.silo")
 
-saveVTK("result0.vtu", density=density, gravity_anomaly=g, gravity_weight=w)
-print("Results saved in result0.vtu")
+  saveVTK("result0.vtu", density=density, gravity_anomaly=g, gravity_weight=w)
+  print("Results saved in result0.vtu")
 
-saveDataCSV("result0.csv", density=density, x=density.getFunctionSpace().getX())
-print("Results saved in result0.csv")
+  saveDataCSV("result0.csv", density=density, x=density.getFunctionSpace().getX())
+  print("Results saved in result0.csv")
 
-print("All done. Have a nice day.!")
+  print("All done. Have a nice day.!")
 
+try:
+  import pyproj
+  havepyproj=True
+except ImportError:
+  havepyproj=False
+
+if havepyproj:
+  work()
+else:
+  print("This example requires the pyproj package which does not appear to be accessible.")
