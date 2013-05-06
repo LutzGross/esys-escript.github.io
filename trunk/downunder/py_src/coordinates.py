b70bda8713d0508fb74beba98f2f17b468b14181
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2013-05-06 01:52:05 +0000

    retab coordinates.py

--- trunk/downunder/py_src/coordinates.py
+++ trunk/downunder/py_src/coordinates.py
@@ -22,16 +22,18 @@ __license__="""Licensed under the Open Software License version 3.0
 http://www.opensource.org/licenses/osl-3.0.php"""
 __url__="https://launchpad.net/escript-finley"
 
-__all__ = [ 'ReferenceSystem', 'CartesianReferenceSystem', 'GeodeticReferenceSystem', 'SphericalReferenceSystem',
-             'WGS84ReferenceSystem', 'GRS80ReferenceSystem', 'SpatialCoordinateTransformation',
-             'GeodeticCoordinateTransformation', 'CartesianCoordinateTransformation', 'makeTranformation']
+__all__ = ['ReferenceSystem', 'CartesianReferenceSystem',
+    'GeodeticReferenceSystem', 'SphericalReferenceSystem',
+    'WGS84ReferenceSystem', 'GRS80ReferenceSystem',
+    'SpatialCoordinateTransformation', 'GeodeticCoordinateTransformation',
+    'CartesianCoordinateTransformation', 'makeTranformation']
 
 from esys.escript import unitsSI as U
 from esys.escript import *
 
 class ReferenceSystem(object):
     """
-    generic identifier for coordinate systems.
+    Generic identifier for coordinate systems.
     """
     def __init__(self,name='none'):
         """
@@ -44,7 +46,7 @@ class ReferenceSystem(object):
 
     def getName(self):
         """
-        return name of the reference system
+        returns the name of the reference system
         """
         return self.__name
 
@@ -89,16 +91,16 @@ class ReferenceSystem(object):
         raise NotImplementedError()
 
 class CartesianReferenceSystem(ReferenceSystem):
-      """
-      Identifies the Cartesian coordinate system
-      """
-      def __init__(self, name="CARTESIAN"):
+    """
+    Identifies the Cartesian coordinate system
+    """
+    def __init__(self, name="CARTESIAN"):
         """
         set up Cartesian coordinate system
         """
         super(CartesianReferenceSystem, self).__init__(name)
 
-      def isTheSame(self, other):
+    def isTheSame(self, other):
         """
         test if argument ``other`` defines the same reference system
 
@@ -106,11 +108,12 @@ class CartesianReferenceSystem(ReferenceSystem):
         :type other: `ReferenceSystem`
         :returns: ``True`` if ``other`` is a `CartesianReferenceSystem` instance.
         :rtype: ``bool``
-        :note: every two `CartesianReferenceSystem` instances considered being the same.
+        :note: every two `CartesianReferenceSystem` instances are considered
+               as being the same.
         """
         return isinstance(other, CartesianReferenceSystem)
 
-      def createTransformation(self, domain):
+    def createTransformation(self, domain):
         """
         creates an appropriate coordinate transformation on a given domain
 
@@ -119,8 +122,8 @@ class CartesianReferenceSystem(ReferenceSystem):
         :rtype: `SpatialCoordinateTransformation`
         """
         return SpatialCoordinateTransformation(domain, reference=self)
-        
-      def isCartesian(self):
+      
+    def isCartesian(self):
         """
         returns if the reference system is Cartesian
         :rtype: ``bool``
@@ -128,66 +131,68 @@ class CartesianReferenceSystem(ReferenceSystem):
         return True
       
 class GeodeticReferenceSystem(ReferenceSystem):
-      """
-      Identifies a Geodetic coordinate system
-      """
-      def __init__(self, a=6378137.0 *U.m, f=1/298.257223563, angular_unit=1.*U.DEG, name="WGS84"):
-           """
-           initializes a geodetic reference system
+    """
+    Identifies a Geodetic coordinate system
+    """
+    def __init__(self, a=6378137.0 *U.m, f=1/298.257223563, angular_unit=1.*U.DEG, name="WGS84"):
+        """
+        initializes a geodetic reference system
 
-           :param a: semi-major axis in meter
-           :type a: positive ``double``
-           :param f: flattening
-           :type f: non-negative ``double``, less than one
-           :param name: name of the reference system
-           :type name: ``str``
-           :param angular_unit: factor to scale the unit of latitude and longitude to radians.
-           :type  angular_unit: positive ``double``
-           """
-           if not a>0:
-               raise ValueError("length of semi-major axis a must be positive.")
-           if not ( f>=0 and f<1 ):
-               raise ValueError("flattening f must be non-negative and less than one.")
-           if not angular_unit>0:
-               raise ValueError("angular_unit  must be positive.")
-           super(GeodeticReferenceSystem, self).__init__(name)
-           self.__a=a
-           self.__f=f
-           self.__angular_unit=angular_unit
-      def isCartesian(self):
+        :param a: semi-major axis in meter
+        :type a: positive ``double``
+        :param f: flattening
+        :type f: non-negative ``double``, less than one
+        :param name: name of the reference system
+        :type name: ``str``
+        :param angular_unit: factor to scale the unit of latitude and
+                             longitude to radians.
+        :type  angular_unit: positive ``double``
+        """
+        if not a>0:
+            raise ValueError("length of semi-major axis a must be positive.")
+        if not ( f>=0 and f<1 ):
+            raise ValueError("flattening f must be non-negative and less than one.")
+        if not angular_unit > 0:
+            raise ValueError("angular_unit must be positive.")
+        super(GeodeticReferenceSystem, self).__init__(name)
+        self.__a=a
+        self.__f=f
+        self.__angular_unit=angular_unit
+
+    def isCartesian(self):
         """
         returns if the reference system is Cartesian
         :rtype: ``bool``
         """
         return False
-        
-      def getAngularUnit(self):
-           """
-           returns the angular unit
-           """
-           return self.__angular_unit
+      
+    def getAngularUnit(self):
+        """
+        returns the angular unit
+        """
+        return self.__angular_unit
 
-      def getSemiMajorAxis(self):
-           """
-           returns the length of semi major axis
-           """
-           return self.__a
+    def getSemiMajorAxis(self):
+        """
+        returns the length of semi major axis
+        """
+        return self.__a
 
-      def getSemiMinorAxis(self):
-           """
-           returns the length of semi minor axis
-           """
-           a=self.getSemiMajorAxis()
-           f=self.getFlattening()
-           return a*(1-f)
+    def getSemiMinorAxis(self):
+        """
+        returns the length of semi minor axis
+        """
+        a=self.getSemiMajorAxis()
+        f=self.getFlattening()
+        return a*(1-f)
 
-      def getFlattening(self):
-           """
-           returns the flattening
-           """
-           return self.__f
+    def getFlattening(self):
+        """
+        returns the flattening
+        """
+        return self.__f
 
-      def isTheSame(self, other):
+    def isTheSame(self, other):
         """
         test if ``other`` defines the same reference system
 
@@ -201,170 +206,169 @@ class GeodeticReferenceSystem(ReferenceSystem):
         """
         if isinstance(other, GeodeticReferenceSystem):
             if self.getSemiMajorAxis() == other.getSemiMajorAxis() \
-                        and self.getFlattening() == other.getFlattening() \
-                        and self.getAngularUnit() == other.getAngularUnit():
+                      and self.getFlattening() == other.getFlattening() \
+                      and self.getAngularUnit() == other.getAngularUnit():
                 return True
             else:
                 return False
         else:
-             return False
+            return False
 
-      def createTransformation(self, domain):
-          """
-          creates an appropriate coordinate transformation on a given domain
+    def createTransformation(self, domain):
+        """
+        creates an appropriate coordinate transformation on a given domain
 
-          :param domain: domain of transformation
-          :type domain:`esys.escript.AbstractDomain'
-          :rtype: `SpatialCoordinateTransformation`
-          """
-          return GeodeticCoordinateTransformation(domain, reference=self)
+        :param domain: domain of transformation
+        :type domain:`esys.escript.AbstractDomain'
+        :rtype: `SpatialCoordinateTransformation`
+        """
+        return GeodeticCoordinateTransformation(domain, reference=self)
 
 def SphericalReferenceSystem(R=6378137.0*U.m):
-     """
-     returns the `GeodeticReferenceSystem` of a sphere
-     :param R: sphere radius
-     :type R: positive ``double``
-     """
-     return GeodeticReferenceSystem( a=R, f=0, angular_unit=1*U.DEG, name="SPHERE")
+    """
+    returns the `GeodeticReferenceSystem` of a sphere
+    :param R: sphere radius
+    :type R: positive ``double``
+    """
+    return GeodeticReferenceSystem(a=R, f=0, angular_unit=1*U.DEG, name="SPHERE")
 
 def WGS84ReferenceSystem():
-     """
-     returns the `GeodeticReferenceSystem` for the WGS84 Ellipsoid
-     """
-     return GeodeticReferenceSystem( a=6378137.0 *U.m, f=1/298.257223563, angular_unit=1*U.DEG, name="WGS84")
+    """
+    returns the `GeodeticReferenceSystem` for the WGS84 Ellipsoid
+    """
+    return GeodeticReferenceSystem(a=6378137.0 *U.m, f=1/298.257223563, angular_unit=1*U.DEG, name="WGS84")
 
 def GRS80ReferenceSystem():
-     """
-     returns the `GeodeticReferenceSystem` for the GRS80 Ellipsoid eg. used by Geocentric Datum of Australia GDA94 
-     """
-     return GeodeticReferenceSystem( a=6378137.0 *U.m, f=1/298.257222101, angular_unit=1*U.DEG, name="GRS80")
+    """
+    returns the `GeodeticReferenceSystem` for the GRS80 Ellipsoid eg. used by Geocentric Datum of Australia GDA94 
+    """
+    return GeodeticReferenceSystem(a=6378137.0 *U.m, f=1/298.257222101, angular_unit=1*U.DEG, name="GRS80")
 
 
 class SpatialCoordinateTransformation(object):
-     """
-     Defines an orthogonal coordinate transformation from a domain into the
-     Cartesian domain using a coordinate transformation.
+    """
+    Defines an orthogonal coordinate transformation from a domain into the
+    Cartesian domain using a coordinate transformation.
 
-     The default implementation is the identity transformation (i.e.
-     no changes are applied to the domain). Overwrite the appropriate
-     methods to define other coordinate systems.
-     """
-     def __init__(self, domain, reference=CartesianReferenceSystem()):
-         """
-         set up the orthogonal coordinate transformation.
+    The default implementation is the identity transformation (i.e.
+    no changes are applied to the domain). Overwrite the appropriate
+    methods to define other coordinate systems.
+    """
+    def __init__(self, domain, reference=CartesianReferenceSystem()):
+        """
+        set up the orthogonal coordinate transformation.
 
-         :param domain: domain in the domain of the coordinate transformation
-         :type domain: `esys.escript.AbstractDomain'
-         :param reference: the reference system
-         :type reference: `ReferenceSystem`
-         """
-         self.__domain = domain
-         self.__reference_system=reference
-         self._volumefactor=Scalar(1., Function(domain))
-         self._scaling_factors = Vector(1., Function(domain))
-     def __eq__(self, other):
-          return self.isTheSame(other)
+        :param domain: domain in the domain of the coordinate transformation
+        :type domain: `esys.escript.AbstractDomain'
+        :param reference: the reference system
+        :type reference: `ReferenceSystem`
+        """
+        self.__domain = domain
+        self.__reference_system=reference
+        self._volumefactor=Scalar(1., Function(domain))
+        self._scaling_factors = Vector(1., Function(domain))
 
-     def __ne__(self, other):
-          return not self.isTheSame(other)
+    def __eq__(self, other):
+         return self.isTheSame(other)
 
-     def isTheSame(self, other):
-          """
-          test if argument ``other`` defines the same coordinate transformation
+    def __ne__(self, other):
+         return not self.isTheSame(other)
 
-          :param other: a second coordinate transformation
-          :type other: `SpatialCoordinateTransformation`
-          :returns: ``True`` if other defines then same coordinate transformation
-          :rtype: ``bool``
-          """
-          if isinstance(other, SpatialCoordinateTransformation):
-                if     self.getDomain() ==  other.getDomain() \
-                  and  self.getReferenceSystem() ==  other.getReferenceSystem() :
-                      return True
-                else:
-                      return False
-          else:
-              return False
+    def isTheSame(self, other):
+        """
+        test if argument ``other`` defines the same coordinate transformation
 
-     def isCartesian(self):
-         """
-         returns ``True`` if the scaling factors (and the volume factor) are equal to 1
-         :rtype: ``bool``
-         """
-         return self.__reference_system.isCartesian()
+        :param other: a second coordinate transformation
+        :type other: `SpatialCoordinateTransformation`
+        :returns: ``True`` if other defines then same coordinate transformation
+        :rtype: ``bool``
+        """
+        if isinstance(other, SpatialCoordinateTransformation):
+            if self.getDomain() == other.getDomain() \
+                    and self.getReferenceSystem() == other.getReferenceSystem():
+                return True
 
-     def getDomain(self):
-         """
-         returns the domain of the coordinate transformation.
-         :rtype: `esys.escript.AbstractDomain'
-         """
-         return self.__domain
+        return False
 
-     def getReferenceSystem(self):
-         """
-         returns the reference system used to to define the coordinate transformation
-         :rtype: `ReferenceSystem`
-         """
-         return self.__reference_system
+    def isCartesian(self):
+        """
+        returns ``True`` if the scaling factors (and the volume factor) are equal to 1
+        :rtype: ``bool``
+        """
+        return self.__reference_system.isCartesian()
 
-     def getVolumeFactor(self):
-         """
-         returns the volume factor for the coordinate transformation
-         :rtype:  `esys.escript.Scalar'
-         """
-         return self._volumefactor
+    def getDomain(self):
+        """
+        returns the domain of the coordinate transformation.
+        :rtype: `esys.escript.AbstractDomain'
+        """
+        return self.__domain
 
-     def getScalingFactors(self):
-         """
-         returns the scaling factors
-         :rtype: `esys.escript.Vector'
-         """
-         return self._scaling_factors
+    def getReferenceSystem(self):
+        """
+        returns the reference system used to to define the coordinate transformation
+        :rtype: `ReferenceSystem`
+        """
+        return self.__reference_system
+
+    def getVolumeFactor(self):
+        """
+        returns the volume factor for the coordinate transformation
+        :rtype:  `esys.escript.Scalar'
+        """
+        return self._volumefactor
+
+    def getScalingFactors(self):
+        """
+        returns the scaling factors
+        :rtype: `esys.escript.Vector'
+        """
+        return self._scaling_factors
 
 def CartesianCoordinateTransformation(domain, reference=CartesianReferenceSystem() ):
 
-     return SpatialCoordinateTransformation(domain, reference)
+    return SpatialCoordinateTransformation(domain, reference)
 
 class GeodeticCoordinateTransformation(SpatialCoordinateTransformation):
-     """
-     A geodetic coordinate transformation
-     """
-     def __init__(self, domain, reference=WGS84ReferenceSystem() ):
-         """
-         set up the orthogonal coordinate transformation.
+    """
+    A geodetic coordinate transformation
+    """
+    def __init__(self, domain, reference=WGS84ReferenceSystem() ):
+        """
+        set up the orthogonal coordinate transformation.
 
-         :param domain: domain in the domain of the coordinate transformation
-         :type domain: `esys.escript.AbstractDomain'
-         :param reference: the reference system
-         :type reference: `ReferenceSystem`
+        :param domain: domain in the domain of the coordinate transformation
+        :type domain: `esys.escript.AbstractDomain'
+        :param reference: the reference system
+        :type reference: `ReferenceSystem`
 
-         """
-         DIM=domain.getDim()
-         super(GeodeticCoordinateTransformation, self).__init__(domain, reference )
+        """
+        DIM=domain.getDim()
+        super(GeodeticCoordinateTransformation, self).__init__(domain, reference )
 
-         a=reference.getSemiMajorAxis()
-         f=reference.getFlattening()
+        a=reference.getSemiMajorAxis()
+        f=reference.getFlattening()
 
-         x=Function(domain).getX()
-         phi=x[0] * reference.getAngularUnit()
-         h=x[DIM-1]
+        x=Function(domain).getX()
+        phi=x[0] * reference.getAngularUnit()
+        h=x[DIM-1]
 
-         e = sqrt(2*f-f**2)
-         N = a/sqrt(1 - e**2 * sin(phi)**2 )
-         M = ( a*(1-e**2) ) /sqrt(1 - e**2 * sin(phi)**2 )**3
-         v_phi = (M + h)
-         v_lam = (N + h) * cos(phi)
-         s= Vector(1., Function(domain)) 
-         if DIM == 2:
-             v= v_phi
-             s=Vector(1., Function(domain)) 
-             s[0]=1/v_phi
-         else:
-             v= v_phi * v_lam
-             s[0]=1/v_phi
-             s[1]=1/v_lam
-         self._volumefactor=v
-         self._scaling_factors = s
+        e = sqrt(2*f-f**2)
+        N = a/sqrt(1 - e**2 * sin(phi)**2 )
+        M = ( a*(1-e**2) ) /sqrt(1 - e**2 * sin(phi)**2 )**3
+        v_phi = (M + h)
+        v_lam = (N + h) * cos(phi)
+        s= Vector(1., Function(domain)) 
+        if DIM == 2:
+            v= v_phi
+            s=Vector(1., Function(domain)) 
+            s[0]=1/v_phi
+        else:
+            v= v_phi * v_lam
+            s[0]=1/v_phi
+            s[1]=1/v_lam
+        self._volumefactor=v
+        self._scaling_factors = s
 
 def makeTranformation(domain, coordinates=None):
     """
@@ -375,17 +379,18 @@ def makeTranformation(domain, coordinates=None):
     :param coordinates: the reference system or spatial coordinate system.
     :type coordinates: `ReferenceSystem` or `SpatialCoordinateTransformation`
     :return: the spatial coordinate system for the given domain of the specified 
-                   reference system ``coordinates``. If ``coordinates`` is already spatial coordinate system based on the 
-                   riven domain ``coordinates`` is returned. Otherwise an appropriate spatial coordinate system 
-               is created.
+             reference system ``coordinates``. If ``coordinates`` is already spatial coordinate system based on the 
+             riven domain ``coordinates`` is returned. Otherwise an appropriate spatial coordinate system 
+             is created.
     :rtype: `SpatialCoordinateTransformation` 
     """
     if coordinates == None:
-         return CartesianCoordinateTransformation(domain)
+        return CartesianCoordinateTransformation(domain)
     elif isinstance(coordinates, ReferenceSystem):
-         return coordinates.createTransformation(domain)
+        return coordinates.createTransformation(domain)
     else:
-         if not coordinates.getDomain() ==  domain:
-	      raise ValueError("Domain of spatial coordinate system and given domain don't match.")
-	 else:
-	      return coordinates
+        if not coordinates.getDomain() == domain:
+            raise ValueError("Domain of spatial coordinate system and given domain don't match.")
+        else:
+            return coordinates
+
