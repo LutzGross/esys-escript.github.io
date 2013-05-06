00f66845633699573782770c5abdc80c5a4ace33
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2013-05-06 02:15:48 +0000

    Do not claim success when saving silo without silo support.

--- trunk/doc/examples/inversion/gravmag_netcdf.py
+++ trunk/doc/examples/inversion/gravmag_netcdf.py
@@ -72,8 +72,10 @@ def work():
 
   g, wg = db.getGravitySurveys()[0]
   B, wB = db.getMagneticSurveys()[0]
-  saveSilo("result_gravmag.silo", density=density, gravity_anomaly=g, gravity_weight=wg, susceptibility=susceptibility, magnetic_anomaly=B,   magnetic_weight=wB)
-  print("Results saved in result_gravmag.silo")
+  if saveSilo("result_gravmag.silo", density=density, gravity_anomaly=g, gravity_weight=wg, susceptibility=susceptibility, magnetic_anomaly=B,   magnetic_weight=wB):
+      print("Results saved in result_gravmag.silo")
+  else:
+      print("Failed to save result_gravmag.silo. Possibly no Silo support.")
 
   saveVTK("result_gravmag.vtu", density=density, gravity_anomaly=g, gravity_weight=wg, susceptibility=susceptibility, magnetic_anomaly=B,   magnetic_weight=wB)
   print("Results saved in result_gravmag.vtu")
