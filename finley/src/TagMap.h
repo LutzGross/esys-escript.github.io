cf9b8a68e7ab14c44a45e165532ae46cd804c155
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2013-05-27 04:48:31 +0000

    SVN server test.

--- finley/src/TagMap.h
+++ finley/src/TagMap.h
@@ -14,27 +14,30 @@
 *****************************************************************************/
 
 
-/************************************************************************************/
+/****************************************************************************
 
-/* Finley: simple linked list to provide clear names for tag keys */
+  Finley: simple linked list to provide clear names for tag keys
 
-/************************************************************************************/
+*****************************************************************************/
 
 #ifndef INC_FINLEY_TAGMAP
 #define INC_FINLEY_TAGMAP
 
 #include "Finley.h"
 
-
 typedef struct Finley_TagMap {
-  char* name;
-  index_t tag_key;
-  struct Finley_TagMap *next;
+    char* name;
+    index_t tag_key;
+    struct Finley_TagMap *next;
 } Finley_TagMap;
 
-void Finley_TagMap_insert(Finley_TagMap**,const char* name, index_t tag_key);
-index_t Finley_TagMap_getTag(Finley_TagMap*,const char* name);
-bool_t Finley_TagMap_isValidTagName(Finley_TagMap*,const char* name);
+void Finley_TagMap_insert(Finley_TagMap**, const char* name, index_t tag_key);
+
+index_t Finley_TagMap_getTag(Finley_TagMap*, const char* name);
+
+bool_t Finley_TagMap_isValidTagName(Finley_TagMap*, const char* name);
+
 void Finley_TagMap_free(Finley_TagMap*);
-#endif /* #ifndef INC_FINLEY_TAGMAP */
+
+#endif // INC_FINLEY_TAGMAP
 
