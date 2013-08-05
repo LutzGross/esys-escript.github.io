b6be22d6f60439cc8cdcc1bc672138fe59600bae
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2013-08-05 00:57:34 +0000

    Dropping VSL random. Random data now uses more general routine.

---
+++ trunk/esysUtils/src/EsysRandom.h
@@ -0,0 +1,24 @@
+/*****************************************************************************
+*
+* Copyright (c) 2013 by University of Queensland
+* http://www.uq.edu.au
+*
+* Primary Business: Queensland, Australia
+* Licensed under the Open Software License version 3.0
+* http://www.opensource.org/licenses/osl-3.0.php
+*
+* Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+* Development since 2012 by School of Earth Sciences
+*
+*****************************************************************************/
+
+
+#ifndef ESYS_RANDOM_H
+#define ESYS_RANDOM_H
+
+/* \brief put n random doubles in array (uses OpenMP).
+   If using this on Data, then be sure to CHECK_EX_WRITE first
+*/
+void randomFillArray(long seed, double* array, size_t n);
+
+#endif
