3c67af0944c696b072a8ba6eaa4f2794d18d5bad
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2013-11-28 00:37:48 +0000

    segy writer added

---
+++ trunk/downunder/py_src/seismic.py
@@ -0,0 +1,163 @@
+
+##############################################################################
+#
+# Copyright (c) 2003-2013 by University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Open Software License version 3.0
+# http://www.opensource.org/licenses/osl-3.0.php
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development since 2012 by School of Earth Sciences
+#
+##############################################################################
+
+__copyright__="""Copyright (c) 2003-2013 by University of Queensland
+http://www.uq.edu.au
+Primary Business: Queensland, Australia"""
+__license__="""Licensed under the Open Software License version 3.0
+http://www.opensource.org/licenses/osl-3.0.php"""
+__url__="https://launchpad.net/escript-finley"
+
+__all__ = ['SimpleSEGYWriter']
+
+
+import numpy as np
+import sys
+import time
+from esys.escript import *
+import esys.escript.unitsSI as U
+
+
+class SimpleSEGYWriter(object):
+	"""
+	as simple writer for 2D and 3D seimic lines in particular for synthetic data
+
+	Typical usage:
+
+    	   from esys.escript import unitsSI as U
+	   sw=SimpleSEGYWriter([0.,100*U.m,200*U,m,300.], source=200*U.m, sampling_interval=4*U.msec)
+	   while n < 10:
+	        sw.addRecord([i*2., i*0.67, i**2, -i*7])
+	   sw.write('example.segy')
+
+	:note: the writer uses `obsy`
+	"""
+	COORDINATE_SCALE = 1000.
+	def __init__(self, receiver_group=None, source=0., sampling_interval=4*U.msec, text="some seimic data"):
+		"""
+		initalize writer
+
+		:param receiver_group: list of receiver coordinates (in meters). For the 2D case a list of floats is given, for the 3D case a list of coordinate tuples
+		                       are given
+		:param source: coordinates of the source (in meters).  For the 2D case a single floats is given, for the 3D case a coordinate tuples
+		:param sampling_interval: sample rate in seconds
+		:param text: a text for the header file (e.g a description)
+		"""
+		if isinstance(source, float) or  isinstance(source, int) :
+			DIM=1
+			source = (source, 0.)
+		elif hasattr(source, '__len__'):
+			DIM=len(source)
+			if DIM == 1:
+			    source = (source[0], 0.)
+		        elif DIM==2:
+			    source = (source[0], source[1] )
+			else:
+		            raise ValueError("Only 1D or 2D arrays accepted.")
+	        else:
+		    raise TypeError("illegal source type.")
+	        if receiver_group== None: 
+			if DIM == 1:
+				receiver_group=[0.]
+			else:
+				receiver_group=[(0.,0.)]
+
+		if isinstance(receiver_group, float) or  isinstance(receiver_group, int) :
+			if DIM == 1:
+			    rg = [ (receiver_group, 0. ) ]
+			else:
+		            raise TypeError("illegal receiver_group type.")
+		elif hasattr(receiver_group, '__len__'):
+			if DIM == 1:
+			    rg = [(c,0)  for c in receiver_group]
+			else:
+			    rg = [(c[0],c[1])  for c in receiver_group]
+	        else:
+		    raise TypeError("illegal receiver_group type.")
+                            
+		self.__source=source
+		self.__receiver_group=rg
+		self.__text=text
+                self.__trace = [ [] for i in self.__receiver_group ]
+                self.__sampling_interval = sampling_interval   
+
+	def addRecord(self, record):
+	       """
+	       adds a record to the traces. a time difference of sample_interval between two records is assumed. 
+	       The record mast be a list of as many values as given receivers or a float if a single receiver is used.
+
+               :param record: list of tracks to be added to the record. 
+	       """
+	       if not len(self.__receiver_group) == len(record): 
+	                 raise ValueError("expected number of records is %s but %s given."%(len(self.__receiver_group), len(record)))
+	       if len(self.__receiver_group) == 1:
+		       if isinstance(self.__receiver_group, float) or isinstance(self.__receiver_group, int):
+			         self.__trace[0].append(record)
+		       else:
+			         self.__trace[0].append(record[0])
+	       else:
+		       for i in xrange(len(record)):
+			       self.__trace[i].append(record[i])
+
+	def getSamplingInterval(self):
+	       """
+	       returns the sampling interval in seconds. 
+	       """
+               return self.__sampling_interval
+
+	def write(self, filename):
+            """
+            writes to segy file 
+            
+            :param filename: file name
+            :note: the function uses the `obspy` module. 
+            """
+            from obspy import Trace, Stream, UTCDateTime
+	    from obspy.segy.segy import SEGYTraceHeader, SEGYBinaryFileHeader
+            from obspy.core import AttribDict
+
+	    stream=Stream()
+	    
+	    for i in xrange(len(self.__receiver_group)):
+		    trace = Trace(data=np.array(self.__trace[i], dtype='float32'))
+                    # Attributes in trace.stats will overwrite everything in
+                    # trace.stats.segy.trace_header (in Hz)
+                    trace.stats.sampling_rate = 1./self.getSamplingInterval()
+                    #trace.stats.starttime = UTCDateTime(2011,11,11,0,0,0)
+                    if not hasattr(trace.stats, 'segy.trace_header'):
+                        trace.stats.segy = {}
+                    trace.stats.segy.trace_header = SEGYTraceHeader()
+                    trace.stats.segy.trace_header.trace_identification_code=1
+                    trace.stats.segy.trace_header.trace_sequence_number_within_line = i + 1
+                    trace.stats.segy.trace_header.scalar_to_be_applied_to_all_coordinates = int(self.COORDINATE_SCALE)
+                    trace.stats.segy.trace_header.coordinate_units=1
+                    trace.stats.segy.trace_header.source_coordinate_x=int(self.__source[0] * self.COORDINATE_SCALE)
+                    trace.stats.segy.trace_header.source_coordinate_y=int(self.__source[1] * self.COORDINATE_SCALE)
+                    trace.stats.segy.trace_header.group_coordinate_x=int(self.__receiver_group[i][0] * self.COORDINATE_SCALE)
+                    trace.stats.segy.trace_header.group_coordinate_y=int(self.__receiver_group[i][1] * self.COORDINATE_SCALE)
+
+                    # Add trace to stream
+                    stream.append(trace)
+            # A SEGY file has file wide headers. This can be attached to the stream
+            # object.  If these are not set, they will be autocreated with default
+            # values.
+            stream.stats = AttribDict()
+            stream.stats.textual_file_header = 'C.. '+self.__text+'\nC.. with esys.escript.downunder r%s at %s'%(getVersion(),time.asctime())
+            stream.stats.binary_file_header = SEGYBinaryFileHeader()
+
+            if getMPIRankWorld()<1:
+                stream.write(filename, format="SEGY", data_encoding=1,byteorder=sys.byteorder)
+            
+
