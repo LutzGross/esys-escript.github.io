5fb6c2ed7a1f5e6ac29ed9f35a98440197d82f77
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2013-11-29 07:44:37 +0000

    some modifications

--- trunk/doc/examples/inversion/synthetic_sonic.py
+++ trunk/doc/examples/inversion/synthetic_sonic.py
@@ -27,10 +27,8 @@ from esys.downunder import Ricker, SonicWave, SimpleSEGYWriter
 from math import ceil
 
 
-DIM=2           # spatial dimension
+DIM=2          # spatial dimension
 
-width_x=2*U.km  # area of the domain
-width_y=2*U.km
 depth=1*U.km    # depth 
 
 v_p_top=1.5*U.km/U.sec
@@ -39,35 +37,42 @@ reflector_at=0.5*depth
 
 ne_z=400
 
-t_end=0.6*U.sec
+t_end=1.0*U.sec
 
 frq=20*U.Hz
 sampling_interval=4*U.msec
+if DIM == 2:
+   rcv_num=100
+else:
+   rcv_num=30
 
+rcv_range=600*U.m
 
+absorption_zone=300*U.m
+
+width_x=rcv_range + 4*absorption_zone
+width_y=width_x
 #
 # create array 
 #
-grid=[ width_x/2 -300*U.m, width_x/2 -200*U.m, width_x/2 -100*U.m, width_x/2, width_x/2 + 100*U.m, width_x/2 + 200*U.m, width_x/2 + 300*U.m ]
+grid=[2*absorption_zone + i * (rcv_range/rcv_num) for i in xrange(rcv_num) ]
+
 array_points=[]
 array_tags=[]
 rg=[]
-if DIM == 2: 
-   src_tag='phone_3'
-   src=grid[len(grid)/2]
+if DIM == 2:
+   src_id=rcv_num/2
    for ix in xrange(len(grid)):
-        array_points.append([grid[ix], depth])
-        array_tags.append('phone_%s'%(ix,))
-        rg.append(grid[ix]) 
+        array_points.append((grid[ix], depth))
+        array_tags.append('phone_%3.3d'%(ix,))
+        rg.append( ( grid[ix], 0.) ) 
 else:
-   src_tag='phone_33'
-   src=( grid[len(grid)/2], grid[len(grid)/2])
+   src_id= (rcv_num/2) * rcv_num + (rcv_num/2) 
    for ix in xrange(len(grid)):
       for iy in xrange(len(grid)):
-           array_points.append([grid[ix], grid[iy], depth])
-           array_tags.append('phone_%s%s'%(ix,iy))
+           array_points.append((grid[ix], grid[iy], depth))
+           array_tags.append('phone_%3.3d-%3.3d'%(ix,iy))
            rg.append( (grid[ix], grid[iy]) )
-
 #
 # create domain:
 #
@@ -78,14 +83,16 @@ else:
    domain=Brick(ceil(ne_z*width_x/depth),ceil(ne_z*width_y/depth),ne_z,l0=width_x,l1=width_y,l2=depth, 
 		diracPoints=array_points, diracTags=array_tags)
 wl=Ricker(frq)
-m=whereNegative(domain.getX()[DIM-1]-reflector_at)
+m=whereNegative(Function(domain).getX()[DIM-1]-reflector_at)
 v_p=v_p_bottom*m+v_p_top*(1-m)
 
-sw=SonicWave(domain, v_p, source_tag=src_tag, wavelet=wl, absorption_zone=300*U.m)
+sw=SonicWave(domain, v_p, source_tag=array_tags[src_id], wavelet=wl, absorption_zone=absorption_zone, lumping=True)
 
 loc=Locator(domain,array_points)
 
-tracer=SimpleSEGYWriter(receiver_group=rg, source=src, sampling_interval=sampling_interval)
+print src_id
+
+tracer=SimpleSEGYWriter(receiver_group=rg, source=array_points[src_id], sampling_interval=sampling_interval)
 
 t=0.
 mkDir('tmp')
@@ -95,7 +102,7 @@ while t < t_end:
 	rec=loc(p)
 	tracer.addRecord(rec)
 	print t, rec[:4], wl.getValue(t)
-	saveSilo("tmp/u_%d.silo"%(n,), p=p)
+	if n%5 == 0 : saveSilo("tmp/u_%d.silo"%(n/5,), p=p)
 	n+=1
 tracer.write('line.sgy')
 
