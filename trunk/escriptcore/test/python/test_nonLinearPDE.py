f6a3a7d8eb94e6ef6514f8c9068824c29301e613
jduplessis <jduplessis@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-02-19 09:31:45 +0000

    Added test when adding incorrect Dim And Shape, for tryin to set Unknown Perameter and a test to make sure the definition of y is correct

--- trunk/escriptcore/test/python/test_nonLinearPDE.py
+++ trunk/escriptcore/test/python/test_nonLinearPDE.py
@@ -32,6 +32,10 @@ __author__="Jaco du Plessis"
 from esys.escript import NonlinearPDE, Symbol, whereZero, grad, sin, cos, symmetric, matrixmult, FunctionOnBoundary
 import numpy
 import unittest
+from esys.escript.linearPDEs import IllegalCoefficient,IllegalCoefficientValue
+import numpy as np
+from esys.escript.pdetools import Locator
+
 
 class Test_nonLinearPDEs(unittest.TestCase):
     DEBUG=False
@@ -135,3 +139,72 @@ class Test_nlpde(Test_nonLinearPDEs):
             self.assertTrue(numpy.ndarray.__eq__(BTest, B).all())
             self.assertTrue(numpy.ndarray.__eq__(CTest, C).all())
             self.assertTrue(numpy.ndarray.__eq__(DTest, D).all())
+
+    def test_DimAndShape1eq(self):
+        dim=self.domain.getDim()
+        if dim==3:
+            u = Symbol('u', dim=2)
+        else:
+            u = Symbol('u', dim=3)
+        nlpde = NonlinearPDE(self.domain, u, debug=0)
+        args=dict(X=grad(u), Y=5*u)
+        self.assertRaises(IllegalCoefficientValue, nlpde.setValue,**args)
+        u = Symbol('u', dim=dim)
+        args=dict(X=u, Y=5*u)
+        self.assertRaises(IllegalCoefficientValue, nlpde.setValue,**args)
+        args=dict(X=grad(u), Y=5*grad(u))
+        self.assertRaises(IllegalCoefficientValue, nlpde.setValue,**args)
+        args=dict(q=u)
+        self.assertRaises(IllegalCoefficientValue, nlpde.setValue,**args)
+    
+    def test_DimAndShape2eq(self):
+        dim=self.domain.getDim()
+        u = Symbol('u',(2,), dim=dim)
+        nlpde = NonlinearPDE(self.domain, u, debug=0)
+        args=dict(X=grad(u), Y=5*u[0])
+        self.assertRaises(IllegalCoefficientValue, nlpde.setValue,**args)
+        args=dict(X=grad(u[0]), Y=5*u)
+        self.assertRaises(IllegalCoefficientValue, nlpde.setValue,**args)
+
+
+    def test_setUnknownPeram(self):
+        dim=self.domain.getDim()
+        u = Symbol('u',(2,), dim=dim)
+        nlpde = NonlinearPDE(self.domain, u, debug=0)
+        args=dict(k=0,f=8)  
+        self.assertRaises(IllegalCoefficient,nlpde.setValue,**args)
+
+
+    def test_yDirection:
+
+        u = Symbol('u',(2,), dim=2)
+        q = Symbol('q', (2,2))
+        theta = Symbol('theta')
+        theta=3.141/6
+        q[0,0]=cos(theta)
+        q[0,1]=-sin(theta)
+        q[1,0]=sin(theta)
+        q[1,1]=cos(theta)
+        sigma = Symbol('sigma',(2,2))
+        p = NonlinearPDE(self.domain, u, debug=0)
+        epsilon0 = symmetric(grad(u))
+        epsilon = matrixmult(matrixmult(q,epsilon0),q.transpose(1))
+        c00=10;c01=8;c05=0
+        c01=8;c11=10;c15=0
+        c05=0;c15=0;c55=1
+        sigma[0,0]=c00*epsilon[0,0]+c01*epsilon[1,1]+c05*2*epsilon[1,0]
+        sigma[1,1]=c01*epsilon[0,0]+c11*epsilon[1,1]+c15*2*epsilon[1,0]
+        sigma[0,1]=c05*epsilon[0,0]+c15*epsilon[1,1]+c55*2*epsilon[1,0]
+        sigma[1,0]=sigma[0,1]
+        sigma0=matrixmult(ixmult(q.transpose(1),epsilon),q)
+        x = mydomain.getX()
+        gammaD=whereZero(x[1])*[1,1]#+whereZero(x[0])*[1,0]+whereZero(x[0]-1)*[1,0]  
+        yconstraint = FunctionOnBoundary(mydomain).getX()[1]
+        p.setValue(X=sigma0,q=gammaD,y=[-50,0]*whereZero(yconstraint-1),r=[1,1])
+        v = p.getSolution(u=[0,0])
+        x=np.ndarray((2,))
+        x[0]=0.5
+        x[1]=0.5
+        loc=Locator(v.getFunctionSpace(),x)
+        valAtX=loc(v)
+        self.assertTrue(valAtX[0]>10*valAtX[1])
