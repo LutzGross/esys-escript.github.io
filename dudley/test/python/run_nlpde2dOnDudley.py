8db97e7454d0b87add852c1892ac40c04efccc6e
jduplessis <jduplessis@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-02-21 03:07:09 +0000

    Modified domain in tests

--- dudley/test/python/run_nlpde2dOnDudley.py
+++ dudley/test/python/run_nlpde2dOnDudley.py
@@ -43,7 +43,7 @@ import sys
 
 class Test_nonLinearPDE(Test_nlpde):
    def setUp(self):
-        self.domain = Rectangle(l0=10.,l1=10.,n0=10, n1=10) 
+        self.domain = Rectangle(l0=1.,l1=1.,n0=10, n1=10) 
    def tearDown(self):
         del self.domain
 
--- dudley/test/python/run_nlpde3dOnDudley.py
+++ dudley/test/python/run_nlpde3dOnDudley.py
@@ -43,7 +43,7 @@ import sys
 
 class Test_nonLinearPDE(Test_nlpde):
    def setUp(self):
-        self.domain = Brick(l0=10.,l1=10.,l2=10.,n0=10, n1=10,n2=10) 
+        self.domain = Brick(l0=1.,l1=1.,l2=1.,n0=10, n1=10,n2=10) 
    def tearDown(self):
         del self.domain
 
