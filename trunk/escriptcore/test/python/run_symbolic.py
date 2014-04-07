8ae2e5d6c64f6d73ff950db59b74325cd74cf1fd
jduplessis <jduplessis@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-04-07 05:59:24 +0000

    added test for evalf

--- trunk/escriptcore/test/python/run_symbolic.py
+++ trunk/escriptcore/test/python/run_symbolic.py
@@ -86,6 +86,20 @@ class SymbolicTestCase(unittest.TestCase):
         for i in range(len(ref)):
             self.assertTrue(isinstance(res[i], numpy.ndarray), "substituted expression not a numpy array object")
             self.assertAlmostEqual(Lsup(res[i]-ref[i]), 0.0, self.TOL_DIGITS, "wrong result after substitution with Data object")
+        u = Symbol('u')
+        p=2*u**2+3*u+1
+        p2=sin(u)
+        p3 = p.diff(u)
+        evalu = Evaluator()
+        evalu.addExpression(p)
+        evalu.addExpression(p2)
+        evalu.addExpression(p3)
+        evalu.subs(u=2*symconstants.pi)
+        evaluated=evalu.evaluate(evalf=True)
+        sumEvaluated=sum(evaluated)
+        self.assertAlmostEqual(numpy.float64(sumEvaluated),126.939132358972,self.TOL_DIGITS,"evalf not working properly")
+
+
 
     #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     def test_acos_Symbol_rank0(self):
