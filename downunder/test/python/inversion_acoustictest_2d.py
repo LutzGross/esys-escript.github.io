e99ba563f03ca9ac944a285f8f0d9b1d5dd857bf
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-04-09 06:17:09 +0000

    FWI is running

--- downunder/test/python/inversion_acoustictest_2d.py
+++ downunder/test/python/inversion_acoustictest_2d.py
@@ -14,7 +14,11 @@
 #
 ##############################################################################
 
-"""this a very simple test for inversion of acoustic data in the freqency domain"""
+"""this a very simple test for inversion of acoustic data in the freqency domain
+
+
+
+"""
 
 __copyright__="""Copyright (c) 2003-2014 by University of Queensland
 http://www.uq.edu.au
@@ -42,8 +46,8 @@ logger.setLevel(logging.INFO)
 # frequence range to be used for the inversion
 OMEGA_MIN=1*U.Hz
 OMEGA_MAX=10*U.Hz
-N_OMEGA=3
-# these are the taeget P-velocity and Q index
+N_OMEGA=1
+# these are the target P-velocity and Q index
 V_P=2.*U.km
 Q=10
 # these are the reference values to be used
@@ -67,10 +71,18 @@ domainbuilder.setVerticalExtents(depth=DEPTH, air_layer=0., num_cells=NE_Z)
 # generate list of omegas:
 omegas=np.linspace(OMEGA_MIN, OMEGA_MAX, num=N_OMEGA, endpoint=True)
 # create data:
+#
+#   - laplace u - o**2*SIGMA * u = 1. 
+# 
+#    -> u=1./o**2/SIGMA = data
+#
+#    to test the scaling an extra factor S is introduced.
+#
 SIGMA=1./( V_P * complex(1., -1./(2.*Q)))**2
+SIGMA_prior=1./( V_P_prior * complex(1., -1./(2.*Q_prior)))**2
 S={}
 for o in omegas:
-    S[o]=complex(sqrt(o), sin(o))
+    S[o]=complex(sqrt(o)**2, sin(o))*0+1
     
     DD=-1./(SIGMA*o**2)*S[o]
     data=np.ones((NE_X+1,))*DD
@@ -90,8 +102,9 @@ DIM=dom.getDim()
   
   # create regularization with two level set functions:
 z=Solution(dom).getX()[DIM-1]
+x=Solution(dom).getX()[0]
 reg_mask=Data(0.,(2,), Solution(dom))
-reg_mask[0] = whereZero(z+inf(z))        # mask for locations where velocity and Q index are assumed to be known
+reg_mask[0] = whereZero(z-inf(z))        # mask for locations where velocity and Q index are assumed to be known
 reg_mask[1] = reg_mask[0]  
 
 regularization=Regularization(dom, numLevelSets=2,
@@ -129,69 +142,39 @@ for ds in domainbuilder.getTags():
     logger.debug("power for frequency %s Hz for source at %s added."%(ds.getFrequency, ds.getLocation()))
     forwardmodels.append((acw, 0))
 
-m=AcousticVelocityMapping(V_P_prior, Q_prior)
+vm=AcousticVelocityMapping(V_P, Q)
+# 
+
+z=Solution(dom).getX()[DIM-1]
+x=Solution(dom).getX()[0]
+z_bar=(z-inf(z))/(sup(z)-inf(z))
+x_bar=(x-inf(x))/(sup(x)-inf(x))
+s0=((SIGMA_prior.real-SIGMA.real)*z_bar+SIGMA.real)+SIGMA.real*0.1*sin((z_bar*x_bar)*15)
+s1=((SIGMA_prior.imag-SIGMA.imag)*z_bar+SIGMA.imag)+SIGMA.imag*0.1*sin((z_bar*x_bar)*15)
+m0=vm.getInverse(s0*[1,0]+s1*[0,1])
+print "m0=",m0[0]
+print "m1=",m0[1]
+print "s0=",s0
+print "s1=",s1
+print "SIGMA=",SIGMA
 # finally we can set up the cost function:
 cf=InversionCostFunction(regularization,
-             (m,) , tuple(forwardmodels) )
+             (vm,) , tuple(forwardmodels) )
+cf.setTradeOffFactorsRegularization(mu=[1e-1,1e-1], mu_c=None)
              
 # and then start running the show:
 solver=MinimizerLBFGS()
 solver.setCostFunction(cf)
-solver.setTolerance(1e-4)
-solver.setMaxIterations(50)
-solver.run(Data(0.,(2,),Solution(dom)))
+solver.setTolerance(1e-6)
+solver.setMaxIterations(180)
+solver.run(m0)
 m=solver.getResult()
 sigma = cf.getProperties(m)
   
-print sigma
+print "m0=",m[0]
+print "m1=",m[1]
+print "s0=",sigma[0]
+print "s1=",sigma[1]
+print "SIGMA=",SIGMA
 
-
-
-
-1/0
-#=============================================================
-# interesting parameters:
-n_humps_h = 3
-n_humps_v = 1
-mu = 100
-n_cells_in_data = 100
-# ignore:
-full_knowledge = False
-depth_offset = 0. * U.km
-#
-DIM = 2
-n_cells_in_data = max(n_humps_h*7, n_cells_in_data)
-l_data = 100. * U.km
-l_pad = 40. * U.km
-THICKNESS = 20. * U.km
-l_air = 20. * U.km
-n_cells_v = max(
-        int((2*l_air+THICKNESS+depth_offset)/l_data*n_cells_in_data + 0.5), 25)
-
-
-source=SyntheticData(DataSource.GRAVITY, n_length=n_humps_h, n_depth=n_humps_v,
-        depth=THICKNESS+depth_offset, depth_offset=depth_offset,
-        DIM=DIM, number_of_elements=n_cells_in_data, length=l_data,
-        data_offset=0, full_knowledge=full_knowledge)
-
-domainbuilder=DomainBuilder(dim=DIM)
-domainbuilder.addSource(source)
-domainbuilder.setVerticalExtents(depth=l_air+THICKNESS+depth_offset,
-                                 air_layer=l_air, num_cells=n_cells_v)
-domainbuilder.setPadding(l_pad)
-domainbuilder.fixDensityBelow(depth=THICKNESS+depth_offset)
-
-inv=GravityInversion()
-inv.setSolverTolerance(1e-4)
-inv.setSolverMaxIterations(50)
-inv.setup(domainbuilder)
-inv.getCostFunction().setTradeOffFactorsModels(mu)
-
-
-rho_new = inv.run()
-rho_ref = source.getReferenceProperty()
-print("rho_new = %s"%rho_new)
-print("rho = %s"%rho_ref)
-g, chi = inv.getCostFunction().getForwardModel().getSurvey(0)
-saveSilo(os.path.join(WORKDIR, 'results_gravity_2d'), density=rho_new, density_ref=rho_ref, g=g, chi=chi)
 
