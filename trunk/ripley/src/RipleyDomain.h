795aeb28a0ae96035874fa69398a3b0f52b12fb5
sshaw <sshaw@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-05-26 23:50:52 +0000

    fixing intel's hidden function warning

--- trunk/ripley/src/RipleyDomain.h
+++ trunk/ripley/src/RipleyDomain.h
@@ -527,6 +527,7 @@ public:
             boost::python::list data,
             Assembler_ptr assembler) const;
 
+    using escript::AbstractContinuousDomain::addPDEToTransportProblem;
     /**
        \brief
        adds a PDE onto a transport problem
