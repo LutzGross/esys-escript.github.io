bbaa48e6f16a172b91311b8f0e291c7d22ca2f2b
sshaw <sshaw@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-06-12 05:28:22 +0000

    fixed domainreader test sillyness and moved gmsh detection into testfile from SConscript

--- trunk/modellib/test/python/run_domainreaders.py
+++ trunk/modellib/test/python/run_domainreaders.py
@@ -30,6 +30,7 @@ __url__="https://launchpad.net/escript-finley"
 #
 
 import os
+from subprocess import PIPE, Popen
 import esys.escriptcore.utestselect as unittest
 from esys.escriptcore.testing import *
 import esys.finley
@@ -47,8 +48,20 @@ try:
 except KeyError:
      MODELLIB_WORKDIR='.'
 
-@unittest.skipIf(esys.escript.getEscriptParamInt("MPIBUILD",0)>0,
-        "Only tested for non-MPI builds")
+GMSH = None
+try:
+    p=Popen(['gmsh', '-info'], stderr=PIPE)
+    _,e=p.communicate()
+    if e.split().count("MPI"):
+        GMSH = 'm'
+    else:
+        GMSH = 's'
+except OSError:
+    pass
+    
+@unittest.skipIf(GMSH is None, "gmsh not available")
+@unittest.skipIf(False and esys.escript.getEscriptParamInt("MPIBUILD",0)>0,
+        "not tested with MPI builds")
 class Test_domainReaders(unittest.TestCase):
     def domain_family(self, dommodule, f):
         dom=RectangularDomain(dommodule, parameters=["fish","dummy"], debug=True)
@@ -56,29 +69,29 @@ class Test_domainReaders(unittest.TestCase):
         dom.domain().write(f+"dr.fly")
         r1=DomainReader(dommodule)
         r1.source=DataSource(uri=f+"dr.fly", fileformat="fly")
-        bdomf=r1.domain()
-        del bdomf
-        r2=DomainReader(dommodule)
-        r2.source=DataSource(uri=os.path.join(MODELLIB_WORKDIR,"TESTgmsh_test.msh"), fileformat="gmsh")
-        bdomm=r2.domain()
-        del bdomm
+        r1.domain()
 
-    def test_domain_families(self):
-        Test_domain(None,os.path.join(MODELLIB_WORKDIR,'TESTnone'))
-        Test_domain(esys.finley,os.path.join(MODELLIB_WORKDIR,'TESTfin'))
-        Test_domain(esys.dudley,os.path.join(MODELLIB_WORKDIR,'TESTDud'))
-
-    def test_design(self):
         des=Design()
         b=Volume(Brick(Point(0,0,0), Point(1,1,1)))
         des.addItems(b)
         des.setMeshFileName(os.path.join(MODELLIB_WORKDIR,"TESTgmsh_test.msh"))
         esys.finley.MakeDomain(des)
+        
+        r2=DomainReader(dommodule)
+        r2.source=DataSource(uri=os.path.join(MODELLIB_WORKDIR,"TESTgmsh_test.msh"), fileformat="gmsh")
+        r2.domain()
+
+    def test_domain_families(self):
+        self.domain_family(None,os.path.join(MODELLIB_WORKDIR,'TESTnone'))
+        self.domain_family(esys.dudley,os.path.join(MODELLIB_WORKDIR,'TESTDud'))
+        self.domain_family(esys.finley,os.path.join(MODELLIB_WORKDIR,'TESTfin'))
    
     def test_finleyReader(self):
+        self.domain_family(esys.finley,os.path.join(MODELLIB_WORKDIR,'TESTfin'))
         rf=FinleyReader(parameters=["fish","dummy"], debug=True)
         rf.source=DataSource(uri=os.path.join(MODELLIB_WORKDIR,"TESTfindr.fly"), fileformat="fly")
         rf.domain()
+
 
 if __name__ == '__main__':
     run_tests(__name__, exit_on_failure=True)
