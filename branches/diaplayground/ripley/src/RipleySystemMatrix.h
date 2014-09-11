f39f3df9545addbfa18b024467e852af0a28b5f8
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-09-11 06:29:28 +0000

    Check for CUDA devices only once when target=GPU.

--- branches/diaplayground/ripley/src/RipleySystemMatrix.h
+++ branches/diaplayground/ripley/src/RipleySystemMatrix.h
@@ -1,7 +1,7 @@
 
 /*****************************************************************************
 *
-* Copyright (c) 2003-2014 by University of Queensland
+* Copyright (c) 2014 by University of Queensland
 * http://www.uq.edu.au
 *
 * Primary Business: Queensland, Australia
@@ -68,7 +68,7 @@ private:
 
     virtual void ypAx(escript::Data& y, escript::Data& x) const;
 
-    void checkCUDA();
+    static void checkCUDA();
 
     /// GPU device IDs supporting CUDA
     static std::vector<int> cudaDevices;
