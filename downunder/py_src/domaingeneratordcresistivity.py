57e43f906a5e5fd961e9829fb0d12c8d4574bbb8
jduplessis <jduplessis@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-10-22 03:50:06 +0000

    added copyright message

--- downunder/py_src/domaingeneratordcresistivity.py
+++ downunder/py_src/domaingeneratordcresistivity.py
@@ -1,4 +1,19 @@
-# from esys.escript import getMPIWorldMax, getMPIRankWorld
+##############################################################################
+#
+# Copyright (c) 2003-2014 by University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Open Software License version 3.0
+# http://www.opensource.org/licenses/osl-3.0.php
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development 2012-2013 by School of Earth Sciences
+# Development from 2014 by Centre for Geoscience Computing (GeoComp)
+#
+##############################################################################
+
+from esys.escript import getMPIWorldMax, getMPIRankWorld
 from esys.escript import *
 from esys.escript.linearPDEs import LinearPDE, SolverOptions
 from esys.finley import ReadGmsh, ReadMesh
