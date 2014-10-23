ff05c6edd9ab3e2cb2f17eafe60ee1097ea66a08
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-10-23 03:50:37 +0000

    Fixes to CDS SpMV symmetric and tests for blocksizes 1-4 symm/non-symm.

--- trunk/ripley/test/SystemMatrixTestCase.h
+++ trunk/ripley/test/SystemMatrixTestCase.h
@@ -1,7 +1,7 @@
 
 /*****************************************************************************
 *
-* Copyright (c) 2003-2014 by University of Queensland
+* Copyright (c) 2014 by University of Queensland
 * http://www.uq.edu.au
 *
 * Primary Business: Queensland, Australia
@@ -19,6 +19,7 @@
 #define _RIPLEY_SYSTEMMATRIXTESTCASE_H_
 
 #include <escript/AbstractDomain.h>
+#include <escript/AbstractSystemMatrix.h>
 
 #include <cppunit/TestFixture.h>
 #include <cppunit/TestSuite.h>
@@ -26,10 +27,25 @@
 class SystemMatrixTestCase : public CppUnit::TestFixture
 {
 public:
-    void testSpMV();
+    virtual void setUp();
+
+    void testSpMV_CPU_blocksize1_nonsymmetric();
+    void testSpMV_CPU_blocksize2_nonsymmetric();
+    void testSpMV_CPU_blocksize3_nonsymmetric();
+    void testSpMV_CPU_blocksize4_nonsymmetric();
+    void testSpMV_CPU_blocksize1_symmetric();
+    void testSpMV_CPU_blocksize2_symmetric();
+    void testSpMV_CPU_blocksize3_symmetric();
+    void testSpMV_CPU_blocksize4_symmetric();
 
     static CppUnit::TestSuite* suite();
 
+private:
+    escript::ASM_ptr createMatrix(int blocksize, bool symmetric);
+    escript::Data createInputVector(int blocksize);
+
+    esysUtils::JMPI mpiInfo;
+    escript::Domain_ptr domain;
 };
 
 #endif // _RIPLEY_SYSTEMMATRIXTESTCASE_H_
