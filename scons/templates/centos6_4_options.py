9f56f203bf81dbab732dd7e8fb41b823e28a768e
sshaw <sshaw@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-11-28 05:38:33 +0000

    updating older templates with v202 options

--- scons/templates/centos6_4_options.py
+++ scons/templates/centos6_4_options.py
@@ -227,6 +227,16 @@ netcdf = True
 # Compiler flags for some optimisations in dudley
 #dudley_assemble_flags = '-funroll-loops'
 
+# launcher, prelaunch, postlaunch: for MPI builds/batch system runs
+# the following substitutions are applied to all three:
+# %b = executable, %n = number of nodes, %p = number of processes,
+# %N = total number of processes, # %t = number of threads,
+# %f = name of hostfile, %h = comma-separated list of hosts,
+# %e = comma-separated list of environment variables to export
+#prelaunch = "EE=$(echo %e|sed -e 's/,/ -x /g')"
+#launcher = "mpirun --gmca mpi_warn_on_fork 0 -x ${EE} --bynode --bind-to-none --host %h -np %N %b"
+#postlaunch = ""
+
 # enables code that is non-standard
 #iknowwhatimdoing = True
 
