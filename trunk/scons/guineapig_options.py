b752b3011b1796d55a916ef2dcabdbc02a724caa
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2014-12-18 00:41:39 +0000

    next try.

--- trunk/scons/guineapig_options.py
+++ trunk/scons/guineapig_options.py
@@ -35,4 +35,4 @@ silo_libs = ['siloh5', 'hdf5_openmpi']
 
 build_shared = False
 
-launcher = "mpirun --gmca mpi_warn_on_fork 0 -x ${EE} --host %h --cpus-per-rank %t -np %N %b"
+launcher = "mpirun --gmca mpi_warn_on_fork 0 -x ${EE} -np %N %b"
