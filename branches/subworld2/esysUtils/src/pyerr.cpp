a8adfc0948792bdc1fcd20df11501b1b7fcc729c
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2015-03-06 00:51:59 +0000

    Nifty function to get string form of a python exception in c++

--- branches/subworld2/esysUtils/src/pyerr.cpp
+++ branches/subworld2/esysUtils/src/pyerr.cpp
@@ -17,6 +17,9 @@
 #include "first.h"
 #include "pyerr.h"
 
+#include <sstream>
+#include <boost/python.hpp>
+
 // Function factored out of SubWorld code
 
 void getStringFromPyException(boost::python::error_already_set e, std::string& errormsg)
@@ -37,6 +40,52 @@ void getStringFromPyException(boost::python::error_already_set e, std::string& e
 #else
 	errormsg=PyString_AsString(errobj);
 #endif
+	Py_XDECREF(errobj);
+
+	Py_XDECREF(ptype);
+	Py_XDECREF(pvalue);
+	Py_XDECREF(ptraceback);
+}
+
+void getTraceStringFromPyException(boost::python::error_already_set e, std::string& errormsg)
+{
+	using namespace boost::python;
+  	PyObject* ptype=0;
+ 	PyObject* pvalue=0;
+ 	PyObject* ptraceback=0;
+ 	PyErr_Fetch(&ptype, &pvalue, &ptraceback);
+	PyErr_NormalizeException(&ptype, &pvalue, &ptraceback);
+ 
+	PyObject* errobj=PyObject_Str(pvalue);
+	
+	// Need to invoke the equivalent of traceback.format_exception(ptype, pvalue, ptraceback)
+	try
+	{
+	    std::ostringstream oss;
+	    boost::python::object tb(boost::python::handle<>(boost::python::borrowed(ptraceback)));
+	    
+	    while (tb!=object())
+	    {
+		int lineno=boost::python::extract<int>(tb.attr("tb_lasti"))();
+		boost::python::object code=tb.attr("tb_frame").attr("f_code");
+		std::string fname=boost::python::extract<std::string>(code.attr("co_filename"))();
+		std::string funct=boost::python::extract<std::string>(code.attr("co_name"))();
+		oss << "File \"" << fname << "\", line " << lineno << ", in " << funct << std::endl;
+		tb=tb.attr("tb_next");
+	    }
+	    boost::python::object eo(boost::python::handle<>(boost::python::borrowed(errobj)));
+	    boost::python::object typ(boost::python::handle<>(boost::python::borrowed(ptype)));
+	    boost::python::object typs=typ.attr("__name__").attr("__str__")();
+	    oss << extract<std::string>(typs)() << ": ";
+	    oss << extract<std::string>(eo)();
+	    oss << std::endl;
+	    errormsg=oss.str();
+	}
+	catch (boost::python::error_already_set zz)
+	{
+	    getStringFromPyException(zz, errormsg);
+	    errormsg="Encountered a problem extracting an exception trace:"+errormsg;
+	}
 	Py_XDECREF(errobj);
 
 	Py_XDECREF(ptype);
