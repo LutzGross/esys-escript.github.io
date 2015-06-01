9fa3a2aeeb32b5a4a7214637b2d2bb7c830d354a
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2015-06-01 04:21:26 +0000

    With some help from Cihan, we now have exception traces for python code called from C++. It also works with python3.

--- trunk/esysUtils/src/pyerr.cpp
+++ trunk/esysUtils/src/pyerr.cpp
@@ -14,6 +14,11 @@
 *****************************************************************************/
 
 #define ESNEEDPYTHON
+#include <boost/python/object.hpp>
+#include <boost/python/import.hpp>
+#include <boost/python/list.hpp>
+#include <boost/python/extract.hpp>
+
 #include "first.h"
 #include "pyerr.h"
 
@@ -22,20 +27,48 @@
 void getStringFromPyException(boost::python::error_already_set e, std::string& errormsg)
 {
 	using namespace boost::python;
+
   	PyObject* ptype=0;
  	PyObject* pvalue=0;
  	PyObject* ptraceback=0;
  	PyErr_Fetch(&ptype, &pvalue, &ptraceback);
 	PyErr_NormalizeException(&ptype, &pvalue, &ptraceback);
- 
-	PyObject* errobj=PyObject_Str(pvalue);
+	object tb = import("traceback"); 
+	object trace(handle<>(borrowed(ptraceback)));
+	object li=tb.attr("extract_tb")(trace);
+	object li2=tb.attr("format_list")(li);
+	list l=extract<list>(li2)();
+	
+
 
 #ifdef ESPYTHON3	
+	std::string ss;
+	for (int i=0;i<len(l);++i) {
+	    object o=l[i];
+	    PyObject* rr=PyUnicode_AsASCIIString(o.ptr());
+	    ss+=PyBytes_AsString(rr);
+	    Py_XDECREF(rr);
+	}
+	
+	PyObject* errobj=PyObject_Str(pvalue);	
+	
 	PyObject* rr=PyUnicode_AsASCIIString(errobj);
 	errormsg=PyBytes_AsString(rr);
+	errormsg+="\n";
 	Py_XDECREF(rr);
+	errormsg+=ss;
 #else
+	
+	std::string ss;
+	for (int i=0;i<len(l);++i) {
+	    ss+=extract<std::string>(l[i])();
+	}
+	
+	PyObject* errobj=PyObject_Str(pvalue);	
+	
 	errormsg=PyString_AsString(errobj);
+	errormsg+="\n";
+	errormsg+=ss;
 #endif
 	Py_XDECREF(errobj);
 
