b8ef44f903873d1707ab15833b43e654fadab697
gross <gross@77569008-7704-0410-b7a0-a92fef0b09fd>
2015-06-19 07:14:10 +0000

    documenation fro Dirac added

---
+++ doc/examples/usersguide/dirac.py
@@ -0,0 +1,58 @@
+from __future__ import division
+from __future__ import print_function
+##############################################################################
+#
+# Copyright (c) 2012-2015 by The University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Open Software License version 3.0
+# http://www.opensource.org/licenses/osl-3.0.php
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development 2012-2013 by School of Earth Sciences
+# Development from 2014 by Centre for Geoscience Computing (GeoComp)
+#
+##############################################################################
+
+"""
+example to demonstrate the use of Dirac Delta functions 
+"""
+
+__copyright__="""Copyright (c) 2012-2015 by The University of Queensland
+http://www.uq.edu.au
+Primary Business: Queensland, Australia"""
+__license__="""Licensed under the Open Software License version 3.0
+http://www.opensource.org/licenses/osl-3.0.php"""
+__url__="https://launchpad.net/escript-finley"
+
+
+from esys.escript import *
+from esys.escript.linearPDEs import LinearSinglePDE
+from esys.weipa import saveVTK
+
+try:
+    from esys.finley import Rectangle
+    HAVE_FINLEY = True
+except ImportError:
+    HAVE_FINLEY = False
+# generate domain:
+if not HAVE_FINLEY:
+    print("Finley module not available")
+else:
+    mydomain=Rectangle(30,30, l0=3, l1=2, 
+                diracPoints=[(1.,1.), (2.,1.)],  diracTags=['in', 'out'])
+    # fix the solution on the boundary
+    x = mydomain.getX()
+    gammaD = whereZero(x[0])+whereZero(x[1])+whereZero(x[0]-3.)+whereZero(x[1]-2.)
+    # fix the solution on the boundary
+    s=Scalar(0., DiracDeltaFunctions(mydomain))
+    s.setTaggedValue('in', +1.)
+    s.setTaggedValue('out', -1.)
+    # define PDE and get its solution u
+    mypde = LinearSinglePDE(domain=mydomain)
+    mypde.setValue(q=gammaD, A=kronecker(2), y_dirac=s)
+    u = mypde.getSolution()
+    print("Solution = ",str(u))
+    # write u to an external file
+    saveVTK("u.vtu",sol=u)
