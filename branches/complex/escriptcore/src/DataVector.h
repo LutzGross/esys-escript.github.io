de1fcde0e4c07bbf7b0646989c558026c79cf83c
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2016-01-13 07:23:44 +0000

    Adding an alternate DataVector which does not use Taipan

--- branches/complex/escriptcore/src/DataVector.h
+++ branches/complex/escriptcore/src/DataVector.h
@@ -15,8 +15,8 @@
 *****************************************************************************/
 
 
-#if !defined escript_DataVector_20050324_H
-#define escript_DataVector_20050324_H
+#if !defined escript_DataVectorTaipan_20050324_H
+#define escript_DataVectorTaipan_20050324_H
 #include "system_dep.h"
 
 #include "esysUtils/EsysAssert.h"
@@ -31,17 +31,17 @@ class WrappedArray;
 
 /**
    \brief
-   DataVector implements an arbitrarily long vector of data values.
-   DataVector is the underlying data container for Data objects.
+   DataVectorTaipan implements an arbitrarily long vector of data values.
+   DataVectorTaipan is the underlying data container for Data objects.
 
    Description:
-   DataVector provides an implementation of a vector of data values for use
+   DataVectorTaipan provides an implementation of a vector of data values for use
    by DataBlocks2D and DataArrayView. Hiding the vector in this container
    allows different implementations to be swapped in without disrupting the
    client classes.
 */
 
-class ESCRIPT_DLL_API DataVector {
+class ESCRIPT_DLL_API DataVectorTaipan {
 
  public:
 
@@ -51,7 +51,7 @@ class ESCRIPT_DLL_API DataVector {
 
   //
   // The underlying type used to implement the vector.
-  typedef ElementType *  ValueType;
+  typedef ElementType *  VectorStorageType;
   typedef const ElementType * ConstValueType;
 
   //
@@ -63,29 +63,29 @@ class ESCRIPT_DLL_API DataVector {
 
   /**
      \brief
-     Default constructor for DataVector.
+     Default constructor for DataVectorTaipan.
 
      Description:
-     Constructs an empty DataVector object.
+     Constructs an empty DataVectorTaipan object.
   */
-  DataVector();
+  DataVectorTaipan();
 
   /**
      \brief
-     Copy constructor for DataVector.
+     Copy constructor for DataVectorTaipan.
 
      Description:
-     Constructs a DataVector object which is a copy of the
-     given DataVector object.
+     Constructs a DataVectorTaipan object which is a copy of the
+     given DataVectorTaipan object.
   */
-  DataVector(const DataVector& other);
+  DataVectorTaipan(const DataVectorTaipan& other);
 
   /**
      \brief
-     Constructor for DataVector.
+     Constructor for DataVectorTaipan.
 
      Description:
-     Constructs a DataVector object of length "size" with all elements
+     Constructs a DataVectorTaipan object of length "size" with all elements
      initilised to "val".
 
      \param size - Input - Number of elements in the vector.
@@ -98,23 +98,23 @@ class ESCRIPT_DLL_API DataVector {
      number of data-points per sample. Size is the total number of elements required to hold
      all elements for all data-points in the given object, ie: number of samples * blocksize.
   */
-  DataVector(const size_type size,
+  DataVectorTaipan(const size_type size,
              const value_type val=0.0,
              const size_type blockSize=1);
 
   /**
      \brief
-     Default destructor for DataVector.
+     Default destructor for DataVectorTaipan.
 
      Description:
-     Destroys the current DataVector object.
+     Destroys the current DataVectorTaipan object.
   */
-  ~DataVector();
+  ~DataVectorTaipan();
 
   /**
      \brief
-     Resize the DataVector to the given length "newSize".
-     All current data is lost. All elements in the new DataVector are
+     Resize the DataVectorTaipan to the given length "newSize".
+     All current data is lost. All elements in the new DataVectorTaipan are
      initialised to "newVal".
 
      \param newSize - Input - New size for the vector.
@@ -141,7 +141,7 @@ class ESCRIPT_DLL_API DataVector {
 
   /**
      \brief
-     Return the number of elements in this DataVector.
+     Return the number of elements in this DataVectorTaipan.
   */
   inline
   size_type
@@ -149,31 +149,31 @@ class ESCRIPT_DLL_API DataVector {
 
   /**
      \brief
-     DataVector assignment operator "=".
-     Assign the given DataVector object to this.
+     DataVectorTaipan assignment operator "=".
+     Assign the given DataVectorTaipan object to this.
   */
-  DataVector&
-  operator=(const DataVector& other);
+  DataVectorTaipan&
+  operator=(const DataVectorTaipan& other);
 
   /**
      \brief
-     DataVector equality comparison operator "==".
-     Return true if the given DataVector is equal to this.
+     DataVectorTaipan equality comparison operator "==".
+     Return true if the given DataVectorTaipan is equal to this.
   */
   bool
-  operator==(const DataVector& other) const;
+  operator==(const DataVectorTaipan& other) const;
 
   /**
      \brief
-     DataVector inequality comparison operator "!=".
-     Return true if the given DataVector is not equal to this.
+     DataVectorTaipan inequality comparison operator "!=".
+     Return true if the given DataVectorTaipan is not equal to this.
   */
   bool
-  operator!=(const DataVector& other) const;
+  operator!=(const DataVectorTaipan& other) const;
 
   /**
     \brief
-    Return a reference to the element at position i in this DataVector.
+    Return a reference to the element at position i in this DataVectorTaipan.
     Will throw an exception if an invalid index "i" is given.
 
     NB: access to the element one past the end of the vector is permitted
@@ -197,9 +197,176 @@ class ESCRIPT_DLL_API DataVector {
   size_type m_N;
 
   //
-  // The container for the elements contained in this DataVector.
-  ValueType m_array_data;
+  // The container for the elements contained in this DataVectorTaipan.
+  VectorStorageType m_array_data;
 };
+
+
+
+class ESCRIPT_DLL_API DataVectorAlt {
+
+ public:
+
+  //
+  // The type of the elements stored in the vector.
+  typedef double ElementType;
+
+  //
+  // Various types exported to clients of this class.
+  
+  typedef const ElementType * ConstValueType;
+  typedef ElementType          value_type;
+  typedef long                 size_type;
+  typedef ElementType &        reference;
+  typedef const ElementType &  const_reference;
+
+  /**
+     \brief
+     Default constructor for DataVectorAlt.
+
+     Description:
+     Constructs an empty DataVectorAlt object.
+  */
+  DataVectorAlt();
+
+  /**
+     \brief
+     Copy constructor for DataVectorAlt.
+
+     Description:
+     Constructs a DataVectorAlt object which is a copy of the
+     given DataVectorAlt object.
+  */
+  DataVectorAlt(const DataVectorAlt& other);
+
+  /**
+     \brief
+     Constructor for DataVectorAlt.
+
+     Description:
+     Constructs a DataVectorAlt object of length "size" with all elements
+     initilised to "val".
+
+     \param size - Input - Number of elements in the vector.
+     \param val - Input - Initial value for all elements in the vector. Default is 0.0.
+     \param blockSize - Input - size of blocks within the vector, overall vector
+                size must be a precise multiple of the block size. Default is 1.
+
+     In escript::Data, blocksize corresponds to the number of elements required to hold all
+     the data-points for a sample, ie: the product of the dimensions of a data-point and the
+     number of data-points per sample. Size is the total number of elements required to hold
+     all elements for all data-points in the given object, ie: number of samples * blocksize.
+  */
+  DataVectorAlt(const size_type size,
+             const value_type val=0.0,
+             const size_type blockSize=1);
+
+  /**
+     \brief
+     Default destructor for DataVectorAlt.
+
+     Description:
+     Destroys the current DataVectorAlt object.
+  */
+  ~DataVectorAlt();
+
+  /**
+     \brief
+     Resize the DataVectorAlt to the given length "newSize".
+     All current data is lost. All elements in the new DataVectorAlt are
+     initialised to "newVal".
+
+     \param newSize - Input - New size for the vector.
+     \param newVal - Input - New initial value for all elements in the vector.
+     \param newBlockSize - Input - New block size for the vector.
+  */
+  void
+  resize(const size_type newSize,
+         const value_type newVal=0.0,
+         const size_type newBlockSize=1);
+
+  /**
+    \brief 
+    Populates the vector with the data from value.
+    This method currently throws an exception if the specified number of copies won't fit.
+    \warning This function does not attempt to perform shape checking.
+  */
+  void
+  copyFromArray(const escript::WrappedArray& value, size_type copies);
+
+  void 
+  copyFromArrayToOffset(const WrappedArray& value, size_type offset, size_type copies);
+
+
+  /**
+     \brief
+     Return the number of elements in this DataVectorAlt.
+  */
+  inline
+  size_type
+  size() const;
+
+  /**
+     \brief
+     DataVectorAlt assignment operator "=".
+     Assign the given DataVectorAlt object to this.
+  */
+  DataVectorAlt&
+  operator=(const DataVectorAlt& other);
+
+  /**
+     \brief
+     DataVectorAlt equality comparison operator "==".
+     Return true if the given DataVectorAlt is equal to this.
+  */
+  bool
+  operator==(const DataVectorAlt& other) const;
+
+  /**
+     \brief
+     DataVectorAlt inequality comparison operator "!=".
+     Return true if the given DataVectorAlt is not equal to this.
+  */
+  bool
+  operator!=(const DataVectorAlt& other) const;
+
+  /**
+    \brief
+    Return a reference to the element at position i in this DataVectorAlt.
+    Will throw an exception if an invalid index "i" is given.
+
+    NB: access to the element one past the end of the vector is permitted
+    in order to provide a facility equivalent to an end() pointer.
+  */
+  inline
+  reference
+  operator[](const size_type i);
+
+  inline
+  const_reference
+  operator[](const size_type i) const;
+
+
+ protected:
+
+ private:
+
+  size_type m_size;
+  size_type m_dim;
+  size_type m_N;
+
+  //
+  // The container for the elements contained in this DataVectorTaipan.
+  std::vector<ElementType> m_array_data;
+};
+
+
+
+// This is the main version we had
+//typedef DataVectorTaipan DataVector;
+
+typedef DataVectorAlt DataVector;
+
 
 /**
   \brief
@@ -211,27 +378,54 @@ ESCRIPT_DLL_API void releaseUnusedMemory();
 
 
 inline
-DataVector::size_type
-DataVector::size() const
+DataVectorTaipan::size_type
+DataVectorTaipan::size() const
 {
   return m_size;
 }
 
 inline
-DataVector::reference
-DataVector::operator[](const DataVector::size_type i)
+DataVectorTaipan::reference
+DataVectorTaipan::operator[](const DataVectorTaipan::size_type i)
 {
-  EsysAssert(i<size(),"DataVector: invalid index specified. " << i << " of " << size());
+  EsysAssert(i<size(),"DataVectorTaipan: invalid index specified. " << i << " of " << size());
   return m_array_data[i];
 }
 
 inline
-DataVector::const_reference
-DataVector::operator[](const DataVector::size_type i) const
+DataVectorTaipan::const_reference
+DataVectorTaipan::operator[](const DataVectorTaipan::size_type i) const
 {
-  EsysAssert(i<size(),"DataVector: invalid index specified. " << i << " of " << size());
+  EsysAssert(i<size(),"DataVectorTaipan: invalid index specified. " << i << " of " << size());
   return m_array_data[i];
 }
+
+
+
+
+inline
+DataVectorAlt::size_type
+DataVectorAlt::size() const
+{
+  return m_size;
+}
+
+inline
+DataVectorAlt::reference
+DataVectorAlt::operator[](const DataVectorAlt::size_type i)
+{
+  EsysAssert(i<size(),"DataVectorAlt: invalid index specified. " << i << " of " << size());
+  return m_array_data[i];
+}
+
+inline
+DataVectorAlt::const_reference
+DataVectorAlt::operator[](const DataVectorAlt::size_type i) const
+{
+  EsysAssert(i<size(),"DataVectorAlt: invalid index specified. " << i << " of " << size());
+  return m_array_data[i];
+}
+
 
 } // end of namespace
 
