f1d566af0cc7257b8fd658c6d817664aeb762919
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2016-01-14 06:50:47 +0000

    Made space in wrapped array for complex values as well. Note: This code passes existing unit tests (for double values) but the complex functionality is _completely_ untested.

--- escriptcore/src/WrappedArray.h
+++ escriptcore/src/WrappedArray.h
@@ -22,6 +22,7 @@
 #include "system_dep.h"
 #include "DataTypes.h"
 #include "boost/python/extract.hpp"
+#include <complex>
 
 namespace escript
 {
@@ -29,24 +30,47 @@ namespace escript
 class WrappedArray
 {
 public:
+	typedef  std::complex<double> complextype;
 	WrappedArray(const boost::python::object& obj_in);
 	~WrappedArray();
 	unsigned int getRank() const;
 	const DataTypes::ShapeType& getShape() const;
+	bool isComplex() const;
 	double getElt() const;
 	double getElt(unsigned int i) const;
 	double getElt(unsigned int i, unsigned int j) const;
 	double getElt(unsigned int i, unsigned int j, unsigned int k) const;
 	double getElt(unsigned int i, unsigned int j, unsigned int k, unsigned int m) const;
+	
+	complextype getEltC() const;
+	complextype getEltC(unsigned int i) const;
+	complextype getEltC(unsigned int i, unsigned int j) const;
+	complextype getEltC(unsigned int i, unsigned int j, unsigned int k) const;
+	complextype getEltC(unsigned int i, unsigned int j, unsigned int k, unsigned int m) const;
+	
+	
 	void convertArray() const;
 private:
+	void convertArrayR() const;
+	void convertArrayC() const;
 	template<typename T> void convertNumpyArray(const T* array, const std::vector<int>& strides) const;
+	template<typename T> void convertNumpyArrayC(const T* array, const std::vector<int>& strides) const;
 	const boost::python::object& obj;
-	int rank;
+	int rank;	
+	mutable bool converted;		// has the array been converted to a C array
+	bool iscomplex;		// is the wrapped array storing complex values?	
 	escript::DataTypes::ShapeType shape;
-	double m_scalar;
-	mutable double* dat;
+	double scalar_r;
+	complextype scalar_c;
+	mutable double* dat_r;			// real data
+	mutable complextype* dat_c;	// complex data   - only one of these members should be used
 };
+
+
+inline bool WrappedArray::isComplex() const
+{
+    return iscomplex;
+}
 
 inline unsigned int 
 WrappedArray::getRank() const
@@ -63,36 +87,110 @@ WrappedArray::getShape() const
 inline double
 WrappedArray::getElt() const
 {
-	return m_scalar;
+	return scalar_r;
 }
 
 
 inline double
 WrappedArray::getElt(unsigned int i) const
 {  // __float__ added to deal with numpy. If this causes problems we may have to register a custom converter
-        return (dat!=0)?dat[i]:(boost::python::extract<double>(obj[i].attr("__float__")()));	
+    if (iscomplex)
+    {
+      return nan("");
+    }
+    return (dat_r!=0)?dat_r[i]:(boost::python::extract<double>(obj[i].attr("__float__")()));	
 }
 
 inline
 double 
 WrappedArray::getElt(unsigned int i, unsigned int j) const
 {
-	return (dat!=0)?dat[DataTypes::getRelIndex(shape,i,j)]:(boost::python::extract<double>(obj[i][j].attr("__float__")()));
+    if (iscomplex)
+    {
+      return nan("");
+    }  
+    return (dat_r!=0)?dat_r[DataTypes::getRelIndex(shape,i,j)]:(boost::python::extract<double>(obj[i][j].attr("__float__")()));
 }
 
 inline
 double 
 WrappedArray::getElt(unsigned int i, unsigned int j, unsigned int k) const
 {
-	return (dat!=0)?dat[DataTypes::getRelIndex(shape,i,j,k)]:(boost::python::extract<double>(obj[i][j][k].attr("__float__")()));
+    if (iscomplex)
+    {
+      return nan("");
+    }    
+    return (dat_r!=0)?dat_r[DataTypes::getRelIndex(shape,i,j,k)]:(boost::python::extract<double>(obj[i][j][k].attr("__float__")()));
 }
 
 inline
 double 
 WrappedArray::getElt(unsigned int i, unsigned int j, unsigned int k, unsigned int m) const
 {
-	return (dat!=0)?dat[DataTypes::getRelIndex(shape,i,j,k,m)]:(boost::python::extract<double>(obj[i][j][k][m].attr("__float__")()));
+    if (iscomplex)
+    {
+      return nan("");
+    }  
+    return (dat_r!=0)?dat_r[DataTypes::getRelIndex(shape,i,j,k,m)]:(boost::python::extract<double>(obj[i][j][k][m].attr("__float__")()));
 }
+
+
+
+
+
+inline WrappedArray::complextype
+WrappedArray::getEltC() const
+{
+	return scalar_c;
+}
+
+
+inline WrappedArray::complextype
+WrappedArray::getEltC(unsigned int i) const
+{
+    if (!iscomplex)
+    {
+      return nan("");
+    }
+    return (dat_c!=0)?dat_c[i]:(boost::python::extract<complextype>(obj[i]));	// don't know if this will work with numpy	
+}
+
+inline
+WrappedArray::complextype 
+WrappedArray::getEltC(unsigned int i, unsigned int j) const
+{
+    if (!iscomplex)
+    {
+      return nan("");
+    }  
+    return (dat_c!=0)?dat_c[DataTypes::getRelIndex(shape,i,j)]:(boost::python::extract<complextype>(obj[i][j]));
+}
+
+inline
+WrappedArray::complextype 
+WrappedArray::getEltC(unsigned int i, unsigned int j, unsigned int k) const
+{
+    if (!iscomplex)
+    {
+      return nan("");
+    }    
+    return (dat_c!=0)?dat_c[DataTypes::getRelIndex(shape,i,j,k)]:(boost::python::extract<complextype>(obj[i][j][k]));
+}
+
+inline
+WrappedArray::complextype 
+WrappedArray::getEltC(unsigned int i, unsigned int j, unsigned int k, unsigned int m) const
+{
+    if (!iscomplex)
+    {
+      return nan("");
+    }  
+    return (dat_c!=0)?dat_c[DataTypes::getRelIndex(shape,i,j,k,m)]:(boost::python::extract<complextype>(obj[i][j][k][m]));
+}
+
+
+
+
 
 }
 
