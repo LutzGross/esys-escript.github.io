27b69537ddcfe4b99317a1e94d3739dc13478b11
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2016-02-22 01:05:32 +0000

    moved random to escript.

--- finley/src/CPPAdapter/MeshAdapter.cpp
+++ finley/src/CPPAdapter/MeshAdapter.cpp
@@ -18,9 +18,9 @@
 #include <esysUtils/first.h>
 
 #include "MeshAdapter.h"
-#include "esysUtils/EsysRandom.h"
 #include "escript/Data.h"
 #include "escript/DataFactory.h"
+#include "escript/Random.h"
 #include "escript/SolverOptions.h"
 
 #include <paso/SystemMatrix.h>
@@ -1816,7 +1816,7 @@ bool MeshAdapter::operator!=(const escript::AbstractDomain& other) const
     return !(operator==(other));
 }
 
-int MeshAdapter::getSystemMatrixTypeId(const boost::python::object& options) const
+int MeshAdapter::getSystemMatrixTypeId(const bp::object& options) const
 {
     const escript::SolverBuddy& sb = bp::extract<escript::SolverBuddy>(options);
 
@@ -2156,14 +2156,13 @@ bool MeshAdapter::supportsContactElements() const
 
 escript::Data MeshAdapter::randomFill(const escript::DataTypes::ShapeType& shape,
        const escript::FunctionSpace& what, long seed,
-       const boost::python::tuple& filter) const
+       const bp::tuple& filter) const
 {
     escript::Data towipe(0, shape, what, true);
     // since we just made this object, no sharing is possible and we don't need to check for
     // exlusive write
     escript::DataTypes::RealVectorType& dv=towipe.getExpandedVectorReference();
-    const size_t dvsize=dv.size();
-    esysUtils::randomFillArray(seed, &(dv[0]), dvsize);
+    escript::randomFillArray(seed, &(dv[0]), dv.size());
     return towipe;	 
 }
 
--- ripley/src/Brick.cpp
+++ ripley/src/Brick.cpp
@@ -16,10 +16,6 @@
 
 #define ESNEEDPYTHON
 #include <esysUtils/first.h>
-#include <esysUtils/esysFileWriter.h>
-#include <esysUtils/EsysRandom.h>
-
-#include <paso/SystemMatrix.h>
 
 #include <ripley/Brick.h>
 #include <ripley/DefaultAssembler3D.h>
@@ -27,6 +23,11 @@
 #include <ripley/WaveAssembler3D.h>
 #include <ripley/blocktools.h>
 #include <ripley/domainhelpers.h>
+
+#include <esysUtils/esysFileWriter.h>
+#include <escript/Random.h>
+
+#include <paso/SystemMatrix.h>
 
 #ifdef USE_NETCDF
 #include <netcdfcpp.h>
@@ -3345,7 +3346,7 @@ namespace
  */
 escript::Data Brick::randomFill(const escript::DataTypes::ShapeType& shape,
                                 const escript::FunctionSpace& what,
-                                long seed, const boost::python::tuple& filter) const
+                                long seed, const bp::tuple& filter) const
 {
     int numvals=escript::DataTypes::noValues(shape);
     if (len(filter) > 0 && numvals != 1) {
@@ -3398,7 +3399,7 @@ that ripley has.
 */
 escript::Data Brick::randomFillWorker(
                         const escript::DataTypes::ShapeType& shape, long seed,
-                        const boost::python::tuple& filter) const
+                        const bp::tuple& filter) const
 {
     unsigned int radius=0;  // these are only used by gaussian
     double sigma=0.5;
@@ -3448,7 +3449,7 @@ escript::Data Brick::randomFillWorker(
     }
 
     double* src=new double[ext[0]*ext[1]*ext[2]*numvals];
-    esysUtils::randomFillArray(seed, src, ext[0]*ext[1]*ext[2]*numvals);
+    escript::randomFillArray(seed, src, ext[0]*ext[1]*ext[2]*numvals);
 
 #ifdef ESYS_MPI
     if ((internal[0]<5) || (internal[1]<5) || (internal[2]<5)) {
@@ -3470,9 +3471,9 @@ escript::Data Brick::randomFillWorker(
     basex=X*m_gNE[0]/m_NX[0];
     basey=Y*m_gNE[1]/m_NX[1];
     basez=Z*m_gNE[2]/m_NX[2];
-std::cout << "basex=" << basex << " basey=" << basey << " basez=" << basez << std::endl;
+    std::cout << "basex=" << basex << " basey=" << basey << " basez=" << basez << std::endl;
 #endif
-    esysUtils::patternFillArray(1, ext[0],ext[1],ext[2], src, 4, basex, basey, basez, numvals);
+    escript::patternFillArray(1, ext[0],ext[1],ext[2], src, 4, basex, basey, basez, numvals);
 */
 
 #ifdef ESYS_MPI
