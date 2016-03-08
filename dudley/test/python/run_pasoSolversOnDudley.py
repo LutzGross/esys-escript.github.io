563ceacb5fd9444a39e6ab32d7af930d51d9217b
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2016-03-08 06:27:14 +0000

    added a generic baseclass for PDE solver tests which is used by all domains. Renamed test files to prepare for Trilinos merge at some point.

---
+++ dudley/test/python/run_pasoSolversOnDudley.py
@@ -0,0 +1,309 @@
+
+##############################################################################
+#
+# Copyright (c) 2003-2016 by The University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Open Software License version 3.0
+# http://www.opensource.org/licenses/osl-3.0.php
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development 2012-2013 by School of Earth Sciences
+# Development from 2014 by Centre for Geoscience Computing (GeoComp)
+#
+##############################################################################
+
+from __future__ import print_function, division
+
+__copyright__="""Copyright (c) 2003-2016 by The University of Queensland
+http://www.uq.edu.au
+Primary Business: Queensland, Australia"""
+__license__="""Licensed under the Open Software License version 3.0
+http://www.opensource.org/licenses/osl-3.0.php"""
+__url__="https://launchpad.net/escript-finley"
+
+"""
+Test suite for PDE solvers on dudley
+"""
+
+from test_simplesolve import SimpleSolveTestCase
+import esys.escriptcore.utestselect as unittest
+from esys.escriptcore.testing import *
+
+from esys.dudley import Rectangle, Brick
+from esys.escript.linearPDEs import SolverOptions
+
+# number of elements in the spatial directions
+NE0=12
+NE1=13
+NE2=8
+OPTIMIZE=True
+
+### BiCGStab + Jacobi
+
+class Test_SimpleSolveDudleyRect_Paso_BICGSTAB_Jacobi(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.BICGSTAB
+        self.preconditioner = SolverOptions.JACOBI
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_BICGSTAB_Jacobi(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.BICGSTAB
+        self.preconditioner = SolverOptions.JACOBI
+
+    def tearDown(self):
+        del self.domain
+
+### PCG + Jacobi
+
+class Test_SimpleSolveDudleyRect_Paso_PCG_Jacobi(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.PCG
+        self.preconditioner = SolverOptions.JACOBI
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_PCG_Jacobi(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.PCG
+        self.preconditioner = SolverOptions.JACOBI
+
+    def tearDown(self):
+        del self.domain
+
+### TFQMR + Jacobi
+
+class Test_SimpleSolveDudleyRect_Paso_TFQMR_Jacobi(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.TFQMR
+        self.preconditioner = SolverOptions.JACOBI
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_TFQMR_Jacobi(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.TFQMR
+        self.preconditioner = SolverOptions.JACOBI
+
+    def tearDown(self):
+        del self.domain
+
+### MINRES + Jacobi
+
+class Test_SimpleSolveDudleyRect_Paso_MINRES_Jacobi(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.MINRES
+        self.preconditioner = SolverOptions.JACOBI
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_MINRES_Jacobi(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.MINRES
+        self.preconditioner = SolverOptions.JACOBI
+
+    def tearDown(self):
+        del self.domain
+
+### BiCGStab + Gauss-Seidel
+
+class Test_SimpleSolveDudleyRect_Paso_BICGSTAB_GaussSeidel(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.BICGSTAB
+        self.preconditioner = SolverOptions.GAUSS_SEIDEL
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_BICGSTAB_GaussSeidel(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.BICGSTAB
+        self.preconditioner = SolverOptions.GAUSS_SEIDEL
+
+    def tearDown(self):
+        del self.domain
+
+### PCG + Gauss-Seidel
+
+class Test_SimpleSolveDudleyRect_Paso_PCG_GaussSeidel(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.PCG
+        self.preconditioner = SolverOptions.GAUSS_SEIDEL
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_PCG_GaussSeidel(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.PCG
+        self.preconditioner = SolverOptions.GAUSS_SEIDEL
+
+    def tearDown(self):
+        del self.domain
+
+### TFQMR + Gauss-Seidel
+
+class Test_SimpleSolveDudleyRect_Paso_TFQMR_GaussSeidel(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.TFQMR
+        self.preconditioner = SolverOptions.GAUSS_SEIDEL
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_TFQMR_GaussSeidel(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.TFQMR
+        self.preconditioner = SolverOptions.GAUSS_SEIDEL
+
+    def tearDown(self):
+        del self.domain
+
+### MINRES + Gauss-Seidel
+
+class Test_SimpleSolveDudleyRect_Paso_MINRES_GaussSeidel(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.MINRES
+        self.preconditioner = SolverOptions.GAUSS_SEIDEL
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_MINRES_GaussSeidel(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.MINRES
+        self.preconditioner = SolverOptions.GAUSS_SEIDEL
+
+    def tearDown(self):
+        del self.domain
+
+### BiCGStab + RILU
+
+class Test_SimpleSolveDudleyRect_Paso_BICGSTAB_RILU(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.BICGSTAB
+        self.preconditioner = SolverOptions.RILU
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_BICGSTAB_RILU(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.BICGSTAB
+        self.preconditioner = SolverOptions.RILU
+
+    def tearDown(self):
+        del self.domain
+
+### PCG + RILU
+
+class Test_SimpleSolveDudleyRect_Paso_PCG_RILU(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.PCG
+        self.preconditioner = SolverOptions.RILU
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_PCG_RILU(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.PCG
+        self.preconditioner = SolverOptions.RILU
+
+    def tearDown(self):
+        del self.domain
+
+### TFQMR + RILU
+
+class Test_SimpleSolveDudleyRect_Paso_TFQMR_RILU(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.TFQMR
+        self.preconditioner = SolverOptions.RILU
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_TFQMR_RILU(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.TFQMR
+        self.preconditioner = SolverOptions.RILU
+
+    def tearDown(self):
+        del self.domain
+
+### MINRES + RILU
+
+class Test_SimpleSolveDudleyRect_Paso_MINRES_RILU(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Rectangle(NE0, NE1, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.MINRES
+        self.preconditioner = SolverOptions.RILU
+
+    def tearDown(self):
+        del self.domain
+
+class Test_SimpleSolveDudleyBrick_Paso_MINRES_RILU(SimpleSolveTestCase):
+    def setUp(self):
+        self.domain = Brick(NE0, NE1, NE2, 1, optimize=OPTIMIZE)
+        self.package = SolverOptions.PASO
+        self.method = SolverOptions.MINRES
+        self.preconditioner = SolverOptions.RILU
+
+    def tearDown(self):
+        del self.domain
+
+
+if __name__ == '__main__':
+    run_tests(__name__, exit_on_failure=True)
