305bdc33e8117781ef586c1f3487b1e6c3aaabed
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2016-03-26 03:06:30 +0000

    Fixing remaining examples

--- trunk/doc/examples/cookbook/example08a.py
+++ trunk/doc/examples/cookbook/example08a.py
@@ -101,7 +101,7 @@ if HAVE_FINLEY:
     src_length = 20; print("src_length = ",src_length)
     # set initial values for first two time steps with source terms
     y=U0*(cos(length(x-xc)*3.1415/src_length)+1)*whereNegative(length(x-xc)-src_length)
-    src_dir=numpy.array([0.,-1.]) # defines direction of point source as down
+    src_dir=np.array([0.,-1.]) # defines direction of point source as down
     y=y*src_dir
     mypde.setValue(y=y) #set the source as a function on the boundary
     # initial value of displacement at point source is constant (U0=0.01)
