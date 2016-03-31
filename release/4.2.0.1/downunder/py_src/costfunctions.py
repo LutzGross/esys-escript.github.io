24ce70ed96897b7b76215f3e7f69ae7ac1bd0648
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2016-03-31 04:57:54 +0000

    Re-licensing.

--- release/4.2.0.1/downunder/py_src/costfunctions.py
+++ release/4.2.0.1/downunder/py_src/costfunctions.py
@@ -5,8 +5,8 @@
 # http://www.uq.edu.au
 #
 # Primary Business: Queensland, Australia
-# Licensed under the Open Software License version 3.0
-# http://www.opensource.org/licenses/osl-3.0.php
+# Licensed under the Apache License, version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Development until 2012 by Earth Systems Science Computational Center (ESSCC)
 # Development 2012-2013 by School of Earth Sciences
@@ -21,8 +21,8 @@ from __future__ import print_function, division
 __copyright__="""Copyright (c) 2003-2016 by The University of Queensland
 http://www.uq.edu.au
 Primary Business: Queensland, Australia"""
-__license__="""Licensed under the Open Software License version 3.0
-http://www.opensource.org/licenses/osl-3.0.php"""
+__license__="""Licensed under the Apache License, version 2.0
+http://www.apache.org/licenses/LICENSE-2.0"""
 __url__="https://launchpad.net/escript-finley"
 
 __all__ = [ 'CostFunction', 'MeteredCostFunction' ]
--- release/4.2.0.1/dudley/src/Mesh_findMatchingFaces.cpp
+++ release/4.2.0.1/dudley/src/Mesh_findMatchingFaces.cpp
@@ -5,8 +5,8 @@
 * http://www.uq.edu.au
 *
 * Primary Business: Queensland, Australia
-* Licensed under the Open Software License version 3.0
-* http://www.opensource.org/licenses/osl-3.0.php
+* Licensed under the Apache License, version 2.0
+* http://www.apache.org/licenses/LICENSE-2.0
 *
 * Development until 2012 by Earth Systems Science Computational Center (ESSCC)
 * Development 2012-2013 by School of Earth Sciences
