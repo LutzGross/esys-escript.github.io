685b8154d652799043dbfcbfbd04084d9aa3581a
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2016-07-22 06:05:44 +0000

    added unroll flag to dudley and finley and enabled tests that were skipped before.

--- dudley/src/DudleyDomain.cpp
+++ dudley/src/DudleyDomain.cpp
@@ -1084,8 +1084,9 @@ escript::ASM_ptr DudleyDomain::newSystemMatrix(int row_blocksize,
 #ifdef ESYS_HAVE_TRILINOS
         const_TrilinosGraph_ptr graph(getTrilinosGraph());
         bool isComplex = (type & (int)SMT_COMPLEX);
-        escript::ASM_ptr sm(new TrilinosMatrixAdapter(m_mpiInfo,
-                    row_blocksize, row_functionspace, graph, isComplex));
+        bool unroll = (type & (int)SMT_UNROLL);
+        escript::ASM_ptr sm(new TrilinosMatrixAdapter(m_mpiInfo, row_blocksize,
+                    row_functionspace, graph, isComplex, unroll));
         return sm;
 #else
         throw DudleyException("newSystemMatrix: dudley was not compiled "
@@ -1367,6 +1368,13 @@ int DudleyDomain::getSystemMatrixTypeId(const bp::object& options) const
         int type = (int)SMT_TRILINOS;
         if (sb.isComplex())
             type |= (int)SMT_COMPLEX;
+        // TODO: This is required because MueLu (AMG) and Amesos2 (direct) do
+        // not support block matrices at this point. Remove once they do...
+        if (sb.getPreconditioner() == escript::SO_PRECONDITIONER_AMG ||
+                sb.getPreconditioner() == escript::SO_PRECONDITIONER_ILUT ||
+                sb.getSolverMethod() == escript::SO_METHOD_DIRECT) {
+            type |= (int)SMT_UNROLL;
+        }
         return type;
 #else
         throw DudleyException("Trilinos requested but not built with Trilinos.");
