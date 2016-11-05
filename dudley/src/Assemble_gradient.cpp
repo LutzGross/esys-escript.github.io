b5079ec04b14ad97762637823bc79cb0e2ee7213
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2016-11-05 11:38:07 +0000

    #386: complex gradients for dudley.

--- dudley/src/Assemble_gradient.cpp
+++ dudley/src/Assemble_gradient.cpp
@@ -24,15 +24,16 @@
 
 namespace dudley {
 
+template<typename Scalar>
 void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
-                       escript::Data& grad_data, const escript::Data& data)
+                       escript::Data& out, const escript::Data& data)
 {
     if (!nodes || !elements)
         return;
 
     const int numComps = data.getDataPointSize();
     const int NN = elements->numNodes;
-    const bool reducedIntegrationOrder = hasReducedIntegrationOrder(grad_data);
+    const bool reducedIntegrationOrder = hasReducedIntegrationOrder(out);
     const int data_type = data.getFunctionSpace().getTypeCode();
 
     dim_t numNodes = 0;
@@ -54,20 +55,21 @@ void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
     const int numDim = jac->numDim;
     const int numShapesTotal = jac->numShapes;
     const int numQuad = jac->numQuad;
-    const size_t localGradSize = sizeof(double) * numDim * numQuad * numComps;
 
     // check the dimensions of data
-    if (!grad_data.numSamplesEqual(numQuad, elements->numElements)) {
+    if (!out.numSamplesEqual(numQuad, elements->numElements)) {
         throw DudleyException("Assemble_gradient: illegal number of samples in gradient Data object");
     } else if (!data.numSamplesEqual(1, numNodes)) {
         throw DudleyException("Assemble_gradient: illegal number of samples of input Data object");
-    } else if (numDim * numComps != grad_data.getDataPointSize()) {
+    } else if (numDim * numComps != out.getDataPointSize()) {
         throw DudleyException("Assemble_gradient: illegal number of components in gradient data object.");
-    } else if (!grad_data.actsExpanded()) {
+    } else if (!out.actsExpanded()) {
         throw DudleyException("Assemble_gradient: expanded Data object is expected for output data.");
     }
 
-    grad_data.requireWrite();
+    const Scalar zero = static_cast<Scalar>(0);
+    const size_t localGradSize = sizeof(Scalar) * numDim * numQuad * numComps;
+    out.requireWrite();
 #pragma omp parallel
     {
         if (data_type == DUDLEY_NODES) {
@@ -75,15 +77,15 @@ void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
                 const int numShapes = 2;
 #pragma omp for
                 for (index_t e = 0; e < elements->numElements; e++) {
-                    double* grad_data_e = grad_data.getSampleDataRW(e);
-                    memset(grad_data_e, 0, localGradSize);
+                    Scalar* gradData_e = out.getSampleDataRW(e, zero);
+                    memset(gradData_e, 0, localGradSize);
                     for (int s = 0; s < numShapes; s++) {
                         const index_t n = elements->Nodes[INDEX2(s, e, NN)];
-                        const double* data_array = data.getSampleDataRO(n);
+                        const Scalar* data_array = data.getSampleDataRO(n, zero);
                         for (int q = 0; q < numQuad; q++) {
 #pragma ivdep
                             for (int l = 0; l < numComps; l++) {
-                                grad_data_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 0, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
                             }
@@ -94,18 +96,18 @@ void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
                 const int numShapes = 3;
 #pragma omp for
                 for (index_t e = 0; e < elements->numElements; e++) {
-                    double* grad_data_e = grad_data.getSampleDataRW(e);
-                    memset(grad_data_e, 0, localGradSize);
+                    Scalar* gradData_e = out.getSampleDataRW(e, zero);
+                    memset(gradData_e, 0, localGradSize);
                     for (int s = 0; s < numShapes; s++) {
                         const index_t n = elements->Nodes[INDEX2(s, e, NN)];
-                        const double* data_array = data.getSampleDataRO(n);
+                        const Scalar* data_array = data.getSampleDataRO(n, zero);
                         for (int q = 0; q < numQuad; q++) {
 #pragma ivdep
                             for (int l = 0; l < numComps; l++) {
-                                grad_data_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 0, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
-                                grad_data_e[INDEX4(l, 1, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 1, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 1, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
                             }
@@ -116,21 +118,21 @@ void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
                 const int numShapes = 4;
 #pragma omp for
                 for (index_t e = 0; e < elements->numElements; e++) {
-                    double* grad_data_e = grad_data.getSampleDataRW(e);
-                    memset(grad_data_e, 0, localGradSize);
+                    Scalar* gradData_e = out.getSampleDataRW(e, zero);
+                    memset(gradData_e, 0, localGradSize);
                     for (int s = 0; s < numShapes; s++) {
                         const index_t n = elements->Nodes[INDEX2(s, e, NN)];
-                        const double* data_array = data.getSampleDataRO(n);
+                        const Scalar* data_array = data.getSampleDataRO(n, zero);
                         for (int q = 0; q < numQuad; q++) {
 #pragma ivdep
                             for (int l = 0; l < numComps; l++) {
-                                grad_data_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 0, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
-                                grad_data_e[INDEX4(l, 1, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 1, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 1, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
-                                grad_data_e[INDEX4(l, 2, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 2, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 2, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
                             }
@@ -144,15 +146,15 @@ void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
                 const int numShapes = 2;
 #pragma omp for
                 for (index_t e = 0; e < elements->numElements; e++) {
-                    double* grad_data_e = grad_data.getSampleDataRW(e);
-                    memset(grad_data_e, 0, localGradSize);
+                    Scalar* gradData_e = out.getSampleDataRW(e, zero);
+                    memset(gradData_e, 0, localGradSize);
                     for (int s = 0; s < numShapes; s++) {
                         const index_t n = elements->Nodes[INDEX2(s, e, NN)];
-                        const double* data_array = data.getSampleDataRO(target[n]);
+                        const Scalar* data_array = data.getSampleDataRO(target[n], zero);
                         for (int q = 0; q < numQuad; q++) {
 #pragma ivdep
                             for (int l = 0; l < numComps; l++) {
-                                grad_data_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 0, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
                             }
@@ -163,18 +165,18 @@ void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
                 const int numShapes = 3;
 #pragma omp for
                 for (index_t e = 0; e < elements->numElements; e++) {
-                    double* grad_data_e = grad_data.getSampleDataRW(e);
-                    memset(grad_data_e, 0, localGradSize);
+                    Scalar* gradData_e = out.getSampleDataRW(e, zero);
+                    memset(gradData_e, 0, localGradSize);
                     for (int s = 0; s < numShapes; s++) {
                         const index_t n = elements->Nodes[INDEX2(s, e, NN)];
-                        const double* data_array = data.getSampleDataRO(target[n]);
+                        const Scalar* data_array = data.getSampleDataRO(target[n], zero);
                         for (int q = 0; q < numQuad; q++) {
 #pragma ivdep
                             for (int l = 0; l < numComps; l++) {
-                                grad_data_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 0, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
-                                grad_data_e[INDEX4(l, 1, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 1, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 1, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
                             }
@@ -185,21 +187,21 @@ void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
                 const int numShapes = 4;
 #pragma omp for
                 for (index_t e = 0; e < elements->numElements; e++) {
-                    double* grad_data_e = grad_data.getSampleDataRW(e);
-                    memset(grad_data_e, 0, localGradSize);
+                    Scalar* gradData_e = out.getSampleDataRW(e, zero);
+                    memset(gradData_e, 0, localGradSize);
                     for (int s = 0; s < numShapes; s++) {
                         const index_t n = elements->Nodes[INDEX2(s, e, NN)];
-                        const double* data_array = data.getSampleDataRO(target[n]);
+                        const Scalar* data_array = data.getSampleDataRO(target[n], zero);
                         for (int q = 0; q < numQuad; q++) {
 #pragma ivdep
                             for (int l = 0; l < numComps; l++) {
-                                grad_data_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 0, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 0, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
-                                grad_data_e[INDEX4(l, 1, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 1, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 1, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
-                                grad_data_e[INDEX4(l, 2, q, 0, numComps, numDim, numQuad)] +=
+                                gradData_e[INDEX4(l, 2, q, 0, numComps, numDim, numQuad)] +=
                                     data_array[l] *
                                     jac->DSDX[INDEX5(s, 2, q, 0, e, numShapesTotal, numDim, numQuad, 1)];
                             }
@@ -210,6 +212,15 @@ void Assemble_gradient(const NodeFile* nodes, const ElementFile* elements,
         }
     } // end parallel region
 }
+
+// instantiate our two supported versions
+template void Assemble_gradient<escript::DataTypes::real_t>(
+                       const NodeFile* nodes, const ElementFile* elements,
+                       escript::Data& out, const escript::Data& data);
+template void Assemble_gradient<escript::DataTypes::cplx_t>(
+                       const NodeFile* nodes, const ElementFile* elements,
+                       escript::Data& out, const escript::Data& data);
+
 
 } // namespace dudley
 
