2e55d06dab1a251263f1dfae26b3c26210e5e65f
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2017-02-28 03:58:20 +0000

    Work on new netcdf

--- trunk/finley/src/DomainFactory.cpp
+++ trunk/finley/src/DomainFactory.cpp
@@ -20,8 +20,18 @@
 #include <escript/SubWorld.h>
 
 #ifdef ESYS_HAVE_NETCDF
-#include <netcdfcpp.h>
+ #ifdef NETCDF4
+  #include <ncDim.h>
+  #include <ncVar.h>
+  #include <ncFile.h>
+  
+ #include <escript/NCHelper.h>  
+  
+ #else
+  #include <netcdfcpp.h>
+ #endif
 #endif
+
 
 #include <boost/python/extract.hpp>
 #include <boost/scoped_array.hpp>
@@ -32,9 +42,32 @@ using namespace std;
 using namespace escript;
 namespace bp = boost::python;
 
+#ifdef NETCDF4
+using namespace netCDF;
+#endif
+
 namespace finley {
 
 #ifdef ESYS_HAVE_NETCDF
+#ifdef NETCDF4
+
+// A convenience method to retrieve an integer attribute from a NetCDF file
+template<typename T>
+T ncReadAtt(NcFile& dataFile, const string& fName, const string& attrName)
+{
+    NcGroupAtt attr = dataFile.getAtt(attrName.c_str());
+    if (attr.isNull()) {
+        stringstream msg;
+        msg << "loadMesh: Error retrieving integer attribute '" << attrName
+            << "' from NetCDF file '" << fName << "'";
+        throw IOError(msg.str());
+    }
+    T value;
+    attr.getValues(&value);
+    return value;
+}
+#else    
+    
 // A convenience method to retrieve an integer attribute from a NetCDF file
 template<typename T>
 T ncReadAtt(NcFile* dataFile, const string& fName, const string& attrName)
@@ -50,6 +83,8 @@ T ncReadAtt(NcFile* dataFile, const string& fName, const string& attrName)
     delete attr;
     return value;
 }
+
+#endif
 #endif
 
 inline void cleanupAndThrow(FinleyDomain* dom, string msg)
@@ -58,6 +93,399 @@ inline void cleanupAndThrow(FinleyDomain* dom, string msg)
     string msgPrefix("loadMesh: NetCDF operation failed - ");
     throw IOError(msgPrefix+msg);
 }
+
+#ifdef NETCDF4
+
+Domain_ptr FinleyDomain::load(const string& fileName)
+{
+#ifdef ESYS_HAVE_NETCDF
+    JMPI mpiInfo = makeInfo(MPI_COMM_WORLD);
+    const string fName(mpiInfo->appendRankToFileName(fileName));
+
+    // Open NetCDF file for reading
+    NcGroupAtt attr;
+    NcVar nc_var_temp;
+    NcFile dataFile;
+    // Create the NetCDF file.
+    if (!openNcFile(dataFile, fileName))
+    {
+        throw IOError("load: opening of netCDF file for input failed.");
+    }    
+    // Read NetCDF integer attributes
+
+    // index_size was only introduced with 64-bit index support so fall back
+    // to 32 bits if not found.
+    int index_size;
+    try {
+        index_size = ncReadAtt<int>(dataFile, fName, "index_size");
+    } catch (IOError& e) {
+        index_size = 4;
+    }
+    // technically we could cast if reading 32-bit data on 64-bit escript
+    // but cost-benefit analysis clearly favours this implementation for now
+    if (sizeof(index_t) != index_size) {
+        throw IOError("loadMesh: size of index types at runtime differ from dump file");
+    }
+
+    int mpi_size = ncReadAtt<int>(dataFile, fName, "mpi_size");
+    int mpi_rank = ncReadAtt<int>(dataFile, fName, "mpi_rank");
+    int numDim = ncReadAtt<int>(dataFile, fName, "numDim");
+    int order = ncReadAtt<int>(dataFile, fName, "order");
+    int reduced_order = ncReadAtt<int>(dataFile, fName, "reduced_order");
+    dim_t numNodes = ncReadAtt<dim_t>(dataFile, fName, "numNodes");
+    dim_t num_Elements = ncReadAtt<dim_t>(dataFile, fName, "num_Elements");
+    dim_t num_FaceElements = ncReadAtt<dim_t>(dataFile, fName, "num_FaceElements");
+    dim_t num_ContactElements = ncReadAtt<dim_t>(dataFile, fName, "num_ContactElements");
+    dim_t num_Points = ncReadAtt<dim_t>(dataFile, fName, "num_Points");
+    int num_Elements_numNodes = ncReadAtt<int>(dataFile, fName, "num_Elements_numNodes");
+    int Elements_TypeId = ncReadAtt<int>(dataFile, fName, "Elements_TypeId");
+    int num_FaceElements_numNodes = ncReadAtt<int>(dataFile, fName, "num_FaceElements_numNodes");
+    int FaceElements_TypeId = ncReadAtt<int>(dataFile, fName, "FaceElements_TypeId");
+    int num_ContactElements_numNodes = ncReadAtt<int>(dataFile, fName, "num_ContactElements_numNodes");
+    int ContactElements_TypeId = ncReadAtt<int>(dataFile, fName, "ContactElements_TypeId");
+    int Points_TypeId = ncReadAtt<int>(dataFile, fName, "Points_TypeId");
+    int num_Tags = ncReadAtt<int>(dataFile, fName, "num_Tags");
+
+    // Verify size and rank
+    if (mpiInfo->size != mpi_size) {
+        stringstream msg;
+        msg << "loadMesh: The NetCDF file '" << fName
+            << "' can only be read on " << mpi_size
+            << " CPUs. Currently running: " << mpiInfo->size;
+        throw FinleyException(msg.str());
+    }
+    if (mpiInfo->rank != mpi_rank) {
+        stringstream msg;
+        msg << "loadMesh: The NetCDF file '" << fName
+            << "' should be read on CPU #" << mpi_rank
+            << " and NOT on #" << mpiInfo->rank;
+        throw FinleyException(msg.str());
+    }
+
+    // Read mesh name
+    if ((attr=dataFile.getAtt("Name")).isNull() ) {
+        stringstream msg;
+        msg << "loadMesh: Error retrieving mesh name from NetCDF file '"
+            << fName << "'";
+        throw IOError(msg.str());
+    }
+    string name;
+    attr.getValues(name);
+
+    // allocate mesh
+    FinleyDomain* dom = new FinleyDomain(name, numDim, mpiInfo);
+
+    // read nodes
+    NodeFile* nodes = dom->getNodes();
+    nodes->allocTable(numNodes);
+
+    try
+    {
+        // Nodes_Id
+        nc_var_temp = dataFile.getVar("Nodes_Id");
+        nc_var_temp.getVar(&nodes->Id[0]); // numNodes values read
+        // Nodes_Tag
+        nc_var_temp = dataFile.getVar("Nodes_Tag");
+        nc_var_temp.getVar(&nodes->Tag[0]);   // numNodes
+        // Nodes_gDOF
+        nc_var_temp = dataFile.getVar("Nodes_gDOF");
+        nc_var_temp.getVar(&nodes->globalDegreesOfFreedom[0]);   //numNodes
+        // Nodes_gNI
+        nc_var_temp = dataFile.getVar("Nodes_gNI");
+        nc_var_temp.getVar(&nodes->globalNodesIndex[0]); // numNodes
+        // Nodes_grDfI
+        nc_var_temp = dataFile.getVar("Nodes_grDfI");
+        nc_var_temp.getVar(&nodes->globalReducedDOFIndex[0]); // numNodes
+        // Nodes_grNI
+        nc_var_temp = dataFile.getVar("Nodes_grNI");
+        nc_var_temp.getVar(&nodes->globalReducedNodesIndex[0]);  // numNodes
+        // Nodes_Coordinates
+        nc_var_temp = dataFile.getVar("Nodes_Coordinates");
+        nc_var_temp.getVar(&nodes->Coordinates[0]); // (numNodes, numDim) 
+    }
+    catch (exceptions::NcException e)
+    {
+        cleanupAndThrow(dom, "Read vars from file");
+    }
+    nodes->updateTagList();
+
+    // read elements
+    const_ReferenceElementSet_ptr refElements(new ReferenceElementSet(
+                (ElementTypeId)Elements_TypeId, order, reduced_order));
+    ElementFile* elements = new ElementFile(refElements, mpiInfo);
+    dom->setElements(elements);
+    elements->allocTable(num_Elements);
+    elements->minColor = 0;
+    elements->maxColor = num_Elements-1;
+    if (num_Elements > 0) {
+        try
+        {
+            // Elements_Id
+            nc_var_temp = dataFile.getVar("Elements_Id");
+            nc_var_temp.getVar(&elements->Id[0]);    // num_Elements
+            // Elements_Tag
+            nc_var_temp = dataFile.getVar("Elements_Tag");
+            nc_var_temp.getVar(&elements->Tag[0]); // num_Elements
+            // Elements_Owner
+            nc_var_temp = dataFile.getVar("Elements_Owner");
+            nc_var_temp.getVar(&elements->Owner[0]);  // num_Elements
+            // Elements_Color
+            nc_var_temp = dataFile.getVar("Elements_Color");
+            nc_var_temp.getVar(&elements->Color[0]); // num_Elements
+        }
+        catch (exceptions::NcException e)
+        {
+            cleanupAndThrow(dom, "Readig element vars");            
+        }
+       // Now we need to adjust maxColor
+       index_t mc = elements->Color[0];
+       for (index_t i = 1; i < num_Elements; ++i) {
+           if (mc < elements->Color[i]) {
+               mc = elements->Color[i];
+           }
+       }
+       elements->maxColor = mc;
+       // Elements_Nodes
+       int* Elements_Nodes = new int[num_Elements*num_Elements_numNodes];       
+       try
+       {
+            nc_var_temp = dataFile.getVar("Elements_Nodes");
+            nc_var_temp.getVar(&Elements_Nodes[0]);    // (num_Elements, num_Elements_numNodes) )             
+       }
+       catch (exceptions::NcException e)
+       {
+           delete[] Elements_Nodes;
+           cleanupAndThrow(dom, "get_var(Elements_Nodes)");
+       }
+       // Copy temp array into elements->Nodes
+       for (index_t i = 0; i < num_Elements; i++) {
+           for (int j = 0; j < num_Elements_numNodes; j++) {
+               elements->Nodes[INDEX2(j,i,num_Elements_numNodes)]
+                    = Elements_Nodes[INDEX2(j,i,num_Elements_numNodes)];
+           }
+       }
+       delete[] Elements_Nodes;
+    } // num_Elements > 0
+    elements->updateTagList();
+
+    // get the face elements
+    const_ReferenceElementSet_ptr refFaceElements(
+            new ReferenceElementSet((ElementTypeId)FaceElements_TypeId,
+                order, reduced_order));
+    ElementFile* faces = new ElementFile(refFaceElements, mpiInfo);
+    dom->setFaceElements(faces);
+    faces->allocTable(num_FaceElements);
+    faces->minColor = 0;
+    faces->maxColor = num_FaceElements-1;
+    if (num_FaceElements > 0) {
+        try
+        {
+            // FaceElements_Id
+            nc_var_temp = dataFile.getVar("FaceElements_Id");
+            nc_var_temp.getVar(&faces->Id[0]); // num_FaceElements
+            // FaceElements_Tag
+            nc_var_temp = dataFile.getVar("FaceElements_Tag");
+            nc_var_temp.getVar(&faces->Tag[0]); // num_FaceElements
+            // FaceElements_Owner
+            nc_var_temp = dataFile.getVar("FaceElements_Owner");
+            nc_var_temp.getVar(&faces->Owner[0]);   // num_FaceElements
+            // FaceElements_Color
+            nc_var_temp = dataFile.getVar("FaceElements_Color");
+            nc_var_temp.getVar(&faces->Color[0]); // num_FaceElements
+        }
+        catch (exceptions::NcException e)
+        {
+            cleanupAndThrow(dom, "read face variables");
+        }
+        // Now we need to adjust maxColor
+        index_t mc = faces->Color[0];
+        for (index_t i = 1; i < num_FaceElements; ++i) {
+            if (mc < faces->Color[i]) {
+                mc = faces->Color[i];
+            }
+        }
+        faces->maxColor = mc;
+        // FaceElements_Nodes
+        int* FaceElements_Nodes = new int[num_FaceElements*num_FaceElements_numNodes];
+        try
+        {
+            nc_var_temp = dataFile.getVar("FaceElements_Nodes");
+            nc_var_temp.getVar(&(FaceElements_Nodes[0])); // num_FaceElements, num_FaceElements_numNodes) ) 
+        }
+        catch (exceptions::NcException e)
+        {
+            delete[] FaceElements_Nodes;
+            cleanupAndThrow(dom, "read face elements");
+        }
+        if ((nc_var_temp = dataFile.getVar("FaceElements_Nodes")), nc_var_temp.isNull()) {
+            delete[] FaceElements_Nodes;
+            cleanupAndThrow(dom, "get_var(FaceElements_Nodes)");
+        }
+        nc_var_temp.getVar(&(FaceElements_Nodes[0]));  // num_FaceElements, num_FaceElements_numNodes
+        // Copy temp array into faces->Nodes
+        for (index_t i = 0; i < num_FaceElements; i++) {
+            for (int j = 0; j < num_FaceElements_numNodes; j++) {
+                faces->Nodes[INDEX2(j,i,num_FaceElements_numNodes)] = FaceElements_Nodes[INDEX2(j,i,num_FaceElements_numNodes)];
+            }
+        }
+        delete[] FaceElements_Nodes;
+    } // num_FaceElements > 0
+    faces->updateTagList();
+
+    // get the Contact elements
+    const_ReferenceElementSet_ptr refContactElements(
+         new ReferenceElementSet((ElementTypeId)ContactElements_TypeId,
+             order, reduced_order));
+    ElementFile* contacts = new ElementFile(refContactElements, mpiInfo);
+    dom->setContactElements(contacts);
+    contacts->allocTable(num_ContactElements);
+    contacts->minColor = 0;
+    contacts->maxColor = num_ContactElements-1;
+    if (num_ContactElements > 0) {
+        // ContactElements_Id
+        if (( nc_var_temp = dataFile.getVar("ContactElements_Id")), nc_var_temp.isNull() )
+            cleanupAndThrow(dom, "get_var(ContactElements_Id)");
+        nc_var_temp.getVar(&contacts->Id[0]);       // num_ContactElements
+        // ContactElements_Tag
+        if (( nc_var_temp = dataFile.getVar("ContactElements_Tag")), nc_var_temp.isNull() )
+            cleanupAndThrow(dom, "get_var(ContactElements_Tag)");
+        nc_var_temp.getVar(&contacts->Tag[0]);  //, num_ContactElements
+        // ContactElements_Owner
+        if (( nc_var_temp = dataFile.getVar("ContactElements_Owner")), nc_var_temp.isNull() )
+            cleanupAndThrow(dom, "get_var(ContactElements_Owner)");
+        nc_var_temp.getVar(&contacts->Owner[0]);    //, num_ContactElements)
+        // ContactElements_Color
+        if (( nc_var_temp = dataFile.getVar("ContactElements_Color")), nc_var_temp.isNull() )
+            cleanupAndThrow(dom, "get_var(ContactElements_Color)");
+        nc_var_temp.getVar(&contacts->Color[0]);  //, num_ContactElements
+        // Now we need to adjust maxColor
+        index_t mc = contacts->Color[0];
+        for (index_t i = 1; i < num_ContactElements; ++i) {
+            if (mc < contacts->Color[i]) {
+                mc = contacts->Color[i];
+            }
+        }
+        contacts->maxColor = mc;
+        // ContactElements_Nodes
+        int* ContactElements_Nodes = new int[num_ContactElements*num_ContactElements_numNodes];
+        if ((nc_var_temp = dataFile.getVar("ContactElements_Nodes")), nc_var_temp.isNull()) {
+            delete[] ContactElements_Nodes;
+            cleanupAndThrow(dom, "get_var(ContactElements_Nodes)");
+        }
+        nc_var_temp.getVar(&ContactElements_Nodes[0]);    // num_ContactElements, num_ContactElements_numNodes
+        // Copy temp array into contacts->Nodes
+        for (index_t i = 0; i < num_ContactElements; i++) {
+            for (int j = 0; j < num_ContactElements_numNodes; j++) {
+                contacts->Nodes[INDEX2(j,i,num_ContactElements_numNodes)] = ContactElements_Nodes[INDEX2(j,i,num_ContactElements_numNodes)];
+            }
+        }
+        delete[] ContactElements_Nodes;
+    } // num_ContactElements > 0
+    contacts->updateTagList();
+
+    // get the Points (nodal elements)
+    const_ReferenceElementSet_ptr refPoints(new ReferenceElementSet(
+                (ElementTypeId)Points_TypeId, order, reduced_order));
+    ElementFile* points = new ElementFile(refPoints, mpiInfo);
+    dom->setPoints(points);
+    points->allocTable(num_Points);
+    points->minColor = 0;
+    points->maxColor = num_Points-1;
+    if (num_Points > 0) {
+        // Points_Id
+        if (( nc_var_temp = dataFile.getVar("Points_Id")), nc_var_temp.isNull())
+            cleanupAndThrow(dom, "get_var(Points_Id)");
+        nc_var_temp.getVar(&points->Id[0]); // num_Points
+        // Points_Tag
+        if (( nc_var_temp = dataFile.getVar("Points_Tag")), nc_var_temp.isNull())
+            cleanupAndThrow(dom, "get_var(Points_Tag)");
+        nc_var_temp.getVar(&points->Tag[0]);    // num_Points
+        // Points_Owner
+        if (( nc_var_temp = dataFile.getVar("Points_Owner")), nc_var_temp.isNull())
+            cleanupAndThrow(dom, "get_var(Points_Owner)");
+        nc_var_temp.getVar(&points->Owner[0]);  // num_Points
+        // Points_Color
+        if (( nc_var_temp = dataFile.getVar("Points_Color")), nc_var_temp.isNull())
+            cleanupAndThrow(dom, "get_var(Points_Color)");
+        nc_var_temp.getVar(&points->Color[0]);  // num_Points
+        // Now we need to adjust maxColor
+        index_t mc = points->Color[0];
+        for (index_t i = 1; i < num_Points; ++i) {
+            if (mc < points->Color[i]) {
+                mc = points->Color[i];
+            }
+        }
+        points->maxColor = mc;
+        // Points_Nodes
+        int* Points_Nodes = new int[num_Points];
+        if ((nc_var_temp = dataFile.getVar("Points_Nodes")), nc_var_temp.isNull()) {
+            delete[] Points_Nodes;
+            cleanupAndThrow(dom, "get_var(Points_Nodes)");
+        }
+        nc_var_temp.getVar(&Points_Nodes[0]);  // num_Points
+        // Copy temp array into points->Nodes
+        for (index_t i = 0; i < num_Points; i++) {
+            points->Id[points->Nodes[INDEX2(0,i,1)]] = Points_Nodes[i];
+        }
+        delete[] Points_Nodes;
+    } // num_Points > 0
+    points->updateTagList();
+
+    // get the tags
+    if (num_Tags > 0) {
+        // Temp storage to gather node IDs
+        int *Tags_keys = new int[num_Tags];
+        char name_temp[4096];
+        int i;
+
+        // Tags_keys
+        if (( nc_var_temp = dataFile.getVar("Tags_keys")), nc_var_temp.isNull() ) {
+            delete[] Tags_keys;
+            cleanupAndThrow(dom, "get_var(Tags_keys)");
+        }
+        nc_var_temp.getVar(&Tags_keys[0]); // num_Tags
+        for (i=0; i<num_Tags; i++) {
+          // Retrieve tag name
+          sprintf(name_temp, "Tags_name_%d", i);
+          if ((attr=dataFile.getAtt(name_temp)), attr.isNull() ) {
+              delete[] Tags_keys;
+              stringstream msg;
+              msg << "get_att(" << name_temp << ")";
+              cleanupAndThrow(dom, msg.str());
+          }
+          std::string name;
+          attr.getValues(name);
+//           boost::scoped_array<char> name(attr->as_string(0));
+//           delete attr;
+//           dom->setTagMap(name.get(), Tags_keys[i]);
+          dom->setTagMap(name.c_str(), Tags_keys[i]);
+        }
+        delete[] Tags_keys;
+    }
+
+    // Nodes_DofDistribution
+    IndexVector first_DofComponent(mpi_size+1);
+    if ((nc_var_temp = dataFile.getVar("Nodes_DofDistribution")), nc_var_temp.isNull() ) {
+        cleanupAndThrow(dom, "get_var(Nodes_DofDistribution)");
+    }
+    nc_var_temp.getVar(&first_DofComponent[0]); // mpi_size+1
+
+    // Nodes_NodeDistribution
+    IndexVector first_NodeComponent(mpi_size+1);
+    if ((nc_var_temp = dataFile.getVar("Nodes_NodeDistribution")), nc_var_temp.isNull() ) {
+        cleanupAndThrow(dom, "get_var(Nodes_NodeDistribution)");
+    }
+    nc_var_temp.getVar(&first_NodeComponent[0]);    // mpi_size+1
+    dom->createMappings(first_DofComponent, first_NodeComponent);
+
+    return dom->getPtr();
+#else
+    throw FinleyException("loadMesh: not compiled with NetCDF. Please contact your installation manager.");
+#endif // ESYS_HAVE_NETCDF
+}
+
+
+#else
 
 Domain_ptr FinleyDomain::load(const string& fileName)
 {
@@ -469,6 +897,8 @@ Domain_ptr FinleyDomain::load(const string& fileName)
     throw FinleyException("loadMesh: not compiled with NetCDF. Please contact your installation manager.");
 #endif // ESYS_HAVE_NETCDF
 }
+
+#endif
 
 Domain_ptr readMesh_driver(const bp::list& args)
 {
