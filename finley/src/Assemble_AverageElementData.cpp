3269ecd606572954c7c94c2392525954433f3e56
caltinay <caltinay@77569008-7704-0410-b7a0-a92fef0b09fd>
2017-05-15 10:17:13 +0000

    complex element data interpolation in finley.

--- finley/src/Assemble_AverageElementData.cpp
+++ finley/src/Assemble_AverageElementData.cpp
@@ -28,30 +28,32 @@
 
 namespace finley {
 
+template<typename Scalar>
 void Assemble_AverageElementData(const ElementFile* elements,
                                  escript::Data& out, const escript::Data& in)
 {
     if (!elements)
         return;
 
-    const double *wq;
+    const double* wq;
     int numQuad_in, numQuad_out;
     if (util::hasReducedIntegrationOrder(in)) {
-        numQuad_in=elements->referenceElementSet->referenceElementReducedQuadrature->Parametrization->numQuadNodes;
-        wq=&elements->referenceElementSet->referenceElementReducedQuadrature->Parametrization->QuadWeights[0];
+        numQuad_in = elements->referenceElementSet->referenceElementReducedQuadrature->Parametrization->numQuadNodes;
+        wq = &elements->referenceElementSet->referenceElementReducedQuadrature->Parametrization->QuadWeights[0];
     } else {
-        numQuad_in=elements->referenceElementSet->referenceElement->Parametrization->numQuadNodes;
-        wq=&elements->referenceElementSet->referenceElement->Parametrization->QuadWeights[0];
+        numQuad_in = elements->referenceElementSet->referenceElement->Parametrization->numQuadNodes;
+        wq = &elements->referenceElementSet->referenceElement->Parametrization->QuadWeights[0];
     }
     if (util::hasReducedIntegrationOrder(out)) {
-        numQuad_out=elements->referenceElementSet->referenceElementReducedQuadrature->Parametrization->numQuadNodes;
+        numQuad_out = elements->referenceElementSet->referenceElementReducedQuadrature->Parametrization->numQuadNodes;
     } else {
-        numQuad_out=elements->referenceElementSet->referenceElement->Parametrization->numQuadNodes;
+        numQuad_out = elements->referenceElementSet->referenceElement->Parametrization->numQuadNodes;
     }
 
     // check out and in
-    const dim_t numElements=elements->numElements;
-    const int numComps=out.getDataPointSize();
+    const dim_t numElements = elements->numElements;
+    const int numComps = out.getDataPointSize();
+    const Scalar zero = static_cast<Scalar>(0);
 
     if (numComps != in.getDataPointSize()) {
         throw escript::ValueError("Assemble_AverageElementData: number of components of input and output data do not match.");
@@ -61,38 +63,48 @@ void Assemble_AverageElementData(const ElementFile* elements,
         throw escript::ValueError("Assemble_AverageElementData: illegal number of samples of output Data object");
     } else if (!out.actsExpanded()) {
         throw escript::ValueError("Assemble_AverageElementData: expanded Data object is expected for output data.");
+    } else if (in.isComplex() != out.isComplex()) {
+        throw escript::ValueError("Assemble_AverageElementData: complexity of input and output data must match.");
     } else {
+        out.requireWrite();
         if (in.actsExpanded()) {
-            double vol=0.;
-            for (int q=0; q<numQuad_in;++q) vol+=wq[q];
-            const double volinv=1./vol;
-            out.requireWrite();
+            double vol = 0.;
+            for (int q = 0; q < numQuad_in; ++q)
+                vol += wq[q];
+            const double volinv = 1./vol;
 #pragma omp parallel for
-            for (index_t n=0; n<numElements; n++) {
-                const double *in_array = in.getSampleDataRO(n);
-                double *out_array = out.getSampleDataRW(n);
-                for (int i=0; i<numComps; ++i) {
-                    double rtmp=0.;
-                    for (int q=0; q<numQuad_in; ++q)
-                        rtmp+=in_array[INDEX2(i,q,numComps)]*wq[q];
-                    rtmp*=volinv;
-                    for (int q=0; q<numQuad_out; ++q)
-                        out_array[INDEX2(i,q,numComps)]=rtmp;
+            for (index_t n = 0; n < numElements; n++) {
+                const Scalar* in_array = in.getSampleDataRO(n, zero);
+                Scalar* out_array = out.getSampleDataRW(n, zero);
+                for (int i = 0; i < numComps; ++i) {
+                    Scalar rtmp = zero;
+                    for (int q = 0; q < numQuad_in; ++q)
+                        rtmp += in_array[INDEX2(i,q,numComps)] * wq[q];
+                    rtmp *= volinv;
+                    for (int q = 0; q < numQuad_out; ++q)
+                        out_array[INDEX2(i,q,numComps)] = rtmp;
                 }
             }
         } else { // constant data
-            const size_t numComps_size=numComps*sizeof(double);
-            out.requireWrite();
+            const size_t numComps_size = numComps * sizeof(Scalar);
 #pragma omp parallel for
-            for (index_t n=0; n<numElements; n++) {
-                const double *in_array = in.getSampleDataRO(n);
-                double *out_array = out.getSampleDataRW(n);
-                for (int q=0; q<numQuad_out; q++)
+            for (index_t n = 0; n < numElements; n++) {
+                const Scalar* in_array = in.getSampleDataRO(n, zero);
+                Scalar* out_array = out.getSampleDataRW(n, zero);
+                for (int q = 0; q < numQuad_out; q++)
                     memcpy(out_array+q*numComps, in_array, numComps_size);
             }
         }
     }
 }
+
+// instantiate our two supported versions
+template void Assemble_AverageElementData<escript::DataTypes::real_t>(
+                const ElementFile* elements,
+                escript::Data& out, const escript::Data& in);
+template void Assemble_AverageElementData<escript::DataTypes::cplx_t>(
+                const ElementFile* elements,
+                escript::Data& out, const escript::Data& in);
 
 } // namespace finley
 
