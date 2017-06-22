c14d9daaf4d6415f7f638f6165008866caae7dae
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2017-06-22 06:37:41 +0000

    Final tests for #269
    Also, add explanation of how hasInf and hasNaN are not mutually exclusive.
    To doc-strings

--- escriptcore/test/python/test_util_NaN_funcs.py
+++ escriptcore/test/python/test_util_NaN_funcs.py
@@ -69,3 +69,57 @@ class Test_util_NaN_funcs(unittest.TestCase):
         sigma.setTaggedValue(1 , es.Lsup(dat))
         sigma.replaceNaN(3+4j)
         self.assertEqual(es.Lsup(sigma), 5)
+
+    def test_replaceInfExpanded(self):
+        dom=self.domain
+        scl=es.Scalar(1,es.ContinuousFunction(dom))
+        scl.expand()
+        sclNaN=scl/0
+        self.assertTrue(sclNaN.hasInf(),"sclNaN should contain Inf but its doesn't")
+        sclNaN.replaceNaN(17.0) # Make sure it is distinguishing between NaN and Inf
+        sclNaN.replaceInf(15.0)
+        self.assertEqual(es.Lsup(sclNaN), 15.0)
+        scl=es.Scalar(10,es.ContinuousFunction(dom))
+        scl.expand()
+        scl.promote()
+        if not es.getEscriptParamInt('AUTOLAZY')==1:            
+            sclNaN=scl/0
+            self.assertTrue(sclNaN.hasInf(),"sclNaN should contain Inf but its doesn't")
+            sclNaN.replaceInf(3+4j)
+            sclNaN.replaceNaN(3+19j)    # Make sure it is distinguishing between NaN and Inf
+            self.assertEqual(es.Lsup(sclNaN), 5.0)
+
+    
+    def test_replaceInfConstant(self):
+        dom=self.domain
+        dat = es.Data(10,es.ContinuousFunction(dom))
+        dat=(dat*1)/0
+        self.assertTrue(dat.hasInf(),"dat should contain Inf but its doesn't")
+        dat.replaceNaN(18)
+        dat.replaceInf(10)
+        self.assertEqual(es.Lsup(dat), 10)
+        dat = es.Data(10,es.ContinuousFunction(dom))
+        dat.promote()
+        dat=(dat*1)/0
+        self.assertTrue(dat.hasInf(),"dat should contain Inf but its doesn't")
+        dat.replaceInf(4+3j)
+        dat.replaceNaN(19+3j)
+        self.assertEqual(es.Lsup(dat), 5)
+
+    def test_replaceInfTagged(self):
+        dom=self.domain
+        sigma = es.Scalar(1,es.FunctionOnBoundary(dom))
+        dat=(sigma*1)/0
+        sigma.setTaggedValue(1 , es.Lsup(dat))
+        sigma.replaceNaN(11)
+        sigma.replaceInf(10)
+        self.assertEqual(es.Lsup(sigma), 10)
+        sigma = es.Scalar(1,es.FunctionOnBoundary(dom))
+        dat=(sigma*1)/0
+        dat.promote()
+        sigma.setTaggedValue(1 , es.Lsup(dat))
+        sigma.replaceInf(3+4j)
+        sigma.replaceNaN(7+12j)
+        self.assertEqual(es.Lsup(sigma), 5)
+
+
