da24ac32b3608f8190f2ba8afb41ac26b56a457e
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2017-11-09 02:26:55 +0000

    Building up options files for capybara.

---
+++ trunk/scons/capybara_options.py
@@ -0,0 +1,31 @@
+
+##############################################################################
+#
+# Copyright (c) 2003-2017 by The University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Apache License, version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development 2012-2013 by School of Earth Sciences
+# Development from 2014 by Centre for Geoscience Computing (GeoComp)
+#
+##############################################################################
+
+from templates.stretch_py3_mpi_options import *
+
+#copied this from guineapig originally
+# disabled until the boost issue is fixed.
+#cuda = True
+#nvccflags = "-ccbin=g++-4.9 -arch=sm_30 -DBOOST_NOINLINE='__attribute__((noinline))'"
+
+#parmetis = True
+#umfpack = True
+#silo = True
+#trilinos = True
+#trilinos_prefix = '/opt/trilinos_hybrid_eti'
+#cxx_extra += " -Wextra -Wno-deprecated-declarations -Wno-unused-parameter"
+launcher = "mpirun ${AGENTOVERRIDE} ${EE} --mca io romio314 --oversubscribe --map-by node:pe=%t -bind-to none -np %N %b"
+
---
+++ trunk/scons/templates/stretch_py3_options.py
@@ -0,0 +1,25 @@
+
+##############################################################################
+#
+# Copyright (c) 2003-2017 by The University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Apache License, version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development 2012-2013 by School of Earth Sciences
+# Development from 2014 by Centre for Geoscience Computing (GeoComp)
+#
+##############################################################################
+
+# This is a template configuration file for escript on Debian/GNU Linux.
+# Refer to README_FIRST for usage instructions.
+
+from scons.templates.stretch_options import *
+
+pythoncmd = 'python3'
+
+boost_libs = boost_py3_libs
+
