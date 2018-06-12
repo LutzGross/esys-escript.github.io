492a8466f51f91d1e8ca64e33e364d34d89171e4
jfenwick <jfenwick@77569008-7704-0410-b7a0-a92fef0b09fd>
2018-06-12 01:00:29 +0000

    Remove MathJax replacement (this should be handled from options file now).

--- trunk/localdebian/utils/tweak.sh
+++ trunk/localdebian/utils/tweak.sh
@@ -32,6 +32,3 @@ sed -i -e "s%ESCRIPT_ROOT=/usr/lib/python-escript%ESCRIPT_ROOT=/usr/lib/python3-
 sed -i -e "s%STDLOCATION=0%STDLOCATION=1%" $WORK3M/bin/run-escript3-mpi
 sed -i -e "s%PYTHON_CMD=python%PYTHON_CMD=python3%" $WORK3M/bin/run-escript3-mpi
 
-# documentation
-sed -i -e 's%<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>%%' $WORK3/release/doc/sphinx_api/*.html
-sed -i -e 's%<script type="text/javascript" src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>%%' $WORK3/release/doc/sphinx_api/*.html
