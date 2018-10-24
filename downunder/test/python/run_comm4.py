1453cb9c8b371d51b8c4b33fb568ef797dfc9e7f
aellery <aellery@77569008-7704-0410-b7a0-a92fef0b09fd>
2018-10-24 23:17:17 +0000

    Fixed the bug that was causing the downunder/test/python/run_comm4.py test to fail.
    The cause of the error was a call to numpy.InterpolatedUnivariateSpline on line 626. This function returns an error if the values in the array x are not strictly increasing. I have added a couple of loops (lines 589-610) that sort this array and delete any duplicate entries. This has fixed the error and the test now runs to completion.
    
    I have also changed a line in seismic.py that was causing a different test to crash (doc/examples/synthetic_HTI.py). This error was caused because python could not locate a function.

--- downunder/test/python/run_comm4.py
+++ downunder/test/python/run_comm4.py
@@ -584,6 +584,31 @@ class Test_COMMEMI4(unittest.TestCase):
         y0 = numpy.array( obj_mt2d.loc.getValue(arho_2d[0]) )
         y1 = numpy.array( obj_mt2d.loc.getValue(aphi_2d[0]) )
 
+        # Sort these arrays (in unison) to prevent the call to InterpolatedUnivariateSpline below
+        # from returning an error
+        stillSorting = True
+        while stillSorting:
+            stillSorting = False
+            for i in range(0, len(x)-1):
+                if x[i] > x[i+1]:
+                    x[i],   x[i+1] = x[i+1],   x[i]
+                    y0[i], y0[i+1] = y0[i+1], y0[i]
+                    y1[i], y1[i+1] = y1[i+1], y1[i]
+                    stillSorting = True
+
+        # Delete any duplicates in the array to prevent the call to InterpolatedUnivariateSpline below
+        # from returning an error
+        notDone = True
+        while notDone:
+            notDone = False
+            for i in range(0, len(x)-1):
+                if x[i] == x[i+1]:
+                    notDone = True
+                    x = numpy.delete(x,i)
+                    y0 = numpy.delete(y0,i)
+                    y1 = numpy.delete(y1,i)
+                    break
+
         # Zhdanov et al, 1997, -- Model 2D-1 Table B.33. Model2D-4 (T=1.0, z=0), see 
         # "Methods for modelling electromagnetic fields. Results from COMMEMI -- the
         # international project on the comparison of modelling results for electromag-
@@ -598,7 +623,7 @@ class Test_COMMEMI4(unittest.TestCase):
         xs = numpy.array( [-10, -7, -6, -5, 2, 5] )*1000 + x.max()/2.0
 
         # Setup interpolation to get values at specified stations (for comparison):
-        fi = InterpolatedUnivariateSpline(x, y0) 
+        fi = InterpolatedUnivariateSpline(x, y0)
         # Save esscript values at comparison points in text file:
         # uncomment to investigate
         #numpy.savetxt("mesh/commemi-4/commemi4_"+mode.lower()+".dat", numpy.column_stack((xs,fi(xs))), fmt='%g')
