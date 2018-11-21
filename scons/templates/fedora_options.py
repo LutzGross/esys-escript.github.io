ba20738afa91f02a7105b2742e5bfe29efa58fb6
aellery <aellery@77569008-7704-0410-b7a0-a92fef0b09fd>
2018-11-21 06:02:04 +0000

    Updated the Fedora installation instructions in the installation guide.

--- scons/templates/fedora_options.py
+++ scons/templates/fedora_options.py
@@ -21,4 +21,8 @@ escript_opts_version = 203
 openmp = True
 boost_libs = ['boost_python']
 netcdf = True
-
+pythonlibpath = ['/usr/lib64']
+pythonlibname = ['python2.7']
+pythonincpath = ['/usr/include/python2.7']
+umfpack = True
+umfpack_prefix = ['/usr/include/suitesparse','/usr/lib']
