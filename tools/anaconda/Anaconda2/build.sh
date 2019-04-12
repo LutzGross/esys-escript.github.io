433bdef73bfd0b62c960732ba4eb90f291a2bd69
aellery <aellery@77569008-7704-0410-b7a0-a92fef0b09fd>
2019-04-12 09:39:17 +0000

    Fixed a bug in the anaconda build.
    Also, escript now now be installed / uninstalled inside Anaconda using the commands
    
    conda install -c aellery escript
    conda uninstall escript

--- tools/anaconda/Anaconda2/build.sh
+++ tools/anaconda/Anaconda2/build.sh
@@ -13,17 +13,55 @@ set -x -e
 INCLUDE_PATH="${PREFIX}/include"
 LIBRARY_PATH="${PREFIX}/lib"
 
+CMAKE_INSTALL_PREFIX=${PREFIX}
+INCLUDE_DIRECTORIES="${PREFIX}/include"
+LDFLAGS=-L"${PREFIX}/lib"
+
 if [ "$(uname)" == "Linux" ]; then
 
-    cd ${SRC_DIR}/Boost_Source
+    # None of the gcc libraries on Anaconda contain libm.so
+    cd ${SRC_DIR}/openlibm_source
+    sed -i 's+prefix = /usr/local+prefix = ${PREFIX}+g' Make.inc
+    make -j"${CPU_COUNT}" install | tee openlibm.log 2>&1 
+    ln -s ${PREFIX}/lib/libopenlibm.so ${PREFIX}/lib/libm.so
+    ln -s ${PREFIX}/lib/libopenlibm.so.1 ${PREFIX}/lib/libm.so.1
+    ln -s ${PREFIX}/lib/libopenlibm.so.1.0 ${PREFIX}/lib/libm.so.1.0
+    ln -s ${PREFIX}/lib/libopenlibm.a ${PREFIX}/lib/libm.a
 
+    mkdir ${SRC_DIR}/lapack_build
+    cd ${SRC_DIR}/lapack_build
+    cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+        -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
+        -DCMAKE_CC_FLAGS='-fPIC' \
+        -DCMAKE_CXX_FLAGS='-fPIC' \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DBUILD_SHARED_LIBS=ON \
+        -DBUILD_TESTING=OFF \
+        -DLAPACKE=ON \
+        -DCBLAS=ON \
+        ${SRC_DIR}/lapack_source
+    make -j"${CPU_COUNT}" install | tee lapack.log 2>&1
+
+    # mkdir ${SRC_DIR}/netcdf_build
+    # cd ${SRC_DIR}/netcdf_build
+    # cmake \
+    #      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+    #      -DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS} \
+    #      -DCMAKE_CXX_FLAGS='-fPIC' \
+    #      -DBUILD_SHARED_LIBS=ON \
+    #      ${SRC_DIR}/netcdf_source 
+    # make -j"${CPU_COUNT}" install | tee netcdf.log 2>&1
+
+    # cd ${SRC_DIR}/netcdf_cxx_source
+    # ./configure --prefix=${PREFIX}
+    # make -j"${CPU_COUNT}" install | tee netcdf_cxx.log 2>&1
+
+    cd ${SRC_DIR}/boost_source
     ./bootstrap.sh \
         --prefix="${PREFIX}" \
         --with-python="${PYTHON}" \
-        --with-python-root="${PREFIX} : ${PREFIX}/include/python${PY_VER}m ${PREFIX}/include/python${PY_VER}" \
-        --with-icu="${PREFIX}" \
-        | tee bootstrap.log 2>&1
-
+        --with-python-root="${PREFIX} : "${PREFIX}/include"/python${PY_VER}m "${PREFIX}/include"/python${PY_VER}" \
+        --with-icu="${PREFIX}" | tee bootstrap.log 2>&1
     ./b2 -q -d+2 -a \
         variant=release \
         address-model="${ARCH}" \
@@ -31,11 +69,12 @@ if [ "$(uname)" == "Linux" ]; then
         debug-symbols=off \
         threading=multi \
         runtime-link=shared \
-        link=static,shared \
+        link=shared \
         toolset=gcc \
+        cxxflags='-fPIC -w' \
         python="${PY_VER}" \
-        include="${INCLUDE_PATH}" \
-        linkflags="-L${LIBRARY_PATH}" \
+        include=""${PREFIX}/include"" \
+        linkflags="-L"${PREFIX}/lib"" \
         --with-python \
         --with-iostreams \
         --with-random \
@@ -43,29 +82,44 @@ if [ "$(uname)" == "Linux" ]; then
         -j"${CPU_COUNT}" \
         install | tee b2.log 2>&1
 
-    cd ${SRC_DIR}/Trilinos_Source
+    # trilinos
+    cd ${SRC_DIR}/trilinos_source
     git reset --hard c87c9f7224b36c845aa7ef19a636f9c99c9c02d4
 
     mkdir ${SRC_DIR}/trilinos_build
     cd ${SRC_DIR}/trilinos_build
     cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
         -DCMAKE_MAKE_PROGRAM=${PREFIX}/bin/make \
+        -DCMAKE_VERBOSE_MAKEFILE=FALSE \
         -DTrilinos_ENABLE_CXX11=ON \
-        -DTrilinos_ENABLE_Fortran=OFF \
-        -DBUILD_SHARED_LIBS=ON \
-        -DOpenMP_C_FLAGS='-w -fopenmp' \
-        -DOpenMP_CXX_FLAGS='-w -fopenmp' \
+        -DTrilinos_ENABLE_Fortran=ON \
+        -DBUILD_SHARED_LIBS=OFF \
+        -DOpenMP_C_FLAGS='-w -fopenmp ' \
+        -DOpenMP_CXX_FLAGS='-w -fopenmp ' \
+        -DCMAKE_CXX_FLAGS='-fPIC -L${PREFIX}/lib -lblas -llapack -lumfpack' \
+        -DCMAKE_CC_FLAGS='-fPIC -L${PREFIX}/lib -lblas -llapack -lumfpack' \
         -DTPL_ENABLE_BLAS=ON \
         -DBLAS_LIBRARY_NAMES='blas' \
+        -DTPL_BLAS_INCLUDE_DIRS="${PREFIX}/include" \
+        -DTPL_BLAS_LIBRARY_DIRS="${PREFIX}/lib" \
+        -DTPL_BLAS_LIBRARY_NAMES=${PREFIX}/lib/libblas.so \
         -DTPL_ENABLE_LAPACK=ON \
+        -DTPL_LAPACK_INCLUDE_DIRS="${PREFIX}/include" \
+        -DTPL_LAPACK_LIBRARY_DIRS="${PREFIX}/lib" \
+        -DTPL_LAPACK_LIBRARY_NAMES=${PREFIX}/lib/liblapack.so \
         -DTPL_ENABLE_Boost=ON \
-        -DTPL_ENABLE_Cholmod=OFF \
-        -DTPL_ENABLE_Matio=OFF \
-        -DTPL_ENABLE_METIS=OFF \
-        -DTPL_ENABLE_ParMETIS=OFF \
-        -DMETIS_LIBRARY_NAMES='metis' \
-        -DTPL_ENABLE_ScaLAPACK=OFF \
-        -DTPL_ENABLE_UMFPACK=OFF \
+        -DTPL_Boost_INCLUDE_DIRS="${PREFIX}/include" \
+        -DTPL_Boost_LIBRARY_DIRS="${PREFIX}/lib" \
+        -DTPL_ENABLE_Cholmod=ON \
+        -DTPL_Cholmod_INCLUDE_DIRS="${PREFIX}/include" \
+        -DTPL_Cholmod_LIBRARY_DIRS="${PREFIX}/lib" \
+        -DTPL_Cholmod_LIBRARIES='libcholmod.so;libamd.so;libcolamd.so' \
+        -DTPL_ENABLE_METIS=ON \
+        -DTPL_METIS_INCLUDE_DIRS="${PREFIX}/include" \
+        -DTPL_METIS_LIBRARY_DIRS="${PREFIX}/lib" \
+        -DTPL_ENABLE_UMFPACK=ON \
+        -DTPL_UMFPACK_INCLUDE_DIRS="${PREFIX}/include" \
+        -DTPL_UMFPACK_LIBRARY_DIRS="${PREFIX}/lib" \
         -DTrilinos_ENABLE_Amesos2=ON \
         -DTrilinos_ENABLE_Belos=ON \
         -DTrilinos_ENABLE_Ifpack2=ON \
@@ -75,21 +129,30 @@ if [ "$(uname)" == "Linux" ]; then
         -DTrilinos_ENABLE_Teuchos=ON \
         -DTrilinos_ENABLE_COMPLEX=ON \
         -DTrilinos_ENABLE_OpenMP=ON \
-        -DTrilinos_ENABLE_MPI=OFF \
-        -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=ON \
+        -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF \
         -DTrilinos_ENABLE_EXPLICIT_INSTANTIATION=ON \
-        ${SRC_DIR}/Trilinos_Source
+        ${SRC_DIR}/trilinos_source
     make -j"${CPU_COUNT}" install
 
     cd ${SRC_DIR}/escript
     scons -j"${CPU_COUNT}" \
-        options_file="${SRC_DIR}/escript/scons/templates/sid_options.py" \
+        options_file="${SRC_DIR}/escript/scons/templates/stretch_options.py" \
         prefix="${PREFIX}" \
-        trilinos=True \
-        trilinos_prefix="${PREFIX}" \
+        cxx_extra="-fPIC" \
+        ld_extra="-L${PREFIX}/lib -lblas -llapack -lumfpack" \
+        boost_prefix="${PREFIX}" \
+        boost_libs='boost_python27' \
+        pythoncmd=`which python` \
         pythonlibpath="${PREFIX}/lib" \
         pythonincpath="${PREFIX}/include/python${PY_VER}" \
         pythonlibname="python2.7" \
+        trilinos=1 \
+        trilinos_prefix="${PREFIX}" \
+        umfpack=0 \
+        umfpack_prefix=[""${PREFIX}/include"",""${PREFIX}/lib""] \
+        lapack=0 \
+        lapack_prefix=[""${PREFIX}/include"/atlas",""${PREFIX}/lib""] \
+        lapack_libs=['lapack'] \
         werror=0
 
 fi
