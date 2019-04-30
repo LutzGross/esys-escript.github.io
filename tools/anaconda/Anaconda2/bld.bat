0e2e1705152567121e6997c6f8b52569632f002d
aellery <aellery@77569008-7704-0410-b7a0-a92fef0b09fd>
2019-04-30 06:40:36 +0000

    -Fixed bug in the linux anaconda build script. -Updated the trilinos section of the install guide.

--- tools/anaconda/Anaconda2/bld.bat
+++ tools/anaconda/Anaconda2/bld.bat
@@ -1,156 +1,42 @@
 
-:: Set the right msvc version according to Python version
-if "%PY_VER%"=="2.7" (
-    set MSVC_VER=9.0
-    set LIB_VER=90
-) else if "%PY_VER%"=="3.4" (
-    set MSVC_VER=10.0
-    set LIB_VER=100
-) else (
-    set MSVC_VER=14.1
-    set LIB_VER=141
-)
+set CMAKE_C_COMPILER=clang
+set CMAKE_CXX_COMPILER=clang++
 
-set PREFIX=pwd
+rem :: Set the right msvc version according to Python version
+rem if "%PY_VER%"=="2.7" (
+rem     set MSVC_VER=9.0
+rem     set LIB_VER=90
+rem ) else if "%PY_VER%"=="3.4" (
+rem     set MSVC_VER=10.0
+rem     set LIB_VER=100
+rem ) else (
+rem     set MSVC_VER=14.1
+rem     set LIB_VER=141
+rem )
+
 set INCLUDE_PATH="%PREFIX%/include"
 set LIBRARY_PATH="%PREFIX%/lib"
 set LDFLAGS=-L%LIBRARY_PATH%
 set CMAKE_INSTALL_PREFIX=%PREFIX%
 
-mkdir %SRC_DIR%/lapack_build
-cd %SRC_DIR%/lapack_build
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-    -DCMAKE_MAKE_PROGRAM=%PREFIX%/bin/make ^
-    -DCMAKE_INSTALL_LIBDIR=%LIBRARY_PATH% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DBUILD_SHARED_LIBS=OFF ^
-    -DBUILD_TESTING=OFF ^
-    -DCMAKE_CC_FLAGS='-fPIC' ^
-    -DCMAKE_CXX_FLAGS='-fPIC' ^
-    -DLAPACKE=ON ^
-    -DCBLAS=ON ^
-    %SRC_DIR%/lapack_source
-make -j"%CPU_COUNT%" install
-
-rem mkdir %SRC_DIR%/netcdf_build
-rem cd %SRC_DIR%/netcdf_build
-rem cmake ^
-rem      -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-rem      -DCMAKE_SHARED_LINKER_FLAGS=%LDFLAGS% ^
-rem      -DCMAKE_CC_FLAGS='-fPIC' ^
-rem      -DCMAKE_CXX_FLAGS='-fPIC' ^
-rem      %SRC_DIR%/netcdf_source 
-rem make -j"%CPU_COUNT%" install
-
-rem cd %SRC_DIR%/netcdf_cxx_source
-rem ./configure --prefix=%PREFIX%
-rem make -j"%CPU_COUNT%" install
-
-rem mkdir ${SRC_DIR}/netcdf_cxx_build
-rem cd ${SRC_DIR}/netcdf_cxx_build
-rem cmake ^
-rem      -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-rem      -DCMAKE_SHARED_LINKER_FLAGS=%LDFLAGS% ^
-rem      -DCMAKE_CC_FLAGS='-fPIC' ^
-rem      -DCMAKE_CXX_FLAGS='-fPIC' ^
-rem      ${SRC_DIR}/netcdf_source 
-rem make -j%CPU_COUNT% install
-
-rem cd ${SRC_DIR}/netcdf_cxx_source
-rem ./configure --prefix=%PREFIX%
-rem make -j%CPU_COUNT% install
-
-cd ${SRC_DIR}/boost_source
-./bootstrap.sh ^
-    --prefix="%PREFIX%" ^
-    --with-python="${PYTHON}" ^
-    --with-python-root="%PREFIX% : %PREFIX%/include/python${PY_VER}m %PREFIX%/include/python${PY_VER}" ^
-    --with-icu="%PREFIX%" 
-./b2 -q -d+2 -a ^
-    variant=release ^
-    address-model="${ARCH}" ^
-    architecture=x86 ^
-    debug-symbols=off ^
-    threading=multi ^
-    runtime-link=static ^
-    link=static ^
-    toolset=gcc ^
-    cxxflags='-fPIC -w' ^
-    python="${PY_VER}" ^
-    include="%PREFIX%/include" ^
-    linkflags="-L%LIBRARY_PATH%" ^
-    --with-python ^
-    --with-iostreams ^
-    --with-random ^
-    --layout=system ^
-    -j%CPU_COUNT% ^
-    install
-
-cd ${SRC_DIR}/trilinos_source
-git reset --hard c87c9f7224b36c845aa7ef19a636f9c99c9c02d4
-
-mkdir ${SRC_DIR}/trilinos_build
-cd ${SRC_DIR}/trilinos_build
-cmake -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
-    -DCMAKE_MAKE_PROGRAM=%PREFIX%/bin/make ^
-    -DCMAKE_VERBOSE_MAKEFILE=FALSE ^
-    -DTrilinos_ENABLE_CXX11=ON ^
-    -DTrilinos_ENABLE_Fortran=OFF ^
-    -DBUILD_SHARED_LIBS=OFF ^
-    -DOpenMP_C_FLAGS='-w -fopenmp' ^
-    -DOpenMP_CXX_FLAGS='-w -fopenmp' ^
-    -DCMAKE_CC_FLAGS='-fPIC' ^
-    -DCMAKE_CXX_FLAGS='-fPIC' ^
-    -DTPL_ENABLE_BLAS=ON ^
-    -DBLAS_LIBRARY_NAMES='blas' ^
-    -DTPL_BLAS_INCLUDE_DIRS=%PREFIX%/include ^
-    -DTPL_BLAS_LIBRARY_DIRS=%LIBRARY_PATH% ^
-    -DTPL_BLAS_LIBRARY_NAMES='blas' ^
-    -DTPL_ENABLE_LAPACK=ON ^
-    -DTPL_LAPACK_INCLUDE_DIRS=%PREFIX%/include ^
-    -DTPL_LAPACK_LIBRARY_DIRS=%LIBRARY_PATH% ^
-    -DTPL_LAPACK_LIBRARY_NAMES='lapack' ^
-    -DTPL_ENABLE_Boost=ON ^
-    -DTPL_Boost_INCLUDE_DIRS=%PREFIX%/include ^
-    -DTPL_Boost_LIBRARY_DIRS=%LIBRARY_PATH% ^
-    -DTPL_ENABLE_Cholmod=OFF ^
-    -DTPL_Cholmod_INCLUDE_DIRS=%PREFIX%/include ^
-    -DTPL_Cholmod_LIBRARY_DIRS=%LIBRARY_PATH% ^
-    -DTPL_Cholmod_LIBRARIES='libcholmod.so;libamd.so;libcolamd.so' ^
-    -DTPL_ENABLE_METIS=ON ^
-    -DTPL_METIS_INCLUDE_DIRS=%PREFIX%/include ^
-    -DTPL_METIS_LIBRARY_DIRS=%LIBRARY_PATH% ^
-    -DMETIS_LIBRARY_NAMES='libmetis.a' ^
-    -DTPL_ENABLE_UMFPACK=OFF ^
-    -DTPL_UMFPACK_INCLUDE_DIRS=%PREFIX%/include ^
-    -DTPL_UMFPACK_LIBRARY_DIRS=%LIBRARY_PATH% ^
-    -DTrilinos_ENABLE_Amesos2=ON ^
-    -DTrilinos_ENABLE_Belos=ON ^
-    -DTrilinos_ENABLE_Ifpack2=ON ^
-    -DTrilinos_ENABLE_Kokkos=ON ^
-    -DTrilinos_ENABLE_MueLu=ON ^
-    -DTrilinos_ENABLE_Tpetra=ON ^
-    -DTrilinos_ENABLE_Teuchos=ON ^
-    -DTrilinos_ENABLE_COMPLEX=ON ^
-    -DTrilinos_ENABLE_OpenMP=ON ^
-    -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF ^
-    -DTrilinos_ENABLE_EXPLICIT_INSTANTIATION=ON ^
-    ${SRC_DIR}/trilinos_source
-make -j%CPU_COUNT% install
-
-cd ${SRC_DIR}/escript
+cd %SRC_DIR%/escript
 scons -j%CPU_COUNT% ^
-    options_file="${SRC_DIR}/escript/scons/templates/sid_options.py" ^
+    options_file="%SRC_DIR%/escript/scons/templates/stretch_options.py" ^
     prefix="%PREFIX%" ^
     cxx_extra="-fPIC" ^
+    ld_extra="-L%PREFIX%/lib -lblas -llapack -lumfpack -lnetcdf_c++4" ^
     boost_prefix="%PREFIX%" ^
     boost_libs='boost_python27' ^
-    trilinos=1 ^
-    trilinos_prefix="%PREFIX%" ^
-    pythonlibpath="%LIBRARY_PATH%" ^
-    pythonincpath="%PREFIX%/include/python${PY_VER}" ^
+    pythoncmd=`which python` ^
+    pythonlibpath="%PREFIX%/lib" ^
+    pythonincpath="%PREFIX%/include/python%PY_VER%" ^
     pythonlibname="python2.7" ^
-    umfpack=0 ^
-    umfpack_prefix=["%PREFIX%/include","%LIBRARY_PATH%"] ^
+    trilinos=0 ^
+    trilinos_prefix="%PREFIX%" ^
+    umfpack=1 ^
+    umfpack_prefix=["%PREFIX%/include","%PREFIX%/lib"] ^
+    lapack=1 ^
+    lapack_prefix=["%PREFIX%/include/atlas","%PREFIX%/lib"] ^
+    lapack_libs=['lapack'] ^
     werror=0
 
--- tools/anaconda/Anaconda2/build.sh
+++ tools/anaconda/Anaconda2/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Hints:
-# http://boost.2283326.n4.nabble.com/how-to-build-boost-with-bzip2-in-non-standard-location-td2661155.html
-# http://www.gentoo.org/proj/en/base/amd64/howtos/?part=1&chap=3
-# http://www.boost.org/doc/libs/1_55_0/doc/html/bbv2/reference.html
-
-# Hints for OSX:
-# http://stackoverflow.com/questions/20108407/how-do-i-compile-boost-for-os-x-64b-platforms-with-stdlibc
-
 set -x -e
 
 INCLUDE_PATH="${PREFIX}/include"
@@ -19,127 +11,14 @@ LDFLAGS=-L"${PREFIX}/lib"
 
 if [ "$(uname)" == "Linux" ]; then
 
-    # None of the gcc libraries on Anaconda contain libm.so
-    cd ${SRC_DIR}/openlibm_source
-    sed -i 's+prefix = /usr/local+prefix = ${PREFIX}+g' Make.inc
-    make -j"${CPU_COUNT}" install | tee openlibm.log 2>&1 
-    ln -s ${PREFIX}/lib/libopenlibm.so ${PREFIX}/lib/libm.so
-    ln -s ${PREFIX}/lib/libopenlibm.so.1 ${PREFIX}/lib/libm.so.1
-    ln -s ${PREFIX}/lib/libopenlibm.so.1.0 ${PREFIX}/lib/libm.so.1.0
-    ln -s ${PREFIX}/lib/libopenlibm.a ${PREFIX}/lib/libm.a
-
-    mkdir ${SRC_DIR}/lapack_build
-    cd ${SRC_DIR}/lapack_build
-    cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_INSTALL_LIBDIR="${PREFIX}/lib" \
-        -DCMAKE_CC_FLAGS='-fPIC' \
-        -DCMAKE_CXX_FLAGS='-fPIC' \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DBUILD_SHARED_LIBS=ON \
-        -DBUILD_TESTING=OFF \
-        -DLAPACKE=ON \
-        -DCBLAS=ON \
-        ${SRC_DIR}/lapack_source
-    make -j"${CPU_COUNT}" install | tee lapack.log 2>&1
-
-    # mkdir ${SRC_DIR}/netcdf_build
-    # cd ${SRC_DIR}/netcdf_build
-    # cmake \
-    #      -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    #      -DCMAKE_SHARED_LINKER_FLAGS=${LDFLAGS} \
-    #      -DCMAKE_CXX_FLAGS='-fPIC' \
-    #      -DBUILD_SHARED_LIBS=ON \
-    #      ${SRC_DIR}/netcdf_source 
-    # make -j"${CPU_COUNT}" install | tee netcdf.log 2>&1
-
-    # cd ${SRC_DIR}/netcdf_cxx_source
-    # ./configure --prefix=${PREFIX}
-    # make -j"${CPU_COUNT}" install | tee netcdf_cxx.log 2>&1
-
-    cd ${SRC_DIR}/boost_source
-    ./bootstrap.sh \
-        --prefix="${PREFIX}" \
-        --with-python="${PYTHON}" \
-        --with-python-root="${PREFIX} : "${PREFIX}/include"/python${PY_VER}m "${PREFIX}/include"/python${PY_VER}" \
-        --with-icu="${PREFIX}" | tee bootstrap.log 2>&1
-    ./b2 -q -d+2 -a \
-        variant=release \
-        address-model="${ARCH}" \
-        architecture=x86 \
-        debug-symbols=off \
-        threading=multi \
-        runtime-link=shared \
-        link=shared \
-        toolset=gcc \
-        cxxflags='-fPIC -w' \
-        python="${PY_VER}" \
-        include=""${PREFIX}/include"" \
-        linkflags="-L"${PREFIX}/lib"" \
-        --with-python \
-        --with-iostreams \
-        --with-random \
-        --layout=system \
-        -j"${CPU_COUNT}" \
-        install | tee b2.log 2>&1
-
-    # trilinos
-    cd ${SRC_DIR}/trilinos_source
-    git reset --hard c87c9f7224b36c845aa7ef19a636f9c99c9c02d4
-
-    mkdir ${SRC_DIR}/trilinos_build
-    cd ${SRC_DIR}/trilinos_build
-    cmake -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-        -DCMAKE_MAKE_PROGRAM=${PREFIX}/bin/make \
-        -DCMAKE_VERBOSE_MAKEFILE=FALSE \
-        -DTrilinos_ENABLE_CXX11=ON \
-        -DTrilinos_ENABLE_Fortran=ON \
-        -DBUILD_SHARED_LIBS=OFF \
-        -DOpenMP_C_FLAGS='-w -fopenmp ' \
-        -DOpenMP_CXX_FLAGS='-w -fopenmp ' \
-        -DCMAKE_CXX_FLAGS='-fPIC -L${PREFIX}/lib -lblas -llapack -lumfpack' \
-        -DCMAKE_CC_FLAGS='-fPIC -L${PREFIX}/lib -lblas -llapack -lumfpack' \
-        -DTPL_ENABLE_BLAS=ON \
-        -DBLAS_LIBRARY_NAMES='blas' \
-        -DTPL_BLAS_INCLUDE_DIRS="${PREFIX}/include" \
-        -DTPL_BLAS_LIBRARY_DIRS="${PREFIX}/lib" \
-        -DTPL_BLAS_LIBRARY_NAMES=${PREFIX}/lib/libblas.so \
-        -DTPL_ENABLE_LAPACK=ON \
-        -DTPL_LAPACK_INCLUDE_DIRS="${PREFIX}/include" \
-        -DTPL_LAPACK_LIBRARY_DIRS="${PREFIX}/lib" \
-        -DTPL_LAPACK_LIBRARY_NAMES=${PREFIX}/lib/liblapack.so \
-        -DTPL_ENABLE_Boost=ON \
-        -DTPL_Boost_INCLUDE_DIRS="${PREFIX}/include" \
-        -DTPL_Boost_LIBRARY_DIRS="${PREFIX}/lib" \
-        -DTPL_ENABLE_Cholmod=ON \
-        -DTPL_Cholmod_INCLUDE_DIRS="${PREFIX}/include" \
-        -DTPL_Cholmod_LIBRARY_DIRS="${PREFIX}/lib" \
-        -DTPL_Cholmod_LIBRARIES='libcholmod.so;libamd.so;libcolamd.so' \
-        -DTPL_ENABLE_METIS=ON \
-        -DTPL_METIS_INCLUDE_DIRS="${PREFIX}/include" \
-        -DTPL_METIS_LIBRARY_DIRS="${PREFIX}/lib" \
-        -DTPL_ENABLE_UMFPACK=ON \
-        -DTPL_UMFPACK_INCLUDE_DIRS="${PREFIX}/include" \
-        -DTPL_UMFPACK_LIBRARY_DIRS="${PREFIX}/lib" \
-        -DTrilinos_ENABLE_Amesos2=ON \
-        -DTrilinos_ENABLE_Belos=ON \
-        -DTrilinos_ENABLE_Ifpack2=ON \
-        -DTrilinos_ENABLE_Kokkos=ON \
-        -DTrilinos_ENABLE_MueLu=ON \
-        -DTrilinos_ENABLE_Tpetra=ON \
-        -DTrilinos_ENABLE_Teuchos=ON \
-        -DTrilinos_ENABLE_COMPLEX=ON \
-        -DTrilinos_ENABLE_OpenMP=ON \
-        -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF \
-        -DTrilinos_ENABLE_EXPLICIT_INSTANTIATION=ON \
-        ${SRC_DIR}/trilinos_source
-    make -j"${CPU_COUNT}" install
+    # python -m pip install svn
+    # svn co https://svn.geocomp.uq.edu.au/svn/esys13/trunk ${SRC_DIR}/escript
 
     cd ${SRC_DIR}/escript
     scons -j"${CPU_COUNT}" \
         options_file="${SRC_DIR}/escript/scons/templates/stretch_options.py" \
         prefix="${PREFIX}" \
         cxx_extra="-fPIC" \
-        ld_extra="-L${PREFIX}/lib -lblas -llapack -lumfpack" \
         boost_prefix="${PREFIX}" \
         boost_libs='boost_python27' \
         pythoncmd=`which python` \
@@ -149,10 +28,12 @@ if [ "$(uname)" == "Linux" ]; then
         trilinos=1 \
         trilinos_prefix="${PREFIX}" \
         umfpack=0 \
-        umfpack_prefix=[""${PREFIX}/include"",""${PREFIX}/lib""] \
+        umfpack_prefix=["${PREFIX}/include","${PREFIX}/lib"] \
         lapack=0 \
-        lapack_prefix=[""${PREFIX}/include"/atlas",""${PREFIX}/lib""] \
+        lapack_prefix=["${PREFIX}/include/atlas","${PREFIX}/lib"] \
         lapack_libs=['lapack'] \
+        netcdf_prefix=["${PREFIX}/include","${PREFIX}/lib"] \
+        netcdf_libs=['netcdf_c++4','netcdf'] \
         werror=0
 
 fi
