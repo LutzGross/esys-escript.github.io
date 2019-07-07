58460928167ca5d5542541f891456d2a2f9c99a2
aellery <aellery@77569008-7704-0410-b7a0-a92fef0b09fd>
2019-07-07 00:48:20 +0000

    Fixed a broken link in the linux Anaconda build

--- tools/anaconda/Anaconda2/build.sh
+++ tools/anaconda/Anaconda2/build.sh
@@ -11,29 +11,37 @@ LDFLAGS=-L"${PREFIX}/lib"
 
 if [ "$(uname)" == "Linux" ]; then
 
-    # python -m pip install svn
-    # svn co https://svn.geocomp.uq.edu.au/svn/esys13/trunk ${SRC_DIR}/escript
+    cd ${SRC_DIR}/escript-boost
+    ./bootstrap.sh --with-python-version=2.7 --prefix="${PREFIX}/esys/boost"
+    ./b2 variant=release link=shared runtime-link=shared threading=multi \
+        --with-python --with-iostreams --with-random -j"${CPU_COUNT}" install
+
+    # cp ${PREFIX}/include/boost/parameter/python.hpp ${PREFIX}/include/boost/python.hpp
 
     cd ${SRC_DIR}/escript
     scons -j"${CPU_COUNT}" \
         options_file="${SRC_DIR}/escript/scons/templates/stretch_options.py" \
         prefix="${PREFIX}" \
         cxx_extra="-fPIC" \
-        boost_prefix="${PREFIX}" \
+        boost_prefix="${PREFIX}/esys/boost" \
         boost_libs='boost_python27' \
         pythoncmd=`which python` \
         pythonlibpath="${PREFIX}/lib" \
         pythonincpath="${PREFIX}/include/python${PY_VER}" \
         pythonlibname="python2.7" \
-        trilinos=1 \
+        paso=1 \
+        trilinos=0 \
         trilinos_prefix="${PREFIX}" \
-        umfpack=0 \
-        umfpack_prefix=["${PREFIX}/include","${PREFIX}/lib"] \
+        umfpack=1 \
+        umfpack_prefix="${PREFIX}" \
         lapack=0 \
         lapack_prefix=["${PREFIX}/include/atlas","${PREFIX}/lib"] \
         lapack_libs=['lapack'] \
-        netcdf_prefix=["${PREFIX}/include","${PREFIX}/lib"] \
+        netcdf=no \
+        netcdf_prefix="${PREFIX}"] \
         netcdf_libs=['netcdf_c++4','netcdf'] \
-        werror=0
+        werror=1
 
 fi
+
+# "${CPU_COUNT}"
