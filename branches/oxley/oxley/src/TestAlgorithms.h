3feb79d475cbc991d83894b2d97168e9a8c79b35
aellery <aellery@77569008-7704-0410-b7a0-a92fef0b09fd>
2019-10-15 23:16:44 +0000

    New files

---
+++ branches/oxley/oxley/src/TestAlgorithms.h
@@ -0,0 +1,32 @@
+/*****************************************************************************
+*
+* Copyright (c) 2003-2019 by The University of Queensland
+* http://www.uq.edu.au
+*
+* Primary Business: Queensland, Australia
+* Licensed under the Apache License, version 2.0
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+* Development 2012-2013 by School of Earth Sciences
+* Development from 2014 by Centre for Geoscience Computing (GeoComp)
+*
+*****************************************************************************/
+
+#include <oxley/OxleyData.h>
+
+#include <p4est.h>
+#include <p8est.h>
+
+#ifdef ESYS_HAVE_BOOST_NUMPY
+#include <boost/python/numpy.hpp>
+#endif
+
+// This file contains various callback functions that p4est uses when refining
+// recursively, to test whether to continue at each step
+
+namespace oxley {
+
+
+
+} //namespace oxley
