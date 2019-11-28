31dee0ef9e7b8396d6e6279e5e528bdf248d0e09
aellery <aellery@77569008-7704-0410-b7a0-a92fef0b09fd>
2019-11-28 05:07:45 +0000

    Modified the install guide to include the python 3 versions of packages. Also, added python 3 versions of the options files.

---
+++ trunk/scons/templates/fedora_py3_options.py
@@ -0,0 +1,29 @@
+
+##############################################################################
+#
+# Copyright (c) 2003-2018 by The University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Apache License, version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development 2012-2013 by School of Earth Sciences
+# Development from 2014 by Centre for Geoscience Computing (GeoComp)
+#
+##############################################################################
+
+# This is a template configuration file for escript on Fedora Linux.
+# Refer to README_FIRST for usage instructions.
+
+escript_opts_version = 203
+openmp = True
+boost_libs = ['boost_python37']
+boost_prefix = ['/usr/include','/usr/lib64']
+netcdf = True
+pythonlibpath = ['/usr/lib64']
+pythonlibname = ['python3.7m']
+pythonincpath = ['/usr/include/python3.7m']
+umfpack = True
+umfpack_prefix = ['/usr/include/suitesparse','/usr/lib']
