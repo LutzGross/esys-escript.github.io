791653544501f8509437a593a57705244f73239d
acodd <acodd@77569008-7704-0410-b7a0-a92fef0b09fd>
2019-12-09 05:29:21 +0000

    more updates to userguide

---
+++ doc/examples/usersguide/simpletest.py
@@ -0,0 +1,31 @@
+from esys.escript import *
+from esys.weipa import saveVTK, saveSilo
+from esys.escript.linearPDEs import LinearSinglePDE, SolverOptions
+from esys.finley import ReadGmsh
+from esys.escript.pdetools import Locator
+
+print("read in mesh")   
+domain=ReadGmsh("simplemesh.msh", 3,  optimize=True )
+
+pde = LinearSinglePDE(domain, isComplex=False)
+pde.setSymmetryOn()
+x = domain.getX()
+
+pde.setValue(A=kronecker(3), Y=1, q=whereZero(x[0]-inf(x[0])))
+
+options = pde.getSolverOptions()
+options.setPackage(SolverOptions.TRILINOS)
+options.setSolverMethod(SolverOptions.PCG)
+options.setPreconditioner(SolverOptions.AMG)
+options.setTrilinosParameter("multigrid algorithm", "sa")
+options.setTrilinosParameter("sa: damping factor", 1.3)
+options.setTrilinosParameter("max levels", 10)         
+options.setTrilinosParameter("coarse: max size", 2000)
+options.setTrilinosParameter("coarse: type", "SuperLU")
+options.setTrilinosParameter("verbosity", "low") 
+
+print("solve pde")
+u=pde.getSolution() 
+
+saveSilo("asimple",u=u) 
+print("finished")
