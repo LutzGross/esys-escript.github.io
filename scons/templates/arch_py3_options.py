79fe5529d971d3d7c106966bf9f113376b68b6e0
Adam <adam@ellery.cloud>
2021-02-24 23:23:21 +1000

    Arch Linux options file

---
+++ scons/templates/arch_py3_options.py
@@ -0,0 +1,33 @@
+
+##############################################################################
+#
+# Copyright (c) 2003-2020 by The University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Apache License, version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development 2012-2013 by School of Earth Sciences
+# Development from 2014 by Centre for Geoscience Computing (GeoComp)
+# Development from 2019 by School of Earth and Environmental Sciences
+#
+##############################################################################
+
+# This is a template configuration file for escript on Fedora Linux.
+# Refer to README_FIRST for usage instructions.
+
+escript_opts_version = 203
+openmp = True
+pythoncmd='/usr/bin/python3'
+pythonlibpath = ['/usr/lib64']
+pythonlibname = ['python3.9']
+pythonincpath = ['/usr/include/python3.9']
+boost_libs = ['boost_python39']
+boost_prefix=['/usr/include','/usr/lib64']
+netcdf_prefix=['/usr/include', '/usr/lib64']
+disable_boost_numpy=True
+umfpack=True
+umfpack_prefix=['/usr/include/suitesparse','/usr/lib64']
+netcdf=4
