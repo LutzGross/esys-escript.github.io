256124f4a0063617f86219b5b3c762ef0612babb
Adam Ellery <adam.ellery@outlook.com>
2021-05-28 15:23:32 +1000

    fixed bug

--- scons/testing.py
+++ scons/testing.py
@@ -3,4 +3,4 @@ pythoncmd='/usr/bin/python3'
 boost_libs=boost_python38 
 boost_prefix=['/usr/include','/usr/lib/x86_64-linux-gnu/'] 
 trilinos=0 
-trilinos_prefix=/app/trilinos 
+trilinos_prefix='/app/trilinos'
