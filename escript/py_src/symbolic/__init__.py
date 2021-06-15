85cfce02bafa038dbd975ae40819b6b3ca2a260b
LutzGross <l.gross@uq.edu.au>
2021-06-15 11:41:58 +0200

    added as step to eliminate naming conflicts

---
+++ escript/py_src/symbolic/__init__.py
@@ -0,0 +1,29 @@
+
+##############################################################################
+#
+# Copyright (c) 2003-2021 by The University of Queensland
+# http://www.uq.edu.au
+#
+# Primary Business: Queensland, Australia
+# Licensed under the Apache License, version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
+# Development 2012-2013 by School of Earth Sciences
+# Development from 2014 by Centre for Geoscience Computing (GeoComp)
+#
+##############################################################################
+
+from __future__ import print_function, division
+
+__copyright__="""Copyright (c) 2003-2018 by The University of Queensland
+http://www.uq.edu.au
+Primary Business: Queensland, Australia"""
+__license__="""Licensed under the Apache License, version 2.0
+http://www.apache.org/licenses/LICENSE-2.0"""
+__url__="https://launchpad.net/escript-finley"
+
+
+from .evaluator import *
+from .utils import *
+from esys.escriptcore.symbolic import *
