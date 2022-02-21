e15f9f45bbf8f877894856443e6da81076bf094f
Adam <adam.ellery@outlook.com>
2022-02-21 19:37:20 +1000

    added optional features

--- scons/quoll2_options.py
+++ scons/quoll2_options.py
@@ -5,3 +5,6 @@ pythonlibpath='/usr/lib/x86_64-linux-gnu/'
 pythonincpath='/usr/include/python3.9'
 boost_prefix=['/usr/include','/usr/lib/x86_64-linux-gnu/']
 boost_libs='boost_python39'
+umfpack=1
+openmp=1
+lapack=1
