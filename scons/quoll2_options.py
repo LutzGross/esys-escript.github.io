6a29c62f1d9badf0ebecacaa77675c977a75f2ac
Adam <adam.ellery@outlook.com>
2022-02-21 19:30:37 +1000

    Fixed bug

--- scons/quoll2_options.py
+++ scons/quoll2_options.py
@@ -4,4 +4,4 @@ pythonlibname='python3.9'
 pythonlibpath='/usr/lib/x86_64-linux-gnu/'
 pythonincpath='/usr/include/python3.9'
 boost_prefix=['/usr/include','/usr/lib/x86_64-linux-gnu/']
-boost_libs='boost_python3.9'
+boost_libs='boost_python39'
