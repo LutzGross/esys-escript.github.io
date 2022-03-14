6e867691c1b733d0bdb96da0e7fbdcb1cdd171fd
Adam <adam.ellery@outlook.com>
2022-03-14 15:51:26 +1000

    changed quoll2_options.py

--- scons/quoll2_options.py
+++ scons/quoll2_options.py
@@ -5,3 +5,7 @@ pythonlibpath='/usr/lib/x86_64-linux-gnu/'
 pythonincpath='/usr/include/python3.7m'
 boost_prefix=['/usr/include','/usr/lib/x86_64-linux-gnu/']
 boost_libs='boost_python37'
+debug=1
+cxx_extra+=" -O0 -p -pg "
+cxx_extra+=" -Wno-error=unused-variable -Wno-error=unused-but-set-variable"
+cxx_extra+=" -Wno-error=comment "
