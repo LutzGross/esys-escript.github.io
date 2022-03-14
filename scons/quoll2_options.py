22f903ac1b69eb2e476e8c3517fe93dded02c7bd
Adam <adam.ellery@outlook.com>
2022-03-14 14:30:48 +1000

    Fixed quoll2_options.py

---
+++ scons/quoll2_options.py
@@ -0,0 +1,7 @@
+escript_opts_version=203
+pythoncmd='/usr/bin/python3'
+pythonlibname='python3.9'
+pythonlibpath='/usr/lib/x86_64-linux-gnu/'
+pythonincpath='/usr/include/python3.9'
+boost_prefix=['/usr/include','/usr/lib/x86_64-linux-gnu/']
+boost_libs='boost_python3.9'
