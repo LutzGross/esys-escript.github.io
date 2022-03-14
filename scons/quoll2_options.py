6da11c43e9363485fa01a015a9f729329c822007
Adam <adam.ellery@outlook.com>
2022-03-14 15:27:46 +1000

    updated quoll2_options

--- scons/quoll2_options.py
+++ scons/quoll2_options.py
@@ -2,6 +2,6 @@ escript_opts_version=203
 pythoncmd='/usr/bin/python3'
 pythonlibname='python3.7'
 pythonlibpath='/usr/lib/x86_64-linux-gnu/'
-pythonincpath='/usr/include/python3.7'
+pythonincpath='/usr/include/python3.7m'
 boost_prefix=['/usr/include','/usr/lib/x86_64-linux-gnu/']
 boost_libs='boost_python3.7'
