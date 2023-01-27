ceeebe118a35d2ceb6015778e00faf96404d889b
Adam Ellery <adam.ellery@outlook.com>
2023-01-27 14:07:24 +1000

    Fixed syntax error in last commit

--- trilinos_build/mpi.sh
+++ trilinos_build/mpi.sh
@@ -9,8 +9,8 @@ cmake \
       -D Trilinos_ENABLE_CXX11=ON \
       -D Trilinos_ENABLE_Fortran=OFF \
       -D CMAKE_CXX_FLAGS=" " \
-      -D CMAKE_C_COMPILER=$3 \
-      -D CMAKE_CXX_COMPILER=$2 \
+      -D CMAKE_C_COMPILER=$2 \
+      -D CMAKE_CXX_COMPILER=$3 \
       -D BUILD_SHARED_LIBS=ON \
       -D TPL_ENABLE_BLAS=ON \
       -D TPL_ENABLE_Boost=ON \
