75f06d5fedf362120553f0059fef82b47f48d5b5
Adam Ellery <adam.ellery@outlook.com>
2023-02-05 15:53:32 +1000

    Updated options file for docker unit testing

--- scons/docker_testing.py
+++ scons/docker_testing.py
@@ -1,28 +1,25 @@
-
-##############################################################################
-#
-# Copyright (c) 2003-2020 by The University of Queensland
-# http://www.uq.edu.au
-#
-# Primary Business: Queensland, Australia
-# Licensed under the Apache License, version 2.0
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Development until 2012 by Earth Systems Science Computational Center (ESSCC)
-# Development 2012-2013 by School of Earth Sciences
-# Development from 2014 by Centre for Geoscience Computing (GeoComp)
-# Development from 2019 by School of Earth and Environmental Sciences
-#
-##############################################################################
-
-# This is a template configuration file for escript on Debian/GNU Linux.
-# Refer to README_FIRST for usage instructions.
-
 escript_opts_version=203
 
-pythoncmd = 'python3'
-lapack_prefix=['/usr/include/x86_64-linux-gnu/','/usr/lib/x86_64-linux-gnu/']
-mpi_prefix=['/usr/lib/x86_64-linux-gnu/openmpi/include','/usr/lib/x86_64-linux-gnu/openmpi/lib']
+
+boost_libs=['boost_python39','boost_numpy39']
+boost_prefix=['/usr/include/','/usr/lib/x86_64-linux-gnu/']
+build_trilinos='make'
+lapack=1
+lapack_prefix=['/usr/include/x86_64-linux-gnu','/usr/include/x86_64-linux-gnu/']
+mpi4py=1
+mpi='OPENMPI'
+mpi_prefix='/usr/lib/x86_64-linux-gnu/openmpi/'
+mumps=0
+mumps_prefix=['/usr/include/x86_64-linux-gnu','/usr/include/x86_64-linux-gnu/']
+openmp=1
+paso=1
+pythoncmd='python3'
+pythonlibpath='/usr/lib/x86_64-linux-gnu/'
+pythonincpath='/usr/include/python3.9/'
+pythonlibname='python3.9'
+silo=1
+silo_libs=['siloh5','hdf5_serial']
 silo_prefix=['/usr/include','/usr/lib/x86_64-linux-gnu/']
-silo_libs=['siloh5','hdf5_openmpi']
+umfpack=1
 umfpack_prefix=['/usr/include/suitesparse','/usr/lib/x86_64-linux-gnu/']
+werror=0
