55fe679eae1f5852c134facbe675978b10c33a53
Adam Ellery <adam.ellery@outlook.com>
2023-02-27 12:30:17 +1000

    Added an options file for the nectar cloud instance

---
+++ scons/nectar_options.py
@@ -0,0 +1,7 @@
+escript_opts_version=203
+pythoncmd = 'python3'
+pythonincpath='/usr/include/python3.6m'
+pythonlibpath='/usr/lib/x86_64-linux-gnu'
+pythonlibname='libpython3.6m'
+boost_libs='boost_python3'
+boost_prefix=['/usr/include','/usr/lib/x86_64-linux-gnu/']
