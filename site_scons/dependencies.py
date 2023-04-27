c20eaba8c65518d43612d11f3b7aabee6599bcfc
Adam Ellery <a.ellery@outlook.com>
2023-04-28 09:31:20 +1000

    Added /usr/lib64 to the boost numpy search paths

--- site_scons/dependencies.py
+++ site_scons/dependencies.py
@@ -347,6 +347,7 @@ def checkBoost(env):
                 spath = [x[13:-3] for x in out.split() if 'SEARCH_DIR' in x]
                 spath.append(boost_lib_path)
                 spath.append('/usr/lib/x86_64-linux-gnu/')
+                spath.append('/usr/lib64/')
                 p2name = ''
                 p3name = ''
                 for name in spath:
