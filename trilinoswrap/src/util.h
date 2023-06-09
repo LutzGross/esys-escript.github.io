e14f20564d74294ad8814c63bc1ecd9a3c6e2dac
Adam Ellery <adam.ellery@outlook.com>
2023-06-09 12:13:23 +1000

    Fixed segmentation fault that occurred when using finley with Trilinos 14.

--- trilinoswrap/src/util.h
+++ trilinoswrap/src/util.h
@@ -28,6 +28,10 @@
 
 #include <string>
 
+#ifdef ESYS_TRILINOS_14
+#include "Tpetra_CrsMatrix.hpp"
+#endif
+
 namespace esys_trilinos {
 
 namespace util {
@@ -63,8 +67,8 @@ TrilinosGraph_ptr unrollCrsGraph(const_TrilinosGraph_ptr graph, int blockSize)
     Teuchos::ArrayRCP<GO> colInd(graph->getLocalNumEntries() * blockSize * blockSize);
     for (LO row = 0; row < numMatrixRows; row++) {
         size_t numColumns = graph->getNumEntriesInLocalRow(row);
-        Tpetra::CrsGraph<LO,GO,NT>::nonconst_local_inds_host_view_type indices(0,numColumns);
-        graph->getLocalRowCopy(row, indices, numColumns);
+        Tpetra::CrsMatrix<>::nonconst_local_inds_host_view_type indices;
+        graph->getLocalRowCopy(row, indices, numColumns); //here
         for (int b = 0; b < blockSize; b++) {
             for (size_t c = 0; c < numColumns; c++) {
                 for (int cb = 0; cb < blockSize; cb++) {
