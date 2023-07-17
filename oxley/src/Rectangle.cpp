54be25b00898ab234904f7be5d89fd382e26c496
Adam Ellery <adam.ellery@outlook.com>
2023-07-17 10:28:27 +1000

    Fixed bug

--- oxley/src/Rectangle.cpp
+++ oxley/src/Rectangle.cpp
@@ -1605,7 +1605,7 @@ void Rectangle::refinePoint(double x0, double y0)
     updateElementIds();
     updateFaceOffset();
     updateFaceElementCount();
-updateQuadrantIDinformation();
+    updateQuadrantIDinformation();
 
     oxleytimer.toc("refinePoint...Done");
 }
@@ -1985,10 +1985,10 @@ void Rectangle::renumberNodes()
     std::vector<DoublePair> NormalNodes;
     std::vector<DoublePair> HangingNodes;
 
-    int orient_lookup[4][4]={{-1,3,0,-1}, //p4est_child_corner_faces
-                             {3,-1,1,-1},
-                             {0,-1,-1,2},
-                             {-1,1,2,-1}};
+    int orient_lookup[4][4]={{-1,2,0,-1}, //p4est_child_corner_faces
+                             {2,-1,1,-1},
+                             {0,-1,-1,3},
+                             {-1,1,3,-1}};
 
     // Write in NodeIDs
     int k = 0;
