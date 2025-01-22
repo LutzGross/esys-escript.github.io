83c14f6db4160c61a492796a88d6a9254b370dd8
LutzGross <l.gross@uq.edu.au>
2025-01-22 15:40:42 +1000

    ubuntu options with mpi added

--- scons/templates/ubuntu_options.py
+++ scons/templates/ubuntu_options.py
@@ -24,7 +24,7 @@ mpi='NO'
 mpi_prefix = ['/usr/include/x86_64-linux-gnu/openmpi', '/usr/lib/x86_64-linux-gnu/openmpi' ]
 openmp=1
 paso=1
-cxx_extra= ['-fdiagnostics-color=always', '-Wno-format-truncation']
+cxx_extra= ['-freference fixed. diagnostics-color=always', '-Wno-format-truncation']
 pythoncmd = 'python3'
 
 parmetis = True
