3a1189b297dec9d608f5033cbc6c77b7e8c30ea0
Adam Ellery <adam.ellery@outlook.com>
2025-02-26 20:28:59 +1000

    fixed bug

--- tools/flatpak/flatpak_trilinos.sh
+++ tools/flatpak/flatpak_trilinos.sh
@@ -6,7 +6,9 @@ cmake \
       -D Trilinos_ENABLE_CXX11=ON \
       -D Trilinos_ENABLE_Fortran=OFF \
       -D CMAKE_C_COMPILER=$2 \
+      -D CMAKE_C_FLAGS=" -Wno-error=implicit-function-declaration " \
       -D CMAKE_CXX_COMPILER=$3 \
+      -D CMAKE_CXX_FLAGS=" -Wno-unused-parameter -Wno-error=implicit-function-declaration " \
       -D BUILD_SHARED_LIBS=ON \
       -D Trilinos_ENABLE_Amesos=ON \
       -D Trilinos_ENABLE_Amesos2=ON \
