ec50af367eaf319887165da176b741d17e5ed719
LutzGross <l.gross@uq.edu.au>
2025-03-20 17:25:11 +0800

    pycad reference removed

--- finley/py_src/readers.py
+++ finley/py_src/readers.py
@@ -40,42 +40,6 @@ __author__="Lutz Gross, l.gross@uq.edu.au"
 from .factorywrappers import ReadGmsh, ReadMesh
 from .finleycpp import LoadMesh
 
-def MakeDomain(design,integrationOrder=-1, reducedIntegrationOrder=-1, optimizeLabeling=True, useMacroElements=False):
-    """
-    Creates a Finley `Domain` from a `esys.pycad.design.Design` object.
-    Currently only gmsh is supported.
-
-    :param design: the geometry
-    :type design: `esys.pycad.design.Design`
-    :param integrationOrder: integration order. If -1 the default is used.
-    :type integrationOrder: ``int``
-    :param reducedIntegrationOrder: reduced integration order. If -1 the
-                                    default is used.
-    :type reducedIntegrationOrder: ``int``
-    :param optimizeLabeling: if set the labeling of the mesh nodes is optimized
-    :type optimizeLabeling: ``bool``
-    :param useMacroElements: uses macro elements.
-    :type useMacroElements: ``bool``
-    :return: the Finley domain defined by the design
-    :rtype: `Domain`
-    """
-    if isinstance(design, GMSHDesign):
-        if useMacroElements: design.setElementOrder(2)
-        ff=design.getFileFormat()
-        design.setFileFormat(design.GMSH)
-        mshname=design.getMeshHandler()
-        dom = ReadGmsh(mshname,
-                       design.getDim(),
-                       integrationOrder,
-                       reducedIntegrationOrder,
-                       optimizeLabeling,
-                       useMacroElements)
-        design.setFileFormat(ff)
-    else:
-        raise TypeError("Finley does not support %s designs."%design.__class__.__name__)
-    # fill in the tag map
-    design.getTagMap().passToDomain(dom)
-    return dom
 
 def GetMeshFromFile(filename, **kwargs):
     """
@@ -99,12 +63,19 @@ def GetMeshFromFile(filename, **kwargs):
     useMacroElements=False
     if "useMacroElements" in kwargs:
         integrationOrder=kwargs["useMacroElements"]
+    points = []
+    if 'diracPoints' in kwargs:
+        points=kwargs['diracPoints']
+    tags = [ ]
+    if 'diracTags' in kwargs:
+        tags=kwargs['diracTags']
+
     if ext=="fly":
-        return ReadMesh(filename, integrationOrder, reducedIntegrationOrder, optimize)
+        return ReadMesh(filename, integrationOrder, reducedIntegrationOrder, optimize,  diracPoints=points, diracTags=tags)
     elif ext=="msh":
         if not "numDim" in kwargs:
            raise ValueError("The numDim argument is required in order to read .msh files.")
-        return ReadGmsh(filename, kwargs['numDim'], integrationOrder, reducedIntegrationOrder, optimize, useMacroElements)
+        return ReadGmsh(filename, kwargs['numDim'], integrationOrder, reducedIntegrationOrder, optimize, useMacroElements, diracPoints=points, diracTags=tags)
     else:
 #        return LoadMesh(filename)
         raise ValueError("Unsupported extension .%s"%ext)
